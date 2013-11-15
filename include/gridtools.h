59a478ff49cf18c95eafeb6004e9400f2403f818
mbianco <mbianco@cscs.ch>
2013-11-15 16:03:19 +0100

    added header file for the library

---
+++ include/gridtools.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <storage.h>
+#include <array.h>
+#include <layout_map.h>
+#include <axis.h>
+#include <make_stencils.h>
+#include <arg_type.h>
+#include <execution_types.h>
+#include <domain_type.h>
+#include <intermediate.h>
+
