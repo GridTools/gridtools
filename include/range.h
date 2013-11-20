1536b0a218a8c299b0db590bf70c4f1cbb8430b5
mbianco <mbianco@cscs.ch>
2013-11-20 14:34:42 +0100

    Added comments to range.h

--- include/range.h
+++ include/range.h
@@ -8,6 +8,9 @@
 #include <boost/mpl/push_front.hpp>
 #include <boost/static_assert.hpp>
 
+/**
+ * Class to specify access ranges for stencil functions
+ */
 template <int _iminus=0, int _iplus=0,
           int _jminus=0, int _jplus=0>
 struct range {
@@ -15,10 +18,15 @@ struct range {
     typedef typename boost::mpl::int_<_iplus> iplus;
     typedef typename boost::mpl::int_<_jminus> jminus;
     typedef typename boost::mpl::int_<_jplus> jplus;
-    //typedef range<_iminus, _iplus, _jminus, _jplus> type;
-    //operator int() {return 0;}
 };
 
+/**
+ * Output operator for ranges - for debug purposes
+ * 
+ * @param s The ostream
+ * @param n/a Arguemnt to deduce range type
+ * @return The ostream
+ */
 template <int i1, int i2, int i3, int i4>
 std::ostream& operator<<(std::ostream &s, range<i1,i2,i3,i4>) {
     return s << "[" 
@@ -28,16 +36,26 @@ std::ostream& operator<<(std::ostream &s, range<i1,i2,i3,i4>) {
              << i4 << "]";
 }
 
+/**
+ * Metafunction to check if a type is a range
+ */
 template <typename T>
 struct is_range {
     typedef boost::false_type type;
 };
 
+/**
+ * Metafunction to check if a type is a range - Specialization yielding true
+ */
 template <int I, int J, int K, int L>
 struct is_range<range<I,J,K,L> > {
     typedef boost::true_type type;
 };
 
+
+/**
+ * Metafunction taking two ranges and yielding a range containing them
+ */
 template <typename range1,
           typename range2>
 struct enclosing_range {
@@ -51,6 +69,9 @@ struct enclosing_range {
                   > type;
 };
 
+/**
+ * Metafunction taking two ranges and yielding a range which is the extension of one another
+ */
 template <typename range1,
           typename range2>
 struct sum_range {
@@ -63,29 +84,3 @@ struct sum_range {
                   boost::mpl::plus<typename range1::jplus,  typename range2::jplus>::type::value
                   > type;
 };
-
-// template <typename list_of_ranges>
-// struct prefix_on_ranges {
-
-//     template <typename t_list, typename t_range>
-//     struct state {
-//         typedef t_list list;
-//         typedef t_range range;
-//     };
-
-//     template <typename previous_state, typename current_element>
-//     struct update_state {
-//         typedef typename sum_range<typename previous_state::range,
-//                                    current_element>::type new_range;
-//         typedef typename boost::mpl::push_front<typename previous_state::list, new_range>::type new_list;
-//         typedef state<new_list, new_range> type;
-//     };
-
-//     typedef typename boost::mpl::reverse_fold<
-//         list_of_ranges,
-//         state<boost::mpl::vector<>, range<0,0,0,0> >,
-//         update_state<boost::mpl::_1, boost::mpl::_2> >::type final_state;
-
-//     typedef typename final_state::list type;
-// };
-
