8ffeabae877e438108b94c4c3d36f6882be307a6
mbianco <mbianco@cscs.ch>
2013-12-13 14:52:59 +0100

    Commented example a little more

--- examples/interface1.cpp
+++ examples/interface1.cpp
@@ -2,18 +2,23 @@
 #include <backend_block.h>
 #include <backend_naive.h>
 
+/*
+  This file shows an implementation of the "horizontal diffusion" stencil, similar to the one used in COSMO
+ */
+
 using gridtools::level;
 using gridtools::arg_type;
 using gridtools::range;
 using gridtools::arg;
 
+// This is the definition of the special regions in the "vertical" direction
 typedef gridtools::interval<level<0,-1>, level<1,-1> > x_lap;
 typedef gridtools::interval<level<0,-1>, level<1,-1> > x_flx;
 typedef gridtools::interval<level<0,-1>, level<1,-1> > x_out;
 
 typedef gridtools::interval<level<0,-2>, level<1,3> > axis;
-//typedef extend_by<tight_axis, 2>::type axis;
 
+// These are the stencil operators that compose the multistage stencil in this test
 struct lap_function {
     static const int n_args = 2;
     typedef arg_type<0> out;
@@ -22,7 +27,7 @@ struct lap_function {
 
     template <typename t_domain>
     static void Do(t_domain const & dom, x_lap) {
-        dom(out()) = 3*dom(in()) - 
+        dom(out()) = 3*dom(in()) -
             (dom(in( 1, 0, 0)) + dom(in( 0, 1, 0)) +
              dom(in(-1, 0, 0)) + dom(in( 0,-1, 0)));
     }
@@ -79,6 +84,9 @@ struct out_function {
     }
 };
 
+/*
+ * The following operators and structs are for debugging only
+ */
 std::ostream& operator<<(std::ostream& s, lap_function const) {
     return s << "lap_function";
 }
@@ -113,6 +121,7 @@ struct stampa1 {
     }
 };
 
+
 int main(int argc, char** argv) {
     int d1 = atoi(argv[1]);
     int d2 = atoi(argv[2]);
@@ -120,17 +129,15 @@ int main(int argc, char** argv) {
 
     typedef gridtools::storage<double, gridtools::layout_map<0,1,2> > storage_type;
 
+     // Definition of the actual data fields that are used for input/output
     storage_type in(d1,d2,d3,-1, std::string("in"));
     storage_type out(d1,d2,d3,-7.3, std::string("out"));
     storage_type coeff(d1,d2,d3,-3, std::string("coeff"));
-    storage_type lap(d1,d2,d3,-4, std::string("lap"));
-    storage_type flx(d1,d2,d3,-5, std::string("flx"));
-    storage_type fly(d1,d2,d3,-6, std::string("fly"));
 
     out.print();
 
-    gridtools::array<storage_type*, 2> args;
-
+    // Definition of placeholders. The order of them reflect the order the user will deal with them
+    // especially the non-temporary ones, in the construction of the domain
     typedef arg<3, gridtools::temporary<double> > p_lap;
     typedef arg<2, gridtools::temporary<double> > p_flx;
     typedef arg<4, gridtools::temporary<double> > p_fly;
@@ -138,33 +145,22 @@ int main(int argc, char** argv) {
     typedef arg<1, storage_type > p_in;
     typedef arg<0, storage_type > p_out;
 
+    // An array of placeholders to be passed to the domain
+    // I'm using mpl::vector, but the final API should look slightly simpler
     typedef boost::mpl::vector<p_lap, p_flx, p_fly, p_coeff, p_in, p_out> arg_type_list;
 
+    // construction of the domain. The domain is the physical domain of the problem, with all the physical fields that are used, temporary and not
+    // It must be noted that the only fields to be passed to the constructor are the non-temporary.
+    // The order in which they have to be passed is the order in which they appear scanning the placeholders in order. (I don't particularly like this)
     gridtools::domain_type<arg_type_list> domain
         (boost::fusion::make_vector(&out, &in, &coeff /*,&fly, &flx*/));
 
+    // Definition of the physical dimensions of the problem.
+    // The constructor takes the horizontal plane dimensions,
+    // while the vertical ones are set according the the axis property soon after
     gridtools::coordinates<axis> coords(2,d1-2,2,d2-2);
     coords.value_list[0] = 0;
     coords.value_list[1] = d3;
-
-    // for (int i=0; i<d1; ++i)
-    //     for (int j=0; j<d2; ++j) {
-    //         domain.move_to(i,j,0);
-    //         for (int k=0; k<d3; ++k) {
-    //             domain[p_in()] = 1.0*(i-j*k);
-    //             domain[p_coeff()] = 3.0*(i*j+k);
-    //             domain.increment_along<2>();
-    //         }
-    //     }
-
-    //in.print();
-
-//    auto mss = make_mss(execute_upward, 
-//                        make_esf<lap_function>(p_lap(), p_in()),
-//                        make_esf<flx_function>(p_flx(), p_in(), p_lap()),
-//                        make_esf<fly_function>(p_fly(), p_in(), p_lap()),
-//                        make_esf<out_function>(p_out(), p_in(), p_flx(), p_fly(), p_coeff())
-//                        );
 
 #ifdef BACKEND_BLOCK
 #define BACKEND backend_block
@@ -172,15 +168,27 @@ int main(int argc, char** argv) {
 #define BACKEND backend_naive
 #endif
 
-    gridtools::intermediate::run<gridtools::BACKEND>(gridtools::make_mss(gridtools::execute_upward, 
-                                                   gridtools::make_esf<lap_function>(p_lap(), p_in()),
-                                                   gridtools::make_independent(
-                                                                    gridtools::make_esf<flx_function>(p_flx(), p_in(), p_lap()),
-                                                                    gridtools::make_esf<fly_function>(p_fly(), p_in(), p_lap())
-                                                                    ),
-                                                   gridtools::make_esf<out_function>(p_out(), p_in(), p_flx(), p_fly(), p_coeff())
-                                                   ), 
-                                          domain, coords);
+    /*
+      Here we do lot of stuff
+      1) We pass to the intermediate representation ::run function the description
+      of the stencil, which is a multi-stage stencil (mss)
+      The mss includes (in order of execution) a laplacian, two fluxes which are independent
+      and a final step that is the out_function
+      2) The logical physical domain with the fields to use
+      3) The actual domain dimensions
+     */
+    gridtools::intermediate::run<gridtools::BACKEND>
+    (
+        gridtools::make_mss(gridtools::execute_upward,
+        gridtools::make_esf<lap_function>(p_lap(), p_in()),
+        gridtools::make_independent
+        (
+            gridtools::make_esf<flx_function>(p_flx(), p_in(), p_lap()),
+            gridtools::make_esf<fly_function>(p_fly(), p_in(), p_lap())
+        ),
+        gridtools::make_esf<out_function>(p_out(), p_in(), p_flx(), p_fly(), p_coeff())
+    ),
+    domain, coords);
 
 
     in.print();
