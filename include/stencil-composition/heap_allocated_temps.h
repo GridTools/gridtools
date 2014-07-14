3af93e8dca58fea8d219df632a538e34133b147b
mbianco <mbianco@cscs.ch>
2014-07-14 09:31:03 +0200

    Fixed identation

--- include/stencil-composition/heap_allocated_temps.h
+++ include/stencil-composition/heap_allocated_temps.h
@@ -6,14 +6,14 @@ namespace gridtools {
             int tileI;
             int tileJ;
             int tileK;
-        
+
             GT_FUNCTION
             instantiate_tmps(int tileI, int tileJ, int tileK)
                 : tileI(tileI)
                 , tileJ(tileJ)
                 , tileK(tileK)
             {}
-        
+
             // ElemType: an element in the data field place-holders list
             template <typename ElemType>
             GT_FUNCTION
@@ -24,11 +24,11 @@ namespace gridtools {
                 typedef typename boost::remove_pointer<typename boost::remove_reference<typename boost::fusion::result_of::value_at<ElemType, boost::mpl::int_<0> >::type>::type>::type storage_type;
 
 #ifndef NDEBUG
-                std::cout << "Temporary: " << range_type() << " + (" 
-                          << tileI << "x" 
-                          << tileJ << "x" 
+                std::cout << "Temporary: " << range_type() << " + ("
+                          << tileI << "x"
+                          << tileJ << "x"
                           << tileK << ")"
-                          << std::endl; 
+                          << std::endl;
 #endif
 #ifndef __CUDACC__
                 std::string s = boost::lexical_cast<std::string>(range_type::iminus::value)+
@@ -65,15 +65,12 @@ namespace gridtools {
     struct heap_allocated_temps {
         /**
          * This function is to be called by intermediate representation or back-end
-         * 
+         *
          * @tparam MssType The multistage stencil type as passed to the back-end
-         * @tparam RangeSizes mpl::vector with the sizes of the extents of the 
+         * @tparam RangeSizes mpl::vector with the sizes of the extents of the
          *         access for each functor listed as linear_esf in MssType
-         * @tparam BackEnd This is not currently used and may be dropped in future
-         * 
-         * @param tileI Tile size in the first dimension as used by the back-end
-         * @param tileJ Tile size in the second dimension as used by the back-end
-         * @param tileK Tile size in the third dimension as used by the back-end
+         * @tparam Domain The user domain type - Deduced from argument list
+         * @tparam coords The user coordinates type - Deduced from argument list
          */
         template <typename MssType, typename RangeSizes, typename Domain, typename Coords>
         static void prepare_temporaries(Domain & domain, Coords const& coords) {
@@ -82,7 +79,7 @@ namespace gridtools {
             tileI = (Backend::BI)?
                 (Backend::BI):
                 (coords.i_high_bound()-coords.i_low_bound()+1);
-        
+
             tileJ = (Backend::BJ)?
                 (Backend::BJ):
                 (coords.j_high_bound()-coords.j_low_bound()+1);
@@ -103,27 +100,27 @@ namespace gridtools {
             typedef typename boost::mpl::fold<typename Domain::placeholders,
                 boost::mpl::vector<>,
                 boost::mpl::if_<
-            is_plchldr_to_temp<boost::mpl::_2>,
-                boost::mpl::push_back<boost::mpl::_1, boost::mpl::_2 >,
-                boost::mpl::_1>
+                    is_plchldr_to_temp<boost::mpl::_2>,
+                    boost::mpl::push_back<boost::mpl::_1, boost::mpl::_2 >,
+                    boost::mpl::_1>
             >::type list_of_temporaries;
-        
+
 #ifndef NDEBUG
             std::cout << "BEGIN TMPS" << std::endl;
             for_each<list_of_temporaries>(_debug::print_index());
             std::cout << "END TMPS" << std::endl;
 #endif
-        
-            // Compute a vector of vectors of temp indices of temporaris initialized by each functor
+
+            // Compute a vector of vectors of temp indices of temporaries initialized by each functor
             typedef typename boost::mpl::fold<typename MssType::linear_esf,
-                boost::mpl::vector<>,
-                boost::mpl::push_back<boost::mpl::_1, _impl::get_temps_per_functor<boost::mpl::_2> >
+                    boost::mpl::vector<>,
+                    boost::mpl::push_back<boost::mpl::_1, _impl::get_temps_per_functor<boost::mpl::_2> >
                 >::type temps_per_functor;
 
             typedef typename boost::mpl::transform<
-            list_of_temporaries,
+                list_of_temporaries,
                 _impl::associate_ranges<temps_per_functor, RangeSizes>
-                >::type list_of_ranges;
+            >::type list_of_ranges;
 
 #ifndef NDEBUG
             std::cout << "BEGIN TMPS/F" << std::endl;
@@ -138,8 +135,8 @@ namespace gridtools {
             for_each<typename MssType::linear_esf>(_debug::print_ranges());
             std::cout << "END Fs" << std::endl;
 #endif
-        
-            typedef boost::fusion::filter_view<typename Domain::arg_list, 
+
+            typedef boost::fusion::filter_view<typename Domain::arg_list,
                 is_temporary_storage<boost::mpl::_> > tmp_view_type;
             tmp_view_type fview(domain.storage_pointers);
 
@@ -148,18 +145,18 @@ namespace gridtools {
             boost::fusion::for_each(fview, _debug::print_view());
             std::cout << "END VIEW" << std::endl;
 #endif
-        
+
             list_of_ranges lor;
             typedef boost::fusion::vector<tmp_view_type&, list_of_ranges const&> zipper;
             zipper zzip(fview, lor);
-            boost::fusion::zip_view<zipper> zip(zzip); 
+            boost::fusion::zip_view<zipper> zip(zzip);
             boost::fusion::for_each(zip, _impl::instantiate_tmps(tileI, tileJ, tileK));
 
 #ifndef NDEBUG
             std::cout << "BEGIN VIEW DOPO" << std::endl;
             boost::fusion::for_each(fview, _debug::print_view_());
             std::cout << "END VIEW DOPO" << std::endl;
-#endif        
+#endif
 
             domain.is_ready = true;
         }
