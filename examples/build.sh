6826302ddffe0b424a492e9e7eaa0488bfa2b269
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2014-10-06 13:29:02 +0200

    fixing error

--- examples/build.sh
+++ examples/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ -f "build_$1.sh" ]
+if [ -f "examples/build_$1.sh" ]
 then
     echo \"/build_$1.sh\"
-    eval "./build_$1.sh"
+    eval "./examples/build_$1.sh"
+else
+    echo "ERROR: node $1 not supported"
 fi
