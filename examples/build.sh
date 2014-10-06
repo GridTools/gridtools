4667cd577ff06987f0993fb7262a299068ce8891
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2014-10-06 13:50:31 +0200

    fixing error

--- examples/build.sh
+++ examples/build.sh
@@ -3,6 +3,7 @@
 if [ -f "examples/build_$1.sh" ]
 then
     echo \"/build_$1.sh\"
+    git clone https://github.com/ericniebler/fusion.git ../fusion
     eval "./examples/build_$1.sh"
 else
     echo "ERROR: node $1 not supported"
