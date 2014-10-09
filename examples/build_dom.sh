eeb736346e92744b9c5ba2b8d6ce6bbf326bb21c
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2014-10-09 11:36:49 +0200

    update

--- examples/build_dom.sh
+++ examples/build_dom.sh
@@ -42,7 +42,7 @@ pwd
 #mkdir build; cd build;
 
 /apps/dom/cmake/repository/gnu_446/bin/cmake \
--DCUDA_NVCC_FLAGS:STRING="-arch=sm_35  -G -std=c++11 --ptxas-options -v " \
+-DCUDA_NVCC_FLAGS:STRING="-arch=sm_35  -G -std=c++11 " \
 -DCUDA_SDK_ROOT_DIR:PATH=/opt/nvidia/cudatoolkit/5.5.20-1.0501.7945.8.2 \
 -DCMAKE_CXX_COMPILER:STRING="/apps/dom/gcc/4.8.2/bin/c++" \
 -DCMAKE_C_COMPILER:STRING="/apps/dom/gcc/4.8.2/bin/gcc" \
