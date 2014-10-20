6f03f7a02eb8dd14642c76e5095cd2c61c45aad5
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2014-10-20 11:28:57 +0200

    adding new test

---
+++ examples/vertical_advection.cpp
@@ -0,0 +1,11 @@
+#include "vertical_advection.h"
+
+int main(int argc, char** argv)
+{
+    if (argc != 4) {
+        std::cout << "Usage: interface1_<whatever> dimx dimy dimz\n where args are integer sizes of the data fields" << std::endl;
+        return 1;
+    }
+
+    return vertical_advection::test(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+}
