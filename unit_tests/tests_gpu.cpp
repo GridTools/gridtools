9f05492b7620ddd75b321828623620d6b562a421
Mauro Bianco <mbianco@cscs.ch>
2014-10-30 13:55:15 +0100

    First implementation of storages with 0,1,2 dimensions for using in stencils as 3D fields. Added tests and removed old copy test which would be redundant

--- unit_tests/tests_gpu.cpp
+++ unit_tests/tests_gpu.cpp
@@ -2,16 +2,16 @@
 
 #include "gtest/gtest.h"
 
+#define CUDA_EXAMPLE
 #include "gpu_clone.cu.h"
 #include "cloningstuff.cu.h"
 #include "test_domain.h"
 #include "test_cuda_storage.h"
 #include "test_hybrid_pointer.h"
-#include <../examples/interface1.h>
-#include <../examples/copy_stencil.h>
-#include <../examples/tridiagonal.h>
+#include "../examples/interface1.h"
+#include "copies_2D_1D_0D.h"
+#include "../examples/tridiagonal.h"
 
-#define CUDA_EXAMPLE
 #include "boundary_conditions_test.h"
 
 TEST(testdomain, testallocationongpu) {
@@ -66,8 +66,80 @@ TEST(stencil, horizontaldiffusion) {
     EXPECT_EQ(horizontal_diffusion::test(16, 16, 5), true);
 }
 
-TEST(stencil, copy) {
-    EXPECT_EQ(copy_stencil::test(512, 512, 60), true);
+#define __Size0 512
+#define __Size1 512
+#define __Size2 60
+
+TEST(stencil, copies3D) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<2,1,0> , gridtools::layout_map<2,1,0> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies3Dtranspose) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<0,1,2> , gridtools::layout_map<0,1,2> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2Dij) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<0,1,-1> , gridtools::layout_map<2,1,0> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2Dik) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<0,-1,1> , gridtools::layout_map<2,1,0> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2Djk) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<-1,0,1> , gridtools::layout_map<2,1,0> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2Di) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<0,-1,-1> , gridtools::layout_map<2,1,0> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2Dj) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<-1,0,-1> , gridtools::layout_map<2,1,0> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2Dk) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<-1,-1,0> , gridtools::layout_map<2,1,0> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2DScalar) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<-1,-1,-1> , gridtools::layout_map<2,1,0> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies3DDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<0,1,2> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies3DtransposeDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<2,1,0> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2DijDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<1,0,-1> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2DikDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<1,-1,0> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2DjkDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<-1,1,0> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2DiDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<0,-1,-1> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2DjDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<-1,0,-1> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2DkDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<-1,-1,0> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
+}
+
+TEST(stencil, copies2DScalarDst) {
+    EXPECT_EQ((copy_stencils_3D_2D_1D_0D::test<gridtools::layout_map<-1,-1,-1> , gridtools::layout_map<2,0,1> >(__Size0, __Size1, __Size2)), true);
 }
 
 TEST(stencil, tridiagonal) {
