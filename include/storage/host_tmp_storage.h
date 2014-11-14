43042369b3b4196fecd921a6c98713bbd36f1ff2
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2014-11-14 20:00:56 +0100

    commenting/cleaning

--- include/storage/host_tmp_storage.h
+++ include/storage/host_tmp_storage.h
@@ -25,19 +25,18 @@ This can be extended to higher dimensions and can be rewritten as a recurrency f
 
 namespace gridtools {
 
-
-    //N=0 is the coordinate with stride 1
+    /**@brief Metafunction for computing the coordinate N from the index (not currently used anywhere)
+       N=0 is the coordinate with stride 1*/
     template <uint_t N>
     struct coord_from_index;
-    //specializations outside the class scope
 
+    //specializations for each dimension (supposing we have 3)
     template<>
     struct coord_from_index<2>
     {
         static uint_t apply(uint_t index, uint_t* strides){
             printf("the coord from index: tile along %d is %d\n ", 0, strides[2]);
             return index%strides[2];
-//host_tmp_storage<Backend, ValueType, Layout, TileI, TileJ, MinusI, MinusJ, PlusI, PlusJ>::tile<0>::value
         }
     };
 
@@ -62,7 +61,18 @@ namespace gridtools {
             }
         };
 
-
+/**@brief Temporary storage class for the blocked algorithm
+   This storage contains one tile (see explanation in the file description) plus the halo region.
+   \targ Backend is normally the Host backend
+   \targ ValueType is the scalar type (usually doubles)
+   \targ Layout is the storage layout, defining which dimension corresponds to which stride
+   \targ TlieI is the tile dimension in the x direction
+   \targ TileJ is the tile dimension in the y direction
+   \targ MinusI is the halo dimension in the x direction at the left side
+   \targ MinusJ is the halo dimension in the y direction at the bottom side
+   \targ PlusI is the halo dimension in the x direction at the right side
+   \targ PlusJ is the halo dimension in the y direction at the top side
+*/
     template < enumtype::backend Backend
                , typename ValueType
                , typename Layout
@@ -73,38 +83,36 @@ namespace gridtools {
                , uint_t PlusI
                , uint_t PlusJ
                >
-    struct host_tmp_storage : public storage<base_storage<Backend
-                                                          , ValueType
-                                                          , Layout
-                                                          , true
-                                                          > >
+    struct host_tmp_storage : public base_storage<Backend
+                                                  , ValueType
+                                                  , Layout
+                                                  , true
+                                                  >
     {
 
-        typedef storage<base_storage<Backend
-                                     , ValueType
-                                     , Layout
-                                     , true> > base_type;
+        typedef base_storage<Backend
+                             , ValueType
+                             , Layout
+                             , true> base_type;
 
 
-        typedef typename base_type::super::layout layout;
-        typedef typename base_type::super::value_type value_type;
-        typedef typename base_type::super::iterator_type  iterator_type;
-        typedef typename base_type::super::const_iterator_type const_iterator_type;
+        typedef typename base_type::layout layout;
+        typedef typename base_type::value_type value_type;
+        typedef typename base_type::iterator_type  iterator_type;
+        typedef typename base_type::const_iterator_type const_iterator_type;
 
         typedef static_int<MinusI> minusi;
         typedef static_int<MinusJ> minusj;
         typedef static_int<PlusI> plusi;
         typedef static_int<PlusJ> plusj;
 
-        using base_type::super::m_strides;
-        using base_type::super::is_set;
+        using base_type::m_strides;
+        using base_type::is_set;
 
         static const std::string info_string;
 
         uint_t m_halo[3];
         uint_t m_initial_offsets[3];
-
-        // std::string m_name;
 
         explicit host_tmp_storage(uint_t initial_offset_i,
                                   uint_t initial_offset_j,
@@ -125,12 +133,7 @@ namespace gridtools {
 
         host_tmp_storage() {}
 
-        // ~host_tmp_storage() {
-        //     if (is_set) {
-        //         //std::cout << "deleting " << std::hex << data << std::endl;
-        //         delete[] m_data;
-        //     }
-        // }
+        virtual ~host_tmp_storage() {}
 
 
         virtual void info() const {
@@ -146,158 +149,38 @@ namespace gridtools {
                       << std::endl;
         }
 
-        uint_t move_to(uint_t i,uint_t j,uint_t k) const {
-            return tmp_index(i,j,k);
-            //return &(base_type::m_data[_index(i,j,k)]);
-        }
-
-        GT_FUNCTION
-        value_type& operator()(uint_t i, uint_t j, uint_t k) {
-            return base_type::super::m_data[tmp_index(i,j,k)];
-        }
-
-
-        GT_FUNCTION
-        value_type const & operator()(uint_t i, uint_t j, uint_t k) const {
-            return base_type::super::m_data[tmp_index(i,j,k)];
-        }
-
-
-        uint_t tmp_index(uint_t i, uint_t j, uint_t k) const {
-            uint_t index;
-            // std::cout << "                                                  index "
-            //           << "m_dims_i "
-            //           << m_dims[0]
-            //           << " "
-            //           << "m_dims_j "
-            //           << m_dims[1]
-            //           << " "
-            //           << "m_dims_k "
-            //           << m_dims[2]
-            //           << " - "
-            //           << "m_halo_i "
-            //           << m_halo[0]
-            //           << " "
-            //           << "m_halo_j "
-            //           << m_halo[1]
-            //           << " "
-            //           << "m_halo_k "
-            //           << m_halo[2]
-            //           << " - "
-            //           << "i "
-            //           << i
-            //           << " "
-            //           << "j "
-            //           << j
-            //           << " "
-            //           << "k "
-            //           << k
-            //           << std::endl;
-            // info();
-            assert(false);
-            uint_t _i = ((layout::template find<0>(i,j,k)) - layout::template find<0>(m_initial_offsets) + layout::template find<0>(m_halo));
-            std::cout << "uint_t _i = ((" << layout::template find<0>(i,j,k) << ")-" << layout::template find<0>(m_initial_offsets) << "+" << layout::template find<0>(m_halo) << ")" << std::endl;
-            uint_t _j = ((layout::template find<1>(i,j,k)) - layout::template find<1>(m_initial_offsets) + layout::template find<1>(m_halo));
-            std::cout << "uint_t _j = ((" << layout::template find<1>(i,j,k) << ")-" << layout::template find<1>(m_initial_offsets) << "+" << layout::template find<1>(m_halo) << ")" << std::endl;
-            uint_t _k = ((layout::template find<2>(i,j,k)) - layout::template find<2>(m_initial_offsets) + layout::template find<2>(m_halo));
-
-            index =
-                /*layout::template find<2>(m_dims) * layout::template find<1>(m_dims)*/m_strides[1] * _i +
-                /*layout::template find<2>(m_dims)*/m_strides[2] * _j + _k;
-
-            // index =
-            //     /*layout::template find<2>(m_dims) * layout::template find<1>(m_dims)*/m_strides[1] * (i%4) +
-            //     /*layout::template find<2>(m_dims)*/m_strides[2] * (j%4) + k;
-
-
-
-            // std::cout << " i  = " << _i
-            //           << " j  = " << _j
-            //           << " k  = " << _k
-            //           << "   = " << index
-            //           << std::endl;
-
-            assert(false);
-            assert(index >= 0);
-            assert(index <m_strides[0]);
-
-            return index;
-        }
-
-        // GT_FUNCTION
-        // value_type& operator()(uint_t i, uint_t j, uint_t k) {
-        //     /* std::cout<<"indices= "<<i<<" "<<j<<" "<<k<<std::endl; */
-        //     //backend_traits_t::assertion(_index(i,j,k) >= 0);
-        //     backend_traits_t::assertion(_index(i,j,k) < /* m_size*/ m_strides[0]);
-        //     return m_data[_index(i,j,k)];
-        // }
-
-
-        // GT_FUNCTION
-        // value_type const & operator()(uint_t i, uint_t j, uint_t k) const {
-        //     //backend_traits_t::assertion(_index(i,j,k) >= 0);
-        //     backend_traits_t::assertion(_index(i,j,k) < /*m_size*/m_strides[0]);
-        //     return m_data[_index(i,j,k)];
-        // }
-
-        /**@brief prints a single value of the data field given the coordinates*/
-        void print_value(uint_t i, uint_t j, uint_t k){ printf("value(%d, %d, %d)=%f, at index %d on the data\n", i, j, k, base_type::super::m_data[tmp_index(i, j, k)], tmp_index(i, j, k));}
-
-        //N=0 is the dimension with stride 1
-        template <uint_t N>
-        struct tile
-        {
-            static const uint_t value=layout::template find<N, boost::mpl::vector_c<uint_t, (TileI+MinusI+PlusI)*(TileJ+MinusJ+PlusJ), (TileJ+MinusJ+PlusJ)*6,6> >();
-        };
-
-        // template <uint_t Coordinate>
-        // GT_FUNCTION
-        // void increment(uint_t& b, uint_t* index){
-        //     //printf("NO blocking on K; index before incrementing: %d\n", *index);
-        //     uint_t idx = *index + (base_type::template strides<Coordinate>(m_strides));
-        //     //printf("Nidx: %d\n", idx);
-        //     // uint_t coord=coord_from_index<layout::template pos_<Coordinate>::value >::apply(idx, m_strides);//this is e.g. j*strideJ
-        //     // assert(coord%base_type::template strides<Coordinate>(m_strides)==0);
-        //     // printf("coords before division: %d\n", coord);
-        //     // coord=coord/base_type::template strides<Coordinate>(m_strides);// this is e.g. j
-        //     // printf("coords after division: %d\n", coord);
-        //     // uint dim=base_type::template dims_stridewise<Coordinate>(m_strides);
-        //     // printf("k loop index: %d; idx: %d, coord: %d, dim: %d  \n", *index, idx, coord, base_type::template dims_stridewise<Coordinate>(m_strides));
-        //     // if( coord <= dim)
-        //     *index=idx;
-        //     // else
-        //     //     *index=*index-((coord-1)%dim)*base_type::template strides<Coordinate>(m_strides);
-
-        //     // printf("index : %d -> %d, in direction %d, which in stride order means %d\n", base_type::template strides<Coordinate>(m_strides),  *index, Coordinate, layout::template pos_<Coordinate>::value);
-        // }
-
-        // template <uint_t Coordinate>
-        // GT_FUNCTION
-        // void decrement(uint_t& b, uint_t* index){
-        //     *index = (*index - base_type::template strides<Coordinate>(m_strides))%m_strides[0];//%(Coordinate==0?TileI+MinusI+PlusI:TileJ+MinusJ+PlusJ);
-        // }
-
+        /**@brief increment of 1 step along the specified direction. This method is used to increment in the vertical direction, where at present no blocking is performed.*/
         template <uint_t Coordinate>
         GT_FUNCTION
         void increment(uint_t b, uint_t* index){
-            base_type::super::template increment<Coordinate>(b, index);
+            base_type::template increment<Coordinate>(b, index);
         }
 
+        /** @brief increment in the horizontal direction (i or j). This method updates the storage index, so that an increment of 'dimension' is obtained in the 'Coordinate' direction.
+            The formula for incrementing the indices is the following:
+            given the coordinate direction \f$C\in\{0,1,2\}\f$, the index i defining the increment in the direction C, and the global storage index ID, which identifies univocally the current storage entry and has to be updated with the increment, :
+
+            \f$ID=ID+i-(b*tile)-offset+halo\f$
+
+            where tile is the tile dimension in the C direction, b is the current block index being accessed, offset an halo are respectively the constant offset at the domain boundary for the coordinate C and the dimension of the overlap along C between tiles (identified by the data dependency requirements between tiles).
+        */
         template <uint_t Coordinate>
         GT_FUNCTION
         void increment(uint_t dimension, uint_t b, uint_t* index){
-            std::cout<<"dimension: "<<(int)dimension<<"block: "<<(int)b<<"tile: "<<(int) (Coordinate?TileI:TileJ) << std::endl;
+            // std::cout<<"dimension: "<<(int)dimension<<"block: "<<(int)b<<"tile: "<<(int) (Coordinate?TileI:TileJ) << std::endl;
             uint_t tile=Coordinate==0?TileI:TileJ;
             uint_t var=dimension - b * tile;
 
-            std::cout << "uint_t coor = ((" << var << ")-" << layout::template find<Coordinate>(&m_initial_offsets[0]) << "+" << layout::template find<Coordinate>(&m_halo[0]) << ")";
+            // std::cout << "uint_t coor = ((" << var << ")-" << layout::template find<Coordinate>(&m_initial_offsets[0]) << "+" << layout::template find<Coordinate>(&m_halo[0]) << ")";
             uint_t coor=var-layout::template find<Coordinate>(&m_initial_offsets[0]) + layout::template find<Coordinate>(&m_halo[0]);
-            std::cout << " = "<<coor<<std::endl;
+            // std::cout << " = "<<coor<<std::endl;
             *index += coor*layout::template find<Coordinate>(&m_strides[1]);
-            std::cout << "index = "<<*index<<std::endl;
-            std::cout << "strides = "<<m_strides[0]<<", "<<m_strides[1]<<", "<<m_strides[2] <<std::endl;
+            // std::cout << "index = "<<*index<<std::endl;
+            // std::cout << "strides = "<<m_strides[0]<<", "<<m_strides[1]<<", "<<m_strides[2] <<std::endl;
         }
 
+        /**@brief decrement in the horizontal direction (i or j). Analogous to the increment.
+         TODO avoid code repetition*/
         template <uint_t Coordinate>
         GT_FUNCTION
         void decrement(uint_t& dimension, uint_t& b, uint_t* index){
@@ -307,8 +190,6 @@ namespace gridtools {
             uint_t coor=var-layout::template find<Coordinate>(&m_initial_offsets[0]) + layout::template find<Coordinate>(&m_halo[0]);
             *index -= coor*layout::template find<Coordinate>(&m_strides[1]);
         }
-
-
     };
 
 
@@ -319,6 +200,8 @@ namespace gridtools {
         boost::lexical_cast<std::string>(minusj::value)+
         boost::lexical_cast<std::string>(plusi::value)+
         boost::lexical_cast<std::string>(plusj::value);
+
+//################# below there are template specializations #########################
 
     template <enumtype::backend Backend,
               typename ValueType
