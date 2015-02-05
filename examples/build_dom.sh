484eae3c2d5bfe19cbd22fc3c03c3e48be61feef
Mauro Bianco <mbianco@cscs.ch>
2015-02-05 15:11:54 +0100

    fixing tests

--- examples/build_dom.sh
+++ examples/build_dom.sh
@@ -81,9 +81,7 @@ module load cuda/6.0; # load runtime libs
 find . |grep tests_gpu
 salloc --gres=gpu:2 srun ./tests_gpu
 
-find . |grep test_halo_exchange
-ls -l
-ls -l ..
+find .. |grep test_halo_exchange
 
 salloc --gres=gpu:2 ../examples/communication/run_communication_tests.sh
 else
