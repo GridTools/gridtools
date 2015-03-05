c36c1c0cfeb764eb558ea654a1f5aabac339dd6c
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-03-05 18:21:49 +0100

    fixes for the GPU make_compuation

--- include/stencil-composition/make_computation.h
+++ include/stencil-composition/make_computation.h
@@ -10,8 +10,8 @@
 namespace gridtools {
 #ifdef __CUDACC__
     template <typename Backend, typename LayoutType, typename MssType, typename Domain, typename Coords>
-    computation* make_computation(typename mss_array<boost::mpl::vector1<MssType> >::type const& mss, Domain & domain, Coords const& coords) {
-        return new intermediate<Backend, LayoutType, typename mss_array<boost::mpl::vector1<MssType> >::type, Domain, Coords>(mss, boost::ref(domain), coords);
+    computation* make_computation(MssType const& mss, Domain & domain, Coords const& coords) {
+        return new intermediate<Backend, LayoutType, typename mss_array<boost::mpl::vector1<MssType> >::type, Domain, Coords>(boost::ref(domain), coords);
     }
 #else
     template <typename Backend, typename LayoutType, typename MssType, typename Domain, typename Coords>
