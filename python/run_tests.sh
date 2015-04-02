46c5afa91fa75a4259e6eff8e7ee6fa333ff16b4
Lucas Benedicic <benedicic@cscs.ch>
2015-04-02 10:34:14 +0200

    Using run_tests.sh to generate coverage report.-

---
+++ python/run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# remove any files left from the previous run
+#
+rm -rf /tmp/__gridtools_* 
+
+#
+# run the tests, including a coverage report
+#
+nosetests -v -s -x --with-coverage --cover-package=gridtools --cover-erase --cover-html tests.test_stencils
