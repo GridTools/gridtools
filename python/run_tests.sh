46379c15a0945490674f417b9eae2d48c9606431
Lucas Benedicic <benedicic@cscs.ch>
2015-04-27 10:56:53 +0200

    Test coverage and new requirements.-

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -8,4 +8,4 @@ rm -rf /tmp/__gridtools_*
 #
 # run the tests, including a coverage report
 #
-nosetests -v -s -x --with-coverage --cover-package=gridtools --cover-erase --cover-html tests.test_combined_stencils tests.test_stencils
+nosetests -v -s -x --with-coverage --cover-package=gridtools --cover-erase --cover-html tests.test_combined_stencils tests.test_stencils tests.test_sw
