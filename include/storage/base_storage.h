227c493027678faef331a349de647bcb9e7610ff
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2015-04-27 17:02:24 +0200

    fixing the bug on GPU

--- include/storage/base_storage.h
+++ include/storage/base_storage.h
@@ -614,8 +614,11 @@ namespace gridtools {
     protected:
         bool is_set;
         const char* m_name;
-        // static const uint_t m_strides[/*3*/space_dimensions]={( dim1*dim2*dim3 ),( dims[layout::template get<2>()]*dims[layout::template get<1>()]),( dims[layout::template get<2>()] )};
+#ifdef __CUDACC__ // this is related to the fact that the gridtools::array should not be templated to a const type when CXX11 disabled
+        pointer_type m_fields[field_dimensions];
+#else
         array<pointer_type, field_dimensions> m_fields;
+#endif
         array<uint_t, space_dimensions> m_dims;
         array<uint_t, space_dimensions> m_strides;
 
