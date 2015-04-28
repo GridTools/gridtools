9a3b5c1c2462097247802a8dbf5101ac7f8d3569
Mauro Bianco <mbianco@cscs.ch>
2015-04-28 17:50:12 +0200

    added comments to the script

--- test_compilation.sh
+++ test_compilation.sh
@@ -1,3 +1,10 @@
+
+# Launch the script when you have already a CMake environment set
+# The script will change only those variables that are involved in the
+# testing. If something is wrong it will stop and by running "make"
+# you can see what compilation error you are having. You could also
+# look at the generated files, but they are mangled by the -j option.
+
 FAIL=false
 for gpu in ON OFF; do
     for mpi in ON OFF; do
@@ -7,13 +14,9 @@ for gpu in ON OFF; do
                 if [ "$FAIL" == "false" ]; then
                     echo FILE NAME BEING PRODUCED $name
                     cmake \
-                        -DCUDA_NVCC_FLAGS:STRING="-arch=sm_35" \
                         -DUSE_GPU:BOOL=$gpu \
                         -DUSE_MPI:BOOL=$mpi \
-                        -DUSE_MPI_COMPILER:BOOL=OFF \
                         -DCMAKE_BUILD_TYPE:STRING=$build\
-                        -DCMAKE_CXX_FLAGS:STRING=" -fopenmp -O3 -m64 -mavx -DNDEBUG -DBOOST_SYSTEM_NO_DEPRECATED"  \
-                        -DSINGLE_PRECISION=$precision \
                         -DENABLE_CXX11=$cpp11 \
                         .
 
