41bfdb81cc4d1b6e0d45d5c8683def0f7bafef05
Lucas Benedicic <benedicic@cscs.ch>
2015-04-29 17:54:16 +0200

    One plotting environment for the whole session.-

--- python/gridtools/__init__.py
+++ python/gridtools/__init__.py
@@ -16,3 +16,20 @@ def join_with_prefix (a_list, prefix, attribute=None):
 JinjaEnv = Environment (loader=PackageLoader ('gridtools',
                                               'templates'))
 JinjaEnv.filters["join_with_prefix"] = join_with_prefix
+
+
+#
+# plotting environment
+#
+try:
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import axes3d
+
+except ImportError:
+    logging.error ("Matplotlib not found: plotting is not available")
+
+else:
+    fig = plt.figure ( )
+    ax  = fig.add_subplot (111,
+                           projection='3d',
+                           autoscale_on=False)
