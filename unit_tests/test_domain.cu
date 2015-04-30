7909dc1d428efadcaebdb59cc6b75a14495720c6
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2015-04-30 19:11:06 +0200

    fixing some compilation errors,changing defs.h

--- unit_tests/test_domain.cu
+++ unit_tests/test_domain.cu
@@ -1,9 +1,9 @@
-/* 
+/*
  * File:   test_domain.cu
  * Author: mbianco
  *
  * Created on February 5, 2014, 4:19 PM
- * 
+ *
  * Test domain especially with the cloning to GPU and hybrid_pointers
  */
 
