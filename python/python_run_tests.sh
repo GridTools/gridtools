20f53f134b28abad9dde3c45e6dab2f14d39a5aa
Lucas Benedicic <benedicic@cscs.ch>
2015-05-06 17:15:24 +0200

    Interactive mode corrected.-

--- python/python_run_tests.sh
+++ python/python_run_tests.sh
@@ -9,48 +9,48 @@ CMAKE_SOURCE_DIR=$1
 PYTHON_INSTALL_PREFIX=$2
 
 #
-# run the tests, including a coverage report
+# run interactively without arguments
 #
-# Checking gcc version (it has to be >=4.9.x)
-gcc_vers=`gcc -dumpversion|cut -f1 -d.`
-if [ $gcc_vers -lt 4 ]
-then
-  echo "gcc version 4.x is required. EXIT NOW"
-  exit 1
-else
-  gcc_vers=`gcc -dumpversion|cut -f2 -d.`
-  if [ $gcc_vers -lt 9 ]
-  then
-    echo "gcc version 4.9.x is required. EXIT NOW"
-    exit 1
-  else
-   # gcc version is 4.9.x
-   # Lookig for PYTHON_INSTALL_PREFIX
-   if [ "$PYTHON_INSTALL_PREFIX" != " " ]
-   then
-      # Looking for virtualenv
-      virtualenv_cmd=`which virtualenv`
-      if [ $? -eq 0 ]
+if [ -n "${CMAKE_SOURCE_DIR}" ] && [ -n "${PYTHON_INSTALL_PREFIX}" ]; then
+    # Checking gcc version (it has to be >=4.9.x)
+    gcc_vers=`gcc -dumpversion|cut -f1 -d.`
+    if [ $gcc_vers -lt 4 ]
+    then
+      echo "gcc version 4.x is required. EXIT NOW"
+      exit 1
+    else
+      gcc_vers=`gcc -dumpversion|cut -f2 -d.`
+      if [ $gcc_vers -lt 9 ]
       then
-        source ${PYTHON_INSTALL_PREFIX}/bin/activate
-        export GRIDTOOLS_ROOT=$CMAKE_SOURCE_DIR
-
-        echo "Running python tests in ${PYTHON_INSTALL_PREFIX} ..."
-        nosetests -v -s --with-coverage --cover-package=gridtools --cover-erase --cover-html tests.test_combined_stencils tests.test_stencils tests.test_sw
-        if [ $? -ne 0 ]
-        then
-          echo "Error running python tests. EXIT NOW"
-          exit 1
-        else
-          ${PYTHON_INSTALL_PREFIX}/bin/deactivate
-        fi
+        echo "gcc version 4.9.x is required. EXIT NOW"
+        exit 1
       else
-        echo "Error while running virtualenv. EXIT NOW"
-        exit  1
+        # gcc version is 4.9.x
+        # Looking for PYTHON_INSTALL_PREFIX
+        if [ "$PYTHON_INSTALL_PREFIX" != " " ]
+        then
+          # Looking for virtualenv
+          virtualenv_cmd=`which virtualenv`
+          if [ $? -eq 0 ]
+          then
+            source ${PYTHON_INSTALL_PREFIX}/bin/activate
+            export GRIDTOOLS_ROOT=$CMAKE_SOURCE_DIR
+          else
+            echo "Error while running virtualenv. EXIT NOW"
+            exit  1
+          fi
+        fi 
       fi
-   else
-       echo "Running python tests ..."
-       nosetests -v -s --with-coverage --cover-package=gridtools --cover-erase --cover-html tests.test_combined_stencils tests.test_stencils tests.test_sw
-   fi 
-  fi
+    fi
+fi
+
+echo "Running python tests ..."
+nosetests -v -s -x --with-coverage --cover-package=gridtools --cover-erase --cover-html tests.test_combined_stencils tests.test_stencils tests.test_sw
+if [ $? -ne 0 ]; then
+    echo "Error running python tests. EXIT NOW"
+    exit 1
+else
+    if [ -n "${PYTHON_INSTALL_PREFIX}" ]; then
+        ${PYTHON_INSTALL_PREFIX}/bin/deactivate
+    fi
 fi
