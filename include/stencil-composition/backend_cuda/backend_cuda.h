e1f63c48d8e3c54f6c6ff339f231a2b622a674a6
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-05-08 12:13:19 +0200

    fix for host backend (naive). Block still fails

--- include/stencil-composition/backend_cuda/backend_cuda.h
+++ include/stencil-composition/backend_cuda/backend_cuda.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// This file contains all header files required by the cuda backend
 #include "../execution_policy.h"
 #include "../heap_allocated_temps.h"
 #include "backend_traits_cuda.h"
