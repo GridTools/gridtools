f815b343245b472e627d024cd76da00092e63692
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-05-08 12:00:42 +0200

    fix removing backend_traits.h

--- include/stencil-composition/backend_host/backend_host.h
+++ include/stencil-composition/backend_host/backend_host.h
@@ -1,9 +1,9 @@
 #pragma once
 
+// This file contains all header files required by the host backend
 #include "../execution_policy.h"
 #include "../heap_allocated_temps.h"
-#include "backend_traits.h"
-
 #include "../iteration_policy.h"
 #include "../../common/gridtools_runtime.h"
+#include "backend_traits_host.h"
 #include "../loop_hierarchy.h"
