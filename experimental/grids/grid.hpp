50e4ff3105678fc9507d5ae0f7ef11d84c9b96e0
Mauro Bianco <mbianco@cscs.ch>
2015-05-22 11:35:56 +0200

    works so far

--- experimental/grids/grid.hpp
+++ experimental/grids/grid.hpp
@@ -5,63 +5,14 @@
 
 namespace gridtools {
 
-    class storage_type {
-        int N,M;
-
-    public:
-        storage_type(int N, int M)
-            : N{N}
-            , M{M}
-        {}
-
-        unsigned int size_i() const {return N;}
-        unsigned int size_j() const {return M;}
-
-        int offset(array<unsigned int, 2> const& indices) const {
-            return indices[0]*M+indices[1];
-        }
-    };
-
-    template <typename ValueType>
-    class cell_storage_type: public storage_type
-    {
-    public:
-        using storage_type::storage_type;
-    };
-
-    template <typename ValueType>
-    class vertex_storage_type: public storage_type
-    {
-    public:
-        using storage_type::storage_type;
-    };
-
-    template <typename ValueType>
-    class edge_storage_type: public storage_type
-    {
-    public:
-        using storage_type::storage_type;
-    };
-
     /**
-       This class defines the maps between LocationTypes in the trapezoid grid.
-
-       The entities are: Cells, Vertices, and Edges.
-
-       Sizes are defined in terms of Cells.
-
-       There are two types of maps:
-       1) between index tuples
-       2) between indices and offsets
     */
-    template <typename T = double, typename U = double, typename V = double>
+    template <typename CellStorageT, typename EdgeStorageT, unsigned int TileI, unsigned int TileJ>
     class trapezoid_2D {
-        using cell_storage_t = cell_storage_type<T>;
-        using vertex_storage_t = vertex_storage_type<T>;
-        using edge_storage_t = edge_storage_type<T>;
+        using cell_storage_t = CellStorageT;
+        using edge_storage_t = EdgeStorageT;
 
         cell_storage_t const& cell_storage;
-        vertex_storage_t const& vertex_storage;
         edge_storage_t const& edge_storage;
         const unsigned int M,N; // Sizes as cells in a multi-dimensional Cell array
 
@@ -70,30 +21,33 @@ namespace gridtools {
     public:
         static constexpr unsigned int u_cell_size_j(int _M) {return _M+4;}
         static constexpr unsigned int u_cell_size_i(int _N) {return _N+2;}
-        static constexpr unsigned int u_vertex_size_j(int _M) {return _M/2+1+2;}
-        static constexpr unsigned int u_vertex_size_i(int _N) {return _N+1+2;}
         static constexpr unsigned int u_edge_size_j(int _M) {return 3*(_M/2)+3;}
         static constexpr unsigned int u_edge_size_i(int _N) {return _N+2;}
 
     private:
         int cell_size_j() const {return static_cast<int>(u_cell_size_j(M));}
         int cell_size_i() const {return static_cast<int>(u_cell_size_i(M));}
-        int vertex_size_j() const {return static_cast<int>(u_vertex_size_j(M));}
-        int vertex_size_i() const {return static_cast<int>(u_vertex_size_i(M));}
         int edge_size_j() const {return static_cast<int>(u_edge_size_j(M));}
         int edge_size_i() const {return static_cast<int>(u_edge_size_i(M));}
 
         unsigned int u_cell_size_j() const {return (u_cell_size_j(M));}
         unsigned int u_cell_size_i() const {return (u_cell_size_i(M));}
-        unsigned int u_vertex_size_j() const {return (u_vertex_size_j(M));}
-        unsigned int u_vertex_size_i() const {return (u_vertex_size_i(M));}
         unsigned int u_edge_size_j() const {return (u_edge_size_j(M));}
         unsigned int u_edge_size_i() const {return (u_edge_size_i(M));}
 
+
+        array<unsigned int, cell_storage_t::space_dimensions> cell_api_to_internal(array<unsigned int, Dims> const& indices) const {
+            return array<unsigned int, cell_storage_t::space_dimensions>{indices[0]/TileI,
+                                                                         indices[1]/TileJ,
+                                                                         indices[0]%TileI,
+                                                                         indices[1]&1,
+                                                                         (indices[1]/2)%(TileJ)
+                                                                        };
+        }
+
     public :
-        trapezoid_2D(cell_storage_t const& cs, vertex_storage_t const& vs, edge_storage_t const& es)
+        trapezoid_2D(cell_storage_t const& cs, edge_storage_t const& es)
             : cell_storage(cs)
-            , vertex_storage(vs)
             , edge_storage(es)
             , M{cell_storage.size_j()}
             , N{cell_storage.size_i()}
@@ -102,70 +56,242 @@ namespace gridtools {
             assert((N&1) == 0);
         }
 
-        array<array<unsigned int, Dims>, 3> cell2cells(array<unsigned int, Dims> const& indices) const {
-            return array<array<unsigned int, Dims>, 3>{array<unsigned int, Dims>{indices[0], indices[1]-1},
-                                                       array<unsigned int, Dims>{indices[0], indices[1]+1},
-                                                       array<unsigned int, Dims>{(indices[1]&1)?indices[0]+1:indices[0]-1, indices[1]+((indices[1]&1)?-1:1)}};
-            }
         array<int, 3> cell2cells_offsets(array<unsigned int, Dims> const& indices) const {
             return array<int, 3>{-1, 1, (indices[1]&1)?cell_size_j():-cell_size_j()};
-            }
-        array<array<unsigned int, Dims>, 6> vertex2vertices(array<unsigned int, Dims> const& indices) const {
-            return array<array<unsigned int, Dims>, 6>{ array<unsigned int, Dims>{indices[0]-1, indices[1]},
-                                                        array<unsigned int, Dims>{indices[0]-1, indices[1]+1},
-                                                        array<unsigned int, Dims>{indices[0], indices[1]-2},
-                                                        array<unsigned int, Dims>{indices[0], indices[1]+1},
-                                                        array<unsigned int, Dims>{indices[0]+1, indices[1]-1},
-                                                        array<unsigned int, Dims>{indices[0]+1, indices[1]}
-                                                      };
-        }
-
-        array<int, 6> vertex2vertices_offsets(array<unsigned int, Dims> const&) const {
-            return array<int, 6>{-vertex_size_j(), -vertex_size_j()+1, -1, 1, vertex_size_j()-1, vertex_size_j()};
-        }
-        array<array<unsigned int, Dims>, 4> edge2edges(array<unsigned int, Dims> const& indices) const {
-            return array<array<unsigned int, Dims>, 4>{ {indices[0], indices[1]-1}, 
-                                                        {indices[0], indices[1]+1},
-                                                        {indices[0] + ((indices[1]&1)?1:-1), indices[1]+ ((indices[1]&1)?2:-2)},
-                                                        {indices[0]+1, indices[1]+((indices[1]&1)?-2:2)}
-                                                      };
         }
 
         array<int, 4> edge2edges_offsets(array<unsigned int, Dims> const& indices) const {
             return array<int, 4>{ -1, +1, (indices[1]&1)?-2:2, edge_size_j()+ ((indices[1]&1)?0:1)};
         }
 
-        array<array<unsigned int, Dims>, 3> cell2vertices(array<unsigned int, Dims> const& indices) const {
-            return array<array<unsigned int, Dims>, 3>{ {indices[0], indices[1]}, 
-                                                        {(indices[0]&1)?indices[0]:(indices[0]+1), indices[1]+ ((indices[0]&1)?1:-1)}, 
-                                                        {indices[0]+1, indices[1]}
-                                                      };
-        }
-        array<int, 3> cell2vertices_offsets(array<unsigned int, Dims> const& indices) const {
-            return array<int, 3>{ edge_storage.offset(indices), // no need for computing anything here, sice {indices[0], indices[1]} is also the index of the reference vertex in the neighbohrood of the node
-                                  (indices[0]&1)?vertex_size_j():1,
-                                  (indices[0]&1)?vertex_size_j()+1:vertex_size_j()
-                                };
-        }
-
-        array<array<unsigned int, Dims>, 3> cell2edges(array<unsigned int, Dims> const& indices) const {
-            return array<array<unsigned int, Dims>, 3>{ {indices[0] + ((indices[0]&1)?1:0), (indices[1]/2)*3+1},
-                                                        {indices[0], (indices[1]/2)*3+2},
-                                                        {indices[0], indices[1] + ((indices[0]&1)?3:0)}
-                                                      };
-        }
         array<int, 3> cell2edges_offsets(array<unsigned int, Dims> const& indices) const {
             return array<int, 3>{edge_storage.offset({indices[0], (indices[1]/2)+1}), 1, (indices[0]&1)?3:0};
         }
 
-//         array<array<unsigned int, Dims>, 6> vertex2cells(array<unsigned int, Dims> const&) const {
-//         }
-//         array<array<unsigned int, Dims>, 6> vertex2edges(array<unsigned int, Dims> const&) const {
-//         }
-//         array<array<unsigned int, Dims>, 2> edge2cells(array<unsigned int, Dims> const&) const {
-//         }
-//         array<array<unsigned int, Dims>, 2> edge2verices(array<unsigned int, Dims> const&) const {
-//         }
+    };
+
+    /**
+    */
+    template <typename CellStorageT, typename EdgeStorageT>
+    class trapezoid_2D_no_tile {
+        using cell_storage_t = CellStorageT;
+        using edge_storage_t = EdgeStorageT;
+
+        cell_storage_t const* cell_storage;
+        edge_storage_t const* edge_storage;
+        const unsigned int M,N; // Sizes as cells in a multi-dimensional Cell array
+
+        static constexpr int Dims = 2;
+
+    public:
+        static constexpr unsigned int u_cell_size_j(int _M) {return _M+4;}
+        static constexpr unsigned int u_cell_size_i(int _N) {return _N+2;}
+        static constexpr unsigned int u_edge_size_j(int _M) {return 3*(_M/2)+6;}
+        static constexpr unsigned int u_edge_size_i(int _N) {return _N+2;}
+
+        unsigned int cs0;
+        unsigned int cs1;
+        unsigned int cs2;
+        unsigned int ce0;
+        unsigned int ce1;
+        unsigned int ce2;
+        
+        /** i[1] must be even */
+        /*static constexpr*/ array<unsigned int, Dims+1> u_cell_size(array<unsigned int, Dims> const& i) {
+            cs0 = i[0];
+            cs1 = 2;
+            cs2 = i[1]/2;
+            return array<unsigned int, Dims+1>{i[0], 2, i[1]/2};
+        }
+
+        /** i[1] must be even */
+        /*static constexpr*/ array<unsigned int, Dims+1> u_edge_size(array<unsigned int, Dims> const& i) {
+            ce0 = i[0]+1;
+            ce1 = 3;
+            ce2 = i[1]/3;
+            return array<unsigned int, Dims+1>{i[0]+1, 3, (i[1])/3};
+        }
+
+    private:
+        int cell_size_j() const {return static_cast<int>(u_cell_size_j(M));}
+        int cell_size_i() const {return static_cast<int>(u_cell_size_i(M));}
+        int edge_size_j() const {return static_cast<int>(u_edge_size_j(M));}
+        int edge_size_i() const {return static_cast<int>(u_edge_size_i(M));}
+
+        unsigned int u_cell_size_j() const {return (u_cell_size_j(M));}
+        unsigned int u_cell_size_i() const {return (u_cell_size_i(M));}
+        unsigned int u_edge_size_j() const {return (u_edge_size_j(M));}
+        unsigned int u_edge_size_i() const {return (u_edge_size_i(M));}
+
+        unsigned int cell__index(int i, int j, int k) const {
+            return (cs1*cs2)*i+cs2*j+k;
+        }
+        
+        unsigned int edge__index(int i, int j, int k) const {
+            return (ce1*ce2)*i+ce2*j+k;
+        }
+        
+    public :
+        trapezoid_2D_no_tile()
+            : N{}
+            , M{}
+        {}
+
+        trapezoid_2D_no_tile(cell_storage_t const& cs, edge_storage_t const& es, unsigned int N, unsigned int M)
+            : cell_storage(&cs)
+            , edge_storage(&es)
+            , M{M}
+            , N{N}
+        {
+            u_cell_size({N,M});
+            u_edge_size({N,M});
+            assert((M&1) == 0);
+            assert((N&1) == 0);
+        }
+
+#ifdef USE_STORAGE
+#define DO_THE_MATH(stor, i,j,k)                \
+        stor ## _storage->_index(i,j,k)
+#else
+#define DO_THE_MATH(stor, i,j,k)                \
+        stor ## __index(i,j,k)
+#endif
+        
+        array<unsigned int, 3>
+        cell2cells_ll_p1(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 3>{
+                DO_THE_MATH(cell, i[0], 0, i[1]),
+                DO_THE_MATH(cell, i[0], 0, i[1]+1),
+                DO_THE_MATH(cell, i[0]+1, 0, i[1])};
+        }
+
+        array<unsigned int, 3>
+        cell2cells_ll_p0(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 3>{
+                DO_THE_MATH(cell, i[0], 1, i[1]-1),
+                DO_THE_MATH(cell, i[0], 1, i[1]),
+                DO_THE_MATH(cell, i[0]-1, 1, i[1])};
+        }
+
+        array<unsigned int, 4>
+        edge2edges_ll_p0(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 4>{
+                DO_THE_MATH(edge, i[0], 1, i[1]),
+                DO_THE_MATH(edge, i[0]+1, 1, i[1]-1),
+                DO_THE_MATH(edge, i[0], 2, i[1]),
+                DO_THE_MATH(edge, i[0], 2, i[1]-1)};
+        }
+
+        array<unsigned int, 4>
+        edge2edges_ll_p1(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 4>{
+                DO_THE_MATH(edge, i[0], 0, i[1]),
+                DO_THE_MATH(edge, i[0]-1, 0, i[1]+1),
+                DO_THE_MATH(edge, i[0], 2, i[1]),
+                DO_THE_MATH(edge, i[0]-1, 2, i[1])};
+        }
+
+        array<unsigned int, 4>
+        edge2edges_ll_p2(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 4>{
+                DO_THE_MATH(edge, i[0], 0, i[1]),
+                DO_THE_MATH(edge, i[0], 0, i[1]+1),
+                DO_THE_MATH(edge, i[0], 1, i[1]),
+                DO_THE_MATH(edge, i[0]+1, 1, i[1])};
+        }
+
+        array<unsigned int, 3>
+        cell2edges_ll_p0(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 3>{
+                DO_THE_MATH(edge, i[0], 2, i[1]),
+                DO_THE_MATH(edge, i[0], 0, i[1]+1),
+                DO_THE_MATH(edge, i[0]+1, 1, i[1])};
+        }
+
+        array<unsigned int, 3>
+        cell2edges_ll_p1(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 3>{
+                DO_THE_MATH(edge, i[0], 0, i[1]),
+                DO_THE_MATH(edge, i[0], 1, i[1]),
+                DO_THE_MATH(edge, i[0], 2, i[1])};
+        }
+
+        array<unsigned int, 2>
+        edge2cells_ll_p0(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 2>{
+                DO_THE_MATH(cell, i[0], 1, i[1]-1),
+                DO_THE_MATH(cell, i[0], 0, i[1])};
+        }
+
+        array<unsigned int, 2>
+        edge2cells_ll_p1(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 2>{
+                DO_THE_MATH(cell, i[0]-1, 1, i[1]),
+                DO_THE_MATH(cell, i[0], 0, i[1])};
+        }
+
+        array<unsigned int, 2>
+        edge2cells_ll_p2(array<unsigned int, 2> const& i) const
+        {
+            return array<unsigned int, 2>{
+                DO_THE_MATH(cell, i[0], 0, i[1]),
+                DO_THE_MATH(cell, i[0], 1, i[1])};
+        }
+
+        array<unsigned int, 3>
+        cell2cells(array<unsigned int, 2> const& i) const
+        {
+            if (i[1]&1) {
+                return cell2cells_ll_p1({i[0], i[1]/2});
+            } else {
+                return cell2cells_ll_p0({i[0], i[1]/2});
+            }
+        }        
+
+        array<unsigned int, 4>
+        edge2edges(array<unsigned int, 2> const& i) const
+        {
+            switch (i[1]%3) {
+            case 0:
+                return edge2edges_ll_p0({i[0], i[1]/3});
+            case 1:
+                return edge2edges_ll_p1({i[0], i[1]/3});
+            case 2:
+                return edge2edges_ll_p2({i[0], i[1]/3});
+            }
+        }
+
+        array<unsigned int, 3>
+        cell2edges(array<unsigned int, 2> const& i) const
+        {
+            if (i[1]&1) {
+                return cell2edges_ll_p1({i[0], i[1]/2});
+            } else {
+                return cell2edges_ll_p0({i[0], i[1]/2});
+            }
+        }
+
+        array<unsigned int, 2>
+        edge2cells(array<unsigned int, 2> const& i) const
+        {
+            switch (i[1]%3) {
+            case 0:
+                return edge2cells_ll_p0({i[0], i[1]/3});
+            case 1:
+                return edge2cells_ll_p1({i[0], i[1]/3});
+            case 2:
+                return edge2cells_ll_p2({i[0], i[1]/3});
+            }
+        }
 
     };
 
--- experimental/grids/main.cpp
+++ experimental/grids/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "grid.hpp"
+#include <storage/base_storage.h>
+#include <common/layout_map.h>
 
 namespace gridtools {
     template <typename T, size_t D>
@@ -17,66 +19,64 @@ namespace gridtools {
 #define EVAL(f,x,y)                                                     \
     std::cout << #f << ": " << gridtools::array<int,2>{x,y} << " -> " << (grid.f({x,y})) << std::endl
 
+using gridtools::layout_map;
+using gridtools::wrap_pointer;
+
 int main() {
-    using trapezoid_2D = gridtools::trapezoid_2D<>;
-    using cell_storage_type = gridtools::cell_storage_type<double>;
-    using vertex_storage_type = gridtools::vertex_storage_type<double>;
-    using edge_storage_type = gridtools::edge_storage_type<double>;
+    using cell_storage_type = gridtools::base_storage<wrap_pointer<double>, layout_map<0,1,2> >;
+    using edge_storage_type = gridtools::base_storage<wrap_pointer<double>, layout_map<0,1,2> >;
 
-    trapezoid_2D grid
-        (cell_storage_type(trapezoid_2D::u_cell_size_i(20),trapezoid_2D::u_cell_size_j(8)),
-         vertex_storage_type(trapezoid_2D::u_vertex_size_i(20),trapezoid_2D::u_vertex_size_j(8)),
-         edge_storage_type(trapezoid_2D::u_edge_size_i(20),trapezoid_2D::u_edge_size_j(8)));
+    using trapezoid_2D = gridtools::trapezoid_2D_no_tile<cell_storage_type, edge_storage_type>;
 
+    uint_t NC = trapezoid_2D::u_cell_size_i(6);
+    uint_t MC = trapezoid_2D::u_cell_size_j(12);
+   
+    uint_t NE = trapezoid_2D::u_edge_size_i(6);
+    uint_t ME = trapezoid_2D::u_edge_size_j(12);
+
+    std::cout << "NC = " << NC << " "
+              << "MC = " << MC
+              << std::endl;
+    
+    std::cout << "NE = " << NE << " "
+              << "ME = " << ME
+              << std::endl;
+
+    std::cout << trapezoid_2D().u_cell_size(gridtools::array<unsigned int, 2>{NC, MC}) << std::endl;
+    std::cout << trapezoid_2D().u_edge_size(gridtools::array<unsigned int, 2>{NC, MC}) << std::endl;
+    
+    cell_storage_type cells(trapezoid_2D().u_cell_size(gridtools::array<unsigned int, 2>{NC, MC}));
+    edge_storage_type edges(trapezoid_2D().u_edge_size(gridtools::array<unsigned int, 2>{NE, ME}));
+
+    trapezoid_2D grid(cells, edges, NC, MC);
+
+    cells.info();
+    
+    EVAL(cell2cells_ll_p0, 1, 1);
+    EVAL(cell2cells_ll_p0, 1, 2);
+    EVAL(cell2cells_ll_p1, 1, 3);
+    EVAL(cell2cells_ll_p1, 1, 4);
     EVAL(cell2cells, 2, 3);
     EVAL(cell2cells, 2, 4);
     EVAL(cell2cells, 3, 3);
     EVAL(cell2cells, 3, 4);
 
-    EVAL(cell2cells_offsets, 2, 3);
-    EVAL(cell2cells_offsets, 2, 4);
-    EVAL(cell2cells_offsets, 3, 3);
-    EVAL(cell2cells_offsets, 3, 4);
-
-    EVAL(vertex2vertices, 2, 3);
-    EVAL(vertex2vertices, 2, 4);
-    EVAL(vertex2vertices, 3, 3);
-    EVAL(vertex2vertices, 3, 4);
-
-    EVAL(vertex2vertices_offsets, 2, 3);
-    EVAL(vertex2vertices_offsets, 2, 4);
-    EVAL(vertex2vertices_offsets, 3, 3);
-    EVAL(vertex2vertices_offsets, 3, 4);
-
+    EVAL(edge2edges_ll_p0, 2, 3);
+    EVAL(edge2edges_ll_p1, 2, 3);
+    EVAL(edge2edges_ll_p2, 2, 3);
+    EVAL(edge2edges, 2, 2);
     EVAL(edge2edges, 2, 3);
     EVAL(edge2edges, 2, 4);
-    EVAL(edge2edges, 3, 3);
-    EVAL(edge2edges, 3, 4);
 
-    EVAL(edge2edges_offsets, 2, 3);
-    EVAL(edge2edges_offsets, 2, 4);
-    EVAL(edge2edges_offsets, 3, 3);
-    EVAL(edge2edges_offsets, 3, 4);
+    // EVAL(cell2edges, 2, 3);
+    // EVAL(cell2edges, 2, 4);
+    // EVAL(cell2edges, 3, 3);
+    // EVAL(cell2edges, 3, 4);
 
-    EVAL(cell2vertices, 2, 3);
-    EVAL(cell2vertices, 2, 4);
-    EVAL(cell2vertices, 3, 3);
-    EVAL(cell2vertices, 3, 4);
-
-    EVAL(cell2vertices_offsets, 2, 3);
-    EVAL(cell2vertices_offsets, 2, 4);
-    EVAL(cell2vertices_offsets, 3, 3);
-    EVAL(cell2vertices_offsets, 3, 4);
-
-    EVAL(cell2edges, 2, 3);
-    EVAL(cell2edges, 2, 4);
-    EVAL(cell2edges, 3, 3);
-    EVAL(cell2edges, 3, 4);
-
-    EVAL(cell2edges_offsets, 2, 3);
-    EVAL(cell2edges_offsets, 2, 4);
-    EVAL(cell2edges_offsets, 3, 3);
-    EVAL(cell2edges_offsets, 3, 4);
+    // EVAL(cell2edges_offsets, 2, 3);
+    // EVAL(cell2edges_offsets, 2, 4);
+    // EVAL(cell2edges_offsets, 3, 3);
+    // EVAL(cell2edges_offsets, 3, 4);
 
     return 0;
 }
