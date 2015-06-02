0d4a9bf5b72bd0fd50c952b7975c347a28522523
Mauro Bianco <mbianco@cscs.ch>
2015-06-02 11:35:04 +0200

    moving to laptop

--- experimental/grids/grid.hpp
+++ experimental/grids/grid.hpp
@@ -2,88 +2,45 @@
 
 #include <common/array.h>
 #include <cassert>
+#include <boost/mpl/vector.hpp>
 
 namespace gridtools {
 
-    /**
-    */
-    template <typename CellStorageT, typename EdgeStorageT, unsigned int TileI, unsigned int TileJ>
-    class trapezoid_2D {
-        using cell_storage_t = CellStorageT;
-        using edge_storage_t = EdgeStorageT;
-
-        cell_storage_t const& cell_storage;
-        edge_storage_t const& edge_storage;
-        const unsigned int M,N; // Sizes as cells in a multi-dimensional Cell array
-
-        static constexpr int Dims = 2;
-
-    public:
-        static constexpr unsigned int u_cell_size_j(int _M) {return _M+4;}
-        static constexpr unsigned int u_cell_size_i(int _N) {return _N+2;}
-        static constexpr unsigned int u_edge_size_j(int _M) {return 3*(_M/2)+3;}
-        static constexpr unsigned int u_edge_size_i(int _N) {return _N+2;}
-
-    private:
-        int cell_size_j() const {return static_cast<int>(u_cell_size_j(M));}
-        int cell_size_i() const {return static_cast<int>(u_cell_size_i(M));}
-        int edge_size_j() const {return static_cast<int>(u_edge_size_j(M));}
-        int edge_size_i() const {return static_cast<int>(u_edge_size_i(M));}
-
-        unsigned int u_cell_size_j() const {return (u_cell_size_j(M));}
-        unsigned int u_cell_size_i() const {return (u_cell_size_i(M));}
-        unsigned int u_edge_size_j() const {return (u_edge_size_j(M));}
-        unsigned int u_edge_size_i() const {return (u_edge_size_i(M));}
-
-
-        array<unsigned int, cell_storage_t::space_dimensions> cell_api_to_internal(array<unsigned int, Dims> const& indices) const {
-            return array<unsigned int, cell_storage_t::space_dimensions>{indices[0]/TileI,
-                                                                         indices[1]/TileJ,
-                                                                         indices[0]%TileI,
-                                                                         indices[1]&1,
-                                                                         (indices[1]/2)%(TileJ)
-                                                                        };
-        }
-
-    public :
-        trapezoid_2D(cell_storage_t const& cs, edge_storage_t const& es)
-            : cell_storage(cs)
-            , edge_storage(es)
-            , M{cell_storage.size_j()}
-            , N{cell_storage.size_i()}
-        {
-            assert((M&1) == 0);
-            assert((N&1) == 0);
-        }
-
-        array<int, 3> cell2cells_offsets(array<unsigned int, Dims> const& indices) const {
-            return array<int, 3>{-1, 1, (indices[1]&1)?cell_size_j():-cell_size_j()};
-        }
-
-        array<int, 4> edge2edges_offsets(array<unsigned int, Dims> const& indices) const {
-            return array<int, 4>{ -1, +1, (indices[1]&1)?-2:2, edge_size_j()+ ((indices[1]&1)?0:1)};
-        }
-
-        array<int, 3> cell2edges_offsets(array<unsigned int, Dims> const& indices) const {
-            return array<int, 3>{edge_storage.offset({indices[0], (indices[1]/2)+1}), 1, (indices[0]&1)?3:0};
-        }
-
+    template <int I>
+    struct location_type {
+        static const int value = I;
     };
+    
 
     /**
     */
     template <typename CellStorageT, typename EdgeStorageT>
     class trapezoid_2D_no_tile {
+    public :
+        using cells = location_type<0>;
+        using edges = location_type<1>;
+
+        template <typename T>
+        struct pointer_to;
+
+        template <int I>
+        struct pointer_to<location_type<I>> {
+            using type = double*;
+        };
+
+    private:
         using cell_storage_t = CellStorageT;
         using edge_storage_t = EdgeStorageT;
 
-        cell_storage_t const* cell_storage;
-        edge_storage_t const* edge_storage;
+        //cell_storage_t const* cell_storage;
+        //edge_storage_t const* edge_storage;
         const unsigned int M,N; // Sizes as cells in a multi-dimensional Cell array
 
         static constexpr int Dims = 2;
 
     public:
+        using storage_types = typename boost::mpl::vector<cell_storage_t&, edge_storage_t&>;
+
         static constexpr unsigned int u_cell_size_j(int _M) {return _M+4;}
         static constexpr unsigned int u_cell_size_i(int _N) {return _N+2;}
         static constexpr unsigned int u_edge_size_j(int _M) {return 3*(_M/2)+6;}
@@ -137,10 +94,10 @@ namespace gridtools {
             , M{}
         {}
 
-        trapezoid_2D_no_tile(cell_storage_t const& cs, edge_storage_t const& es, unsigned int N, unsigned int M)
-            : cell_storage(&cs)
-            , edge_storage(&es)
-            , M{M}
+        trapezoid_2D_no_tile(/*cell_storage_t const& cs, edge_storage_t const& es,*/ unsigned int N, unsigned int M)
+            //: cell_storage(&cs)
+            //  , edge_storage(&es)
+            : M{M}
             , N{N}
         {
             u_cell_size({N,M});
@@ -149,16 +106,12 @@ namespace gridtools {
             assert((N&1) == 0);
         }
 
-#ifdef USE_STORAGE
 #define DO_THE_MATH(stor, i,j,k)                \
-        stor ## _storage->_index(i,j,k)
-#else
-#define DO_THE_MATH(stor, i,j,k)                \
-        stor ## __index(i,j,k)
-#endif
+        stor ## _storage._index(i,j,k)
+
         
         array<unsigned int, 3>
-        cell2cells_ll_p1(array<unsigned int, 2> const& i) const
+        cell2cells_ll_p1(cell_storage_t const& cell_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 3>{
                 DO_THE_MATH(cell, i[0], 0, i[1]),
@@ -167,7 +120,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 3>
-        cell2cells_ll_p0(array<unsigned int, 2> const& i) const
+        cell2cells_ll_p0(cell_storage_t const& cell_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 3>{
                 DO_THE_MATH(cell, i[0], 1, i[1]-1),
@@ -176,7 +129,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 4>
-        edge2edges_ll_p0(array<unsigned int, 2> const& i) const
+        edge2edges_ll_p0(edge_storage_t const& edge_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 4>{
                 DO_THE_MATH(edge, i[0], 1, i[1]),
@@ -186,7 +139,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 4>
-        edge2edges_ll_p1(array<unsigned int, 2> const& i) const
+        edge2edges_ll_p1(edge_storage_t const& edge_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 4>{
                 DO_THE_MATH(edge, i[0], 0, i[1]),
@@ -196,7 +149,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 4>
-        edge2edges_ll_p2(array<unsigned int, 2> const& i) const
+        edge2edges_ll_p2(edge_storage_t const& edge_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 4>{
                 DO_THE_MATH(edge, i[0], 0, i[1]),
@@ -206,7 +159,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 3>
-        cell2edges_ll_p1(array<unsigned int, 2> const& i) const
+        cell2edges_ll_p1(edge_storage_t const& edge_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 3>{
                 DO_THE_MATH(edge, i[0], 2, i[1]),
@@ -215,7 +168,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 3>
-        cell2edges_ll_p0(array<unsigned int, 2> const& i) const
+        cell2edges_ll_p0(edge_storage_t const& edge_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 3>{
                 DO_THE_MATH(edge, i[0], 0, i[1]),
@@ -224,7 +177,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 2>
-        edge2cells_ll_p0(array<unsigned int, 2> const& i) const
+        edge2cells_ll_p0(cell_storage_t const& cell_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 2>{
                 DO_THE_MATH(cell, i[0], 1, i[1]-1),
@@ -232,7 +185,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 2>
-        edge2cells_ll_p1(array<unsigned int, 2> const& i) const
+        edge2cells_ll_p1(cell_storage_t const& cell_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 2>{
                 DO_THE_MATH(cell, i[0]-1, 1, i[1]),
@@ -240,7 +193,7 @@ namespace gridtools {
         }
 
         array<unsigned int, 2>
-        edge2cells_ll_p2(array<unsigned int, 2> const& i) const
+        edge2cells_ll_p2(cell_storage_t const& cell_storage, array<unsigned int, 2> const& i) const
         {
             return array<unsigned int, 2>{
                 DO_THE_MATH(cell, i[0], 0, i[1]),
@@ -248,48 +201,48 @@ namespace gridtools {
         }
 
         array<unsigned int, 3>
-        cell2cells(array<unsigned int, 2> const& i) const
+        cell2cells(cell_storage_t const& cell_storage, array<unsigned int, 2> const& i) const
         {
             if (i[1]&1) {
-                return cell2cells_ll_p1({i[0], i[1]/2});
+                return cell2cells_ll_p1(cell_storage, {i[0], i[1]/2});
             } else {
-                return cell2cells_ll_p0({i[0], i[1]/2});
+                return cell2cells_ll_p0(cell_storage, {i[0], i[1]/2});
             }
         }        
 
         array<unsigned int, 4>
-        edge2edges(array<unsigned int, 2> const& i) const
+        edge2edges(edge_storage_t const& edge_storage, array<unsigned int, 2> const& i) const
         {
             switch (i[1]%3) {
             case 0:
-                return edge2edges_ll_p0({i[0], i[1]/3});
+                return edge2edges_ll_p0(edge_storage, {i[0], i[1]/3});
             case 1:
-                return edge2edges_ll_p1({i[0], i[1]/3});
+                return edge2edges_ll_p1(edge_storage, {i[0], i[1]/3});
             case 2:
-                return edge2edges_ll_p2({i[0], i[1]/3});
+                return edge2edges_ll_p2(edge_storage, {i[0], i[1]/3});
             }
         }
 
         array<unsigned int, 3>
-        cell2edges(array<unsigned int, 2> const& i) const
+        cell2edges(edge_storage_t const& edge_storage, array<unsigned int, 2> const& i) const
         {
             if (i[1]&1) {
-                return cell2edges_ll_p1({i[0], i[1]/2});
+                return cell2edges_ll_p1(edge_storage, {i[0], i[1]/2});
             } else {
-                return cell2edges_ll_p0({i[0], i[1]/2});
+                return cell2edges_ll_p0(edge_storage, {i[0], i[1]/2});
             }
         }
 
         array<unsigned int, 2>
-        edge2cells(array<unsigned int, 2> const& i) const
+        edge2cells(cell_storage_t const& cell_storage, array<unsigned int, 2> const& i) const
         {
             switch (i[1]%3) {
             case 0:
-                return edge2cells_ll_p0({i[0], i[1]/3});
+                return edge2cells_ll_p0(cell_storage, {i[0], i[1]/3});
             case 1:
-                return edge2cells_ll_p1({i[0], i[1]/3});
+                return edge2cells_ll_p1(cell_storage, {i[0], i[1]/3});
             case 2:
-                return edge2cells_ll_p2({i[0], i[1]/3});
+                return edge2cells_ll_p2(cell_storage, {i[0], i[1]/3});
             }
         }
 
--- experimental/grids/main.cpp
+++ experimental/grids/main.cpp
@@ -2,6 +2,130 @@
 #include "grid.hpp"
 #include <storage/base_storage.h>
 #include <common/layout_map.h>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/range_c.hpp>
+#include <boost/mpl/transform.hpp>
+#include <boost/fusion/container/vector.hpp>
+#include <boost/fusion/include/as_vector.hpp>
+#include <boost/fusion/adapted/mpl/detail/size_impl.hpp>
+
+template <int I, typename LocationType>
+struct arg {
+    using location_type = LocationType;
+};
+
+template <int I>
+struct accessor {
+    static const int value = I;
+};
+
+template <typename Functor,
+          typename Grid,
+          typename PlcHldrs>
+struct esf
+{
+    using functor = Functor;
+    using grid = Grid;
+    using plcs = PlcHldrs;
+};
+
+
+template <typename Functor,
+          typename Grid,
+          typename PlcHldr0,
+          typename PlcHldr1>
+esf<Functor, Grid, boost::mpl::vector<PlcHldr0, PlcHldr1> >
+make_esf(PlcHldr0, PlcHldr1) {
+    return esf<Functor, Grid, boost::mpl::vector<PlcHldr0, PlcHldr1>>();
+}
+
+template <typename Accessor, typename Lambda>
+struct on_cells_impl {
+    using function = Lambda;
+};
+
+template <typename Accessor, typename Lambda>
+on_cells_impl<Accessor, Lambda>
+on_cells(Lambda) {
+    return on_cells_impl<Accessor, Lambda>();
+}
+    
+template <typename PlcVector, typename GridType>
+struct accessor_type {
+
+private:
+    template <typename GridType_>
+    struct get_pointer {
+        template <typename PlcType>
+        struct apply {
+            using type = typename GridType_::template pointer_to<typename PlcType::location_type>::type;
+        };
+    };
+public:
+    using storage_types = typename boost::fusion::result_of::as_vector<typename GridType::storage_types>::type;
+    //typedef typename boost::fusion::result_of::as_vector<boost::mpl::vector<int, char> >::type storage_types;
+    
+    // using mpl_pointers_t_ = typename boost::mpl::transform<PlcVector,
+    //                                                       get_pointer<GridType>
+    //                                                        >::type;
+
+    // using x = typename mpl_pointers_t::template ciccia<GridType>;
+    // using pointers_t = typename boost::fusion::result_of::as_vector<mpl_pointers_t_>::type;
+
+    using pointers_t = boost::fusion::vector<double*, double*>;
+private:
+    storage_types storages;
+    pointers_t pointers;
+    GridType const& grid;
+    unsigned int m_pos_i;
+    unsigned int m_pos_j;
+
+    struct _set_pointers {
+        template <typename Index>
+        void operator()(Index) const {
+            std::cout << Index::value << std::endl;
+            // boost::fusion::at_c<Index::value>(pointers) = const_cast<double*>((boost::fusion::at_c<Index::value>(storage_types)).min_addr());
+        }
+    };
+    
+public:
+    accessor_type(storage_types const& storages, GridType const& grid, unsigned int pos_i, unsigned int pos_j)
+        : storages(storages)
+        , grid(grid)
+        , m_pos_i(pos_i)
+        , m_pos_j(pos_j)
+    {
+        using indices = typename boost::mpl::range_c<int, 0, boost::fusion::result_of::size<storage_types>::type::value >;
+        boost::mpl::for_each<indices>(_set_pointers());
+    }
+
+    void inc_i() {++m_pos_i;}
+    void inc_j() {++m_pos_j;}
+    void reset_i() {m_pos_i = 0;}
+    void reset_j() {m_pos_j = 0;}
+    void set_ij(int i, int j) {
+        m_pos_i = i;
+        m_pos_j = j;
+    }
+    unsigned int i() const {return m_pos_i;}
+    unsigned int j() const {return m_pos_j;}
+
+    template <typename Arg, typename Accumulator>
+    double operator()(on_cells_impl<Arg, Accumulator> oncells, double initial = 0.0) const {
+        auto neighbors = grid.cell2cells(boost::fusion::at_c<Arg::value>(storages), {m_pos_i, m_pos_j});
+        double result = initial;
+        for (int i = 0; i<neighbors.size(); ++i) {
+            result = on_cells_impl<Arg, Accumulator>::function(boost::fusion::at_c<Arg::value>(pointers), result);
+        }
+    }
+
+    template <int I>
+    double& operator()(accessor<I> const& arg) const {
+        return *(boost::fusion::at_c<I>(pointers));
+    }
+};
+
 
 namespace gridtools {
     template <typename T, size_t D>
@@ -16,8 +140,23 @@ namespace gridtools {
     }
 }
 
-#define EVAL(f,x,y)                                                     \
-    std::cout << #f << ": " << gridtools::array<int,2>{x,y} << " -> " << (grid.f({x,y})) << std::endl
+struct on_cells_f {
+    typedef accessor<0> out;
+    typedef accessor<1> in;
+    
+    template <typename GridAccessors>
+    void
+    operator()(GridAccessors const& eval/*, region*/) const {
+        std::cout << "i = " << eval.i()
+                  << " j = " << eval.j()
+                  << std::endl;
+        auto ff = [](const double _in, const double _res){return _in+_res;};
+        eval(out()) = eval(on_cells<in>(ff), 0.0);
+    }
+};
+
+#define EVAL(f,storage,x,y)                                             \
+    std::cout << #f << ": " << gridtools::array<int,2>{x,y} << " -> " << (grid.f(storage,{x,y})) << std::endl
 
 using gridtools::layout_map;
 using gridtools::wrap_pointer;
@@ -48,38 +187,66 @@ int main() {
     cell_storage_type cells(trapezoid_2D().u_cell_size(gridtools::array<unsigned int, 2>{NC, MC}));
     edge_storage_type edges(trapezoid_2D().u_edge_size(gridtools::array<unsigned int, 2>{NE, ME}));
 
-    trapezoid_2D grid(cells, edges, NC, MC);
+    trapezoid_2D grid(/*cells, edges,*/ NC, MC);
 
     cells.info();
     
-    EVAL(cell2cells_ll_p0, 1, 1);
-    EVAL(cell2cells_ll_p0, 1, 2);
-    EVAL(cell2cells_ll_p1, 1, 3);
-    EVAL(cell2cells_ll_p1, 1, 4);
-    EVAL(cell2cells, 2, 3);
-    EVAL(cell2cells, 2, 4);
-    EVAL(cell2cells, 3, 3);
-    EVAL(cell2cells, 3, 4);
+    EVAL(cell2cells_ll_p0, cells, 1, 1);
+    EVAL(cell2cells_ll_p0, cells, 1, 2);
+    EVAL(cell2cells_ll_p1, cells, 1, 3);
+    EVAL(cell2cells_ll_p1, cells, 1, 4);
+    EVAL(cell2cells, cells, 2, 3);
+    EVAL(cell2cells, cells, 2, 4);
+    EVAL(cell2cells, cells, 3, 3);
+    EVAL(cell2cells, cells, 3, 4);
 
-    EVAL(edge2edges_ll_p0, 2, 3);
-    EVAL(edge2edges_ll_p1, 2, 3);
-    EVAL(edge2edges_ll_p2, 2, 3);
-    EVAL(edge2edges, 2, 2);
-    EVAL(edge2edges, 2, 3);
-    EVAL(edge2edges, 2, 4);
+    EVAL(edge2edges_ll_p0, edges, 2, 3);
+    EVAL(edge2edges_ll_p1, edges, 2, 3);
+    EVAL(edge2edges_ll_p2, edges, 2, 3);
+    EVAL(edge2edges, edges, 2, 2);
+    EVAL(edge2edges, edges, 2, 3);
+    EVAL(edge2edges, edges, 2, 4);
 
-    EVAL(cell2edges_ll_p0, 2, 3);
-    EVAL(cell2edges_ll_p1, 2, 3);
-    EVAL(cell2edges, 2, 3);
-    EVAL(cell2edges, 2, 4);
+    EVAL(cell2edges_ll_p0, edges, 2, 3);
+    EVAL(cell2edges_ll_p1, edges, 2, 3);
+    EVAL(cell2edges, edges, 2, 3);
+    EVAL(cell2edges, edges, 2, 4);
 
-    EVAL(edge2cells_ll_p0, 2, 3);
-    EVAL(edge2cells_ll_p1, 2, 3);
-    EVAL(edge2cells_ll_p2, 2, 3);
-    EVAL(edge2cells, 2, 3);
-    EVAL(edge2cells, 2, 4);
-    EVAL(edge2cells, 2, 5);
+    EVAL(edge2cells_ll_p0, cells, 2, 3);
+    EVAL(edge2cells_ll_p1, cells, 2, 3);
+    EVAL(edge2cells_ll_p2, cells, 2, 3);
+    EVAL(edge2cells, cells, 2, 3);
+    EVAL(edge2cells, cells, 2, 4);
+    EVAL(edge2cells, cells, 2, 5);
 
 
+    cell_storage_type cells_out(trapezoid_2D().u_cell_size(gridtools::array<unsigned int, 2>{NC, MC}));
+    edge_storage_type edges_out(trapezoid_2D().u_edge_size(gridtools::array<unsigned int, 2>{NE, ME}));
+
+    //    trapezoid_2D grid_out(cells_out, edges_out, NC, MC);
+
+    typedef arg<0, trapezoid_2D::cells> in_cells;
+    typedef arg<1, trapezoid_2D::cells> out_cells;
+
+    auto x = make_esf<on_cells_f, trapezoid_2D>(in_cells(), out_cells());
+
+    accessor_type<boost::mpl::vector<in_cells, out_cells>, trapezoid_2D> acc
+        (boost::fusion::vector<cell_storage_type&, cell_storage_type&>(cells_out, cells), grid, 0,0);
+
+    for (int i = 1; i < NC-1; ++i) {
+        acc.set_ij(i, 1);
+        for (int j = 2; j < MC-2; ++j) {
+            acc.inc_j();
+            decltype(x)::functor()(acc);
+        }
+    }
+
+    // gridtools::computation * oncells =
+    //     gridtools::make_computation<gridtools::backend<Host, Naive>
+    //     (
+    //      gridtools::make_mss
+    //      (
+    //       gridtools::execute<forward>(),
+    //       gridtools::make_esf<on_cells_f>(in_grid_cells(), out_grid_cells())
     return 0;
 }
