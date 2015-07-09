75cc3a6db65cefc8cb89229f02832a797312eb4c
Paolo Crosetto <crosetto@greina18.cm.cluster>
2015-07-09 20:12:32 +0200

    *.h -> *.hpp, adding shallow water documentation

--- include/common/gt_assert.hpp
+++ include/common/gt_assert.hpp
@@ -3,7 +3,7 @@
 #ifdef __CUDACC__
   #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ >= 200)
   // we take the cuda assert for arch greater than 2.x
-    #include <assert.hpp>
+    #include <assert.h>
   #else
     #undef assert
     #define assert(e)
