68d6afb9da84711f1792d92352f750cd93b3085e
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-07-27 17:03:47 +0200

    add host to timerdummy

--- include/stencil-composition/timer_dummy.hpp
+++ include/stencil-composition/timer_dummy.hpp
@@ -11,22 +11,27 @@ namespace gridtools{
 class timer_dummy : public timer<timer_dummy> // CRTP
 {
 public:
+    __host__
     timer_dummy(std::string name) : timer(name) {}
+    __host__
     ~timer_dummy() {}
 
     /**
     * Reset counters
     */
+    __host__
     void reset_impl() {}
 
     /**
     * Start the stop watch
     */
+    __host__
     void start_impl() {}
 
     /**
     * Pause the stop watch
     */
+    __host__
     double pause_impl() { return 0.0; }
 };
 }
