14938a57490aded6a77374a8d064f8d449b61423
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-07-28 21:37:14 +0200

    yet another important fix

--- include/stencil-composition/caches/cache_storage.hpp
+++ include/stencil-composition/caches/cache_storage.hpp
@@ -30,6 +30,7 @@ struct cache_storage
 
     typedef static_uint<tile_i::value - iminus::value + iplus::value> j_stride_t;
     typedef static_uint<1> i_stride_t;
+    typedef static_uint<(tile_i::value-iminus::value+iplus::value)*(tile_j::value-jminus::value+jplus::value)> storage_size_t;
     explicit cache_storage() {}
 
     template<typename Offset>
@@ -39,12 +40,13 @@ struct cache_storage
         GRIDTOOLS_STATIC_ASSERT((is_offset_tuple<Offset>::value), "Error type is not offset tuple");
         // TODO assert not working, problem with PRETTY_FUNCTION
 //        assert(true);
+
         return m_values[(thread_pos[0] + offset.template get<Offset::n_args-1>() - iminus::value) * i_stride_t::value +
                 (thread_pos[1] + offset.template get<Offset::n_args-2>() -  jminus::value) * j_stride_t::value];
     }
 
 private:
-    Value m_values[(tile_i::value-iminus::value+iplus::value)*(tile_j::value-jminus::value+jminus::value)];
+    Value m_values[storage_size_t::value];
 };
 
 } // namespace gridtools
