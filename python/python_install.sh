4cf010c0435213fbc85107047e94d13485b45094
Lucas Benedicic <benedicic@cscs.ch>
2015-07-30 16:11:38 +0200

    Integration with CMake. Closes #51.-

--- python/python_install.sh
+++ python/python_install.sh
@@ -6,7 +6,7 @@ CMAKE_SOURCE_DIR=$1
 #PYTHON_INSTALL_PREFIX=$4
 PYTHON_INSTALL_PREFIX=$2
 
-# Checking gcc version (it has to be >=4.9.x)
+# Checking gcc version (it has to be >=4.8.x)
 gcc_vers=`gcc -dumpversion|cut -f1 -d.`
 if [ $gcc_vers -lt 4 ]
 then
@@ -14,16 +14,17 @@ then
   exit 1
 else
   gcc_vers=`gcc -dumpversion|cut -f2 -d.`
-  if [ $gcc_vers -lt 9 ]
+  if [ $gcc_vers -lt 8 ]
   then
-    echo "gcc version 4.9.x is required. EXIT NOW"
+    echo "gcc version 4.8.x is required. EXIT NOW"
     exit 1
   else
-   # gcc version is 4.9.x
+   # gcc version is 4.8.x
    # Looking for virtualenv
    virtualenv_cmd=`which virtualenv`
    if [ $? -eq 0 ]
    then
+       ${virtualenv_cmd} --no-site-packages ${PYTHON_INSTALL_PREFIX}
        source ${PYTHON_INSTALL_PREFIX}/bin/activate
        echo "Installing Python bindings in virtual environment at ${PYTHON_INSTALL_PREFIX} ..."
        #which python
