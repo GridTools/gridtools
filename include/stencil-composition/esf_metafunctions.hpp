f9206407930398b8f768351296d66eae2d3575fc
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-08-04 12:19:44 +0200

    move metafunctions to esf_metafunctions

--- include/stencil-composition/esf_metafunctions.hpp
+++ include/stencil-composition/esf_metafunctions.hpp
@@ -20,5 +20,31 @@ struct esf_args
     typedef typename Esf::args_t type;
 };
 
+template <typename Esf>
+struct esf_get_arg_index {
+    template <typename Index>
+    struct apply {
+        typedef typename boost::mpl::at<typename Esf::args_t, Index>::type type;
+    };
 };
+
+template <typename EsfF>
+struct esf_get_temps_per_functor {
+    typedef boost::mpl::range_c<uint_t, 0, boost::mpl::size<typename EsfF::args_t>::type::value> range;
+    typedef typename boost::mpl::fold<
+        range,
+        boost::mpl::vector<>,
+        boost::mpl::if_<
+            typename is_written_temp<EsfF>::template apply<boost::mpl::_2>,
+            boost::mpl::push_back<
+                boost::mpl::_1,
+                typename esf_get_arg_index<EsfF>::template apply<boost::mpl::_2>
+            >,
+            boost::mpl::_1
+        >
+    >::type type;
+};
+
+
+} //namespace gridtools
 
