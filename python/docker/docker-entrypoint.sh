e999fe8459432528996654e12f6f296a38888083
Lucas Benedicic <benedicic@cscs.ch>
2015-08-20 16:38:42 +0200

    Second version of the Docker container.-

--- python/docker/docker-entrypoint.sh
+++ python/docker/docker-entrypoint.sh
@@ -8,18 +8,16 @@ usermod -u ${HOST_UID} dev > /dev/null 2>&1
 
 #
 # environment setup
-# BOOST_ROOT and CUDATOOLKIT_HOME are set when building the image
+# BOOST_ROOT and CUDATOOLKIT_HOME are set when building the container image
 #
 export CXX=/usr/bin/g++
 export GRIDTOOLS_ROOT=$HOME/gridtools
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDATOOLKIT_HOME/lib64
 
-
 #
-# run some tests
+# start the IPython notebook
 #
-#cd $HOME/gridtools/python
-#nosetests -v -s -x tests.test_stencils
-
-EXE_CMD="$@"
-su -m -c "${EXE_CMD}" dev
+MY_IP="$( ifconfig | grep -A1 'Ethernet' | grep inet | cut -d':' -f2 | cut -d' ' -f1 )"
+echo ">>> Point your browser to $MY_IP:8888 <<<"
+cd $HOME/gridtools/python
+su -m -c "ipython notebook --ip=0.0.0.0 Gridtools4Py.ipynb" dev
--- python/docker/run.sh
+++ python/docker/run.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-MNT_DIR="$( pwd )"
-MNT_PNT="/home/dev/src"
-
-#
-# try to mount a Git repository root if no parameter were given
-#
-if [ ! -d ".git" ]; then
-    git rev-parse --git-dir > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        MNT_DIR="$( git rev-parse --git-dir | sed -e 's/git$//' )"
-    fi
-fi
-
-echo "Mounting directory '${MNT_DIR}' under '${MNT_PNT}'"
-docker run --rm                      \
-           -it                       \
-           --env HOST_UID=$( id -u ) \
-           gridtools4py:latest       \
+docker run --rm=true                    \
+           -it                          \
+           -P                           \
+           gridtools4py:latest          \
            /bin/bash
