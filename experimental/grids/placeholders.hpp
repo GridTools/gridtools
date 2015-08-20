295c887c37782755bf26050f0ca47cb11c9f5950
Mauro Bianco <mbianco@cscs.ch>
2015-08-20 10:39:56 +0200

    preparing for domain_type

--- experimental/grids/placeholders.hpp
+++ experimental/grids/placeholders.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <stencil-composition/arg_storage_pair.hpp>
+
 template <int I, typename LocationType>
 struct arg {
     using location_type = LocationType;
+
+    template<typename Storage>
+    gridtools::arg_storage_pair<arg<I,LocationType>, Storage>
+    operator=(Storage& ref) {
+
+        return gridtools::arg_storage_pair<arg<I,LocationType>, Storage>(&ref);
+    }
 };
 
 template <int I, typename T>
