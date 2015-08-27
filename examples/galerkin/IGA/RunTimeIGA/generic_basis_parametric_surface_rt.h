5e2822bb7decd8d90152cc6ef8b716e1e90d9d37
bignamic <bignamini@cscs.ch>
2015-08-27 16:19:13 +0200

    knots removed from generic class; purte virtual method for jacobian calculation added

--- examples/galerkin/IGA/RunTimeIGA/generic_basis_parametric_surface_rt.h
+++ examples/galerkin/IGA/RunTimeIGA/generic_basis_parametric_surface_rt.h
@@ -11,7 +11,7 @@ namespace iga_rt
 
 	/**
 	 * @class Generic function basis curve calculation class
-	 * @brief Class for the calculation of a generic function basis curve value at a given point in parametric space given a set of knots and control points
+	 * @brief Class for the calculation of a generic function basis curve value at a given point in parametric space given a set of control points
 	 * @tparam DIM co-domain number of dimensions
 	 */
 	template<int DIM>
@@ -28,10 +28,9 @@ namespace iga_rt
 		 * @param i_knots basis knot set
 		 * @param i_controlPoints control point set in R^DIM
 		 */
-		PolinomialParametricCurve(const double* i_knots, const std::vector<Point<DIM> >& i_controlPoints)// TODO: change constructor signature and add setControlPoint methods (same for surface and volumes)
+		PolinomialParametricCurve(const std::vector<Point<DIM> >& i_controlPoints)// TODO: change constructor signature and add setControlPoint methods (same for surface and volumes)
 								 :m_controlPoints(i_controlPoints)
 								 ,m_basis(0)
-								 ,m_knots(i_knots)
 								 {}
 
 		virtual ~PolinomialParametricCurve(void) { delete m_basis; }
@@ -43,6 +42,12 @@ namespace iga_rt
 		 */
 		Point<DIM> evaluate(double i_csi) const;
 
+		/**
+		 * @brief Curve jacobian evaluation method
+		 * @param i_csi Parametric space point value
+		 * @return Point in R^DIM
+		 */
+		virtual Point<DIM> evaluateJacobian(double i_csi) const = 0;
 
 	protected:
 
@@ -61,11 +66,9 @@ namespace iga_rt
 		 */
 		virtual PolinomialParametricBasis* generateBasis(void) const = 0;
 
-		const std::vector<Point<DIM> >& m_controlPoints;// TODO: this can be avoided with the above modification (see constructor comment)
+		const std::vector<Point<DIM> > m_controlPoints;// TODO: this can be avoided with the above modification (see constructor comment)
 
 		mutable PolinomialParametricBasis* m_basis;// TODO: bad design, avoid mutable
-
-		const double* m_knots;
 
 	};
 
@@ -95,7 +98,7 @@ namespace iga_rt
 	// TODO: change class names, they are just the interfaces for generic basis functions (or functions in R^N)
 	/**
 	 * @class Generic function basis surface calculation class
-	 * @brief Class for the calculation of a generic function basis surface value at a given point in parametric space given a set of knots and control points
+	 * @brief Class for the calculation of a generic function basis surface value at a given point in parametric space given a set of control points
 	 * @tparam DIM co-domain number of dimensions
 	 */
 	// Use variadic templates
@@ -110,15 +113,11 @@ namespace iga_rt
 
 		/**
 		 * @brief Constructor
-		 * @param i_knots1 polinomial basis knot set (first direction)
-		 * @param i_knots2 polinomial basis knot set (first direction)
 		 * @param i_controlPoints control point set in R^DIM
 		 */
-		PolinomialParametricSurface(const double* i_knots1, const double* i_knots2, const std::vector<Point<DIM> >& i_controlPoints)
+		PolinomialParametricSurface(const std::vector<Point<DIM> >& i_controlPoints)
 								   :m_controlPoints(i_controlPoints)
 								   ,m_basis(0)
-								   ,m_knots1(i_knots1)
-								   ,m_knots2(i_knots2)
 								   {}
 
 		virtual ~PolinomialParametricSurface(void) { delete m_basis; }
@@ -131,6 +130,13 @@ namespace iga_rt
 		 */
 		Point<DIM> evaluate(double i_csi, double i_eta) const;
 
+		/**
+		 * @brief Surface jacobian evaluation method
+		 * @param i_csi Parametric space point value
+		 * @param i_eta Parametric space point value (second direction)
+		 * @return Point in R^DIM (first and second parameter derivatives)
+		 */
+		virtual std::array<Point<DIM>,2> evaluateJacobian(double i_csi, double i_eta) const = 0;
 
 	protected:
 
@@ -146,18 +152,13 @@ namespace iga_rt
 
 		/**
 		 * @brief basis generation method (to be implemented by derived specific classes)
-		 * @param i_knots1 polinomial basis knot set (first direction)
-		 * @param i_knots2 polinomial basis knot set (first direction)
 		 */
 		virtual BivariatePolinomialParametricBasis* generateBasis(void) const = 0;
 
-		const std::vector<Point<DIM> >& m_controlPoints;
+		const std::vector<Point<DIM> > m_controlPoints;
 
 		mutable BivariatePolinomialParametricBasis* m_basis;// TODO: bad design, avoid mutable
 
-		const double* m_knots1;
-
-		const double* m_knots2;
 	};
 
 	template<int DIM>
