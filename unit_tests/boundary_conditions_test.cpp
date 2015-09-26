7749ffbf0e086a3f29ed34aa841311b6454820d3
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-09-26 17:31:51 +0200

    and remove and remove more prints

--- unit_tests/boundary_conditions_test.cpp
+++ unit_tests/boundary_conditions_test.cpp
@@ -151,18 +151,6 @@ bool basic() {
         }
     }
 
-#ifndef NDEBUG
-    for (uint_t i=0; i<d1; ++i) {
-        for (uint_t j=0; j<d2; ++j) {
-            for (uint_t k=0; k<d3; ++k) {
-                printf("%d ", in(i,j,k));
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-#endif
-
     gridtools::array<gridtools::halo_descriptor, 3> halos;
     halos[0] = gridtools::halo_descriptor(1,1,1,d1-2,d1);
     halos[1] = gridtools::halo_descriptor(1,1,1,d2-2,d2);
@@ -177,18 +165,6 @@ bool basic() {
     in.d2h_update();
 #else
     gridtools::boundary_apply<bc_basic>(halos, bc_basic()).apply(in);
-#endif
-
-#ifndef NDEBUG
-    for (uint_t i=0; i<d1; ++i) {
-        for (uint_t j=0; j<d2; ++j) {
-            for (uint_t k=0; k<d3; ++k) {
-                printf("%d ", in(i,j,k));
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
 #endif
 
     bool result = true;
@@ -289,18 +265,6 @@ bool predicate() {
         }
     }
 
-#ifndef NDEBUG
-    for (uint_t i=0; i<d1; ++i) {
-        for (uint_t j=0; j<d2; ++j) {
-            for (uint_t k=0; k<d3; ++k) {
-                printf("%d ", in(i,j,k));
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-#endif
-
     gridtools::array<gridtools::halo_descriptor, 3> halos;
     halos[0] = gridtools::halo_descriptor(1,1,1,d1-2,d1);
     halos[1] = gridtools::halo_descriptor(1,1,1,d2-2,d2);
@@ -317,27 +281,12 @@ bool predicate() {
     gridtools::boundary_apply<bc_basic, minus_predicate>(halos, bc_basic(), minus_predicate()).apply(in);
 #endif
 
-#ifndef NDEBUG
-    for (uint_t i=0; i<d1; ++i) {
-        for (uint_t j=0; j<d2; ++j) {
-            for (uint_t k=0; k<d3; ++k) {
-                printf("%d ", in(i,j,k));
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-#endif
-
     bool result = true;
 
     for (uint_t i=0; i<d1; ++i) {
         for (uint_t j=0; j<d2; ++j) {
             for (uint_t k=0; k<1; ++k) {
                 if (in(i,j,k) != 0) {
-#ifndef NDEBUG
-                    printf("%d %d %d %d\n", i,j,k, in(i,j,k));
-#endif
                     result = false;
                 }
             }
@@ -348,9 +297,6 @@ bool predicate() {
         for (uint_t j=1; j<d2; ++j) {
             for (uint_t k=d3-1; k<d3; ++k) {
                 if (in(i,j,k) != i+j+k) {
-#ifndef NDEBUG
-                    printf("%d %d %d %d\n", i,j,k, in(i,j,k));
-#endif
                     result = false;
                 }
             }
@@ -361,9 +307,6 @@ bool predicate() {
         for (uint_t j=0; j<1; ++j) {
             for (uint_t k=0; k<d3; ++k) {
                 if (in(i,j,k) != 0) {
-#ifndef NDEBUG
-                    printf("%d %d %d %d\n", i,j,k, in(i,j,k));
-#endif
                     result = false;
                 }
             }
@@ -374,9 +317,6 @@ bool predicate() {
         for (uint_t j=d2-1; j<d2; ++j) {
             for (uint_t k=1; k<d3; ++k) {
                 if (in(i,j,k) != i+j+k) {
-#ifndef NDEBUG
-                    printf("%d %d %d %d\n", i,j,k, in(i,j,k));
-#endif
                     result = false;
                 }
             }
@@ -387,9 +327,6 @@ bool predicate() {
         for (uint_t j=0; j<d2; ++j) {
             for (uint_t k=0; k<d3; ++k) {
                 if (in(i,j,k) != 0) {
-#ifndef NDEBUG
-                    printf("%d %d %d %d\n", i,j,k, in(i,j,k));
-#endif
                     result = false;
                 }
             }
@@ -400,9 +337,6 @@ bool predicate() {
         for (uint_t j=1; j<d2; ++j) {
             for (uint_t k=1; k<d3; ++k) {
                 if (in(i,j,k) != i+j+k) {
-#ifndef NDEBUG
-                    printf("%d %d %d %d\n", i,j,k, in(i,j,k));
-#endif
                     result = false;
                 }
             }
@@ -413,9 +347,6 @@ bool predicate() {
         for (uint_t j=1; j<d2-1; ++j) {
             for (uint_t k=1; k<d3-1; ++k) {
                 if (in(i,j,k) != 0) {
-#ifndef NDEBUG
-                    printf("%d %d %d %d\n", i,j,k, in(i,j,k));
-#endif
                     result = false;
                 }
             }
@@ -1000,11 +931,9 @@ bool usingcopy_3() {
         for (uint_t j=0; j<d2; ++j) {
             for (uint_t k=0; k<1; ++k) {
                 if (one(i,j,k) != i+j+k) {
-                    std::cout << "1 one " << i << ", " << j << ", " << k << ": " << one(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
                 if (two(i,j,k) != i+j+k) {
-                    std::cout << "1 two " << i << ", " << j << ", " << k << ": " << two(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
             }
@@ -1015,11 +944,9 @@ bool usingcopy_3() {
         for (uint_t j=0; j<d2; ++j) {
             for (uint_t k=d3-1; k<d3; ++k) {
                 if (one(i,j,k) != i+j+k) {
-                    std::cout << "2 one " << i << ", " << j << ", " << k << ": " << one(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
                 if (two(i,j,k) != i+j+k) {
-                    std::cout << "2 two " << i << ", " << j << ", " << k << ": " << two(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
             }
@@ -1030,11 +957,9 @@ bool usingcopy_3() {
         for (uint_t j=0; j<1; ++j) {
             for (uint_t k=0; k<d3; ++k) {
                 if (one(i,j,k) != i+j+k) {
-                    std::cout << "3 one " << i << ", " << j << ", " << k << ": " << one(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
                 if (two(i,j,k) != i+j+k) {
-                    std::cout << "3 two " << i << ", " << j << ", " << k << ": " << two(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
             }
@@ -1045,11 +970,9 @@ bool usingcopy_3() {
         for (uint_t j=d2-1; j<d2; ++j) {
             for (uint_t k=0; k<d3; ++k) {
                 if (one(i,j,k) != i+j+k) {
-                    std::cout << "4 one " << i << ", " << j << ", " << k << ": " << one(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
                 if (two(i,j,k) != i+j+k) {
-                    std::cout << "4 two " << i << ", " << j << ", " << k << ": " << two(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
             }
@@ -1060,11 +983,9 @@ bool usingcopy_3() {
         for (uint_t j=0; j<d2; ++j) {
             for (uint_t k=0; k<d3; ++k) {
                 if (one(i,j,k) != i+j+k) {
-                    std::cout << "5 one " << i << ", " << j << ", " << k << ": " << one(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
                 if (two(i,j,k) != i+j+k) {
-                    std::cout << "5 two " << i << ", " << j << ", " << k << ": " << two(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
             }
@@ -1075,11 +996,9 @@ bool usingcopy_3() {
         for (uint_t j=0; j<d2; ++j) {
             for (uint_t k=0; k<d3; ++k) {
                 if (one(i,j,k) != i+j+k) {
-                    std::cout << "6 one " << i << ", " << j << ", " << k << ": " << one(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
                 if (two(i,j,k) != i+j+k) {
-                    std::cout << "6 two " << i << ", " << j << ", " << k << ": " << two(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
             }
@@ -1090,11 +1009,9 @@ bool usingcopy_3() {
         for (uint_t j=1; j<d2-1; ++j) {
             for (uint_t k=1; k<d3-1; ++k) {
                 if (one(i,j,k) != -1) {
-                    std::cout << "7 one " << i << ", " << j << ", " << k << ": " << one(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
                 if (two(i,j,k) != 0) {
-                    std::cout << "7 two " << i << ", " << j << ", " << k << ": " << two(i,j,k) << " != " << i+j+k << std::endl;
                     result = false;
                 }
             }
