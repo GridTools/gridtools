ee219245307bff39b3c2b381d99e55bf32c76ebc
Mauro Bianco <mbianco@cscs.ch>
2015-10-28 15:43:59 +0100

    bug fix: return ref instead of value

--- include/stencil-composition/iterate_domain.hpp
+++ include/stencil-composition/iterate_domain.hpp
@@ -128,14 +128,14 @@ namespace gridtools {
         };
 
         /**
-         * metafunction that determines if a given accessor is associated with an arg holding a data field
+         * metafunction that determines if a given accessor is associated with an placeholder holding a data field
          */
         template<typename Accessor>
         struct accessor_holds_data_field
         {
             typedef typename boost::mpl::eval_if<
                 is_accessor<Accessor>,
-                arg_holds_data_field_h<get_arg_from_accessor<Accessor> >,
+                placeholder_holds_data_field_h<get_arg_from_accessor<Accessor> >,
                 boost::mpl::identity<boost::mpl::false_>
             >::type type;
         };
@@ -471,7 +471,7 @@ namespace gridtools {
             typename StoragePointer
         >
         GT_FUNCTION
-        ReturnType get_gmem_value(StoragePointer RESTRICT & storage_pointer, const uint_t pointer_offset) const
+        ReturnType& get_gmem_value(StoragePointer RESTRICT & storage_pointer, const uint_t pointer_offset) const
         {
             return *(storage_pointer+pointer_offset);
         }
