b3586bd8e27e4a43ae63d219077e34325e83ce09
Lucas Benedicic <benedicic@cscs.ch>
2015-11-02 16:16:35 +0100

    Docker container updated.-

--- python/docker/build.sh
+++ python/docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "The container image will use the NVIDIA driver version 352.21"
+echo "This container image builds on top of the official NVIDIA CUDA container"
 echo "Press any key to continue or Ctrl+C to exit ..."
 read -n 1
 
--- python/docker/connect.sh
+++ python/docker/connect.sh
@@ -2,12 +2,10 @@
 
 CONTAINER_NAME="$( docker ps | grep gridtools4py | awk '{ print $1; }' )"
 CONTAINER_IP=$( docker inspect ${CONTAINER_NAME} | grep \"IPAddress | awk '{ print $2; }' | tr -d ',' | tr -d '"' )
-EXE_CMD='export BOOST_ROOT=/usr/local/boost-1.56.0      && \
-         export CUDATOOLKIT_HOME=/usr/local/cuda-7.0    && \
-         export CXX=/usr/bin/g++                        && \
-         export GRIDTOOLS_ROOT=$HOME/gridtools          && \
-         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDATOOLKIT_HOME/lib64 && \
-         cd $HOME/gridtools/python                      && \
+EXE_CMD='export BOOST_ROOT=/usr/local                                       && \
+         export GRIDTOOLS_ROOT=$HOME/gridtools                              && \
+         export CUDATOOLKIT_HOME=/usr/local/cuda                            && \
+         cd $HOME/gridtools/python                                          && \
          ipython notebook --ip=0.0.0.0 --port=8888 --no-browser Gridtools4Py.ipynb'
 echo ">>> IPython notebook listening at ${CONTAINER_IP} <<<"
-ssh -i ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -YC dev@${CONTAINER_IP} -p 22 "${EXE_CMD}"
+ssh -i ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -YC docker@${CONTAINER_IP} -p 22 "${EXE_CMD}"
