1dd8ca491e99bbcc4f7416641b509fe90315f38d
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-11-05 09:35:58 +0100

    indent

--- include/stencil-composition/other_grids/backend_metafunctions.hpp
+++ include/stencil-composition/other_grids/backend_metafunctions.hpp
@@ -4,14 +4,14 @@
 
 namespace gridtools {
 
-template<enumtype::backend BackendId, enumtype::strategy StrategyType >
-struct is_backend<backend<BackendId, StrategyType> > : boost::mpl::true_{};
+    template<enumtype::backend BackendId, enumtype::strategy StrategyType >
+    struct is_backend<backend<BackendId, StrategyType> > : boost::mpl::true_{};
 
-template<typename T> struct backend_id;
+    template<typename T> struct backend_id;
 
-template< enumtype::backend BackendId, enumtype::strategy StrategyType >
-struct backend_id< backend<BackendId, StrategyType> > :
-        enumtype::enum_type<enumtype::backend, BackendId>{
-};
+    template< enumtype::backend BackendId, enumtype::strategy StrategyType >
+    struct backend_id< backend<BackendId, StrategyType> > :
+            enumtype::enum_type<enumtype::backend, BackendId>{
+    };
 
 } // gridtools
