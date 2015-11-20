4176b490177b8b90ce3363e73f17f4c0538df015
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-11-20 20:37:50 +0100

    Add variadic assert file

---
+++ include/common/generic_metafunctions/variadic_assert.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+template<typename Lambda, typename First>
+void variadic_assert(Lambda fn, First first)
+{
+    assert(fn(first));
+}
+
+template<typename Lambda, typename First, typename ... T>
+void variadic_assert(Lambda fn, First first, T ... args)
+{
+    assert(fn(first));
+    variadic_assert(fn, args...);
+}
+
