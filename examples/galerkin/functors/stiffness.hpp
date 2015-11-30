4ebf8ba5221f844ddfc3fda657533e43b4ce380d
bignamic <bignamini@cscs.ch>
2015-11-30 16:54:04 +0100

    Added loop over derivation variable index (required for 2/3D case). Fixed bug in jac_inv*dp calculation (rox/col index swap)

--- examples/galerkin/functors/stiffness.hpp
+++ examples/galerkin/functors/stiffness.hpp
@@ -3,12 +3,11 @@
 namespace functors{
 
     typedef gridtools::interval<gridtools::level<0,-1>, gridtools::level<1,-1> > x_interval;
-    typedef gridtools::interval<gridtools::level<0,-2>, gridtools::level<1,1> > axis;
 
     template <typename FE, typename Cubature>
     struct stiffness {
-        using fe=FE;
-        using cub=Cubature;
+
+    	using fe=FE;
 
         //![accessors]
         using jac_det =accessor<0, range<0,0,0,0> , 4> const;
@@ -45,16 +44,27 @@ namespace functors{
                     for(short_t q=0; q<num_cub_points; ++q){
                         //inner product of the gradients
                         double gradients_inner_product=0.;
-                        for(short_t icoor=0; icoor< fe::fe::spaceDim; ++icoor)
+                        for(short_t icoor=0; icoor< fe::spaceDim; ++icoor)
                         {
-                            gradients_inner_product +=
-                                eval((jac_inv(qp+q, dimx+0, dimy+icoor)*!dphi(P_i,q,(uint_t)0)+
-                                      jac_inv(qp+q, dimx+1, dimy+icoor)*!dphi(P_i,q,(uint_t)1)+
-                                      jac_inv(qp+q, dimx+2, dimy+icoor)*!dphi(P_i,q,(uint_t)2))
-                                     *
-                                     (jac_inv(qp+q, dimx+0, dimy+icoor)*!dpsi(Q_i,q,(uint_t)0)+
-                                      jac_inv(qp+q, dimx+1, dimy+icoor)*!dpsi(Q_i,q,(uint_t)1)+
-                                      jac_inv(qp+q, dimx+2, dimy+icoor)*!dpsi(Q_i,q,(uint_t)2)));
+//                            gradients_inner_product +=
+//                                eval((jac_inv(qp+q, dimx+0, dimy+icoor)*!dphi(P_i,q,(uint_t)0)+
+//                                      jac_inv(qp+q, dimx+1, dimy+icoor)*!dphi(P_i,q,(uint_t)1)+
+//                                      jac_inv(qp+q, dimx+2, dimy+icoor)*!dphi(P_i,q,(uint_t)2))
+//                                     *
+//                                     (jac_inv(qp+q, dimx+0, dimy+icoor)*!dpsi(Q_i,q,(uint_t)0)+
+//                                      jac_inv(qp+q, dimx+1, dimy+icoor)*!dpsi(Q_i,q,(uint_t)1)+
+//                                      jac_inv(qp+q, dimx+2, dimy+icoor)*!dpsi(Q_i,q,(uint_t)2)));
+
+                            // TODO: CRTP or loop? In case of loop, split?
+                            // TODO: Local variable memory allocation limit (registers..)?
+                            double jac_inv_dphi=0.;
+                            double jac_inv_dpsi=0.;
+                            for(short_t jcoor=0; jcoor< fe::spaceDim; ++jcoor)
+                            {
+                            	jac_inv_dphi += eval(jac_inv(qp+q, dimx+icoor, dimy+jcoor)*!dphi(P_i,q,(uint_t)jcoor));
+                            	jac_inv_dpsi += eval(jac_inv(qp+q, dimx+icoor, dimy+jcoor)*!dpsi(Q_i,q,(uint_t)jcoor));
+                            }
+                            gradients_inner_product += jac_inv_dphi*jac_inv_dpsi;
                         }
                         //summing up contributions (times the measure and quad weight)
                         eval(stiff(0,0,0,P_i,Q_i)) += gradients_inner_product * eval(jac_det(qp+q)*!weights(q,0,0));
