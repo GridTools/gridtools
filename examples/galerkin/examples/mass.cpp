e6801aa9d441f0b32ffdc1399a7d58737d28646a
bignamic <bignamini@cscs.ch>
2015-12-03 10:40:30 +0100

    Added temporary initialization of dof map storage and usage of mass functor header

--- examples/galerkin/examples/mass.cpp
+++ examples/galerkin/examples/mass.cpp
@@ -1,72 +1,45 @@
 /**
 \file
 */
-#pragma once
 #define PEDANTIC_DISABLED
 #include "../numerics/assembly.hpp"
 #include "test_assembly.hpp"
+#include "../functors/mass.hpp"
 
-// [integration]
-template <typename FE, typename Cubature>
-struct integration {
-    using jac_det =accessor<0, range<0,0,0,0> , 4> const;
-    using weights =accessor<1, range<0,0,0,0> , 3> const;
-    using mass    =accessor<2, range<0,0,0,0> , 5> ;
-    using phi    =accessor<3, range<0,0,0,0> , 3> const;
-    using psi    =accessor<4, range<0,0,0,0> , 3> const;
-    using arg_list= boost::mpl::vector<jac_det, weights, mass, phi, psi> ;
-    using quad=dimension<4>;
-
-    using fe=FE;
-    using cub=Cubature;
-
-    template <typename Evaluation>
-    GT_FUNCTION
-    static void Do(Evaluation const & eval, x_interval) {
-
-        quad::Index qp;
-        dimension<5>::Index dimx;
-        dimension<6>::Index dimy;
-        // static int_t dd=fe::hypercube_t::boundary_w_codim<2>::n_points::value;
-
-        //projection of f on a (e.g.) P1 FE space ReferenceFESpace1:
-        //loop on quadrature nodes, and on nodes of the P1 element (i,j,k) with i,j,k\in {0,1}
-        for(short_t P_i=0; P_i<fe::basisCardinality; ++P_i) // current dof
-        {
-            for(short_t Q_i=0; Q_i<fe::basisCardinality; ++Q_i)
-            {//other dofs whose basis function has nonzero support on the element
-            	for(short_t q=0; q<cub::numCubPoints(); ++q){
-                    eval(mass(0,0,0,P_i,Q_i))  +=
-                        eval(!phi(P_i,q,0)*(!psi(Q_i,q,0))*jac_det(qp+q)*!weights(q,0,0));
-                }
-            }
-        }
-    }
-};
-// [integration]
 
 int main(){
+
 	//![definitions]
     //dimensions of the problem (in number of elements per dimension)
     auto d1=8;
     auto d2=8;
     auto d3=1;
+    const auto num_dofs = 1;
+    //![definitions]
+
     //![definitions]
     //defining the assembler, based on the Intrepid definitions for the numerics
 	using matrix_storage_info_t=storage_info< layout_tt<3,4> , __COUNTER__>;
     using matrix_type=storage_t< matrix_storage_info_t >;
     using fe=reference_element<1, Lagrange, Hexa>;
     using geo_map=reference_element<1, Lagrange, Hexa>;
-    using cub=cubature<fe::order, fe::shape>;
+    using cub=cubature<fe::order+1, fe::shape>;
     using geo_t = intrepid::geometry<geo_map, cub>;
     using discr_t = intrepid::discretization<fe, cub>;
+    //![definitions]
 
     //![instantiation]
     geo_t geo_;
     discr_t fe_;
     fe_.compute(Intrepid::OPERATOR_VALUE);
-    fe_.compute(Intrepid::OPERATOR_GRAD);
+    geo_.compute(Intrepid::OPERATOR_VALUE);
+    geo_.compute(Intrepid::OPERATOR_GRAD);
     //![instantiation]
+
+    constexpr meta_storage_base<__COUNTER__,layout_map<0,1,2>,false> indexing{3, 3, 1};
+    dimension<1>::Index i;
+    dimension<2>::Index j;
+    dimension<4>::Index row;
 
     using as=assembly<geo_t>;
     using as_base=assembly_base<geo_t>;
@@ -74,24 +47,26 @@ int main(){
     //![as_instantiation]
     //constructing the integration tools
     as assembler(geo_,d1,d2,d3);
-    as assembler_grad(geo_,d1,d2,d3);
-    as_base assembler_base(d1,d2,d3);
+    as_base assembler_base(d1,d2,d3,num_dofs);
     //![as_instantiation]
 
-    using domain_tuple_t = domain_type_tuple< as, as, as_base>;
-    domain_tuple_t domain_tuple_ (assembler, assembler_grad, assembler_base);
+    using domain_tuple_t = domain_type_tuple< as, as_base>;
+    domain_tuple_t domain_tuple_ (assembler, assembler_base);
 
     //![grid]
     //constructing a structured cartesian grid
     for (uint_t i=0; i<d1; i++)
         for (uint_t j=0; j<d2; j++)
             for (uint_t k=0; k<d3; k++)
+            {
                 for (uint_t point=0; point<fe::basisCardinality; point++)
                 {
                     assembler_base.grid()( i,  j,  k,  point,  0)= (i + geo_.grid()(point, 0));
                     assembler_base.grid()( i,  j,  k,  point,  1)= (j + geo_.grid()(point, 1));
                     assembler_base.grid()( i,  j,  k,  point,  2)= (k + geo_.grid()(point, 2));
+                    assembler_base.grid_map()(i,j,k,point)=0;//Global DOF // TODO: assign correct values
                 }
+            }
     //![grid]
 
 
@@ -100,7 +75,6 @@ int main(){
     matrix_storage_info_t meta_(d1,d2,d3,fe::basisCardinality,fe::basisCardinality);
     matrix_type mass_(meta_, 0., "mass");
     //![instantiation_mass]
-
 
     using dt = domain_tuple_t;
 
@@ -113,23 +87,21 @@ int main(){
 
 
     // appending the placeholders to the list of placeholders already in place
-    auto domain=domain_tuple_.template domain<p_phi, p_dphi, p_mass>(fe_.val(), fe_.grad(), mass_);
+    auto domain=domain_tuple_.template domain<p_phi, p_dphi, p_mass>(fe_.val(), geo_.grad(), mass_);
     //![placeholders]
 
-
-    auto coords=coordinates<axis>({1, 0, 1, d1-1, d1},
-                            	  {1, 0, 1, d2-1, d2});
+    auto coords=coordinates<axis>({0, 0, 0, d1-1, d1},
+                            	  {0, 0, 0, d2-1, d2});
     coords.value_list[0] = 0;
     coords.value_list[1] = d3-1;
 
-
     //![computation]
     auto computation=make_computation<gridtools::BACKEND>(make_mss(execute<forward>(),
-								   make_esf<functors::update_jac<geo_t> >(dt::p_grid_points(), p_dphi(), dt::p_jac()),
-								   make_esf<functors::det<geo_t> >(dt::p_jac(), dt::p_jac_det()),
-								   make_esf<integration<fe, cub> >(dt::p_jac_det(), dt::p_weights(), p_mass(), p_phi(), p_phi())),
-							  domain,
-							  coords);
+    															   make_esf<functors::update_jac<geo_t> >(dt::p_grid_points(), p_dphi(), dt::p_jac()),
+    															   make_esf<functors::det< geo_t > >(dt::p_jac(), dt::p_jac_det()),
+																   make_esf<functors::mass<fe, cub> >(dt::p_jac_det(), dt::p_weights(), p_phi(), p_phi(), p_mass())),
+														  domain,
+														  coords);
 
 
     computation->ready();
@@ -139,4 +111,5 @@ int main(){
     //![computation]
 
     return test_mass(assembler_base, assembler, fe_, mass_)==true;
+
 }
