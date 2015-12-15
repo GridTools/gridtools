7cd6c32999a252cecc3e8de24d5eeb18be6e5df6
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2015-12-15 15:32:58 +0100

    add implementation of a 2 stages hori diff

--- examples/horizontal_diffusion_repository.hpp
+++ examples/horizontal_diffusion_repository.hpp
@@ -24,16 +24,15 @@ using gridtools::int_t;
 #else
     typedef gridtools::layout_map<0,1,2> layout_ijk;//stride 1 on k
     typedef gridtools::layout_map<0,1,-1> layout_ij;
+    typedef gridtools::layout_map<-1,1,-1> layout_j;
 #endif
 
     typedef gridtools::layout_map<-1,-1,-1> layout_scalar;
 
     typedef gridtools::storage_info<0,layout_ijk> storage_info_ijk_t;
     typedef gridtools::storage_info<1,layout_ij> storage_info_ij_t;
-    typedef gridtools::storage_info<2,layout_scalar> storage_info_scalar_t;
-    typedef gridtools::storage_info<0,layout_ijk> storage_info_ijk_tmp_t;
-    typedef gridtools::storage_info<1,layout_ij> storage_info_ij_tmp_t;
-    typedef gridtools::storage_info<2,layout_scalar> storage_info_scalar_tmp_t;
+    typedef gridtools::storage_info<2,layout_j> storage_info_j_t;
+    typedef gridtools::storage_info<3,layout_scalar> storage_info_scalar_t;
 
 class repository
 {
@@ -41,16 +40,22 @@ class repository
 
     typedef hd_backend::storage_type<gridtools::float_type, storage_info_ijk_t >::type storage_type;
     typedef hd_backend::storage_type<gridtools::float_type, storage_info_ij_t >::type ij_storage_type;
+    typedef hd_backend::storage_type<gridtools::float_type, storage_info_j_t >::type j_storage_type;
 
     typedef hd_backend::temporary_storage_type<gridtools::float_type, storage_info_scalar_t >::type scalar_storage_type;
-    typedef hd_backend::temporary_storage_type<gridtools::float_type, storage_info_ijk_tmp_t >::type tmp_storage_type;
-    typedef hd_backend::temporary_storage_type<gridtools::float_type, storage_info_scalar_tmp_t>::type tmp_scalar_storage_type;
+    typedef hd_backend::temporary_storage_type<gridtools::float_type, storage_info_ijk_t >::type tmp_storage_type;
+    typedef hd_backend::temporary_storage_type<gridtools::float_type, storage_info_scalar_t>::type tmp_scalar_storage_type;
 
     repository(const uint_t idim, const uint_t jdim, const uint_t kdim, const uint_t halo_size):
         m_storage_info_ijk(idim, jdim, kdim),
         m_storage_info_ij(idim, jdim, kdim),
+        m_storage_info_j(idim, jdim, kdim),
         m_storage_info_scalar(idim, jdim, kdim),
         in_(m_storage_info_ijk, -1., "in"),
+        crlato_(m_storage_info_j, -1, "crlato"),
+        crlatu_(m_storage_info_j, -1, "crlatu"),
+        crlat0_(m_storage_info_j, -1, "crlat0"),
+        crlat1_(m_storage_info_j, -1, "crlat1"),
         out_(m_storage_info_ijk, -1., "out"),
         out_ref_(m_storage_info_ijk, -1., "out_ref"),
         coeff_(m_storage_info_ijk, -1., "coeff"),
@@ -76,7 +81,13 @@ class repository
         double dy = 1./(double)(j_end-j_begin);
         double dz = 1./(double)(k_end-k_begin);
 
+        double delta0 = (0.995156-0.994954)/(double)(jdim_-1);
+        double delta1 = (0.995143-0.994924)/(double)(jdim_-1);
+
         for( uint_t j=j_begin; j<j_end; j++ ){
+            crlat0_(0,j,0) = 0.994954+(double)(j)*delta0;
+            crlat1_(0,j,0) = 0.994924+(double)(j)*delta1;
+
             for( uint_t i=i_begin; i<i_end; i++ ){
                 double x = dx*(double)(i-i_begin);
                 double y = dy*(double)(j-j_begin);
@@ -84,12 +95,18 @@ class repository
                 {
                     double z = dz*(double)(k-k_begin);
                     // in values between 5 and 9
-                    in_(i,j,k) = 5. + 4*(2.+cos(PI*(x+1.5*y)) + sin(2*PI*(x+1.5*y)))/4.;
+                    in_(i,j,k) = 5. + 8*(2.+cos(PI*(x+1.5*y)) + sin(2*PI*(x+1.5*y)))/4.;
+
                     // coefficient values
-                    coeff_(i,j,k) = 3e-6*(-1 + 2*(2.+cos(PI*(x+y)) + cos(PI*y*z))/4.+ 0.05*(0.5-35.5/50.));
+                    coeff_(i,j,k) = 0.025;
                 }
             }
         }
+        for( uint_t j=j_begin+1; j<j_end; j++ ){
+            crlato_(0,j,0) = crlat1_(0,j  ,0) / crlat0_(0,j,0);
+            crlatu_(0,j,0) = crlat1_(0,j-1,0) / crlat0_(0,j,0);
+        }
+        crlato_(0,j_begin,0) = crlat1_(0,j_begin,0)/crlat0_(0,j_begin,0);
 
     }
 
@@ -159,6 +176,42 @@ class repository
             }
         }
     }
+
+    void generate_reference_simple()
+    {
+        ij_storage_type lap(m_storage_info_ij, -1., "lap");
+        init_field_to_value(lap, 0.0);
+
+        //kbody
+        for(uint_t k=0; k < kdim_; ++k)
+        {
+            for (uint_t i = halo_size_-1; i < idim_-halo_size_+1; ++i)
+            {
+                for (uint_t j = halo_size_-1; j < jdim_-halo_size_+1; ++j)
+                {
+                    lap(i,j,(uint_t)0) = in_(i+1,j,k) + in_(i-1,j,k) - (gridtools::float_type)2*in_(i,j,k)
+                            +crlato_(i,j,k) * (in_(i,j+1,k) - in_(i,j,k))
+                            +crlatu_(i,j,k) * (in_(i,j-1,k) - in_(i,j,k));
+                }
+
+            }
+            for (uint_t i = halo_size_; i < idim_-halo_size_; ++i)
+            {
+                for (uint_t j = halo_size_; j < jdim_-halo_size_; ++j)
+                {
+                    gridtools::float_type fluxx = lap(i+1,j,k) - lap(i,j,k);
+                    gridtools::float_type fluxx_m = lap(i,j,k) - lap(i-1,j,k);
+
+                    gridtools::float_type fluxy = crlato_(i,j,k)*(lap(i,j+1,k) - lap(i,j,k));
+                    gridtools::float_type fluxy_m = crlato_(i,j,k)*(lap(i,j,k) - lap(i,j-1,k));
+
+                    out_ref_(i,j,k) = in_(i,j,k)+ ((fluxx_m - fluxx) + (fluxy_m - fluxy))*coeff_(i,j,k);
+                }
+            }
+        }
+    }
+
+
     void update_cpu()
     {
 #ifdef CUDA_EXAMPLE
@@ -170,14 +223,19 @@ class repository
     storage_type& out() {return out_;}
     storage_type& out_ref() {return out_ref_;}
     storage_type& coeff() {return coeff_;}
+    j_storage_type& crlato() {return crlato_;}
+    j_storage_type& crlatu() {return crlatu_;}
+
     storage_info_ijk_t& storage_info_ijk() {return m_storage_info_ijk;}
 
     storage_info_ijk_t m_storage_info_ijk;
+    storage_info_j_t m_storage_info_j;
 private:
 
     storage_info_ij_t m_storage_info_ij;
     storage_info_scalar_t m_storage_info_scalar;
     storage_type in_, out_, out_ref_, coeff_;
+    j_storage_type crlato_, crlatu_, crlat0_, crlat1_;
     const uint_t halo_size_;
     const uint_t idim_, jdim_, kdim_;
 };
