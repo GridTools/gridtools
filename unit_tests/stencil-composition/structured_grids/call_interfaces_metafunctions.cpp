14f7abd8eceafa721293a2ebbd9c23dbcf491ea5
Mauro Bianco <mbianco@cscs.ch>
2016-01-08 14:21:26 +0100

    added test for call interfaces

--- unit_tests/stencil-composition/structured_grids/call_interfaces_metafunctions.cpp
+++ unit_tests/stencil-composition/structured_grids/call_interfaces_metafunctions.cpp
@@ -7,34 +7,93 @@
 #include <stencil-composition/structured_grids/call_interfaces.hpp>
 #include <stencil-composition/structured_grids/call_interfaces_metafunctions.hpp>
 #include <type_traits>
-
+#include <tuple>
+#include <common/generic_metafunctions/v_item_to_vector.hpp>
 
 struct pretent_aggregator {
     using value_type = double;
 
-    template <typename T0, typename T1, typename T2>
-    value_type operator()(T0, T1, T2) const {
-        return 0;
+    template <gridtools::uint_t I, gridtools::enumtype::intend Intent,
+              typename Range, gridtools::ushort_t N>
+    constexpr
+    value_type operator()(gridtools::accessor<I,Intent, Range, N> ) const {
+        return static_cast<value_type>(I+1000);
     }
 };
 
 
+struct pretent_function {
+    typedef gridtools::accessor<0,  gridtools::enumtype::in> a0;
+    typedef gridtools::accessor<1,  gridtools::enumtype::inout> a1;
+    typedef gridtools::accessor<2,  gridtools::enumtype::in> a2;
+    typedef gridtools::accessor<3,  gridtools::enumtype::inout> a3;
+
+    template <typename Eval>
+    static void Do(Eval const& eval) {
+        eval(a1()) += eval(a0());
+        eval(a3()) += eval(a2());
+    }
+};
+
 template <typename... Args>
-void complex_test(Args const&... args)
+void complex_test(Args &... args)
 {
     using namespace gridtools;
 
-    typedef function_aggregator_procedure<
+    using packtype =  typename _impl::package_args<Args...>::type;
+
+    GRIDTOOLS_STATIC_ASSERT((std::is_same<typename std::tuple_element<0, std::tuple<Args...>>::type,
+                             typename boost::mpl::at_c<packtype, 0>::type>::value), "0");
+    GRIDTOOLS_STATIC_ASSERT((std::is_same<typename _impl::wrap_reference
+                             <typename std::tuple_element<1, std::tuple<Args...>>::type>,
+                             typename boost::mpl::at_c<packtype, 1>::type>::value), "1");
+    GRIDTOOLS_STATIC_ASSERT((std::is_same<typename std::tuple_element<2, std::tuple<Args...>>::type,
+                             typename boost::mpl::at_c<packtype, 2>::type>::value), "2");
+    GRIDTOOLS_STATIC_ASSERT((std::is_same<typename _impl::wrap_reference
+                             <typename std::tuple_element<3, std::tuple<Args...>>::type>,
+                             typename boost::mpl::at_c<packtype, 3>::type>::value), "3");
+
+    typedef _impl::function_aggregator_procedure<
         pretent_aggregator,
         0,0,0,
-        typename _impl::package_args<Args...>::type
+        packtype
         > f_aggregator_t;
 
+    GRIDTOOLS_STATIC_ASSERT((_impl::contains_value<
+                             typename f_aggregator_t::non_accessor_indices,
+                             boost::mpl::integral_c<int, 3>
+                             >::type::value), "Contains 3");
+
+    GRIDTOOLS_STATIC_ASSERT((_impl::contains_value<
+                             typename f_aggregator_t::non_accessor_indices,
+                             boost::mpl::integral_c<int, 1>
+                             >::type::value), "Contains 1");
+
+    GRIDTOOLS_STATIC_ASSERT((not _impl::contains_value<
+                             typename f_aggregator_t::non_accessor_indices,
+                             boost::mpl::integral_c<int, 0>
+                             >::type::value), "Contains 0");
+
+    GRIDTOOLS_STATIC_ASSERT((not _impl::contains_value<
+                             typename f_aggregator_t::non_accessor_indices,
+                             boost::mpl::integral_c<int, 2>
+                             >::type::value), "Contains 2");
+
     auto y = typename f_aggregator_t::accessors_list_t(_impl::make_wrap(args)...);
+
+    pretent_function::Do
+        (
+         f_aggregator_t
+         (
+          pretent_aggregator(),
+          y
+          )
+         );
+
 }
 
 
-TEST(call_interfaces_metafunctions, compile_time_tests) {
+TEST(call_interfaces_metafunctions, compile_time_basic_tests) {
     using namespace gridtools;
 
     unsigned int v = 666;
@@ -60,8 +119,20 @@ TEST(call_interfaces_metafunctions, compile_time_tests) {
     GRIDTOOLS_STATIC_ASSERT((std::is_same<decltype(a2), boost::mpl::at_c<pack, 2>::type>::value), "3");
     GRIDTOOLS_STATIC_ASSERT((std::is_same<_impl::wrap_reference<decltype(a3)>, boost::mpl::at_c<pack, 3>::type>::value), "4");
 
+}
+
+TEST(call_interfaces_metafunctions, call_pretent_procedure) {
+    using namespace gridtools;
+
+    accessor<0, enumtype::in, extent<1,1,1,1>> a0;
+    accessor<1, enumtype::inout> a2;
+    double a1 = 3.14;
+    double a3 = 666;
 
     complex_test(a0, a1, a2, a3);
+
+    EXPECT_TRUE(a1 == 1003.14);
+    EXPECT_TRUE(a3 == 1667);
 }
 
 #endif
