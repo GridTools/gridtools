c5773bb9da595a0b1fc5c0bf4ea17ebd2f6c2a4a
Lucas Benedicic <benedicic@cscs.ch>
2016-01-11 14:40:23 +0100

    Correcting GPU container image.-

--- python/docker/build_gpu.sh
+++ python/docker/build_gpu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "About to build a container with a GPU backend for GridTools."
-echo "This container image builds on top of the official NVIDIA CUDA container 'cuda:latest'"
 echo "Press any key to continue or Ctrl+C to exit ..."
 read -n 1
-docker build --rm --tag=gridtools4py:cpu --file=Dockerfile.gpu .
+docker build --rm --tag=gridtools4py:gpu --file=Dockerfile .
--- python/docker/start_gpu.sh
+++ python/docker/start_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 GPU=0 ./nvidia-docker/nvidia-docker run --rm                                     \
-					-it                                      \
-					-P                                       \
-					gridtools4py:latest
+                                        -it                                      \
+                                        -P                                       \
+                                        gridtools4py:gpu
