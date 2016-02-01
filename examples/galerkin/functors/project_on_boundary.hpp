b6a873557db009565fc50278396e31103cdbb7e7
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2016-02-01 11:09:24 +0000

    add files

---
+++ examples/galerkin/functors/project_on_boundary.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+namespace gdl{
+    namespace functors{
+
+        /** takes the boundary trace of the vector field beta, and projects it along the direction
+         specified by the input vector field int_normals.*/
+        struct project_on_boundary {
+
+            using beta=gt::accessor<0, enumtype::in, gt::extent<> , 5>;
+            using int_normals=gt::accessor<1, enumtype::in, gt::extent<> , 6>;
+            using bd_mass=gt::accessor<2, enumtype::in, gt::extent<> , 6>;
+            using out=gt::accessor<3, enumtype::inout, gt::extent<> , 5>;
+            using arg_list=boost::mpl::vector<beta, int_normals, bd_mass, out> ;
+
+            template <typename Evaluation>
+            GT_FUNCTION
+            static void Do(Evaluation const & eval, x_interval) {
+                gt::dimension<4>::Index row;
+                gt::dimension<5>::Index face;
+                gt::dimension<5>::Index col;
+                gt::dimension<5>::Index dim;
+                gt::dimension<6>::Index Mface;
+
+                const auto N_dofs=eval.get().template get_storage_dims<3>(out());
+                const auto N_faces=eval.get().template get_storage_dims<5>(int_normals());
+                const auto N_dims=eval.get().template get_storage_dims<4>(int_normals());
+
+                for (uint_t dof = 0; dof<N_dofs; ++dof)
+                    for (uint_t k = 0; k<N_dofs; ++k)
+                        for (uint_t f =0; f<N_faces; ++f)
+                        {
+                            float_type product = 0.;
+                            for (uint_t d =0; d<N_dims; ++d)
+                                product += eval(beta(row+k, dim+d)
+                                               *int_normals(row+k, dim+d, Mface+f));
+                            eval(out(row+dof, face+f)) +=
+                                eval(bd_mass(row+dof, col+k, Mface+f)
+                                     *product );
+                        }
+
+
+            }
+        };
+    } //namespace functors
+} //namespace gdl
---
+++ examples/galerkin/galerkin_defs.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace gdl{
+
+    namespace gt=gridtools;
+
+    namespace enumtype{
+        using namespace gt::enumtype;
+        // using gt::enumtype;
+        enum Basis {Lagrange, BSplines};
+        enum Shape {Hexa, Tetra, Quad, Tri,  Line, Point};
+    }
+
+    using uint_t = gt::uint_t;
+    using int_t = gt::uint_t;
+    using short_t = gt::uint_t;
+    using ushort_t = gt::uint_t;
+
+    using float_type = gt::float_type;
+
+    template<int_t T>
+    using static_int=gt::static_int<T>;
+    template<uint_t T>
+    using static_uint=gt::static_uint<T>;
+    template<short_t T>
+    using static_short=gt::static_short<T>;
+    template<ushort_t T>
+    using static_ushort=gt::static_ushort<T>;
+}
