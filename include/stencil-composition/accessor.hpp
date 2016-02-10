dd254548956f5daab866db43389d0355b7a08704
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-02-10 19:16:13 +0100

    move global accessor to standalone header. Do not pollute main forwarder classes that forward to the corresponding grid backend

--- include/stencil-composition/accessor.hpp
+++ include/stencil-composition/accessor.hpp
@@ -3,18 +3,6 @@
 //TODOMEETING protect this with define and ifdef inside
 //TODOMEETING inline namespaces to protect grid backends
 
-namespace gridtools {
-    template <uint_t I, enumtype::intend Intend>
-    struct global_accessor{
-
-        typedef global_accessor<I, Intend> type;
-        // static const ushort_t n_dim=Dim;
-        typedef static_uint<I> index_type;
-        // typedef Range range_type;
-    };
-
-}//namespace gridtools
-
 #ifdef STRUCTURED_GRIDS
     #include "stencil-composition/structured_grids/accessor_metafunctions.hpp"
     #include "stencil-composition/structured_grids/accessor.hpp"
@@ -22,3 +10,5 @@ namespace gridtools {
     #include "stencil-composition/icosahedral_grids/accessor_metafunctions.hpp"
     #include "stencil-composition/icosahedral_grids/accessor.hpp"
 #endif
+
+#include "global_accessor.hpp"
---
+++ include/stencil-composition/global_accessor.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace gridtools {
+    template <uint_t I, enumtype::intend Intend>
+    struct global_accessor{
+
+        typedef global_accessor<I, Intend> type;
+        // static const ushort_t n_dim=Dim;
+        typedef static_uint<I> index_type;
+        // typedef Range range_type;
+    };
+
+}//namespace gridtools
+
