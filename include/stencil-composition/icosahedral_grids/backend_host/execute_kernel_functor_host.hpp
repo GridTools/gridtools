fcbd53058a39058762e04361d47793d227bfebaa
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-02-10 20:03:39 +0100

    fixes after merge with cleanup_after_long

--- include/stencil-composition/icosahedral_grids/backend_host/execute_kernel_functor_host.hpp
+++ include/stencil-composition/icosahedral_grids/backend_host/execute_kernel_functor_host.hpp
@@ -83,8 +83,8 @@ namespace gridtools {
 
                 iterate_domain_t it_domain(m_local_domain, m_grid.grid_topology());
 
-                it_domain.set_data_pointer(&data_pointer);
-                it_domain.set_strides_pointer(&strides);
+                it_domain.set_data_pointer_impl(&data_pointer);
+                it_domain.set_strides_pointer_impl(&strides);
 
                 it_domain.template assign_storage_pointers<backend_traits_t >();
                 it_domain.template assign_stride_pointers <backend_traits_t, strides_t>();
