c6861afe3943683410089dc4ac7d3457e691231e
Carlos Osuna <carlos.osuna@env.ethz.ch>
2016-02-14 11:30:12 +0100

    revert back to personal cmake to test isystem version of cmake

--- jenkins/env_greina.sh
+++ jenkins/env_greina.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 #we need a decent cmake version in order to pass the HOST_COMPILER to nvcc
-#module load /home/cosuna/privatemodules/cmake-3.3.2
-module load cmake
+module load /home/cosuna/privatemodules/cmake-3.3.2
+#module load cmake
 module load python/3.4.3
 module load boost/1.56_gcc4.8.4
 module load mvapich2/gcc/64/2.2-gcc-4.8.4-cuda-7.0
