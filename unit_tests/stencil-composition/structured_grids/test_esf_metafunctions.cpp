b4b843d2ebf47bc6a787f45ca3e5ece95426a9d7
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-02-17 16:41:56 +0100

    fixes #199

--- unit_tests/stencil-composition/structured_grids/test_esf_metafunctions.cpp
+++ unit_tests/stencil-composition/structured_grids/test_esf_metafunctions.cpp
@@ -5,12 +5,12 @@
 #include "stencil-composition/caches/cache_metafunctions.hpp"
 #include "stencil-composition/caches/define_caches.hpp"
 #include "stencil-composition/interval.hpp"
-#include "stencil-composition/make_computation.hpp"
+#include "stencil-composition/stencil-composition.hpp"
 
 using namespace gridtools;
 using namespace gridtools::enumtype;
 using gridtools::accessor;
-using gridtools::range;
+using gridtools::extent;
 using gridtools::layout_map;
 using gridtools::float_type;
 using gridtools::arg;
@@ -26,10 +26,10 @@ struct print_r {
 };
 
 struct functor0{
-    typedef accessor<0, enumtype::in, range<-1, 0, -2, 1, -3, 2>> in0;
-    typedef accessor<1, enumtype::in, range<-3, 2, -2, 0, 0, 2>> in1;
+    typedef accessor<0, enumtype::in, extent<-1, 0, -2, 1, -3, 2>> in0;
+    typedef accessor<1, enumtype::in, extent<-3, 2, -2, 0, 0, 2>> in1;
     typedef accessor<2, enumtype::inout> out;
-    typedef accessor<3, enumtype::in, range<-1, 2, 0, 0, -3, 1>> in3;
+    typedef accessor<3, enumtype::in, extent<-1, 2, 0, 0, -3, 1>> in3;
 
     typedef boost::mpl::vector<in0,in1,out,in3> arg_list;
 
@@ -47,7 +47,7 @@ std::ostream& operator<<(std::ostream& s, functor0) { return s << "functor0"; }
 #endif
 
 typedef layout_map<2,1,0> layout_ijk_t;
-typedef storage_info<0, layout_ijk_t> storage_info_ijk_t;
+typedef gridtools::BACKEND::storage_info<0, layout_ijk_t> storage_info_ijk_t;
 typedef gridtools::BACKEND::storage_type<float_type, storage_info_ijk_t >::type storage_type;
 typedef gridtools::BACKEND::temporary_storage_type<float_type, storage_info_ijk_t >::type tmp_storage_type;
 
@@ -57,7 +57,7 @@ typedef arg<1, storage_type> in0;
 typedef arg<2, storage_type> in1;
 typedef arg<3, storage_type> in2;
 
-TEST(esf_metafunctions, compute_ranges_of)
+TEST(esf_metafunctions, compute_extents_of)
 {
     typedef decltype(gridtools::make_esf<functor0>(in0(), in1(), o0(), in2())) functor0__;
     typedef decltype( gridtools::make_mss
@@ -67,16 +67,16 @@ TEST(esf_metafunctions, compute_ranges_of)
     ) mss_t;
     typedef boost::mpl::vector<o0, in0, in1, in2> placeholders;
 
-    typedef gridtools::compute_extents_of<placeholders>::for_mss<mss_t>::type final_map;
+    typedef gridtools::strgrid::compute_extents_of<placeholders>::for_mss<mss_t>::type final_map;
 
-GRIDTOOLS_STATIC_ASSERT((std::is_same<boost::mpl::at<final_map, o0>::type, range<0, 0, 0, 0, 0, 0>>::type::value),
-                          "o0 range<0, 0, 0, 0, 0, 0>");
-GRIDTOOLS_STATIC_ASSERT((std::is_same<boost::mpl::at<final_map, in0>::type, range<-1, 0, -2, 1, -3, 2>>::type::value),
-                          "in0 range<-1, 0, -2, 1, -3, 2>");
-GRIDTOOLS_STATIC_ASSERT((std::is_same<boost::mpl::at<final_map, in1>::type, range<-3, 2, -2, 0, 0, 2>>::type::value),
-                          "in1 range<-3, 2, -2, 0, 0, 2>");
-GRIDTOOLS_STATIC_ASSERT((std::is_same<boost::mpl::at<final_map, in2>::type, range<-1, 2, 0, 0, -3, 1>>::type::value),
-                          "in2 range<-1, 2, 0, 0, -3, 1>");
+GRIDTOOLS_STATIC_ASSERT((std::is_same<boost::mpl::at<final_map, o0>::type, extent<0, 0, 0, 0, 0, 0>>::type::value),
+                          "o0 extent<0, 0, 0, 0, 0, 0>");
+GRIDTOOLS_STATIC_ASSERT((std::is_same<boost::mpl::at<final_map, in0>::type, extent<-1, 0, -2, 1, -3, 2>>::type::value),
+                          "in0 extent<-1, 0, -2, 1, -3, 2>");
+GRIDTOOLS_STATIC_ASSERT((std::is_same<boost::mpl::at<final_map, in1>::type, extent<-3, 2, -2, 0, 0, 2>>::type::value),
+                          "in1 extent<-3, 2, -2, 0, 0, 2>");
+GRIDTOOLS_STATIC_ASSERT((std::is_same<boost::mpl::at<final_map, in2>::type, extent<-1, 2, 0, 0, -3, 1>>::type::value),
+                          "in2 extent<-1, 2, 0, 0, -3, 1>");
 /* total placeholders (rounded to 10) _SIZE = 10*/
     ASSERT_TRUE(true);
 }
