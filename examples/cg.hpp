46f9c6087773768de44cd014d5fc329f3ee196fb
Juraj Kardos <juraj.kardos@usi.ch>
2016-03-04 13:04:42 +0100

    Added second step of CG method

--- examples/cg.hpp
+++ examples/cg.hpp
@@ -256,9 +256,11 @@ bool solver(uint_t xdim, uint_t ydim, uint_t zdim, uint_t nt) {
     storage_type b(metadata_, 0., "RHS vector");
     storage_type x(metadata_, 0., "Solution vector t");
     storage_type xNew(metadata_, 0., "Solution vector t+1");
-    storage_type Ax(metadata_, 0., "Solution vector");
-    storage_type r(metadata_, 0., "Residual");
+    storage_type Ax(metadata_, 0., "Multiplied sol. vector Ax at time t");
+    storage_type r(metadata_, 0., "Residual t");
+    storage_type rNew(metadata_, 0., "Residual t+1");
     storage_type d(metadata_, 0., "Direction vector");
+    storage_type Ad(metadata_, 0., "Multiplied direction vector");
     //storage_type *ptr_in7pt = &in7pt, *ptr_out7pt = &out7pt;
 
 
@@ -312,6 +314,16 @@ bool solver(uint_t xdim, uint_t ydim, uint_t zdim, uint_t nt) {
                                p_x_step1,
                                p_d_step1,
                                p_alpha_step1 > accessor_list_step1;
+
+    typedef arg<0, storage_type > p_rNew_step2; //residual t+1
+    typedef arg<1, storage_type > p_r_step2; //residual t
+    typedef arg<2, storage_type > p_Ad_step2; //A*d
+    typedef arg<3, parameter >    p_alpha_step2; //step size
+
+    typedef boost::mpl::vector<p_rNew_step2,
+                               p_r_step2,
+                               p_Ad_step2,
+                               p_alpha_step2 > accessor_list_step2;
 
 
     //--------------------------------------------------------------------------
@@ -396,7 +408,9 @@ bool solver(uint_t xdim, uint_t ydim, uint_t zdim, uint_t nt) {
 
     MPI_Barrier(GCL_WORLD);
 
-    // perform iterations of the CG
+    /** 
+        Perform iterations of the CG
+    */
     for(int i=0; i < TIME_STEPS; i++) {
 
         //compute step size alpha
@@ -407,7 +421,10 @@ bool solver(uint_t xdim, uint_t ydim, uint_t zdim, uint_t nt) {
         gridtools::domain_type<accessor_list_step1> domain_step1
             (boost::fusion::make_vector(&xNew, &x, &d, &alpha));
 
-        //instantiate stencil to perform fist step of CG
+        gridtools::domain_type<accessor_list_step2> domain_step2
+            (boost::fusion::make_vector(&rNew, &r, &Ad, &alpha));
+
+        //instantiate stencil to perform step of CG
         #ifdef __CUDACC__
             gridtools::computation* stencil_step1 =
         #else
@@ -426,14 +443,44 @@ bool solver(uint_t xdim, uint_t ydim, uint_t zdim, uint_t nt) {
                     domain_step1, coords3d7pt
                 );
 
+        #ifdef __CUDACC__
+            gridtools::computation* stencil_step2 =
+        #else
+                boost::shared_ptr<gridtools::computation> stencil_step2 =
+        #endif
+              gridtools::make_computation<gridtools::BACKEND>
+                (
+                    gridtools::make_mss // mss_descriptor
+                    (
+                        execute<forward>(),
+                        gridtools::make_esf<add_functor>(p_rNew_step2(),
+                                                         p_r_step2(),
+                                                         p_Ad_step2(),
+                                                         p_alpha_step2()) // r_(i+1) = r_i + alpha * Ad_i
+                    ),
+                    domain_step2, coords3d7pt
+                );
 
-        //prepare and run single step of stencil computation
+
+
+        //prepare and run single steps of stencil computation
         stencil_step1->ready();
         stencil_step1->steady();
-        boost::timer::cpu_timer time_run;
+        boost::timer::cpu_timer time_run1;
         stencil_step1->run();
-        lapse_time_run = lapse_time_run + time_run.elapsed();
+        lapse_time_run = lapse_time_run + time_run1.elapsed();
         stencil_step1->finalize();
+
+        stencil_step2->ready();
+        stencil_step2->steady();
+        boost::timer::cpu_timer time_run2;
+        stencil_step2->run();
+        lapse_time_run = lapse_time_run + time_run2.elapsed();
+        stencil_step2->finalize();
+
+        //compute Gram–Schmidt orthogonalization parameter beta
+        double orto = 1.;
+        beta.setValue(orto);
 
         //swap input and output fields
         //storage_type* tmp = ptr_out7pt;
