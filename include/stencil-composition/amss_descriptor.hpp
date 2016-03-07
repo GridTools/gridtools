36fb3eadd378a07a5f3f68dbd3f84f374609774c
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-03-07 13:36:09 +0100

    add missing amss descriptor, that can be mss or reduction

---
+++ include/stencil-composition/amss_descriptor.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace gridtools {
+    template < typename T >
+    struct is_amss_descriptor : boost::mpl::false_ {};
+
+    template < typename ExecutionEngine, typename EsfDescrSequence, typename CacheSequence >
+    struct is_amss_descriptor< mss_descriptor< ExecutionEngine, EsfDescrSequence, CacheSequence > >
+        : boost::mpl::true_ {};
+
+    template < typename ReductionType, typename EsfDescrSequence >
+    struct is_amss_descriptor< reduction_descriptor< ReductionType, EsfDescrSequence > >
+        : boost::mpl::true_ {};
+
+    template < typename T >
+    struct amss_descriptor_is_reduction;
+
+    template < typename ExecutionEngine, typename EsfDescrSequence >
+    struct amss_descriptor_is_reduction< mss_descriptor< ExecutionEngine, EsfDescrSequence > >
+        : boost::mpl::false_ {};
+
+    template < typename ReductionType, typename EsfDescrSequence >
+    struct amss_descriptor_is_reduction< reduction_descriptor< ReductionType, EsfDescrSequence> >
+        : boost::mpl::true_ {};
+}
