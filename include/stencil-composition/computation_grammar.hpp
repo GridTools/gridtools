86a4d8c4cd33887cb2d84a2f488382b0ee31c78e
Paolo Crosetto <crosetto@lifev.org>
2016-03-14 19:09:18 +0100

    adding file

---
+++ include/stencil-composition/computation_grammar.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "../common/defs.hpp"
+namespace gridtools{
+
+template <typename T>
+struct is_computation_token :
+    boost::mpl::or_<
+    is_condition<T>,
+    is_mss_descriptor<T>
+    >::type {};
+
+}
