855078fdaead2bb102076e834e4dbce88f609adc
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-03-14 17:14:26 +0100

    change layout for icosahedrla GPU

--- include/stencil-composition/icosahedral_grids/grid_traits.hpp
+++ include/stencil-composition/icosahedral_grids/grid_traits.hpp
@@ -19,10 +19,10 @@ namespace gridtools {
             typedef icgrid::grid_traits_arch< BackendId > type;
         };
 
-        typedef static_uint<0> dim_i_t;
-        typedef static_uint<1> dim_c_t;
-        typedef static_uint<2> dim_j_t;
-        typedef static_uint<3> dim_k_t;
+        typedef static_uint<3> dim_i_t;
+        typedef static_uint<2> dim_c_t;
+        typedef static_uint<1> dim_j_t;
+        typedef static_uint<0> dim_k_t;
 
     };
 }
