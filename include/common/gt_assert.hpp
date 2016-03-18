368afb946df456b6fc770ab87a2386c018bf03ed
Carlos Osuna <carlos.osuna@env.ethz.ch>
2016-03-18 18:41:29 +0100

    define a macro that get the line and file for a custom assert for gcc

--- include/common/gt_assert.hpp
+++ include/common/gt_assert.hpp
@@ -13,9 +13,27 @@
   #include <cassert>
 #endif
 
+#ifdef __GNUC__
+
+#define GTASSERT(cond)  gt_assert(cond, __LINE__, __FILE__)
+
+namespace gridtools {
+    GT_FUNCTION
+    void gt_assert(bool cond, int line, const char* filename) {
+        if(! cond) printf("Assert triggered in %s:%d \n", filename, line);
+        assert(cond);
+    }
+}
+
+#else
+
+#define GTASSERT(cond)  gt_assert(cond)
+
 namespace gridtools {
     GT_FUNCTION
     void gt_assert(bool cond) {
         assert(cond);
     }
 }
+
+#endif
