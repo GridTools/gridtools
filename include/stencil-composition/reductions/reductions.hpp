aadc01dc1293d4a0642a48f8d31b2bbe6bf03f90
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-03-22 11:40:15 +0100

    add cxx03 macros for make_reduction

--- include/stencil-composition/reductions/reductions.hpp
+++ include/stencil-composition/reductions/reductions.hpp
@@ -2,7 +2,12 @@
 
 #include "../esf.hpp"
 #include "../make_esf.hpp"
-#include "make_reduction.hpp"
+#ifdef CXX11_ENABLE
+#include "make_reduction_cxx11.hpp"
+#else
+#include "make_reduction_cxx03.hpp"
+#endif
+
 #include "../make_computation.hpp"
 #include "../axis.hpp"
 #include "../../common/binops.hpp"
