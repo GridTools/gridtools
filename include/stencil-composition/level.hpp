a6f0b0cd49ea63503584eaebe83603d8d56f1c7f
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-03-31 16:57:45 +0200

    dummy comm

--- include/stencil-composition/level.hpp
+++ include/stencil-composition/level.hpp
@@ -25,8 +25,6 @@ namespace gridtools {
         GRIDTOOLS_STATIC_ASSERT(
             -cLevelOffsetLimit <= VOffset && VOffset <= cLevelOffsetLimit, "check offset and splitter value ranges \n\
          (note that non negative splitter values simplify the index computation)");
-  typedef int pp;
- pp a;
 #ifdef CXX11_ENABLED
         // define splitter and level offset
         typedef static_uint< VSplitter > Splitter;
