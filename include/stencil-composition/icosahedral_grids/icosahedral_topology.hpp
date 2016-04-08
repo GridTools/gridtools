GridTools/gridtools

commit 88df3505cb1c969e1a327057bca601a9be15e25d

fixes for stencil on vertexes

include/stencil-composition/icosahedral_grids/icosahedral_topology.hpp
@@ -526,8 +526,8 @@ namespace gridtools {
         }
 
         GT_FUNCTION
-        static return_t< array< uint_t, 4 > > get_index(array< uint_t, 3 > const &i) {
-            return return_t< array< uint_t, 4 > >{{
+        static return_t< array< uint_t, 6 > > get_index(array< uint_t, 3 > const &i) {
+            return return_t< array< uint_t, 6 > >{{
                 {i[0] - 1, 1, i[1] - 1, i[2]},
                 {i[0] - 1, 0, i[1], i[2]},
                 {i[0] - 1, 1, i[1], i[2]},
@@ -558,8 +558,8 @@ namespace gridtools {
         }
 
         GT_FUNCTION
-        static return_t< array< uint_t, 4 > > get_index(array< uint_t, 3 > const &i) {
-            return return_t< array< uint_t, 4 > >{{
+        static return_t< array< uint_t, 6 > > get_index(array< uint_t, 3 > const &i) {
+            return return_t< array< uint_t, 6 > >{{
                 {i[0], 1, i[1] - 1, i[2]},
                 {i[0] - 1, 0, i[1], i[2]},
                 {i[0] - 1, 2, i[1], i[2]},

examples/icosahedral/stencil_on_vertexes.hpp
@@ -28,11 +28,7 @@ namespace sov {
     struct test_on_vertexes_functor {
         typedef in_accessor< 0, icosahedral_topology_t::vertexes, extent< 1 > > in;
         typedef inout_accessor< 1, icosahedral_topology_t::vertexes > out;
-        typedef in_accessor< 2, icosahedral_topology_t::vertexes, extent< 1 > > ipos;
-        typedef in_accessor< 3, icosahedral_topology_t::vertexes, extent< 1 > > cpos;
-        typedef in_accessor< 4, icosahedral_topology_t::vertexes, extent< 1 > > jpos;
-        typedef in_accessor< 5, icosahedral_topology_t::vertexes, extent< 1 > > kpos;
-        typedef boost::mpl::vector6< in, out, ipos, cpos, jpos, kpos > arg_list;
+        typedef boost::mpl::vector2< in, out > arg_list;
 
         template < typename Evaluation >
         GT_FUNCTION static void Do(Evaluation const &eval, x_interval) {
@@ -53,7 +49,7 @@ namespace sov {
 
         typedef gridtools::layout_map< 2, 1, 0 > layout_t;
 
-        using vertex_storage_type = typename backend_t::storage_t< icosahedral_topology_t::vertexes, double >;
+        using cell_storage_type = typename backend_t::storage_t< icosahedral_topology_t::vertexes, double >;
 
         const uint_t halo_nc = 1;
         const uint_t halo_mc = 1;
@@ -61,10 +57,6 @@ namespace sov {
         icosahedral_topology_t icosahedral_grid(d1, d2, d3);
 
         auto in_vertexes = icosahedral_grid.make_storage< icosahedral_topology_t::vertexes, double >("in");
-        auto i_vertexes = icosahedral_grid.make_storage< icosahedral_topology_t::vertexes, double >("i");
-        auto j_vertexes = icosahedral_grid.make_storage< icosahedral_topology_t::vertexes, double >("j");
-        auto c_vertexes = icosahedral_grid.make_storage< icosahedral_topology_t::vertexes, double >("c");
-        auto k_vertexes = icosahedral_grid.make_storage< icosahedral_topology_t::vertexes, double >("k");
         auto out_vertexes = icosahedral_grid.make_storage< icosahedral_topology_t::vertexes, double >("out");
         auto ref_vertexes = icosahedral_grid.make_storage< icosahedral_topology_t::vertexes, double >("ref");
 
@@ -74,10 +66,6 @@ namespace sov {
                     for (int k = 0; k < d3; ++k) {
                         in_vertexes(i, c, j, k) = (uint_t)in_vertexes.meta_data().index(
                             array< uint_t, 4 >{(uint_t)i, (uint_t)c, (uint_t)j, (uint_t)k});
-                        i_vertexes(i, c, j, k) = i;
-                        c_vertexes(i, c, j, k) = c;
-                        j_vertexes(i, c, j, k) = j;
-                        k_vertexes(i, c, j, k) = k;
                     }
                 }
             }
@@ -85,19 +73,15 @@ namespace sov {
         out_vertexes.initialize(0.0);
         ref_vertexes.initialize(0.0);
 
-        typedef arg< 0, vertex_storage_type > p_in_vertexes;
-        typedef arg< 1, vertex_storage_type > p_out_vertexes;
-        typedef arg< 2, vertex_storage_type > p_i_vertexes;
-        typedef arg< 3, vertex_storage_type > p_c_vertexes;
-        typedef arg< 4, vertex_storage_type > p_j_vertexes;
-        typedef arg< 5, vertex_storage_type > p_k_vertexes;
+        typedef arg< 0, cell_storage_type > p_in_vertexes;
+        typedef arg< 1, cell_storage_type > p_out_vertexes;
 
         typedef boost::mpl::
-            vector< p_in_vertexes, p_out_vertexes, p_i_vertexes, p_c_vertexes, p_j_vertexes, p_k_vertexes >
+            vector< p_in_vertexes, p_out_vertexes >
                 accessor_list_t;
 
         gridtools::domain_type< accessor_list_t > domain(boost::fusion::make_vector(
-            &in_vertexes, &out_vertexes, &i_vertexes, &c_vertexes, &j_vertexes, &k_vertexes));
+            &in_vertexes, &out_vertexes));
         array< uint_t, 5 > di = {halo_nc, halo_nc, halo_nc, d1 - halo_nc - 1, d1};
         array< uint_t, 5 > dj = {halo_mc, halo_mc, halo_mc, d2 - halo_mc - 1, d2};
 
@@ -113,11 +97,8 @@ namespace sov {
                                                          gridtools::make_esf< test_on_vertexes_functor,
                                                              icosahedral_topology_t,
                                                              icosahedral_topology_t::vertexes >(p_in_vertexes(),
-                                                             p_out_vertexes(),
-                                                             p_i_vertexes(),
-                                                             p_c_vertexes(),
-                                                             p_j_vertexes(),
-                                                             p_k_vertexes())));
+                                                             p_out_vertexes()
+                                                             )));
         stencil_->ready();
         stencil_->steady();
         stencil_->run();

commit f9dda6e91bd7fb200526db200c2c1a276b7ee80b

fix to bug introduced in 88df350 and remove BOOST_NO_CXX11...

CMakeLists.txt
@@ -35,7 +35,7 @@ endif()
 set(BOOST_FUSION_MAX_SIZE 20 CACHE STRING "max sizes of boost fusion containers" )
 add_definitions(-DFUSION_MAX_VECTOR_SIZE=${BOOST_FUSION_MAX_SIZE})
 add_definitions(-DFUSION_MAX_MAP_SIZE=${BOOST_FUSION_MAX_SIZE})
-add_definitions(-DBOOST_NO_CXX11_RVALUE_REFERENCES)
+#add_definitions(-DBOOST_NO_CXX11_RVALUE_REFERENCES)
 
 if(ENABLE_CXX11)
     add_definitions(-DBOOST_RESULT_OF_USE_TR1 -DBOOST_NO_CXX11_DECLTYPE)

include/stencil-composition/icosahedral_grids/icosahedral_topology.hpp
@@ -526,8 +526,8 @@ namespace gridtools {
         }
 
         GT_FUNCTION
-        static return_t< array< uint_t, 6 > > get_index(array< uint_t, 3 > const &i) {
-            return return_t< array< uint_t, 6 > >{{
+        static return_t< array< uint_t, 4 > > get_index(array< uint_t, 3 > const &i) {
+            return return_t< array< uint_t, 4 > >{{
                 {i[0] - 1, 1, i[1] - 1, i[2]},
                 {i[0] - 1, 0, i[1], i[2]},
                 {i[0] - 1, 1, i[1], i[2]},
@@ -558,8 +558,8 @@ namespace gridtools {
         }
 
         GT_FUNCTION
-        static return_t< array< uint_t, 6 > > get_index(array< uint_t, 3 > const &i) {
-            return return_t< array< uint_t, 6 > >{{
+        static return_t< array< uint_t, 4 > > get_index(array< uint_t, 3 > const &i) {
+            return return_t< array< uint_t, 4 > >{{
                 {i[0], 1, i[1] - 1, i[2]},
                 {i[0] - 1, 0, i[1], i[2]},
                 {i[0] - 1, 2, i[1], i[2]},