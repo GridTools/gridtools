b7e9a5dbc957058a09fcbd9757abf4697a9c5c9b
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-04-16 11:38:44 +0200

    adding an is_aggregate trait

---
+++ include/common/is_aggregate.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <type_traits>
+#include <boost/mpl/or.hpp>
+#include "array.hpp"
+
+namespace gridtools {
+
+    /**
+     * type trait to check if a type is an aggregate
+     * Note: see discussion here
+     * http://stackoverflow.com/questions/33648044/boostprotois-aggregate-returning-false-when-it-is-an-aggregate-type
+     * there is not general way of detecting whether a type in C++ is an aggregate, and there probably wont be.
+     * Instead we use specific traits for the types that are used in our library
+     * (in the future this might be extended to using concepts)
+     */
+    template < typename T >
+    struct is_aggregate : boost::mpl::or_< is_array< T >, std::is_integral< T > > {};
+}
