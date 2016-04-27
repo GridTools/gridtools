b1d30b93491ceb7b2bbef71d502d434e32e06ae2
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-04-27 09:29:44 +0200

    extend the explode to operate also on tuple

--- unit_tests/common/explode_array_test.cpp
+++ unit_tests/common/explode_array_test.cpp
@@ -1,7 +1,8 @@
 #include "gtest/gtest.h"
 
 #include <common/explode_array.hpp>
-
+#include <common/tuple.hpp>
+#include <tools/verifier.hpp>
 
 class PackChecker{
 
@@ -19,6 +20,23 @@ public:
 
 };
 
+class TuplePackChecker{
+
+public:
+    static bool check(int i1, double i2, unsigned short i3)
+    {
+        return i1==-35 && gridtools::compare_below_threshold(i2, 23.3, 1-8) &&  i3==9;
+    }
+
+    template<typename ... UInt>
+    static bool apply(UInt ... args)
+    {
+        return check(args...);
+    }
+
+};
+
+
 struct _impl_index{
     template<typename ... UInt>
     static bool apply(const PackChecker& me, UInt ... args){
@@ -26,6 +44,12 @@ struct _impl_index{
     }
 };
 
+struct _impl_index_tuple{
+    template<typename ... UInt>
+    static bool apply(const TuplePackChecker& me, UInt ... args){
+        return me.check(args...);
+    }
+};
 
 using namespace gridtools;
 
@@ -39,3 +63,15 @@ TEST(explode_array, test_explode_with_object) {
     PackChecker checker;
     ASSERT_TRUE(( explode<int, _impl_index>(a, checker)));
 }
+
+TEST(explode_array, tuple) {
+    tuple<int, float, unsigned short> a(-35,23.3,9);
+    ASSERT_TRUE(( explode<bool, TuplePackChecker>(a)));
+}
+
+TEST(explode_array, tuple_with_object) {
+    tuple<int, float, unsigned short> a(-35,23.3,9);
+    TuplePackChecker checker;
+    ASSERT_TRUE(( explode<bool, _impl_index_tuple>(a, checker)));
+}
+
