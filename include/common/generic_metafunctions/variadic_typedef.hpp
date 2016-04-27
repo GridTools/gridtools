1e6bf06c19f32859abe483bdcfc9ca4b794611a7
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-04-27 09:35:26 +0200

    add functionality to return a item by index from a variadic pack

--- include/common/generic_metafunctions/variadic_typedef.hpp
+++ include/common/generic_metafunctions/variadic_typedef.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "../defs.hpp"
+#include "../host_device.hpp"
 
 namespace gridtools {
 
@@ -29,4 +30,23 @@ namespace gridtools {
             typedef typename impl_::template get_elem<Idx, First, Args...>::type type;
         };
     };
+
+    template < int Idx >
+    struct get_from_variadic_pack {
+        template < typename First, typename... Accessors >
+        GT_FUNCTION static constexpr typename variadic_typedef< First, Accessors... >::template get_elem< Idx >::type
+        apply(First first, Accessors... args) {
+            return get_from_variadic_pack< Idx - 1 >::apply(args...);
+        }
+    };
+
+    template <>
+    struct get_from_variadic_pack< 0 > {
+        template < typename First, typename... Accessors >
+        GT_FUNCTION static constexpr typename variadic_typedef< First, Accessors... >::template get_elem< 0 >::type
+        apply(First first, Accessors... args) {
+            return first;
+        }
+    };
+
 }
