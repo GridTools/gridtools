7b81b93b0e905ef06ba5f5f24a7c3d1e95e08810
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-04-29 09:04:55 +0200

    move helper to impl

--- include/common/tuple.hpp
+++ include/common/tuple.hpp
@@ -4,25 +4,26 @@
 
 namespace gridtools {
 
-    template < ushort_t Idx, typename Args, typename First, typename Super >
-    struct return_helper {
-        GT_FUNCTION constexpr typename Args::template get_elem< Idx >::type operator()(
-            const First f, const Super x) {
-            return x.template get< Idx - 1 >();
-        }
-    };
+    namespace _impl {
+        template < ushort_t Idx, typename Args, typename First, typename Super >
+        struct return_helper {
+            GT_FUNCTION constexpr typename Args::template get_elem< Idx >::type operator()(
+                const First f, const Super x) {
+                return x.template get< Idx - 1 >();
+            }
+        };
 
-    template <typename Args, typename First, typename Super >
-    struct return_helper< 0, Args, First, Super > {
-        GT_FUNCTION constexpr First operator()(const First f, const Super x) { return f; }
-    };
+        template < typename Args, typename First, typename Super >
+        struct return_helper< 0, Args, First, Super > {
+            GT_FUNCTION constexpr First operator()(const First f, const Super x) { return f; }
+        };
+    }
 
-
-    template <typename... Args >
+    template < typename... Args >
     struct tuple;
 
     template < typename First, typename... Args >
-    struct tuple<First, Args...> : public tuple< Args... > {
+    struct tuple< First, Args... > : public tuple< Args... > {
 
         typedef tuple< First, Args... > type;
         typedef variadic_typedef< First, Args... > args_t;
@@ -35,8 +36,7 @@ namespace gridtools {
            (in order to get assigned to the other dimensions).
            When this constructor is used all the arguments have to be specified and passed to the function call in
            order. No check is done on the order*/
-        GT_FUNCTION constexpr tuple(const First t, Args const... x)
-            : super(x...), m_offset(t) {}
+        GT_FUNCTION constexpr tuple(const First t, Args const... x) : super(x...), m_offset(t) {}
 
         GT_FUNCTION constexpr First operator()() { return m_offset; }
 
@@ -45,7 +45,7 @@ namespace gridtools {
         /**@brief returns the offset array*/
         GT_FUNCTION constexpr typename args_t::template get_elem< Idx >::type get() const {
 
-            typedef return_helper< Idx, args_t, First, super > helper;
+            typedef _impl::return_helper< Idx, args_t, First, super > helper;
             return helper()(m_offset, *this);
         }
 
@@ -53,15 +53,16 @@ namespace gridtools {
         First m_offset;
     };
 
-    template < typename First>
-    struct tuple<First> {
+    template < typename First >
+    struct tuple< First > {
 
         static const size_t n_dimensions = 1;
 
-        typedef tuple< First> type;
-        typedef variadic_typedef< First> args_t;
+        typedef tuple< First > type;
+        typedef variadic_typedef< First > args_t;
 
-        template<ushort_t Idx> struct get_elem{
+        template < ushort_t Idx >
+        struct get_elem {
             typedef typename args_t::template get_elem< Idx >::type type;
         };
 
@@ -70,15 +71,14 @@ namespace gridtools {
            (in order to get assigned to the other dimensions).
            When this constructor is used all the arguments have to be specified and passed to the function call in
            order. No check is done on the order*/
-        GT_FUNCTION constexpr tuple(const First t)
-            : m_offset(t) {}
+        GT_FUNCTION constexpr tuple(const First t) : m_offset(t) {}
 
         GT_FUNCTION constexpr First operator()() { return m_offset; }
 
         /**@brief returns the offset at a specific index Idx*/
         template < ushort_t Idx >
         GT_FUNCTION constexpr typename get_elem< Idx >::type get() const {
-            GRIDTOOLS_STATIC_ASSERT((Idx==0), "Error: out of bound tuple access");
+            GRIDTOOLS_STATIC_ASSERT((Idx == 0), "Error: out of bound tuple access");
             return m_offset;
         }
 
@@ -86,14 +86,15 @@ namespace gridtools {
         First m_offset;
     };
 
-    template<typename T> struct is_tuple : boost::mpl::false_{};
+    template < typename T >
+    struct is_tuple : boost::mpl::false_ {};
 
-    template <typename... Args >
-    struct is_tuple<tuple<Args...> > : boost::mpl::true_{};
+    template < typename... Args >
+    struct is_tuple< tuple< Args... > > : boost::mpl::true_ {};
 
-    template<typename ... Args>
-    tuple<Args...> make_tuple(Args ... args) {
-        return tuple<Args...>(args...);
+    template < typename... Args >
+    tuple< Args... > make_tuple(Args... args) {
+        return tuple< Args... >(args...);
     }
 
 } // namespace gridtools
