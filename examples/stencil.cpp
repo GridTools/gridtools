af11c874b28e6d12100d20939ba2ccfd005f6d5d
Juraj Kardos <juraj.kardos@usi.ch>
2016-05-03 08:58:39 +0200

    Initial version of 3d 7pt stencil

---
+++ examples/stencil.cpp
@@ -0,0 +1,17 @@
+#include "stencil.hpp"
+
+int main(int argc, char** argv)
+{
+#ifdef CXX11_ENABLED
+    if (argc != 5) {
+        std::cout << "Usage: interface1_<whatever> dimx dimy dimz nt\n where args are integer sizes of the data fields and number of time iterations" << std::endl;
+        return 1;
+    }
+
+    return cg::solver(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+
+#else
+    assert(false);
+    return -1;
+#endif
+}
