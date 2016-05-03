8c17e1738aa07d70d9e49e5f32ed88ddfb8395f9
Mauro Bianco <mbianco@cscs.ch>
2016-05-03 16:16:21 +0200

    re-introducing a check that misteriously disappeared

--- include/stencil-composition/structured_grids/esf.hpp
+++ include/stencil-composition/structured_grids/esf.hpp
@@ -75,6 +75,10 @@ namespace gridtools {
             "(NOTE: the \'global_accessor\' types are excluded from this list). Example: \n\n using v1=accessor<0>; \n "
             "using v2=generic_accessor<1, enumtype::in>; \n using v3=accessor<2>; \n using "
             "arg_list=boost::mpl::vector<v1, v3>;");
+
+        GRIDTOOLS_STATIC_ASSERT(_impl::check_arg_list<typename esf_function::arg_list>::value,
+                                "Arg List of functor is not listed by increasing index");
+
         // checking that all the placeholders have a different index
         /**
          * \brief Get a sequence of the same type as original_placeholders, containing the indexes relative to the
