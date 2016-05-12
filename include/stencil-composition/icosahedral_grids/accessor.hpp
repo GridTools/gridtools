fb2686a3b61e8324e3f8a971762b9be940b9ba57
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-05-12 12:00:23 +0200

    make icosahedral accessor inherit from accessor base, in order to provide access to offsets evaluation

--- include/stencil-composition/icosahedral_grids/accessor.hpp
+++ include/stencil-composition/icosahedral_grids/accessor.hpp
@@ -1,13 +1,18 @@
 #pragma once
 #include "extent.hpp"
+#include "../accessor_base.hpp"
 
 namespace gridtools {
     /**
     * This is the type of the accessors accessed by a stencil functor.
     * It's a pretty minima implementation.
     */
-    template < uint_t ID, enumtype::intend Intend, typename LocationType, typename Extent = extent< 0 > >
-    struct accessor {
+    template < uint_t ID,
+        enumtype::intend Intend,
+        typename LocationType,
+        typename Extent = extent< 0 >,
+        ushort_t FieldDimensions = 3 >
+    struct accessor : public accessor_base< ID, Intend, Extent, FieldDimensions > {
         GRIDTOOLS_STATIC_ASSERT((is_location_type< LocationType >::value), "Error: wrong type");
         using type = accessor< ID, Intend, LocationType, Extent >;
         using location_type = LocationType;
@@ -17,22 +22,16 @@ namespace gridtools {
         location_type location() const { return location_type(); }
     };
 
-    template < uint_t ID, typename LocationType, typename Extent = extent< 0 > >
-    using in_accessor = accessor< ID, enumtype::in, LocationType, Extent >;
+    template < uint_t ID, typename LocationType, typename Extent = extent< 0 >, ushort_t FieldDimensions = 3 >
+    using in_accessor = accessor< ID, enumtype::in, LocationType, Extent, FieldDimensions >;
 
-    template < uint_t ID, typename LocationType >
-    using inout_accessor = accessor< ID, enumtype::inout, LocationType, extent< 0 > >;
+    template < uint_t ID, typename LocationType, ushort_t FieldDimensions = 3 >
+    using inout_accessor = accessor< ID, enumtype::inout, LocationType, extent< 0 >, FieldDimensions >;
 
     template < typename T >
     struct is_accessor : boost::mpl::false_ {};
 
-    template < uint_t ID, enumtype::intend Intend, typename LocationType, typename Extent >
-    struct is_accessor< accessor< ID, Intend, LocationType, Extent > > : boost::mpl::true_ {};
+    template < uint_t ID, enumtype::intend Intend, typename LocationType, typename Extent, ushort_t FieldDimensions >
+    struct is_accessor< accessor< ID, Intend, LocationType, Extent, FieldDimensions > > : boost::mpl::true_ {};
 
-    /**
-    * Struct to test if an argument is a temporary
-    */
-    // TODO for the moment we dont support temporaries
-    template < typename T >
-    struct is_plchldr_to_temp : boost::mpl::false_ {};
 } // namespace gridtools
