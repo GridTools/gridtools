f29f0715eabcdf29160c5518ea1bf45389ee6d20
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-05-13 20:42:50 +0200

    fixes a workaround for CUDA with double variadic pack

--- include/common/generic_metafunctions/repeat_template.hpp
+++ include/common/generic_metafunctions/repeat_template.hpp
@@ -28,11 +28,24 @@ namespace gridtools {
         template < typename UInt, UInt C >
         struct expand_to_gt_integer_sequence< UInt, C, 1 > : gt_integer_sequence< UInt, C > {};
 
-        template < typename Seq, typename Value, template < Value... > class Lambda, Value... InitialValues >
+        template < typename Seq, template < ushort_t... > class Lambda, ushort_t... InitialValues>
         struct expand_recursively;
 
+        template < template < ushort_t... > class Lambda, ushort_t... Ints, ushort_t... InitialValues >
+        struct expand_recursively< gt_integer_sequence< ushort_t, Ints... >, Lambda, InitialValues...> {
+            typedef Lambda< InitialValues..., Ints... > type;
+        };
+
+    }
+
+#if defined(CXX11_ENABLED) && !defined(__CUDACC__)
+
+    namespace _impl {
+        template < typename Seq, typename Value, template < Value... > class Lambda, Value... InitialValues >
+        struct expand_recursively_v;
+
         template < typename Value, template < Value... > class Lambda, Value... Ints, Value... InitialValues >
-        struct expand_recursively< gt_integer_sequence< Value, Ints... >, Value, Lambda, InitialValues... > {
+        struct expand_recursively_v< gt_integer_sequence< Value, Ints... >, Value, Lambda, InitialValues... > {
             typedef Lambda< InitialValues..., Ints... > type;
         };
     }
@@ -47,9 +60,13 @@ namespace gridtools {
        \endverbatim
        Optionally a set of initial values to start filling the template class can be passed
      */
-    template < typename Constant, typename Length, typename Value, template < Value... T > class Lambda, Value... InitialValues >
-    struct repeat_template {
-        typedef typename _impl::expand_recursively<
+    template < typename Constant,
+        typename Length,
+        typename Value,
+        template < Value... T > class Lambda,
+        Value... InitialValues >
+    struct repeat_template_v {
+        typedef typename _impl::expand_recursively_v<
             typename _impl::expand_to_gt_integer_sequence< Value, Constant::value, Length::value >::type,
             Value,
             Lambda,
@@ -62,17 +79,30 @@ namespace gridtools {
        Usage example:
        I have a class template halo< .... >, and I want to fill it by repeating N times the same number H
        \verbatim
-       repeat_template_c<H, N, halo>
+       repeat_template_v_c<H, N, halo>
        \endverbatim
        Optionally a set of initial values to start filling the template class can be passed
     */
-    template < ushort_t Constant, ushort_t Length, typename Value, template < Value... T > class Lambda, Value... InitialValues >
-    struct repeat_template_c {
-        typedef typename _impl::expand_recursively<
+    template < ushort_t Constant,
+        ushort_t Length,
+        typename Value,
+        template < Value... T > class Lambda,
+        Value... InitialValues >
+    struct repeat_template_v_c {
+        typedef typename _impl::expand_recursively_v<
             typename _impl::expand_to_gt_integer_sequence< Value, Constant, Length >::type,
             Value,
             Lambda,
             InitialValues... >::type type;
     };
+#endif
+    template < ushort_t Constant, ushort_t Length, template < ushort_t... T > class Lambda, ushort_t ... InitialValues >
+    struct repeat_template_c {
+        typedef typename _impl::expand_recursively<
+            typename _impl::expand_to_gt_integer_sequence< ushort_t, Constant, Length >::type,
+            Lambda, InitialValues...>::type type;
+    };
+
+
 
 } // namespace gridtools
--- unit_tests/common/test_repeat_template.cpp
+++ unit_tests/common/test_repeat_template.cpp
@@ -5,17 +5,17 @@
 using namespace gridtools;
 
 TEST(test_common_metafunctions, test_repeat_template) {
-    typedef repeat_template< static_int< 5 >, static_int< 3 >, uint_t, halo >::type test1;
+    typedef repeat_template_v< static_int< 5 >, static_int< 3 >, uint_t, halo >::type test1;
     GRIDTOOLS_STATIC_ASSERT((boost::is_same< test1, halo< 5, 5, 5 > >::type::value), "internal error");
 
-    typedef typename repeat_template_c< 5, 3, uint_t, halo >::type test2;
+    typedef typename repeat_template_v_c< 5, 3, uint_t, halo >::type test2;
     GRIDTOOLS_STATIC_ASSERT((boost::is_same< test2, halo< 5, 5, 5 > >::type::value), "internal error");
 }
 
 TEST(test_common_metafunctions, test_repeat_template_with_initial_values) {
-    typedef repeat_template< static_int< 5 >, static_int< 3 >, uint_t, halo, 4, 7, 8 >::type test1;
+    typedef repeat_template_v< static_int< 5 >, static_int< 3 >, uint_t, halo, 4, 7, 8 >::type test1;
     GRIDTOOLS_STATIC_ASSERT((boost::is_same< test1, halo< 4, 7, 8, 5, 5, 5 > >::type::value), "internal error");
 
-    typedef typename repeat_template_c< 5, 3, uint_t, halo, 4, 7, 8 >::type test2;
+    typedef typename repeat_template_v_c< 5, 3, uint_t, halo, 4, 7, 8 >::type test2;
     GRIDTOOLS_STATIC_ASSERT((boost::is_same< test2, halo< 4, 7, 8, 5, 5, 5 > >::type::value), "internal error");
 }
