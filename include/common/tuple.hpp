d753e1a41acb065146b08b645a0d156e0a5d263c
Carlos Osuna <Carlos.Osuna.Escamilla@gmail.com>
2016-05-18 14:21:31 +0200

    renaming and add test tuple to list of common tests

--- include/common/tuple.hpp
+++ include/common/tuple.hpp
@@ -1,3 +1,6 @@
+/**
+ * Implementation of a tuple with constexpr ctr and random element getter
+ */
 #pragma once
 #include "host_device.hpp"
 #include "generic_metafunctions/variadic_typedef.hpp"
@@ -5,16 +8,16 @@
 namespace gridtools {
 
     namespace _impl {
-        template < ushort_t Idx, typename Args, typename First, typename Super >
+        template < ushort_t Idx, typename VariadicArgs, typename First, typename Super >
         struct return_helper {
-            GT_FUNCTION constexpr typename Args::template get_elem< Idx >::type operator()(
+            GT_FUNCTION constexpr typename VariadicArgs::template get_elem< Idx >::type operator()(
                 const First f, const Super x) {
                 return x.template get< Idx - 1 >();
             }
         };
 
-        template < typename Args, typename First, typename Super >
-        struct return_helper< 0, Args, First, Super > {
+        template < typename VariadicArgs, typename First, typename Super >
+        struct return_helper< 0, VariadicArgs, First, Super > {
             GT_FUNCTION constexpr First operator()(const First f, const Super x) { return f; }
         };
     }
@@ -22,68 +25,59 @@ namespace gridtools {
     template < typename... Args >
     struct tuple;
 
-    template < typename First, typename... Args >
-    struct tuple< First, Args... > : public tuple< Args... > {
+    template < typename ElementType, typename... OtherElements >
+    struct tuple< ElementType, OtherElements... > : public tuple< OtherElements... > {
 
-        typedef tuple< First, Args... > type;
-        typedef variadic_typedef< First, Args... > args_t;
-        typedef tuple< Args... > super;
+        typedef tuple< ElementType, OtherElements... > type;
+        typedef variadic_typedef< ElementType, OtherElements... > tuple_elements_t;
+        typedef tuple< OtherElements... > super;
 
-        static const size_t n_dimensions = sizeof...(Args) + 1;
+        static const size_t n_dimensions = sizeof...(OtherElements) + 1;
 
-        /**@brief constructor taking an integer as the first argument, and then other optional arguments.
-           The integer gets assigned to the current extra dimension and the other arguments are passed to the base class
-           (in order to get assigned to the other dimensions).
-           When this constructor is used all the arguments have to be specified and passed to the function call in
-           order. No check is done on the order*/
-        GT_FUNCTION constexpr tuple(const First t, Args const... x) : super(x...), m_offset(t) {}
+        // ctr
+        GT_FUNCTION constexpr tuple(const ElementType t, OtherElements const... x) : super(x...), m_elem(t) {}
 
-        GT_FUNCTION constexpr First operator()() { return m_offset; }
+        GT_FUNCTION constexpr ElementType operator()() { return m_elem; }
 
         /**@brief returns the offset at a specific index Idx*/
         template < ushort_t Idx >
         /**@brief returns the offset array*/
-        GT_FUNCTION constexpr typename args_t::template get_elem< Idx >::type get() const {
+        GT_FUNCTION constexpr typename tuple_elements_t::template get_elem< Idx >::type get() const {
 
-            typedef _impl::return_helper< Idx, args_t, First, super > helper;
-            return helper()(m_offset, *this);
+            typedef _impl::return_helper< Idx, tuple_elements_t, ElementType, super > helper;
+            return helper()(m_elem, *this);
         }
 
       protected:
-        First m_offset;
+        ElementType m_elem;
     };
 
-    template < typename First >
-    struct tuple< First > {
+    template < typename ElementType >
+    struct tuple< ElementType > {
 
         static const size_t n_dimensions = 1;
 
-        typedef tuple< First > type;
-        typedef variadic_typedef< First > args_t;
+        typedef tuple< ElementType > type;
+        typedef variadic_typedef< ElementType > tuple_elements_t;
 
         template < ushort_t Idx >
         struct get_elem {
-            typedef typename args_t::template get_elem< Idx >::type type;
+            typedef typename tuple_elements_t::template get_elem< Idx >::type type;
         };
 
-        /**@brief constructor taking an integer as the first argument, and then other optional arguments.
-           The integer gets assigned to the current extra dimension and the other arguments are passed to the base class
-           (in order to get assigned to the other dimensions).
-           When this constructor is used all the arguments have to be specified and passed to the function call in
-           order. No check is done on the order*/
-        GT_FUNCTION constexpr tuple(const First t) : m_offset(t) {}
+        GT_FUNCTION constexpr tuple(const ElementType t) : m_elem(t) {}
 
-        GT_FUNCTION constexpr First operator()() { return m_offset; }
+        GT_FUNCTION constexpr ElementType operator()() { return m_elem; }
 
         /**@brief returns the offset at a specific index Idx*/
         template < ushort_t Idx >
         GT_FUNCTION constexpr typename get_elem< Idx >::type get() const {
             GRIDTOOLS_STATIC_ASSERT((Idx == 0), "Error: out of bound tuple access");
-            return m_offset;
+            return m_elem;
         }
 
       protected:
-        First m_offset;
+        ElementType m_elem;
     };
 
     template < typename T >
