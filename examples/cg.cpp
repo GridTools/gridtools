9d4b609d416deb881874aa39028c102e55e02b0d
Juraj Kardos <juraj.kardos@usi.ch>
2016-05-20 10:13:59 +0200

    Change in help message

--- examples/cg.cpp
+++ examples/cg.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 {
 #ifdef CXX11_ENABLED
     if (argc != 6) {
-        std::cout << "Usage: interface1_<whatever> dimx dimy dimz maxit eps\n where args are integer sizes of the data fields, max number of iterations and eps is required tolerance" << std::endl;
+        std::cout << "Usage: cg<whatever> dimx dimy dimz maxit eps\n where args are integer sizes of the data fields, max number of iterations and eps is required tolerance" << std::endl;
         return 1;
     }
 
