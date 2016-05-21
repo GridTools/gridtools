473c0904f4fa336ac42311ac94410c17d924da40
Xiaolin Guo <guoxi@student.ethz.ch>
2016-05-21 17:57:34 +0200

    evaluate approaches to div

--- examples/operator_examples/div.hpp
+++ examples/operator_examples/div.hpp
@@ -63,6 +63,51 @@ namespace operator_examples {
         }
     };
 
+    struct div_functor_reduction_into_scalar {
+        typedef in_accessor<0, icosahedral_topology_t::edges, extent<1> > in_edges;
+        typedef in_accessor<1, icosahedral_topology_t::cells, extent<1>, 5 > weights;
+        typedef inout_accessor<2, icosahedral_topology_t::cells> out_cells;
+        typedef boost::mpl::vector<in_edges, weights, out_cells> arg_list;
+
+        template<typename Evaluation>
+        GT_FUNCTION static void Do(Evaluation const &eval, x_interval)
+        {
+            typedef typename icgrid::get_grid_topology< Evaluation >::type grid_topology_t;
+
+            using edge_of_cells_dim = dimension< 5 >;
+            edge_of_cells_dim::Index edge;
+
+            double t{0.};
+            auto neighbors_offsets = connectivity< cells , edges >::offsets(eval.position()[1]);
+            ushort_t e=0;
+            for (auto neighbor_offset : neighbors_offsets) {
+                t += eval(in_edges(neighbor_offset)) * eval(weights(edge+e));
+                e++;
+            }
+            eval(out_cells()) = t;
+        }
+    };
+
+    struct div_functor_flow_convention {
+        typedef in_accessor<0, icosahedral_topology_t::edges, extent<1> > in_edges;
+        typedef in_accessor<1, icosahedral_topology_t::edges, extent<1> > edge_length;
+        typedef in_accessor<2, icosahedral_topology_t::cells, extent<1> > cell_area;
+        typedef inout_accessor<3, icosahedral_topology_t::cells> out_cells;
+        typedef boost::mpl::vector<in_edges, edge_length, cell_area, out_cells> arg_list;
+
+        template<typename Evaluation>
+        GT_FUNCTION static void Do(Evaluation const &eval, x_interval)
+        {
+            auto ff = [](const double _in1, const double _in2, const double _res) -> double { return _in1 * _in2 + _res; };
+
+            double t{0.};
+            t = eval(on_edges(ff, 0.0, in_edges(), edge_length())) / eval(cell_area());
+            if (eval.position()[1] == 1)
+                t = -t;
+            eval(out_cells()) = t;
+        }
+    };
+
     bool test_div( uint_t t_steps, char *mesh_file)
     {
         operator_examples::repository repository(mesh_file);
@@ -79,6 +124,17 @@ namespace operator_examples {
         const uint_t halo_k = repository.halo_k;
 
         typedef gridtools::layout_map<2, 1, 0> layout_t;
+
+        array<uint_t, 5> di = {halo_nc, halo_nc, halo_nc, d1 - halo_nc - 1, d1};
+        array<uint_t, 5> dj = {halo_mc, halo_mc, halo_mc, d2 - halo_mc - 1, d2};
+
+        gridtools::grid<axis, icosahedral_topology_t> grid_(icosahedral_grid, di, dj);
+        grid_.value_list[0] = 0;
+        grid_.value_list[1] = d3 - 1;
+
+        verifier ver(1e-10);
+
+        array<array<uint_t, 2>, 4> halos = {{{halo_nc, halo_nc}, {0, 0}, {halo_mc, halo_mc}, {halo_k, halo_k}}};
 
         using edge_storage_type = repository::edge_storage_type;
         using cell_storage_type = repository::cell_storage_type;
@@ -98,58 +154,131 @@ namespace operator_examples {
         out_cells.initialize(0.0);
         div_weights.initialize(0.0);
 
-        typedef arg<0, edge_storage_type> p_in_edges;
-        typedef arg<1, cell_storage_type> p_out_cells;
-        typedef arg<2, cell_storage_type> p_cell_area;
-        typedef arg<3, edges_of_cells_storage_type> p_orientation_of_normal;
-        typedef arg<4, edge_storage_type> p_edge_length;
-        typedef arg<5, edges_of_cells_storage_type> p_div_weights;
+        typedef arg<0, edge_storage_type> p_edge_length;
+        typedef arg<1, cell_storage_type> p_cell_area;
+        typedef arg<2, edges_of_cells_storage_type> p_orientation_of_normal;
+        typedef arg<3, edges_of_cells_storage_type> p_div_weights;
 
-        typedef boost::mpl::vector<p_in_edges, p_out_cells, p_cell_area, p_orientation_of_normal, p_edge_length, p_div_weights>
+        typedef arg<4, edge_storage_type> p_in_edges;
+        typedef arg<5, cell_storage_type> p_out_cells;
+
+        typedef boost::mpl::vector< p_edge_length, p_cell_area, p_orientation_of_normal, p_div_weights, p_in_edges, p_out_cells>
             accessor_list_t;
 
         gridtools::domain_type<accessor_list_t> domain(
-            boost::fusion::make_vector(&in_edges, &out_cells, &cell_area, &orientation_of_normal, &edge_length, &div_weights));
-        array<uint_t, 5> di = {halo_nc, halo_nc, halo_nc, d1 - halo_nc - 1, d1};
-        array<uint_t, 5> dj = {halo_mc, halo_mc, halo_mc, d2 - halo_mc - 1, d2};
+            boost::fusion::make_vector(&edge_length, &cell_area, &orientation_of_normal, &div_weights, &in_edges, &out_cells));
 
-        gridtools::grid<axis, icosahedral_topology_t> grid_(icosahedral_grid, di, dj);
-        grid_.value_list[0] = 0;
-        grid_.value_list[1] = d3 - 1;
-
-        auto stencil_ = gridtools::make_computation<backend_t>(
+        auto stencil_prep = gridtools::make_computation<backend_t>(
             domain,
             grid_,
             gridtools::make_mss // mss_descriptor
                 (execute<forward>(),
                  gridtools::make_esf<div_prep_functor, icosahedral_topology_t, icosahedral_topology_t::cells>(
-                         p_edge_length(), p_cell_area(), p_orientation_of_normal(), p_div_weights()),
-                 gridtools::make_esf<div_functor, icosahedral_topology_t, icosahedral_topology_t::cells>(
-                         p_in_edges(), p_div_weights(), p_out_cells())
+                         p_edge_length(), p_cell_area(), p_orientation_of_normal(), p_div_weights())
                 )
         );
-        stencil_->ready();
-        stencil_->steady();
-        stencil_->run();
+        stencil_prep->ready();
+        stencil_prep->steady();
+        stencil_prep->run();
 
     #ifdef __CUDACC__
         out_edges.d2h_update();
             in_edges.d2h_update();
     #endif
 
-        verifier ver(1e-10);
+        /*
+         * stencil of div
+         */
 
-        array<array<uint_t, 2>, 4> halos = {{{halo_nc, halo_nc}, {0, 0}, {halo_mc, halo_mc}, {halo_k, halo_k}}};
+        auto stencil_ = gridtools::make_computation<backend_t>(
+                domain,
+                grid_,
+                gridtools::make_mss // mss_descriptor
+                        (execute<forward>(),
+                         gridtools::make_esf<div_functor, icosahedral_topology_t, icosahedral_topology_t::cells>(
+                                 p_in_edges(), p_div_weights(), p_out_cells())
+                        )
+        );
+        stencil_->ready();
+        stencil_->steady();
+        stencil_->run();
+
+#ifdef __CUDACC__
+        out_edges.d2h_update();
+            in_edges.d2h_update();
+#endif
+
         bool result = ver.verify(grid_, ref_cells, out_cells, halos);
 
-    #ifdef BENCHMARK
+#ifdef BENCHMARK
         for (uint_t t = 1; t < t_steps; ++t)
         {
             stencil_->run();
         }
         stencil_->finalize();
-        std::cout << stencil_->print_meter() << std::endl;
-    #endif
+        std::cout << "div: "<< stencil_->print_meter() << std::endl;
+#endif
+
+        /*
+         * stencil of div reduction into scalar
+         */
+        auto stencil_reduction_into_scalar = gridtools::make_computation<backend_t>(
+                domain,
+                grid_,
+                gridtools::make_mss // mss_descriptor
+                        (execute<forward>(),
+                         gridtools::make_esf<div_functor_reduction_into_scalar, icosahedral_topology_t, icosahedral_topology_t::cells>(
+                                 p_in_edges(), p_div_weights(), p_out_cells())
+                        )
+        );
+        stencil_reduction_into_scalar->ready();
+        stencil_reduction_into_scalar->steady();
+        stencil_reduction_into_scalar->run();
+
+        result = result && ver.verify(grid_, ref_cells, out_cells, halos);
+
+#ifdef BENCHMARK
+        for (uint_t t = 1; t < t_steps; ++t)
+        {
+            stencil_reduction_into_scalar->run();
+        }
+        stencil_reduction_into_scalar->finalize();
+        std::cout << "reduction into scalar: " << stencil_reduction_into_scalar->print_meter() << std::endl;
+#endif
+
+        /*
+         * stencil of div flow convention
+         */
+
+        auto stencil_flow_convention = gridtools::make_computation<backend_t>(
+                domain,
+                grid_,
+                gridtools::make_mss // mss_descriptor
+                        (execute<forward>(),
+                         gridtools::make_esf<div_functor_flow_convention, icosahedral_topology_t, icosahedral_topology_t::cells>(
+                                 p_in_edges(), p_edge_length(), p_cell_area(), p_out_cells())
+                        )
+        );
+        stencil_flow_convention->ready();
+        stencil_flow_convention->steady();
+        stencil_flow_convention->run();
+
+#ifdef __CUDACC__
+        out_edges.d2h_update();
+            in_edges.d2h_update();
+#endif
+
+        result = result && ver.verify(grid_, ref_cells, out_cells, halos);
+
+#ifdef BENCHMARK
+        for (uint_t t = 1; t < t_steps; ++t)
+        {
+            stencil_flow_convention->run();
+        }
+        stencil_flow_convention->finalize();
+        std::cout << "flow convention: "<< stencil_flow_convention->print_meter() << std::endl;
+#endif
+
 
         return result;
     }
