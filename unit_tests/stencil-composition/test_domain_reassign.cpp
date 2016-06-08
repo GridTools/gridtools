fad866f6938b0732974f90bae6cd4cae62e67f34
Paolo Crosetto <crosetto@greina17.cm.cluster>
2016-06-08 15:47:38 +0200

    adding forgotten file

---
+++ unit_tests/stencil-composition/test_domain_reassign.cpp
@@ -0,0 +1,37 @@
+#include "gtest/gtest.h"
+#include "test_domain_reassign.hpp"
+#include "../../examples/Options.hpp"
+
+int main(int argc, char** argv)
+{
+
+    // Pass command line arguments to googltest
+    ::testing::InitGoogleTest(&argc, argv);
+
+    if (argc < 4) {
+        printf( "Usage: test_domain_reassign_<whatever> dimx dimy dimz\n where args are integer sizes of the data fields\n" );
+        return 1;
+    }
+
+    for(int i=0; i!=3; ++i) {
+        Options::getInstance().m_size[i] = atoi(argv[i+1]);
+    }
+
+    if(argc==5) {
+        Options::getInstance().m_size[3] = atoi(argv[4]);
+    }
+
+    return RUN_ALL_TESTS();
+}
+
+
+TEST(ReassignDomain, Test)
+{
+    uint_t x = Options::getInstance().m_size[0];
+    uint_t y = Options::getInstance().m_size[1];
+    uint_t z = Options::getInstance().m_size[2];
+    uint_t t = Options::getInstance().m_size[3];
+    if(t==0) t=1;
+
+    ASSERT_TRUE(domain_reassign::test(x, y, z, t));
+}
