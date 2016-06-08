12b03525dad670cf3143f974644f37f9d5f47357
Paolo Crosetto <crosetto@greina17.cm.cluster>
2016-06-08 15:59:40 +0200

    removing a definition of accessor with a negative index

--- unit_tests/stencil-composition/structured_grids/test_accessor.cpp
+++ unit_tests/stencil-composition/structured_grids/test_accessor.cpp
@@ -8,7 +8,6 @@
 TEST(accessor, is_accessor) {
     using namespace gridtools;
     GRIDTOOLS_STATIC_ASSERT((is_accessor<accessor<6, enumtype::inout, extent<3,4,4,5> > >::value) == true, "");
-    GRIDTOOLS_STATIC_ASSERT((is_accessor<accessor<-2,  enumtype::in> >::value) == true, "");
     GRIDTOOLS_STATIC_ASSERT((is_accessor<int>::value) == false, "");
     GRIDTOOLS_STATIC_ASSERT((is_accessor<double&>::value) == false, "");
     GRIDTOOLS_STATIC_ASSERT((is_accessor<double const&>::value) == false, "");
