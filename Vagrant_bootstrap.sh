f821e58ab89c2a16dd0b5850e03bb6242b2d67e7
Lucas Benedicic <benedicic@cscs.ch>
2016-06-13 16:43:15 +0200

    Display verbose information while bootstrapping the Vagrant image.-

--- Vagrant_bootstrap.sh
+++ Vagrant_bootstrap.sh
@@ -1,72 +1,54 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+HOME_DIR=/home/vagrant
+VENV_DIR=${HOME_DIR}/venv
 
 #
-# install needed Python packages
+# install the required dependencies
 #
 apt-get update
-apt-get install -y cmake                \
+apt-get install -y chromium             \
+                   cmake                \
                    libfreetype6-dev     \
                    libpng12-dev         \
                    pkg-config           \
                    python3              \
                    python3-dev          \
-                   python-virtualenv    \
                    qt4-bin-dbg          \
-                   qt-sdk
+                   qt-sdk               \
+                   virtualenv           \
+                --no-install-recommends
+
 #
 # create the virtual environment
 #
-cd /home/vagrant
-virtualenv --python=/usr/bin/python3.4 venv
-source venv/bin/activate
+virtualenv --python=/usr/bin/python3.4 ${VENV_DIR}
+source ${VENV_DIR}/bin/activate
 pip install --upgrade pip distribute
 
 #
 # install Gridtools dependencies
 #
-pip install -r /vagrant/python/requirements.txt
+pip --verbose install -r /vagrant/python/requirements.txt
 
 #
 # set the backend for matplotlib to Qt4/PySide
 #
-cat ./venv/lib/python3.4/site-packages/matplotlib/mpl-data/matplotlibrc | sed -e 's/^backend.*: pyside/backend : Qt4Agg/g' | sed -e 's/^#backend.qt4.*/backend.qt4 : PySide/g' > /tmp/.matplotlibrc
-cp /tmp/.matplotlibrc ./venv/lib/python3.4/site-packages/matplotlib/mpl-data/matplotlibrc
+cat ${VENV_DIR}/lib/python3.4/site-packages/matplotlib/mpl-data/matplotlibrc | sed -e 's/^backend.*: pyside/backend : Qt4Agg/g' | sed -e 's/^#backend.qt4.*/backend.qt4 : PySide/g' > /tmp/.matplotlibrc
+cp /tmp/.matplotlibrc ${VENV_DIR}/lib/python3.4/site-packages/matplotlib/mpl-data/matplotlibrc
 
 #
 # change virtualenv ownership to regular user
 #
 deactivate
-chown --recursive vagrant:vagrant /home/vagrant/venv
-
-#
-# environment variables setup for regular user
-#
-echo "#                                             " >> /home/vagrant/.bashrc
-echo "# environment setup for Gridtools development " >> /home/vagrant/.bashrc
-echo "#                                             " >> /home/vagrant/.bashrc
-echo "export CXX=/usr/bin/g++                       " >> /home/vagrant/.bashrc
-echo "export BOOST_ROOT=/usr/local                  " >> /home/vagrant/.bashrc
-echo "export CUDATOOLKIT_HOME=/usr/local/cuda-7.0   " >> /home/vagrant/.bashrc
-echo "export GRIDTOOLS_ROOT=/vagrant                " >> /home/vagrant/.bashrc
-echo "export LD_LIBRARY_PATH=/usr/local/lib         " >> /home/vagrant/.bashrc
-echo "export PATH=${PATH}:${CUDATOOLKIT_HOME}/bin   " >> /home/vagrant/.bashrc
-
-#
-# remove unused packages
-#
-apt-get remove --purge -y libfreetype6-dev  \
-                          libpng12-dev      \
-                          python3-dev       \
-                          qt-sdk            \
-                          xterm
-apt-get autoremove -y
+chown --recursive vagrant:vagrant ${VENV_DIR}
 
 #
 # install CUDA 7.0
 #
 wget -q -O cuda_7.0.run http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run
 chmod +x cuda_7.0.run
-./cuda_7.0.run --verbose --samples --samplespath=/home/vagrant --silent --toolkit
+./cuda_7.0.run --verbose --samples --samplespath=${HOME_DIR} --silent --toolkit
 rm cuda_7.0.run /tmp/cuda?install*
 
 #
@@ -80,3 +62,16 @@ cd boost_1_58_0
 ./b2 install
 cd ..
 rm -rf boost_1_58_0 boost_1_58_0.tar.gz
+
+#
+# environment setup for regular user
+#
+echo "#                                             " >> ${HOME_DIR}/.bashrc
+echo "# environment setup for Gridtools development " >> ${HOME_DIR}/.bashrc
+echo "#                                             " >> ${HOME_DIR}/.bashrc
+echo "export CXX=/usr/bin/g++                       " >> ${HOME_DIR}/.bashrc
+echo "export BOOST_ROOT=/usr/local                  " >> ${HOME_DIR}/.bashrc
+echo "export CUDATOOLKIT_HOME=/usr/local/cuda-7.0   " >> ${HOME_DIR}/.bashrc
+echo "export GRIDTOOLS_ROOT=/vagrant                " >> ${HOME_DIR}/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/lib         " >> ${HOME_DIR}/.bashrc
+echo "export PATH=${PATH}:${CUDATOOLKIT_HOME}/bin   " >> ${HOME_DIR}/.bashrc
