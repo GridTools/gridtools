a17ffb34fb31a61f0be4e8f57e83cbc4d058d8df
Carlos Osuna <carlos.osuna@env.ethz.ch>
2016-06-22 11:34:27 +0200

    fix after clang-format

--- include/stencil-composition/caches/define_caches_cxx03.hpp
+++ include/stencil-composition/caches/define_caches_cxx03.hpp
@@ -17,14 +17,14 @@ namespace gridtools {
 #define _DEFINE_CACHE(z, n, nil)                                                                                       \
     template < BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), typename T) >                                                     \
         typename flatten< BOOST_PP_CAT(boost::mpl::vector, BOOST_PP_INC(n)) <                                          \
-                          BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), T) >>                                                  \
+                          BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), T) > >                                                 \
         ::type define_caches(BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), T)) {                                               \
         GRIDTOOLS_STATIC_ASSERT((is_sequence_of< typename flatten< BOOST_PP_CAT(boost::mpl::vector, BOOST_PP_INC(n)) < \
                                                                    BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), T) > >::type, \
                                     is_cache > ::value),                                                               \
             "argument provided to define_caches construct is not of the type cache");                                  \
         typedef typename flatten< BOOST_PP_CAT(boost::mpl::vector, BOOST_PP_INC(n)) <                                  \
-                                  BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), T) >> ::type res_type;                         \
+                                  BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), T) > > ::type res_type;                        \
         return res_type();                                                                                             \
     }
 
