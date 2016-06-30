e6a6832dcff79f3faf47bdc9f576081345612453
Stefan Moosbrugger <stefanmoosbrugger@gmail.com>
2016-06-30 11:53:56 +0200

    update alignment test, get_size method for wrap_pointer (to keep consistency with hybrid_pointer)

--- unit_tests/storage/test_aligned_index.cpp
+++ unit_tests/storage/test_aligned_index.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
 #include <iostream>
+#include <vector>
 #include <storage/storage-facility.hpp>
 
 using namespace gridtools;
@@ -30,12 +31,17 @@ TEST(aligned_index_test, unaligned_access) {
             data(i,j) = bla++;
         }
     }
-    print_me(data, 3);
+    // print_me(data, 3);
+    // validity check
+    double values[9] = {0, 3, 6, 1, 4, 7, 2, 5, 8};
+    for(int i=0; i<9; ++i) { 
+        EXPECT_TRUE(((*data.fields_view())[i] == values[i]));
+    }     
 }
 
 
 TEST(aligned_index_test, aligned_access) {
-    typedef meta_storage< meta_storage_aligned< meta_storage_base<0, layout, false>, aligned<8>, halo<2,0> > > meta_storage_t;
+    typedef meta_storage< meta_storage_aligned< meta_storage_base<0, layout, false>, aligned<4>, halo<1,0> > > meta_storage_t;
     typedef storage< base_storage< wrap_pointer< double, true >, meta_storage_t, 1 > > storage_t;
     meta_storage_t meta(3,3);
     storage_t data(meta, 0.0, "data");
@@ -46,6 +52,11 @@ TEST(aligned_index_test, aligned_access) {
             data(i,j) = bla++;
         }
     }
-    print_me(data, 16);    
+    // print_me(data, 8);
+    // validity check, tests for correct alignment
+    double values[24] = {0, 0, 0, 0, 3, 6, 0, 0, 0, 0, 0, 1, 4, 7, 0, 0, 0, 0, 0, 2, 5, 8, 0, 0};
+    for(int i=0; i<24; ++i) { 
+        EXPECT_TRUE(((*data.fields_view())[i] == values[i]));
+    }     
 }
 
