57897810ba5445c826dedd91e11685cb13fd7a09
carlos osuna <carlos.osuna@env.ethz.ch>
2016-07-19 11:41:16 +0200

    adapt check for extent in iterate_domain_cuda

--- include/stencil-composition/icosahedral_grids/backend_cuda/iterate_domain_cuda.hpp
+++ include/stencil-composition/icosahedral_grids/backend_cuda/iterate_domain_cuda.hpp
@@ -107,8 +107,9 @@ namespace gridtools {
          */
         template < typename Extent >
         GT_FUNCTION bool is_thread_in_domain() const {
-            return (m_thread_pos[0] >= -Extent::value && m_thread_pos[0] < ((int)m_block_size_i + Extent::value) &&
-                    m_thread_pos[1] >= -Extent::value && m_thread_pos[1] < ((int)m_block_size_j + Extent::value));
+            GRIDTOOLS_STATIC_ASSERT((is_extent<Extent>::value), "Error");
+            return (m_thread_pos[0] >= -Extent::iminus::value && m_thread_pos[0] < ((int)m_block_size_i + Extent::iplus::value) &&
+                    m_thread_pos[1] >= -Extent::jminus::value && m_thread_pos[1] < ((int)m_block_size_j + Extent::jplus::value));
         }
 
         GT_FUNCTION
