d1921af13673c59ff0588c688203187f4fcad3a1
Paolo Crosetto <crosetto@greina0.cm.cluster>
2016-07-21 17:46:36 +0200

    compiling test only for CXX11

--- unit_tests/stencil-composition/structured_grids/test_domain_reassign.hpp
+++ unit_tests/stencil-composition/structured_grids/test_domain_reassign.hpp
@@ -38,11 +38,9 @@ namespace domain_reassign {
     typedef gridtools::layout_map< 0, 1, 2 > layout_t; // stride 1 on k
 #endif
 
-    // This is the definition of the special regions in the "vertical" direction
     typedef gridtools::interval< level< 0, -1 >, level< 1, -1 > > x_interval;
     typedef gridtools::interval< level< 0, -2 >, level< 1, 1 > > axis;
 
-    // These are the stencil operators that compose the multistage stencil in this test
     struct test_functor {
 
         typedef accessor< 0, enumtype::in, extent<>, 3 > in;
@@ -55,9 +53,6 @@ namespace domain_reassign {
         }
     };
 
-    /*
-     * The following operators and structs are for debugging only
-     */
     std::ostream &operator<<(std::ostream &s, test_functor const) { return s << "test_functor"; }
 
     void handle_error(int_t) { std::cout << "error" << std::endl; }
@@ -100,33 +95,15 @@ namespace domain_reassign {
         typedef arg< 1, storage_type > p_out;
 
         typedef boost::mpl::vector< p_in, p_out > accessor_list;
-        // construction of the domain. The domain is the physical domain of the problem, with all the physical fields
-        // that are used, temporary and not
-        // It must be noted that the only fields to be passed to the constructor are the non-temporary.
-        // The order in which they have to be passed is the order in which they appear scanning the placeholders in
-        // order. (I don't particularly like this)
+
         gridtools::aggregator_type< accessor_list > domain((p_in() = in), (p_out() = out));
 
-        // Definition of the physical dimensions of the problem.
-        // The constructor takes the horizontal plane dimensions,
-        // while the vertical ones are set according the the axis property soon after
-        // gridtools::grid<axis> grid(2,d1-2,2,d2-2);
         uint_t di[5] = {0, 0, 0, d1 - 1, d1};
         uint_t dj[5] = {0, 0, 0, d2 - 1, d2};
 
         gridtools::grid< axis > grid(di, dj);
         grid.value_list[0] = 0;
         grid.value_list[1] = d3 - 1;
-
-        /*
-          Here we do lot of stuff
-          1) We pass to the intermediate representation ::run function the description
-          of the stencil, which is a multi-stage stencil (mss)
-          The mss includes (in order of execution) a laplacian, two fluxes which are independent
-          and a final step that is the out_function
-          2) The logical physical domain with the fields to use
-          3) The actual domain dimensions
-        */
 
         auto copy = gridtools::make_computation< gridtools::BACKEND >(
             domain,
