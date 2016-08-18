1981d51d0e2e2b37ff1abe706b99acbbdff524d0
Alberto Madonna <madonna@cscs.ch>
2016-08-18 10:15:08 +0200

    Split Nose tests in 4 bash background processes
    PIDs are collected to gather and combine exit statuses.
    Also, the kill commands for all processes are connected to the Interrupt Signal, so test execution can be interrupted with Ctrl-C.

--- python/python_run_tests.sh
+++ python/python_run_tests.sh
@@ -59,17 +59,51 @@ if [ "$DISABLE_GPU" == true ] ; then
   echo "Excluding GPU tests"
 fi
 
-#
-# Run the tests
-# The command is created as a string and executed using "eval" to avoid some
-# obscure string conversion mechanisms that cause it to fail otherwise
-#
-echo "Running Python tests ..."
-NOSE_CMD="nosetests -v -s ${NOSE_NO_GPU} tests.test_stencils tests.test_ifstatement \
-          tests.test_invalid_stencils tests.test_sw tests.test_gtdd tests.test_fastwaves"
-eval "$NOSE_CMD"
+# Create commands as strings to be executed using "eval" to avoid some
+# obscure string conversion mechanisms that cause them to fail otherwise
+NOSE_CMD[1]="nosetests -v -s ${NOSE_NO_GPU} tests.test_stencils"
+NOSE_CMD[2]="nosetests -v -s ${NOSE_NO_GPU} tests.test_ifstatement tests.test_fastwaves"
+NOSE_CMD[3]="nosetests -v -s ${NOSE_NO_GPU} tests.test_sw"
+NOSE_CMD[4]="nosetests -v -s ${NOSE_NO_GPU} tests.test_gtdd tests.test_invalid_stencils"
 
-TEST_STATUS=$?
+#
+# Run the tests using multiple background processes
+# The PID of each process is collected to check the return value and to
+# build a string with the kill commands for all processes
+#
+KILL_STRING=""
+echo "Running Python tests ..."
+for i in `seq ${#NOSE_CMD[@]}`;
+do
+    eval "${NOSE_CMD[$i]} &"
+    PIDS[$i]=$!
+    KILL_STRING="$KILL_STRING kill ${PIDS[$i]};"
+done
+
+#
+# Connect the execution of the KILL_STRING with the reception
+# of the Interrupt Signal (SIGINT), sent with Ctrl-C.
+#
+trap "${KILL_STRING}" SIGINT
+
+#
+# Wait for processes to finish and increment status counter if any of them returns
+# non-zero
+#
+TEST_STATUS=0
+for job in "${PIDS[@]}"
+do
+    wait $job || let "TEST_STATUS+=1"
+done
+
+#
+# Clear commands bound to the Interrupt Signal
+#
+trap - SIGINT
+
+#
+# Check tests return value
+#
 if [ ${TEST_STATUS} == 0 ]; then
     echo "All Python tests OK"
     if [ -n "${PYTHON_INSTALL_PREFIX}" ]; then
