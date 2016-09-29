984919a553dbe603f9c28648a3efc63f59ca5b62
Hannes Vogt <hannes@havogt.de>
2016-09-29 08:26:20 +0200

    fix exit status of run_tests.sh

--- cmake/definitions.cmake
+++ cmake/definitions.cmake
@@ -181,8 +181,6 @@ if(DOXYGEN_FOUND)
 endif()
 
 ## test script generator ## 
-file(WRITE ${TEST_SCRIPT} "#!/bin/sh\n")
-file(APPEND ${TEST_SCRIPT} "res=0\n")
 function(gridtools_add_test test_name test_script test_exec)
   file(APPEND ${test_script} "${test_exec}" " ${ARGN}" "\n")
   file(APPEND ${test_script} "res=$((res || $? ))\n")
