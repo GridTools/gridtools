dbc8009a4c4d1b8f4ad0661fe91effcd4ac57490
Juraj Kardos <juraj.kardos@usi.ch>
2016-11-15 16:09:23 +0100

    changed interface to cg_naive, added argument  nrhs

--- examples/cg_naive.cpp
+++ examples/cg_naive.cpp
@@ -6,8 +6,9 @@ int main(int argc, char** argv)
     // Initialize MPI
     gridtools::GCL_Init();
 
-    if (argc != 6) {
-        std::cout << "Usage: cg_naive<whatever> dimx dimy dimz maxit eps\n where args are integer sizes of the data fields, max number of iterations and eps is required tolerance" << std::endl;
+    if (argc != 7) {
+        if (gridtools::PID == 0)
+        std::cout << "Usage: cg_naive<whatever> dimx dimy dimz maxit eps nrhs,\nwhere args are integer sizes of the data fields, max number of iterations, eps is required tolerance and nsamples is number of RHS" << std::endl;
         return 1;
     }
 
@@ -15,14 +16,14 @@ int main(int argc, char** argv)
     Timers timers;
 
     //run GC solver for NS samples
-    int NS = 10;
+    int NS = atoi(argv[6]);
     for (int i = 0; i < NS; i++)
     {
         cg_naive::solver(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), std::stod(argv[5]), &timers);
     }
 
     //print timing info
-    if(gridtools::PID == 0)
+    if (gridtools::PID == 0)
     {
         timers.print_timers();
     }
