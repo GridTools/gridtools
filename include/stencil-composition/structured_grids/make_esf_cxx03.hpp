ba3431d53003e77fcc1a43758a520784e9697df2
Paolo Crosetto <crosetto@greina0.cm.cluster>
2016-11-24 11:11:59 +0100

    reverting formatting with cxx03 files containing macros

--- include/stencil-composition/structured_grids/make_esf_cxx03.hpp
+++ include/stencil-composition/structured_grids/make_esf_cxx03.hpp
@@ -10,22 +10,21 @@
 
 namespace gridtools {
 
-/*!
-   \fn esf_descriptor<ESF, ...> make_esf(plc1, plc2, plc3, ...)
-   \brief Function to create a Elementary Stencil Function
-   \param plc{i} placeholder which represents the i-th argument to the functor ESF
+    /*!
+       \fn esf_descriptor<ESF, ...> make_esf(plc1, plc2, plc3, ...)
+       \brief Function to create a Elementary Stencil Function
+       \param plc{i} placeholder which represents the i-th argument to the functor ESF
 
-   Use this function to associate a stencil functor (stage) to
-   arguments (actually, placeholders to arguments)
- */
+       Use this function to associate a stencil functor (stage) to
+       arguments (actually, placeholders to arguments)
+     */
 
-#define _MAKE_ESF(z, n, nil)                                                                                           \
-    template < typename ESF, BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), typename A) >                                       \
-        esf_descriptor< ESF,                                                                                           \
-            BOOST_PP_CAT(boost::mpl::vector, BOOST_PP_INC(n)) < BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), A) >>            \
-        make_esf(BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), A)) {                                                           \
-        return esf_descriptor< ESF,                                                                                    \
-                   BOOST_PP_CAT(boost::mpl::vector, BOOST_PP_INC(n)) < BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), A) >> (); \
+#define _MAKE_ESF(z, n, nil)                                            \
+    template <typename ESF,                                             \
+              BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), typename A)>        \
+    esf_descriptor<ESF, BOOST_PP_CAT(boost::mpl::vector, BOOST_PP_INC(n)) <BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), A)> > \
+    make_esf(BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), A)) {                \
+        return esf_descriptor<ESF, BOOST_PP_CAT(boost::mpl::vector, BOOST_PP_INC(n)) <BOOST_PP_ENUM_PARAMS(BOOST_PP_INC(n), A)> >(); \
     }
 
     BOOST_PP_REPEAT(GT_MAX_ARGS, _MAKE_ESF, _)
