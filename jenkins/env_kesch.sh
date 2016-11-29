821d091bd189aa9ec014d6933802af685fd7a563
Mauro Bianco <mbianco@cscs.ch>
2016-11-29 14:22:43 +0100

    Updated environment on kesch - proper MPI for G2G support.

--- jenkins/env_kesch.sh
+++ jenkins/env_kesch.sh
@@ -1,12 +1,15 @@
 #/bin/bash
 
-module load PrgEnv-gnu
+module load craype-haswell
+module load craype-network-infiniband
+module load mvapich2gdr_gnu/2.1_cuda_7.0
+module load GCC/4.9.3-binutils-2.25
+module load cudatoolkit/7.0.28
 #we need a decent cmake version in order to pass the HOST_COMPILER to nvcc
 module load CMake/3.3.2
-#module load python/3.4.3
-#module load boost/1.56_gcc4.8.4
-#module load mvapich2/gcc/64/2.0-gcc-4.8.2-cuda-6.0
-module load cudatoolkit
+
+
+echo $LD_LIBRARY_PATH
 export Boost_NO_SYSTEM_PATHS=true
 export Boost_NO_BOOST_CMAKE=true
 export GRIDTOOLS_ROOT_BUILD=$PWD/build
@@ -20,8 +23,8 @@ export BOOST_INCLUDE=/scratch/cosuna/software/boost_1_59_0/include/
 export CUDA_ARCH=sm_37
 export DEFAULT_QUEUE=debug
 export LAUNCH_MPI_TEST="srun"
-export JOB_ENV="export CUDA_AUTO_BOOST=0; export GCLOCK=875; export G2G=1; export CUDA_AUTO_BOOST=0;"
-export DEFAULT_QUEUE=dev
+export JOB_ENV="export ENABLE_CUDA=1; export CUDA_AUTO_BOOST=0; export GCLOCK=875; export G2G=1; export CUDA_AUTO_BOOST=0;"
 export USE_MPI_COMPILER=ON
 export MPI_NODES=1
 export MPI_TASKS=4
+export CXX=`which g++`
