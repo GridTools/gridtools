b6bc7b02de8b4782d2a2250d29b25bb4dc47ad1e
carlos osuna <carlos.osuna@meteoswiss.ch>
2016-12-01 10:52:39 +0100

    move chevron_fixer gwak to file

--- tools/chevron_fixer/chevron_fixer.sh
+++ tools/chevron_fixer/chevron_fixer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+DIR=/code/gridtools2/tools/chevron_fixer/
 for file in `find . -regextype posix-egrep -regex ".*\.(hpp|cpp|cu)$" `; do 
-	cat $file | gawk '{ if(match($0,/<[ \t]*<[ \t]*<.*>[ \t]*>[ \t]*>/)) { sub(/<[ \t]*<[ \t]*</,"<<<", $0); sub(/>[ \t]*>[ \t]*>/, ">>>", $0); } print $0 }' > $file.tmp; 
+	cat $file | ${DIR}/chevron_fixer_file.sh > $file.tmp
+#gawk '{ if(match($0,/<[ \t]*<[ \t]*<.*>[ \t]*>[ \t]*>/)) { sub(/<[ \t]*<[ \t]*</,"<<<", $0); sub(/>[ \t]*>[ \t]*>/, ">>>", $0); } print $0 }' > $file.tmp; 
 	mv $file.tmp $file; 
 done
 
