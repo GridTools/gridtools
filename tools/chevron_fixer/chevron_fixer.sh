71d19bf5853c097b6dbdcdcb4a6efe82b9f38b65
carlos osuna <carlos.osuna@meteoswiss.ch>
2016-12-01 12:24:55 +0100

    delete comment

--- tools/chevron_fixer/chevron_fixer.sh
+++ tools/chevron_fixer/chevron_fixer.sh
@@ -6,7 +6,6 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for file in `find . -regextype posix-egrep -regex ".*\.(hpp|cpp|cu)$" `; do 
 	cat $file | ${DIR}/chevron_fixer_file.sh > $file.tmp
-#gawk '{ if(match($0,/<[ \t]*<[ \t]*<.*>[ \t]*>[ \t]*>/)) { sub(/<[ \t]*<[ \t]*</,"<<<", $0); sub(/>[ \t]*>[ \t]*>/, ">>>", $0); } print $0 }' > $file.tmp; 
 	mv $file.tmp $file; 
 done
 
