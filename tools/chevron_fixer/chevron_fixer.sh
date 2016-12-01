43767fad3c58ed8f8ba420400fc6bad83f8b0a61
carlos osuna <carlos.osuna@meteoswiss.ch>
2016-12-01 12:16:45 +0100

    correct script dir

--- tools/chevron_fixer/chevron_fixer.sh
+++ tools/chevron_fixer/chevron_fixer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=/code/gridtools2/tools/chevron_fixer/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for file in `find . -regextype posix-egrep -regex ".*\.(hpp|cpp|cu)$" `; do 
 	cat $file | ${DIR}/chevron_fixer_file.sh > $file.tmp
 #gawk '{ if(match($0,/<[ \t]*<[ \t]*<.*>[ \t]*>[ \t]*>/)) { sub(/<[ \t]*<[ \t]*</,"<<<", $0); sub(/>[ \t]*>[ \t]*>/, ">>>", $0); } print $0 }' > $file.tmp; 
