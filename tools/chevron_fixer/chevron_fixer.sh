1abb2a13783c2fe1a41c51632471aea39458c6d3
carlos osuna <carlos.osuna@meteoswiss.ch>
2016-12-01 12:20:47 +0100

    add protection for script

--- tools/chevron_fixer/chevron_fixer.sh
+++ tools/chevron_fixer/chevron_fixer.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -z ${BASH} ]; then
+  echo "ERROR: need to execute the script in bash" 
+  exit 1
+fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for file in `find . -regextype posix-egrep -regex ".*\.(hpp|cpp|cu)$" `; do 
 	cat $file | ${DIR}/chevron_fixer_file.sh > $file.tmp
