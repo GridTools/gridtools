692eeca55282491358f0bf8ff2520d5263b9ad58
Paolo Crosetto <crosetto@greina0.cm.cluster>
2016-12-05 18:07:08 +0100

    update license checkesr

--- tools/check_license/check_license.sh
+++ tools/check_license/check_license.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # run this script from the root directory
 if [ ! -f $PWD/LICENSE ] ; then
-    echo "you have to run the scripto ./tools/check_license/check_license.sh from the root of the sources"
+    echo "you have to run the script ./tools/check_license/check_license.sh from the root of the sources"
     exit -1
 fi
 
 for file in `find . -regextype posix-egrep -regex ".*\.(hpp|cpp|cu)$" `; do
-	if ! grep -q -f $PWD/LICENSE $file ; then
-            echo $file
+    if ! grep -q "Copyright (c) 2016, GridTools Consortium" $file ; then
+        echo $file
+        if [ $1="fix" ]; then
+            "/*
+$(cat LICENSE)
+*/
+$(cat $file)" > $file
         fi
+    fi
 done
