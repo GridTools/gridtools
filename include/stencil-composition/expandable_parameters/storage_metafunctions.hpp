9621709e100858f05e39f2acec0145876bc0c09a
Paolo Crosetto <crosetto@greina0.cm.cluster>
2016-12-16 16:41:38 +0100

    fix for CXX11

--- include/stencil-composition/expandable_parameters/storage_metafunctions.hpp
+++ include/stencil-composition/expandable_parameters/storage_metafunctions.hpp
@@ -53,15 +53,6 @@ namespace gridtools {
     struct is_any_storage< std::vector< T > > : is_any_storage< T > {};
 
     template < typename T >
-    struct get_space_dimensions< std::vector< pointer< T > > > : get_space_dimensions< T > {};
-    template < typename T >
-    struct get_base_storage< std::vector< pointer< T > > > {
-        typedef T type;
-    };
-
-#endif
-
-    template < typename T >
     struct extract_storage_info_type< std::vector< pointer< T > > > {
         typedef typename T::storage_info_type type;
     };
