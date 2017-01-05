ca38a2f63c18f2ed8332daba99341d600dd5d754
Stefan Moosbrugger <stefanmoosbrugger@gmail.com>
2017-01-05 15:52:46 +0100

    new storage infrastructure: port copy_stencil

--- examples/copy_stencil.hpp
+++ examples/copy_stencil.hpp
@@ -53,13 +53,6 @@ using namespace gridtools;
 using namespace enumtype;
 
 namespace copy_stencil {
-#ifdef __CUDACC__
-    typedef gridtools::layout_map< 2, 1, 0 > layout_t; // stride 1 on i
-#else
-    //                   strides  1 x xy
-    //                      dims  x y z
-    typedef gridtools::layout_map< 0, 1, 2 > layout_t; // stride 1 on k
-#endif
 
     // This is the definition of the special regions in the "vertical" direction
     typedef gridtools::interval< level< 0, -1 >, level< 1, -1 > > x_interval;
@@ -92,8 +85,10 @@ namespace copy_stencil {
         uint_t d3 = z;
 
 #ifdef __CUDACC__
+#define BACKEND_V Cuda
 #define BACKEND backend< Cuda, GRIDBACKEND, Block >
 #else
+#define BACKEND_V Host
 #ifdef BACKEND_BLOCK
 #define BACKEND backend< Host, GRIDBACKEND, Block >
 #else
@@ -101,23 +96,30 @@ namespace copy_stencil {
 #endif
 #endif
 
-        typedef BACKEND::storage_info< __COUNTER__, layout_t > meta_data_t;
-        typedef BACKEND::storage_type< float_type, meta_data_t >::type storage_t;
+        typedef storage_traits< BACKEND_V >::storage_info_t< 0, 3 > storage_info_t;
+        typedef storage_traits< BACKEND_V >::data_store_t< float_type, storage_info_t > data_store_t;
 
-        meta_data_t meta_data_(x, y, z);
+        storage_info_t meta_data_(x, y, z);
 
         // Definition of the actual data fields that are used for input/output
-        typedef storage_t storage_type;
-        storage_type in(meta_data_, "in");
-        storage_type out(meta_data_, float_type(-1.));
+        data_store_t in(meta_data_);
+        data_store_t out(meta_data_);
+
+        in.allocate();
+        out.allocate();
+
+        auto in_v = make_host_view(in);
+        auto out_v = make_host_view(out);
+
         for (uint_t i = 0; i < d1; ++i)
             for (uint_t j = 0; j < d2; ++j)
                 for (uint_t k = 0; k < d3; ++k) {
-                    in(i, j, k) = i + j + k;
+                    in_v(i, j, k) = i + j + k;
+                    out_v(i,j,k) = -1.0;
                 }
 
-        typedef arg< 0, storage_type > p_in;
-        typedef arg< 1, storage_type > p_out;
+        typedef arg< 0, data_store_t > p_in;
+        typedef arg< 1, data_store_t > p_out;
 
         typedef boost::mpl::vector< p_in, p_out > accessor_list;
         // construction of the domain. The domain is the physical domain of the problem, with all the physical fields
@@ -125,7 +127,7 @@ namespace copy_stencil {
         // It must be noted that the only fields to be passed to the constructor are the non-temporary.
         // The order in which they have to be passed is the order in which they appear scanning the placeholders in
         // order. (I don't particularly like this)
-        gridtools::aggregator_type< accessor_list > domain(boost::fusion::make_vector(&in, &out));
+        gridtools::aggregator_type< accessor_list > domain(in, out);
 
         // Definition of the physical dimensions of the problem.
         // The constructor takes the horizontal plane dimensions,
@@ -138,53 +140,39 @@ namespace copy_stencil {
         grid.value_list[0] = 0;
         grid.value_list[1] = d3 - 1;
 
-/*
-  Here we do lot of stuff
-  1) We pass to the intermediate representation ::run function the description
-  of the stencil, which is a multi-stage stencil (mss)
-  The mss includes (in order of execution) a laplacian, two fluxes which are independent
-  and a final step that is the out_function
-  2) The logical physical domain with the fields to use
-  3) The actual domain dimensions
-*/
+        /*
+        Here we do lot of stuff
+        1) We pass to the intermediate representation ::run function the description
+        of the stencil, which is a multi-stage stencil (mss)
+        The mss includes (in order of execution) a laplacian, two fluxes which are independent
+        and a final step that is the out_function
+        2) The logical physical domain with the fields to use
+        3) The actual domain dimensions
+        */
 
-#ifdef CXX11_ENABLED
-        auto
-#else
-#ifdef __CUDACC__
-        gridtools::stencil *
-#else
-        boost::shared_ptr< gridtools::stencil >
-#endif
-#endif
-            copy = gridtools::make_computation< gridtools::BACKEND >(
+        auto copy = gridtools::make_computation< gridtools::BACKEND >(
                 domain,
                 grid,
                 gridtools::make_multistage // mss_descriptor
                 (execute< forward >(),
-                    gridtools::make_stage< copy_functor >(p_in() // esf_descriptor
-                        ,
-                        p_out())));
+                    gridtools::make_stage< copy_functor >(p_in(),p_out())
+                ));
 
         copy->ready();
-
         copy->steady();
-
         copy->run();
 
-#ifdef __CUDACC__
-        out.d2h_update();
-        in.d2h_update();
-#endif
+        out.sync();
+        in.sync();
 
         bool success = true;
         if (verify) {
             for (uint_t i = 0; i < d1; ++i) {
                 for (uint_t j = 0; j < d2; ++j) {
                     for (uint_t k = 0; k < d3; ++k) {
-                        if (in(i, j, k) != out(i, j, k)) {
+                        if (in_v(i, j, k) != out_v(i, j, k)) {
                             std::cout << "error in " << i << ", " << j << ", " << k << ": "
-                                      << "in = " << in(i, j, k) << ", out = " << out(i, j, k) << std::endl;
+                                      << "in = " << in_v(i, j, k) << ", out = " << out_v(i, j, k) << std::endl;
                             success = false;
                         }
                     }
@@ -199,3 +187,4 @@ namespace copy_stencil {
         return success;
     }
 } // namespace copy_stencil
+
