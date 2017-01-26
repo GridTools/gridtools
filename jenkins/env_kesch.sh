fb09d1283490af72ab69d56fb85e2e71caef7319
Mauro Bianco <mbianco@cscs.ch>
2017-01-26 14:07:17 +0100

    updated kesch env file

--- jenkins/env_kesch.sh
+++ jenkins/env_kesch.sh
@@ -4,22 +4,30 @@ if [[ -z ${VERSION} ]]; then
   VERSION="5.3"
 fi
 
+module load craype-haswell
+module load craype-network-infiniband
+module load mvapich2gdr_gnu/2.1_cuda_7.0
+module load GCC/4.9.3-binutils-2.25
+module load cray-libsci_acc/3.3.0
+export CXX=`which g++`
+
 if [[ ${VERSION} == "5.3" ]] && [[ "${TARGET}" != "gpu" ]]; then
   module unload GCC/4.9.3-binutils-2.25
   module load GCC/5.3.0-binutils-2.25
 else
-  VERSION="4.9"
-  module load PrgEnv-gnu
-  module swap GCC/4.9.3-binutils-2.25
-  export HOST_COMPILER=`which g++`
+    echo 0000
+  # VERSION="4.9"
+  # module load PrgEnv-gnu
+  # module swap GCC/4.9.3-binutils-2.25
+  # export HOST_COMPILER=`which g++`
 fi
 
 #we need a decent cmake version in order to pass the HOST_COMPILER to nvcc
 
-module load mvapich2gdr_gnu/2.1_cuda_7.0
-module load craype-haswell
-module load craype-network-infiniband
-module load cudatoolkit/7.0.28
+# module load mvapich2gdr_gnu/2.1_cuda_7.0
+# module load craype-haswell
+# module load craype-network-infiniband
+# module load cudatoolkit/7.0.28
 module load CMake/3.3.2
 
 
@@ -36,7 +44,7 @@ export DEFAULT_QUEUE=debug
 export LAUNCH_MPI_TEST="srun"
 export JOB_ENV="export ENABLE_CUDA=1; export CUDA_AUTO_BOOST=0; export GCLOCK=875; export CUDA_AUTO_BOOST=0; export G2G=1"
 export MPI_HOST_JOB_ENV=""
-export MPI_CUDA_JOB_ENV="export MV2_USE_CUDA=1; export MV2_USE_GPUDIRECT=0; export CUDA_AUTO_BOOST=0; export GCLOCK=875; export CUDA_AUTO_BOOST=0; export G2G=2"
+export MPI_CUDA_JOB_ENV="export MV2_USE_CUDA=1; export MV2_USE_GPUDIRECT=1 ; export GCLOCK=875; export CUDA_AUTO_BOOST=0; export G2G=2"
 export USE_MPI_COMPILER=ON
 export MPI_NODES=1
 export MPI_TASKS=4
