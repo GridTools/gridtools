63b059e87e146ff010f1a88acf0fcf238c0d24e9
carlos osuna <carlos.osuna@meteoswiss.ch>
2017-02-06 13:58:20 +0100

    remove commented code

--- include/stencil-composition/aggregator_type_impl.hpp
+++ include/stencil-composition/aggregator_type_impl.hpp
@@ -226,12 +226,6 @@ namespace gridtools {
                 typedef typename boost::is_same< T1, T2 >::type type;
             };
 
-// // specialization for base_storage
-// template < typename T1, typename T2, uint_t Size, ushort_t ID, typename Cond >
-// struct matching< arg< ID, std::vector< pointer< no_storage_type_yet< T1 > > >, Cond >,
-//                  arg< ID, no_storage_type_yet< expandable_parameters< T2, Size > >, Cond > > {
-//     typedef typename boost::is_same< T1, T2 >::type type;
-// };
 #ifdef CXX11_ENABLED
             // specialization for storage
             template < typename T1, typename T2, uint_t Size, ushort_t ID, typename Cond >
