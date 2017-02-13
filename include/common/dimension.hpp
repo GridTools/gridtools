88cec5f4ee78200295bc8a0240096b512325f71b
Hannes Vogt <hannes@havogt.de>
2017-02-13 16:29:35 +0100

    offset should allow negative values...

--- include/common/dimension.hpp
+++ include/common/dimension.hpp
@@ -55,10 +55,10 @@ namespace gridtools {
         GT_FUNCTION constexpr dimension(IntType val)
             : value
 #if ((!defined(CXX11_ENABLED)))
-              ((uint_t)val)
+              ((int_t)val)
 #else
         {
-            (uint_t) val
+            (int_t) val
         }
 #endif
         {
@@ -69,12 +69,12 @@ namespace gridtools {
 
         /**@brief Constructor*/
         GT_FUNCTION
-        constexpr dimension(dimension const &other) : value((uint_t)other.value) {}
+        constexpr dimension(dimension const &other) : value(other.value) {}
 
         // TODO can I rename direction by index?
         static const ushort_t direction = Coordinate;
         static const ushort_t index = Coordinate;
-        uint_t value;
+        int_t value;
 
         /**@brief syntactic sugar for user interface
 
