2a8835ea592b7ee70f7cbb865f9254d57c9a358c
Mauro Bianco <mbianco@cscs.ch>
2017-02-22 11:49:31 +0100

    trying to get jenking to run

--- jenkins/env_kesch.sh
+++ jenkins/env_kesch.sh
@@ -4,25 +4,20 @@ if [[ -z ${VERSION} ]]; then
   VERSION="5.3"
 fi
 
-module load craype-haswell
-module load craype-network-infiniband
-module load mvapich2gdr_gnu/2.1_cuda_7.0
-module load GCC/4.9.3-binutils-2.25
-module load cray-libsci_acc/3.3.0
-
 if [[ ${VERSION} == "5.3" ]] && [[ "${TARGET}" != "gpu" ]]; then
   module unload GCC/4.9.3-binutils-2.25
+  module load mvapich2gdr_gnu/2.1_cuda_7.0
   module load GCC/5.3.0-binutils-2.25
+else
+  module load PrgEnv-gnu
 fi
 
 #we need a decent cmake version in order to pass the HOST_COMPILER to nvcc
-
-# module load mvapich2gdr_gnu/2.1_cuda_7.0
-# module load craype-haswell
-# module load craype-network-infiniband
-# module load cudatoolkit/7.0.28
-module load CMake/3.3.2
-
+module load CMake/3.3.2 
+#module load python/3.4.3
+#module load boost/1.56_gcc4.8.4
+#module load mvapich2/gcc/64/2.0-gcc-4.8.2-cuda-6.0
+module load cudatoolkit
 
 echo $LD_LIBRARY_PATH
 export Boost_NO_SYSTEM_PATHS=true
