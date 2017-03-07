504ece915535fda33203f291b87744523052c183
cosunae <carlos.osuna@meteoswiss.ch>
2017-03-07 14:11:47 +0100

    Fix initialization of irregular operators (GC-148, #581, #580)
    Bug description: The initialization of fields in icgrid examples is based on random numbers which led to randomly failing tests. Fixes #580.
    
    Solution: Initialize the fields with a smooth function.
    
    Additional changes:
    - Use float_type instead of double in the examples and initialize the verifier with a matching precision.
    - Adds max(), min(), fabs() implementation to gt_math, because the "compare_below_threshold()" function of the verifier is sometimes used on device.

--- examples/icosahedral/stencil_fused.hpp
+++ examples/icosahedral/stencil_fused.hpp
@@ -197,7 +197,11 @@ namespace sf {
                 }
             }
 
+#if FLOAT_PRECISION == 4
+            verifier ver(1e-6);
+#else
             verifier ver(1e-10);
+#endif
 
             array< array< uint_t, 2 >, 4 > halos = {{{halo_nc, halo_nc}, {0, 0}, {halo_mc, halo_mc}, {halo_k, halo_k}}};
             result = ver.verify(grid_, ref_on_cells, out_cells, halos);
