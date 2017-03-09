9970025f67e0cfda1eab656ac440c4130c3bc1e3
Paolo Crosetto <paolo.crosetto@usys.ethz.ch>
2017-03-09 14:14:08 +0100

    temporarily hardcoding the dimensions for the shallow water test, reordering the link line for the mpi targets

--- examples/shallow_water_enhanced.cpp
+++ examples/shallow_water_enhanced.cpp
@@ -38,12 +38,13 @@
 
 int main(int argc, char **argv) {
 
-    if (argc != 5) {
-        std::cout << "Usage: shallow_water_<whatever> dimx dimy dimz timesteps\n where args are integer sizes of the "
-                     "data fields and the number of timesteps performed"
-                  << std::endl;
-        return 1;
-    }
+    // if (argc != 5) {
+    //     std::cout << "Usage: shallow_water_<whatever> dimx dimy dimz timesteps\n where args are integer sizes of the "
+    //                  "data fields and the number of timesteps performed"
+    //               << std::endl;
+    //     return 1;
+    // }
 
-    return !shallow_water::test(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+    // return !shallow_water::test(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+    return !shallow_water::test(60, 83, 1, 1);
 }
