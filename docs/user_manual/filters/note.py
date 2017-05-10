ab18130c5b102d962f065dae71aa499bc534ff5c
Mauro Bianco <mbianco@cssc.ch>
2017-05-10 15:24:16 +0000

    A version of note that is more flexible in what can be put in a note. The filter requires the defines.md files to be present \(for \GT macro to be defined\). Plus, there were some strange characters in the quick guide, I substituted those

--- docs/user_manual/filters/note.py
+++ docs/user_manual/filters/note.py
@@ -13,21 +13,52 @@ pandoc sample.md -t json | python ./note.py | pandoc -f json -t html -s
 """
 
 from pandocfilters import toJSONFilter, CodeBlock, Table, Str, Header, Link, Space, split_string, AlignLeft, AlignDefault, Plain, Image
-
+import commands as cm
+from os import getpid, remove
+import json
 
 def substitute_note(key, value, format, meta):
     if key == 'CodeBlock':
         [[ident, classes, namevals], code] = value
 
         if classes != [] and classes[0] == "note" :
-            return Table([],
-                         [AlignLeft(),AlignDefault()],
-                         [0,0],
-                         [[],[]],
-                         [
-                             [[Plain([Image(["", [], [["width", "20px"], ["height", "20px"]]], [{"c": "Tip", "t": "Str"}], ["figures/hint.gif", ""])])], [Plain([Str(" ")])]],
-                             [[], [Plain(split_string(code))]],
-                         ])
+            lines = code.split("\n")
+            indent_lines = ["                                                      " + s for s in lines]
+            indent_code = "\n".join(indent_lines)
+
+            table = "---------------------------------------------------   --------------------------------------------------------\n![Tip](figures/hintsmall.gif)\n" + indent_code + "\n---------------------------------------------------   --------------------------------------------------------"
+            fname = 'tmp-' + str(getpid())
+            f = open(fname, 'w')
+            f.write(table);
+            f.close();
+            
+            command = 'pandoc defines.md ' + fname + ' --from markdown --to json'
+
+            (v, out) = cm.getstatusoutput(command)
+
+            remove(fname)
+
+            if v != 0:
+                raise ValueError("Something went wrong")
+
+            source = out.decode('utf-8')
+
+            doc = json.loads(source)
+
+            if 'meta' in doc:
+                doc = doc['blocks']
+            else:
+                doc = doc[1]
+                
+            return doc
+            #    Table([],
+            #             [AlignLeft(),AlignDefault()],
+            #             [0,0],
+            #             [[],[]],
+            #             [
+            #                 [[Plain([Image(["", [], [["width", "20px"], ["height", "20px"]]], [{"c": "Tip", "t": "Str"}], ["figures/hint.gif", ""])])], [Plain([Str(" ")])]],
+            #                 [[Plain([Str(" ")])], doc],
+            #             ])
         
         return CodeBlock([ident, classes, namevals], code)
 
