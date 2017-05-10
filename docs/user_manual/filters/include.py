87315bbf7288833a15d75620e59a13f5bc2949ae
Mauro Bianco <mbianco@cssc.ch>
2017-05-10 11:19:38 +0000

    Added capability to include source code from file in the include filter

--- docs/user_manual/filters/include.py
+++ docs/user_manual/filters/include.py
@@ -2,14 +2,30 @@
 
 """
 Pandoc filter to process code blocks to include one .md file
-from a relative path
+from a relative path. This works with code, too if "lang=the_lang"
+is used.
+
+In case of the lang, the laguage tag is substituted to `include`
+while the other name-values pairs remain are passed to the new 
+code-block, so that the options are propagated there. 
+
+example: test.md:
+>>>>>>>>>>>>>>>>>>>>>>>
+text
 
 ```include
-filepath/name.md
+./included.md
 ```
 
+```{.include lang=cpp propagated=pass_this_value}
+./included.md
+```
+
+text
+<<<<<<<<<<<<<<<<<<<<<<<<
+
 Typical Usage:
-pandoc sample.md -t json | python ./include.py | pandoc -f json -t html -s
+pandoc test.md -t json | python ./include.py | pandoc -f json -t html -s
 """
 
 from pandocfilters import toJSONFilter, CodeBlock, Table, Str, Header, Link, Space, split_string, AlignLeft, AlignDefault, Plain, Image
@@ -21,20 +37,38 @@ import json
 def code_include(key, value, format, meta):
     if key == 'CodeBlock':
         [[ident, classes, namevals], code] = value
-
+        print classes
+        print namevals
         if classes != [] and classes[0] == "include" :
-            
-            lines = code.split("\n")
-            if len(lines) > 1:
-                raise ValueError("Only one file at a time can be included")
+
+            tags = [l[0] for l in namevals if l[0]]
+            if "lang" in tags:
+                print "LANG"
+                # switch to including into a codeblock
+                vals = [l[1] for l in namevals if l[1]]
+                lang = vals[tags.index("lang")]
+
+                namevals.pop(tags.index("lang"))
+                classes[0]="cpp"
+
+                f = open(code, 'r')
+                newcode = f.read()
+                f.close()
+                return CodeBlock([ident, classes, namevals], newcode)
+            else:
+                print "REGULAR"
+                lines = code.split("\n")
+                if len(lines) > 1:
+                    raise ValueError("Only one file at a time can be included")
 
 
-            (v, out) = cm.getstatusoutput('pandoc ' + code + ' --from markdown --to json ')
-            if v != 0:
-                raise ValueError("Something went wrong: maybe included file was not found")
-            source = out.decode('utf-8')
-            doc = json.loads(source)
-            return doc[1]
+                (v, out) = cm.getstatusoutput('pandoc ' + code + ' --from markdown --to json ')
+                if v != 0:
+                    raise ValueError("Something went wrong: maybe included file was not found")
+
+                source = out.decode('utf-8')
+                doc = json.loads(source)
+                return doc[1]
 
         return CodeBlock([ident, classes, namevals], code)
 
