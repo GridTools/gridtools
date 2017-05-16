5dc1c607fe37bd0379b2e2e304724443220f532c
Stefan Moosbrugger <stefanmoosbrugger@gmail.com>
2017-05-16 20:58:10 +0200

    Fix issue 639: Document get_layout

--- include/storage/common/storage_traits_metafunctions.hpp
+++ include/storage/common/storage_traits_metafunctions.hpp
@@ -74,10 +74,45 @@ namespace gridtools {
         typedef layout_map< Dims... > type;
     };
 
-    /* get a standard layout_map (either 3 or n-dimensional and ascending or descending) */
+    /* get a standard layout_map (n-dimensional and ascending or descending) */
     template < uint_t Dim, bool Asc >
     struct get_layout;
 
+    // get a multidimensional layout in ascending order (e.g., host backend)
+    /**
+     * @brief metafunction used to retrieve a layout_map with n-dimensions
+     * that can be used in combination with the host backend (k-first order).
+     * E.g., get_layout< 5, true > will return following type: layout_map< 2, 3, 4, 0, 1 >.
+     * This means the k-dimension (value: 4) is coalesced in memory, followed
+     * by the j-dimension (value: 3), followed by the i-dimension (value: 2), followed
+     * by the fifth dimension (value: 1), etc. The reason for having k as innermost
+     * is because of the gridtools execution model. The CPU backend will give best
+     * performance (in most cases) when using the provided layout.
+     */
+    template < uint_t Dim >
+    struct get_layout< Dim, true > {
+        static_assert(Dim > 0, "Zero dimensional layout makes no sense.");
+        typedef typename layout_map_ext_asc< Dim - 3, 0, layout_map< Dim - 3, Dim - 2, Dim - 1 > >::type type;
+    };
+
+    // get a multidimensional layout in descending order (e.g., gpu backend)
+    /**
+     * @brief metafunction used to retrieve a layout_map with n-dimensions
+     * that can be used in combination with the GPU backend (i-first order).
+     * E.g., get_layout< 5, false > will return following type: layout_map< 4, 3, 2, 1, 0 >.
+     * This means the i-dimension (value: 4) is coalesced in memory, followed
+     * by the j-dimension (value: 3), followed by the k-dimension (value: 2), followed
+     * by the fourth dimension (value: 1), etc. The reason for having i as innermost
+     * is because of the gridtools execution model. The GPU backend will give best
+     * performance (in most cases) when using the provided layout.
+     */
+    template < uint_t Dim >
+    struct get_layout< Dim, false > {
+        static_assert(Dim > 0, "Zero dimensional layout makes no sense.");
+        typedef typename layout_map_ext_dsc< Dim - 1, layout_map< Dim - 1 > >::type type;
+    };
+
+    /* specializations up to 3-dimensional for both i-first and k-first layouts */
     template <>
     struct get_layout< 1, true > {
         typedef layout_map< 0 > type;
@@ -106,20 +141,6 @@ namespace gridtools {
     template <>
     struct get_layout< 3, false > {
         typedef layout_map< 2, 1, 0 > type;
-    };
-
-    // get a multidimensional layout in ascending order (e.g., host backend)
-    template < uint_t Dim >
-    struct get_layout< Dim, true > {
-        static_assert(Dim > 0, "Zero dimensional layout makes no sense.");
-        typedef typename layout_map_ext_asc< Dim - 3, 0, layout_map< Dim - 3, Dim - 2, Dim - 1 > >::type type;
-    };
-
-    // get a multidimensional layout in descending order (e.g., gpu backend)
-    template < uint_t Dim >
-    struct get_layout< Dim, false > {
-        static_assert(Dim > 0, "Zero dimensional layout makes no sense.");
-        typedef typename layout_map_ext_dsc< Dim - 1, layout_map< Dim - 1 > >::type type;
     };
 
     /* special layout construction mechanisms */
