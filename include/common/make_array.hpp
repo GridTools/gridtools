2da02f69ce465eebfab26927f43d3846984eab11
cosunae <carlos.osuna@env.ethz.ch>
2017-05-24 12:27:05 +0200

    fix inline

--- include/common/make_array.hpp
+++ include/common/make_array.hpp
@@ -38,13 +38,13 @@
 #include "array.hpp"
 
 namespace gridtools {
-    GT_FUNCTION inline gridtools::array< int, 2 > make_array(int i1, int i2) {
+    GT_FUNCTION gridtools::array< int, 2 > make_array(int i1, int i2) {
         gridtools::array< int, 2 > a;
         a[0] = i1;
         a[1] = i2;
         return a;
     }
-    GT_FUNCTION inline gridtools::array< int, 3 > make_array(int i1, int i2, int i3) {
+    GT_FUNCTION gridtools::array< int, 3 > make_array(int i1, int i2, int i3) {
         gridtools::array< int, 3 > a;
         a[0] = i1;
         a[1] = i2;
