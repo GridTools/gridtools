e156c1eaf715ac7f59529632a0c43674425532b7
Mauro Bianco <mbianco@cscs.ch>
2017-06-13 10:26:23 +0200

    added few of the examples to not using \& in the evaluator

--- examples/interface1_functions.hpp
+++ examples/interface1_functions.hpp
@@ -76,7 +76,7 @@ namespace horizontal_diffusion_functions {
         typedef boost::mpl::vector< out, in > arg_list;
 
         template < typename Evaluation >
-        GT_FUNCTION static void Do(Evaluation &eval) {
+        GT_FUNCTION static void Do(Evaluation eval) {
             auto x = (gridtools::float_type)4.0 * eval(in()) -
                      (eval(in(-1, 0, 0)) + eval(in(0, -1, 0)) + eval(in(0, 1, 0)) + eval(in(1, 0, 0)));
             eval(out()) = x;
@@ -92,7 +92,7 @@ namespace horizontal_diffusion_functions {
         typedef boost::mpl::vector< out, in > arg_list;
 
         template < typename Evaluation >
-        GT_FUNCTION static void Do(Evaluation &eval) {
+        GT_FUNCTION static void Do(Evaluation eval) {
 #ifdef FUNCTIONS_MONOLITHIC
             gridtools::float_type _x_ =
                 (gridtools::float_type)4.0 * eval(in()) -
@@ -137,7 +137,7 @@ namespace horizontal_diffusion_functions {
         typedef boost::mpl::vector< out, in > arg_list;
 
         template < typename Evaluation >
-        GT_FUNCTION static void Do(Evaluation &eval) {
+        GT_FUNCTION static void Do(Evaluation eval) {
 
 #ifdef FUNCTIONS_MONOLITHIC
             gridtools::float_type _x_ =
@@ -185,7 +185,7 @@ namespace horizontal_diffusion_functions {
         typedef boost::mpl::vector< out, in, flx, fly, coeff > arg_list;
 
         template < typename Evaluation >
-        GT_FUNCTION static void Do(Evaluation &eval) {
+        GT_FUNCTION static void Do(Evaluation eval) {
             eval(out()) =
                 eval(in()) - eval(coeff()) * (eval(flx()) - eval(flx(-1, 0, 0)) + eval(fly()) - eval(fly(0, -1, 0)));
         }
