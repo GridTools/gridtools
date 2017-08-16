feb66b8e5b16f7178afe260378f4f2d70e822b1f
Hannes Vogt <hannes@havogt.de>
2017-08-16 08:46:14 +0200

    add CMAKE_CXX_FLAGS to generate repo target

--- jenkins/env_daint.sh
+++ jenkins/env_daint.sh
@@ -10,7 +10,7 @@ function exit_if_error {
 
 module load daint-gpu
 module load cudatoolkit
-module load CMake
+module load CMake/3.8.1
 module rm   PrgEnv-cray
 module load PrgEnv-gnu/6.0.3
 module load Boost/1.63.0-CrayGNU-2016.11-Python-3.5.2
