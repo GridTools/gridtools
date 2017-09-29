64430c5ccd47b6abd94ca71f41299de7b801efd0
Hannes Vogt <hannes@havogt.de>
2017-09-29 15:13:26 +0200

    fix implict nothrow in dtor

--- include/storage/storage_host/host_storage.hpp
+++ include/storage/storage_host/host_storage.hpp
@@ -101,7 +101,7 @@ namespace gridtools {
         /*
          * @brief host_storage destructor.
          */
-        ~host_storage() {
+        ~host_storage() noexcept(false) {
             ASSERT_OR_THROW(
                 ((m_ownership == ownership::ExternalCPU) || m_cpu_ptr), "This would end up in a double-free.");
             if (m_ownership == ownership::Full && m_cpu_ptr)
