00a990af1c64f227ddfeda7032b1ff40348a18e6
Hannes Vogt <hannes@havogt.de>
2017-10-05 16:03:46 +0200

    update license & clang format

--- include/common/halo_descriptor.hpp
+++ include/common/halo_descriptor.hpp
@@ -92,7 +92,9 @@ namespace gridtools {
         GT_FUNCTION halo_descriptor(uint_t m, uint_t p, uint_t b, uint_t e, uint_t l)
             : m_minus(m), m_plus(p), m_begin(b), m_end(e), m_total_length(l) {
             ASSERT_OR_THROW((m_minus + m_plus + (m_end - m_begin + 1) <= m_total_length),
-                            "Invalid halo_descriptor: compute range (length) plus halos exceed total length." + std::to_string(m) + ", " + std::to_string(p) + ", " + std::to_string(b) + ", " + std::to_string(e) + ", " + std::to_string(l) );
+                "Invalid halo_descriptor: compute range (length) plus halos exceed total length." + std::to_string(m) +
+                    ", " + std::to_string(p) + ", " + std::to_string(b) + ", " + std::to_string(e) + ", " +
+                    std::to_string(l));
             ASSERT_OR_THROW((m_begin <= m_end), "Invalid halo_descriptor: the compute range is empty (end <= begin).");
             ASSERT_OR_THROW(
                 (m_plus <= m_total_length - m_end - 1), "Invalid halo_descriptor: end of compute domain inside halo.");
