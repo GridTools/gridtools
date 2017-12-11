b2d6bcec2764c1c7044487fb699e1c2a16f29e74
cosunae <carlos.osuna@env.ethz.ch>
2017-12-11 10:29:38 +0100

    try to fix the env

--- jenkins/env_kesch.sh
+++ jenkins/env_kesch.sh
@@ -5,9 +5,7 @@ module load craype-haswell
 module load craype-accel-nvidia35
 module load cray-libsci
 module load cudatoolkit/8.0.61
-#module load mvapich2gdr_gnu/2.2_cuda_8.0
-module load mvapich2/2.2-5.4.0-2.27_cuda_8.0_gdr
-module unload mvapich2gdr_gnu/2.2_cuda_8.0
+module load mvapich2gdr_gnu/2.2_cuda_8.0
 module load gcc/5.4.0-2.26
 module load cmake/3.9.1
 
@@ -21,9 +19,9 @@ export BOOST_INCLUDE=/users/vogtha/boost_1_65_1/include/
 export CUDA_ARCH=sm_37
 export DEFAULT_QUEUE=debug
 export LAUNCH_MPI_TEST="srun"
-export JOB_ENV="export CUDA_AUTO_BOOST=0; export GCLOCK=875; export G2G=1"
+export JOB_ENV="export CUDA_AUTO_BOOST=0; export GCLOCK=875; export G2G=1; export LD_PRELOAD=/opt/mvapich2/gdr/no-mcast/2.2/cuda8.0/mpirun/gnu4.8.5/lib64/libmpi.so"
 export MPI_HOST_JOB_ENV=""
-export MPI_CUDA_JOB_ENV="export GCLOCK=875; export CUDA_AUTO_BOOST=0; export G2G=2"
+export MPI_CUDA_JOB_ENV="export GCLOCK=875; export CUDA_AUTO_BOOST=0; export G2G=2; export LD_PRELOAD=/opt/mvapich2/gdr/no-mcast/2.2/cuda8.0/mpirun/gnu4.8.5/lib64/libmpi.so"
 export USE_MPI_COMPILER=ON
 export MPI_NODES=1
 export MPI_TASKS=4
