a7d9b31518e292316af950cd3229d97dca319231
Mauro Bianco <mbianco@cscs.ch>
2018-01-29 13:26:06 +0100

    Update test_data_store.cu

--- unit_tests/storage/storage_cuda/test_data_store.cu
+++ unit_tests/storage/storage_cuda/test_data_store.cu
@@ -102,8 +102,6 @@ TEST(DataStoreTest, Simple) {
     GRIDTOOLS_STATIC_ASSERT(csih.stride< 1 >() == 7, "stride check failed.");
     GRIDTOOLS_STATIC_ASSERT(csih.stride< 2 >() == 35, "stride check failed.");
 
-    //    GRIDTOOLS_STATIC_ASSERT(csiha.dim< 0 >() == 16, "stride check failed.");
-
     EXPECT_EQ(csiha.dim< 0 >(), 7);
     EXPECT_EQ(csiha.dim< 1 >(), 5);
     EXPECT_EQ(csiha.dim< 2 >(), 3);
