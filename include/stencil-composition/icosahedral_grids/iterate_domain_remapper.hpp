5f4e4eb2ded972eba8ffff931bfab287cd114783
Anton Afanasyev <anstaf@gmail.com>
2018-01-29 16:58:24 +0100

    fix icgrid gcc compilation

--- include/stencil-composition/icosahedral_grids/iterate_domain_remapper.hpp
+++ include/stencil-composition/icosahedral_grids/iterate_domain_remapper.hpp
@@ -202,16 +202,6 @@ namespace gridtools {
             };
 
             /**
-             * returns true if variadic pack is a pack of accessors and the location type of the neighbors is not the
-             * same
-             * as
-             * the location type of the ESF.
-             */
-            template < typename NeighborsLocationType, typename EsfLocationType, typename... Accessors >
-            using accessors_on_different_color_neighbors = meta::conjunction< is_accessor< Accessors >...,
-                meta::negation< std::is_same< NeighborsLocationType, EsfLocationType > > >;
-
-            /**
              * data structure that holds data needed by the reduce_tuple functor
              * @tparam ValueType value type of the computation
              * @tparam NeighborsArray type locates the position of a neighbor element in the grid. If can be:
@@ -332,11 +322,11 @@ namespace gridtools {
                 typename LocationTypeT,
                 typename Reduction,
                 typename EsfLocationType,
-                typename... Accessors >
-            GT_FUNCTION typename boost::enable_if<
-                typename accessors_on_different_color_neighbors< LocationTypeT, EsfLocationType, Accessors... >::type,
-                ValueType >::type
-            evaluate(EsfLocationType,
+                typename... Accessors,
+                typename std::enable_if< !std::is_same< LocationTypeT, EsfLocationType >::value &&
+                                             meta::conjunction< is_accessor< Accessors >... >::value,
+                    int >::type = 0 >
+            GT_FUNCTION ValueType evaluate(EsfLocationType,
                 on_neighbors_impl< ValueType, SrcColor, LocationTypeT, Reduction, Accessors... > onneighbors) const {
 
                 // the neighbors are described as an array of absolute indices in the storage, i.e. an array<uint?t,
