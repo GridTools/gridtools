c6945d2525c18509ebe36743f558140158f3828e
Felix Thaler <thaler@cscs.ch>
2018-02-23 11:40:53 +0100

    Removed unused utils.Registry

--- jenkins/perftest/utils.py
+++ jenkins/perftest/utils.py
@@ -4,17 +4,6 @@ from perftest import ArgumentError
 from datetime import datetime, timezone
 
 
-class Registry(type):
-    def __init__(cls, name, bases, attrs):
-        if not hasattr(cls, 'registry'):
-            cls.registry = []
-        else:
-            cls.registry.append(cls)
-
-    def instantiate_all(cls, *args, **kwargs):
-        return [t(*args, **kwargs) for t in cls.registry]
-
-
 def timestr(time=None):
     if time is None:
         time = datetime.now(timezone.utc)
