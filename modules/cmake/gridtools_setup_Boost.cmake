b3003aefdc88922bd277c4cb6d62dd8f71edb0c2
carlos osuna <carlos.osuna@meteoswiss.ch>
2018-03-09 10:55:26 +0100

    remote result of use

--- modules/cmake/gridtools_setup_Boost.cmake
+++ modules/cmake/gridtools_setup_Boost.cmake
@@ -8,7 +8,6 @@ macro( gridtools_setup_boost )
 
   # A bug in boost requires these definitions to be exported
   list( APPEND GRIDTOOLS_DEFINITIONS
-    "-DBOOST_RESULT_OF_USE_TR1"
     "-DBOOST_NO_CXX11_DECLTYPE"
   )
 
