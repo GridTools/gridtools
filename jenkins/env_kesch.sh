e173658606d32bab2e15ec962c039620e91345bf
Hannes Vogt <hannes@havogt.de>
2018-03-13 22:21:43 +0100

    Test cuda 9.0

--- jenkins/env_kesch.sh
+++ jenkins/env_kesch.sh
@@ -4,8 +4,9 @@ module load craype-network-infiniband
 module load craype-haswell
 module load craype-accel-nvidia35
 module load cray-libsci
-module load cudatoolkit/8.0.61
 module load mvapich2gdr_gnu/2.2_cuda_8.0
+module unload cudatoolkit
+module load /users/vogtha/modules/cudatoolkit/9.0
 module load gcc/5.4.0-2.26
 module load cmake/3.9.1
 
