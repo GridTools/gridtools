c3dddf916e6d83d6d3c98ea8ad06773fe542b84c
Felix Thaler <thaler@cscs.ch>
2018-03-27 14:50:47 +0200

    Fix config kesch_gcc

--- pyutils/perftest/config/kesch_gcc.py
+++ pyutils/perftest/config/kesch_gcc.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from perftest.config import kesch_common as common
-
+from perftest.config.kesch_common import cmake_command, make_command, sbatch
 
 modules = common.modules | {'cudatoolkit/8.0.61',
                             'gcc/5.4.0-2.26'}
