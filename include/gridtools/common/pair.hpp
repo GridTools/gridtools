ea8c4d41d6028c0ac538d3cc208ea1ca4f9f5193
Hannes Vogt <hannes@havogt.de>
2018-03-27 17:30:19 +0200

    make pair more std compliant

--- include/gridtools/common/pair.hpp
+++ include/gridtools/common/pair.hpp
@@ -64,8 +64,28 @@ namespace gridtools {
      */
     template < typename T1, typename T2 >
     struct pair {
-        constexpr GT_FUNCTION pair(T1 t1_, T2 t2_) : first(t1_), second(t2_) {}
         constexpr GT_FUNCTION pair() : first(), second() {}
+        constexpr GT_FUNCTION pair(const T1 &t1_, const T2 &t2_) : first(t1_), second(t2_) {}
+        template < class U1,
+            class U2,
+            class = typename std::enable_if< std::is_convertible< U1, T1 >::value &&
+                                             std::is_convertible< U2, T2 >::value >::type >
+        constexpr GT_FUNCTION pair(U1 &&t1_, U2 &&t2_)
+            : first(std::forward< U1 >(t1_)), second(std::forward< U2 >(t2_)) {}
+        template < class U1,
+            class U2,
+            class = typename std::enable_if< std::is_convertible< U1, T1 >::value &&
+                                             std::is_convertible< U2, T2 >::value >::type >
+        constexpr GT_FUNCTION pair(const pair< U1, U2 > &p)
+            : first(p.first), second(p.second) {}
+        template < class U1,
+            class U2,
+            class = typename std::enable_if< std::is_convertible< U1, T1 >::value &&
+                                             std::is_convertible< U2, T2 >::value >::type >
+        constexpr GT_FUNCTION pair(pair< U1, U2 > &&p)
+            : first(std::forward< U1 >(p.first)), second(std::forward< U2 >(p.second)) {}
+        pair(const pair &) = default;
+        pair(pair &&) = default;
 
         T1 first;
         T2 second;
@@ -102,6 +122,14 @@ namespace gridtools {
             static constexpr GT_FUNCTION const T1 &const_get(const pair< T1, T2 > &p) noexcept {
                 return p.first;
             }
+            template < typename T1, typename T2 >
+            static constexpr GT_FUNCTION T1 &get(pair< T1, T2 > &p) noexcept {
+                return p.first;
+            }
+            template < typename T1, typename T2 >
+            static constexpr GT_FUNCTION T1 &&move_get(pair< T1, T2 > &&p) noexcept {
+                return std::move(p.first);
+            }
         };
         template <>
         struct pair_get< 1 > {
@@ -109,10 +137,25 @@ namespace gridtools {
             static constexpr GT_FUNCTION const T2 &const_get(const pair< T1, T2 > &p) noexcept {
                 return p.second;
             }
+            template < typename T1, typename T2 >
+            static constexpr GT_FUNCTION T2 &get(pair< T1, T2 > &p) noexcept {
+                return p.second;
+            }
+            template < typename T1, typename T2 >
+            static constexpr GT_FUNCTION T2 &&move_get(pair< T1, T2 > &&p) noexcept {
+                return std::move(p.second);
+            }
         };
     }
 
     template < size_t I, class T1, class T2 >
+    constexpr GT_FUNCTION auto get(pair< T1, T2 > &p) noexcept GT_AUTO_RETURN(impl_::pair_get< I >::get(p));
+
+    template < size_t I, class T1, class T2 >
     constexpr GT_FUNCTION auto get(const pair< T1, T2 > &p) noexcept GT_AUTO_RETURN(impl_::pair_get< I >::const_get(p));
+
+    template < size_t I, class T1, class T2 >
+    constexpr GT_FUNCTION auto get(pair< T1, T2 > &&p) noexcept GT_AUTO_RETURN(
+        impl_::pair_get< I >::move_get(std::move(p)));
 
 } // namespace gridtools
