8690bb3d8c38d07b0ff3f07617ca3b6671828db1
Mauro Bianco <mbianco@cscs.ch>
2018-03-28 13:29:27 +0200

    modified alignment test to highlight a possible bug

--- examples/alignment.hpp
+++ examples/alignment.hpp
@@ -73,59 +73,59 @@ namespace aligned_copy_stencil {
             \param it_domain iterate domain, used to get the pointers and offsets
             \param alignment ordinal number identifying the alignment
         */
-        template < unsigned Index, typename ItDomain >
-        GT_FUNCTION static bool check_pointer_alignment(ItDomain &it_domain, uint_t alignment) {
+        template < typename Ptr >
+        GT_FUNCTION static bool check_pointer_alignment(Ptr const *ptr, uint_t alignment) {
             bool result_ = true;
             if (threadIdx.x == 0) {
-                auto ptr = (static_cast< float_type * >(it_domain.get().data_pointer().template get< Index >()[0]) +
-                            it_domain.get().index()[0]);
-                result_ = (((uintptr_t)ptr & (alignment - 1)) == 0);
+                result_ = (((uintptr_t)ptr % alignment) == 0);
             }
             return result_;
         }
 #endif
 
-        typedef accessor< 0, enumtype::in, extent< 0, 0, 0, 0 >, 3 > in;
+        typedef accessor< 0, enumtype::inout, extent< 0, 0, 0, 0 >, 3 > in;
         typedef accessor< 1, enumtype::inout, extent< 0, 0, 0, 0 >, 3 > out;
         typedef boost::mpl::vector< in, out > arg_list;
 
         template < typename Evaluation >
         GT_FUNCTION static void Do(Evaluation &eval) {
 
-#ifdef __CUDACC__
-#ifndef NDEBUG
-            if (!check_pointer_alignment< 0 >(eval, meta_data_t::alignment_t::value) ||
-                !check_pointer_alignment< 1 >(eval, meta_data_t::alignment_t::value)) {
-                printf("alignment error in some storages with first meta_storage \n");
-                assert(false);
-            }
-#endif
-#endif
+            // #ifdef __CUDACC__
+            // #ifndef NDEBUG
+            //             if (!check_pointer_alignment(&eval(in()), 8 * meta_data_t::alignment_t::value) ||
+            //                 !check_pointer_alignment(&eval(out()), 8 * meta_data_t::alignment_t::value)) {
+            //                 printf("alignment error in some storages with first meta_storage \n");
+            //                 assert(false);
+            //             }
+            // #endif
+            // #endif
             eval(out()) = eval(in());
         }
     };
 
-    bool test(uint_t d1, uint_t d2, uint_t d3) {
+    bool test_tmp(uint_t d1, uint_t d2, uint_t d3) {
 
         meta_data_t meta_data_(d1 + 2 * halo_t::at< 0 >(), d2 + 2 * halo_t::at< 1 >(), d3 + 2 * halo_t::at< 2 >());
 
         // Definition of the actual data fields that are used for input/output
-        storage_t in(meta_data_, [](int i, int j, int k) { return i + j + k; }, "in");
         storage_t out(meta_data_, (float_type)-1., "out");
+        storage_t tmp(meta_data_, (float_type)-1., "tmp");
+        storage_t in(meta_data_, [](int i, int j, int k) { return i + j + k; }, "in");
 
         auto inv = make_host_view(in);
         auto outv = make_host_view(out);
 
         typedef arg< 0, storage_t > p_in;
         typedef arg< 1, storage_t > p_out;
+        typedef tmp_arg< 2, storage_t > p_tmp;
 
-        typedef boost::mpl::vector< p_in, p_out > accessor_list;
+        typedef boost::mpl::vector< p_in, p_out, p_tmp > accessor_list;
         // construction of the domain. The domain is the physical domain of the problem, with all the physical fields
         // that are used, temporary and not
         // It must be noted that the only fields to be passed to the constructor are the non-temporary.
         // The order in which they have to be passed is the order in which they appear scanning the placeholders in
         // order. (I don't particularly like this)
-        gridtools::aggregator_type< accessor_list > domain(in, out);
+        gridtools::aggregator_type< accessor_list > domain((p_in() = in), (p_out() = out));
 
         // Definition of the physical dimensions of the problem.
         // The constructor takes the horizontal plane dimensions,
@@ -136,9 +136,13 @@ namespace aligned_copy_stencil {
 
         grid< axis< 1 >::axis_interval_t > grid(di, dj, {halo_t::at< 2 >(), d3 + halo_t::at< 2 >() - 1});
 
-        auto copy = gridtools::make_computation< backend_t >(domain,
+        auto copy = gridtools::make_positional_computation< backend_t >(
+            domain,
             grid,
-            gridtools::make_multistage(execute< forward >(), gridtools::make_stage< copy_functor >(p_in(), p_out())));
+            gridtools::make_multistage(execute< forward >(),
+                // gridtools::define_caches(cache< IJ, cache_io_policy::local>(p_tmp())),
+                gridtools::make_stage< copy_functor >(p_in(), p_tmp()),
+                gridtools::make_stage< copy_functor >(p_tmp(), p_out())));
 
         copy->ready();
         copy->steady();
@@ -161,11 +165,13 @@ namespace aligned_copy_stencil {
             for (uint_t j = halo_t::at< 1 >(); j < d2 + halo_t::at< 1 >(); ++j)
                 for (uint_t k = halo_t::at< 2 >(); k < d3 + halo_t::at< 2 >(); ++k) {
                     if (inv(i, j, k) != outv(i, j, k) || outv(i, j, k) != i + j + k) {
-                        std::cout << "error in " << i << ", " << j << ", " << k << ": "
+                        std::cout << "error @ (" << i << ", " << j << ", " << k << "): "
+                                  << "i+j+k = " << i + j + k << ", "
                                   << "in = " << inv(i, j, k) << ", out = " << outv(i, j, k) << std::endl;
                         success = false;
                     }
                 }
+
         return success;
     }
 } // namespace aligned_copy_stencil
