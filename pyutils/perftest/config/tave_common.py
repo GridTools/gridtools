884ec2a8e914d6e6adf8e6d20aec0134c52ec879
Felix Thaler <thaler@cscs.ch>
2018-04-04 11:31:39 +0200

    Updated Tave config

--- pyutils/perftest/config/tave_common.py
+++ pyutils/perftest/config/tave_common.py
@@ -9,9 +9,15 @@ cmake_command = default.cmake_command + [
 
 
 def sbatch(command):
+    # We allocate the KNL with 4 hyperthreads even if we only use 2 as SLURM
+    # seems to select the wrong threads otherwise
     return textwrap.dedent(f"""\
         #!/bin/bash -l
         #SBATCH --job-name=gridtools-test
+        #SBATCH --nodes=1
+        #SBATCH --ntasks-per-core=4
+        #SBATCH --ntasks-per-node=1
+        #SBATCH --cpus-per-task=256
         #SBATCH --time=00:02:00
         #SBATCH --constraint=flat,quad
 
