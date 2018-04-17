e19ba3514c81410ade938ce75e49a0c69552200a
Hannes Vogt <hannes@havogt.de>
2018-04-17 16:48:36 +0200

    better fix for host/device call errror

--- include/gridtools/stencil-composition/run_esf_functor.hpp
+++ include/gridtools/stencil-composition/run_esf_functor.hpp
@@ -108,14 +108,11 @@ namespace gridtools {
         GT_FUNCTION
         explicit run_esf_functor(iterate_domain_t &iterate_domain) : m_iterate_domain(iterate_domain) {}
 
-/**
- * \brief given the index of a functor in the functors
- * list, it calls a kernel on the GPU executing the
- * operations defined on that functor.
- */
-#if defined(__CUDACC__) && CUDART_VERSION <= 8000
-#pragma nv_exec_check_disable
-#endif
+        /**
+         * \brief given the index of a functor in the functors
+         * list, it calls a kernel on the GPU executing the
+         * operations defined on that functor.
+         */
         template < typename Index >
         GT_FUNCTION void operator()(Index const &) const {
 
