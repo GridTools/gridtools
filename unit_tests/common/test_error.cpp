0522adfc50691a0cd8c7db75074d7cd9af067b19
Hannes Vogt <hannes@havogt.de>
2018-04-20 09:56:10 +0200

    test for error or return

---
+++ unit_tests/common/test_error.cpp
@@ -0,0 +1,66 @@
+/*
+  GridTools Libraries
+
+  Copyright (c) 2017, ETH Zurich and MeteoSwiss
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  1. Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  For information: http://eth-cscs.github.io/gridtools/
+*/
+
+#include "common/error.hpp"
+#include "gtest/gtest.h"
+
+using namespace gridtools;
+
+TEST(error_or_return, true_condition) {
+    size_t return_val = 1;
+    bool true_condition = true;
+
+    ASSERT_EQ(return_val, error_or_return(true_condition, return_val, ""));
+}
+
+TEST(error_or_return, false_condition) {
+    size_t return_val = 1;
+    bool false_condition = false;
+
+    ASSERT_ANY_THROW(error_or_return(false_condition, return_val, ""));
+}
+
+constexpr size_t call_error_or_return(bool condition, size_t return_val) {
+    return error_or_return(condition, return_val, "");
+}
+
+TEST(error_or_return, in_constexpr_context) {
+    const size_t return_val = 1;
+    const bool true_condition = true;
+
+    constexpr size_t result = call_error_or_return(true_condition, return_val);
+    ASSERT_EQ(return_val, result);
+}
