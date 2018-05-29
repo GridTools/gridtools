31a5fe1141731e8e47348969a8c2be55de663493
Anton Afanasyev <anstaf@gmail.com>
2018-05-29 14:35:24 +0200

    content is restructured a bit

--- examples/toy.cpp
+++ examples/toy.cpp
@@ -28,15 +28,14 @@ using namespace gridtools;
  * Concepts:
  *   Accessor - a pure functor that has a signatures compatible with `double(ptrdiff_t = 0, ptrdiff_t = 0)`
  *
- *   ElementaryStencil - a pure generic functor that takes accessors and returns double: `double(Accessor const&...)`
+ *   Stencil - a pure generic functor that takes accessors and returns double: `double(Accessor const&...)`
  *
- *   Evaluator - a functor that takes ElementaryStencil as a first arguments. Other parameters correspond to
- *   the arguments of that ElementaryStencil.
+ *   Evaluator - a functor that takes Stencil as a first argument. Other parameters correspond to
+ *   the arguments of that Stencil.
  *
  *   StencilComposition - a pure generic functor that takes Evaluator as a first parameter. Other parameters represent
  *   inputs of the StencilComposition. StencilComposition defines the way how the composition of stencils is expressed
- *   via composition of functions. The actual invocation of the composition (like `a(x, b(x, y))`) should be never
- *   written directly but delegated to evaluator (like `eval(a, eval(b, x, y))`)
+ *   via composition of functions.
  */
 
 struct lap_f {
@@ -85,6 +84,11 @@ struct diffusion_f {
 
 /////////////////// THE END OF STENCIL COMPOSITION SPEC /////////////////////
 
+/*
+ *  Concept of 2D(Type) : has x and y members of type Type
+ */
+
+// Models 2D
 template < class T >
 struct _2d {
     T x;
@@ -101,25 +105,93 @@ struct range {
 
 using grid_t = _2d< range >;
 
-template < class Impl, class Offset >
-struct shifted_f {
-    Impl m_impl;
-    Offset m_offset;
-
-    double operator()(ptrdiff_t i = 0, ptrdiff_t j = 0) const { return m_impl(i + m_offset.x, j + m_offset.y); }
-    double &operator()(ptrdiff_t i = 0, ptrdiff_t j = 0) { return m_impl(i + m_offset.x, j + m_offset.y); }
-};
-
-template < class Impl >
-shifted_f< Impl, ptrdiff_2d_t > shift(Impl const &impl, ptrdiff_t x, ptrdiff_t y) {
-    return {impl, {x, y}};
+size_2d_t size_2d(grid_t const &grid) {
+    return {static_cast< size_t >(grid.x.end - grid.x.begin), static_cast< size_t >(grid.y.end - grid.y.begin)};
 }
 
-template < class Container >
-size_t size(Container const &container) {
-    using std::begin;
-    using std::end;
-    return end(container) - begin(container);
+// models Accessor
+// decayed Offset should model 2D of smth. converitble to ptrdiff_t
+template < class Accessor, class Offset >
+struct shifted_f {
+    Accessor m_accessor;
+    Offset m_offset;
+
+    double operator()(ptrdiff_t i = 0, ptrdiff_t j = 0) const { return m_accessor(i + m_offset.x, j + m_offset.y); }
+    double &operator()(ptrdiff_t i = 0, ptrdiff_t j = 0) { return m_accessor(i + m_offset.x, j + m_offset.y); }
+};
+// Decorator for Accessor
+template < class Accessor >
+shifted_f< Accessor, ptrdiff_2d_t > shift(Accessor const &accessor, ptrdiff_t x, ptrdiff_t y) {
+    return {accessor, {x, y}};
+}
+
+/////////// Implementation of the Nested Call ////////////////////
+
+template < class Stencil, class Indices, class... Accessors >
+struct result_accessor_impl_f;
+
+// Models Accessor
+template < class Stencil, template < class... > class L, class... Is, class... Accessors >
+struct result_accessor_impl_f< Stencil, L< Is... >, Accessors... > {
+    Stencil m_stencil;
+    std::tuple< Accessors... > m_args;
+    double operator()(ptrdiff_t i = 0, ptrdiff_t j = 0) const {
+        return m_stencil(shift(std::get< Is::value >(m_args), i, j)...);
+    }
+};
+
+// Models Accessor
+template < class Stencil, class... Accessors >
+using result_accessor_f =
+    result_accessor_impl_f< Stencil, GT_META_CALL(meta::make_indices_c, sizeof...(Accessors)), Accessors... >;
+
+template < class Stencil >
+struct lifted_f {
+    Stencil m_stencil;
+    template < class... Accessors >
+    result_accessor_f< Stencil, Accessors... > operator()(Accessors const &... accessors) const {
+        return {m_stencil, {accessors...}};
+    }
+};
+// Lift Stencil to the functor that takes Accessors and returns Accessors. aka LiftedStencil
+template < class Stencil >
+lifted_f< Stencil > lift(Stencil const &stencil) {
+    return {stencil};
+}
+
+// Models Stencil
+template < class LiftedStencil >
+struct degraded_f {
+    LiftedStencil m_lifted_stencil;
+    template < class... Accessors >
+    auto operator()(Accessors const &... accessors) const GT_AUTO_RETURN(m_lifted_stencil(accessors...)(0, 0));
+};
+
+// Does the opposite of lift
+template < class LiftedStencil >
+degraded_f< LiftedStencil > degrade(LiftedStencil const &lifted_stencil) {
+    return {lifted_stencil};
+}
+
+// Models Evaluator
+struct fuse_evaluator_f {
+    template < class Stencil, class... Accessors >
+    auto operator()(Stencil const &stencil, Accessors const &... accessors) const
+        GT_AUTO_RETURN(lift(stencil)(accessors...));
+};
+
+// Models LiftedStencil
+template < class Composition >
+struct fused_f {
+    Composition m_composition;
+    template < class... Accessors >
+    auto operator()(Accessors const &... accessors) const
+        GT_AUTO_RETURN(m_composition(fuse_evaluator_f{}, accessors...));
+};
+
+template < class Composition >
+constexpr fused_f< Composition > fuse(Composition const &composition) {
+    return {composition};
 }
 
 /*
@@ -128,37 +200,47 @@ size_t size(Container const &container) {
  */
 
 template < class Container >
+size_t size(Container const &container) {
+    using std::begin;
+    using std::end;
+    return std::end(container) - begin(container);
+}
+
+template < class Container >
 size_2d_t size_2d(Container const &container) {
+    using std::begin;
     size_t x = size(container);
-    size_t y = x ? size(*std::begin(container)) : 0;
+    size_t y = x ? size(*begin(container)) : 0;
     return {x, y};
 }
 
-size_2d_t size_2d(grid_t const &grid) {
-    return {static_cast< size_t >(grid.x.end - grid.x.begin), static_cast< size_t >(grid.y.end - grid.y.begin)};
+template < class Container >
+grid_t make_grid(Container const &container) {
+    auto size = size_2d(container);
+    return {{0, static_cast< ptrdiff_t >(size.x)}, {0, static_cast< ptrdiff_t >(size.y)}};
 }
 
 static double ignored;
 
+// Models (Output)Accessor
 template < class Container >
 struct adapt_f {
-    adapt_f(Container &container) : m_impl(container), m_size{size_2d(container)} {}
+    adapt_f(Container &container) : m_container(container), m_size{size_2d(container)} {}
     double operator()(size_t i = 0, size_t j = 0) const {
         using std::begin;
-        return i < m_size.x && j < m_size.y ? *(begin(*(begin(m_impl) + i)) + j) : NAN;
+        return i < m_size.x && j < m_size.y ? *(begin(*(begin(m_container) + i)) + j) : NAN;
     }
     double &operator()(size_t i = 0, size_t j = 0) {
         using std::begin;
-        return i < m_size.x && j < m_size.y ? *(begin(*(begin(m_impl) + i)) + j) : ignored;
+        return i < m_size.x && j < m_size.y ? *(begin(*(begin(m_container) + i)) + j) : ignored;
     }
-    Container &m_impl;
+    Container &m_container;
     size_2d_t m_size;
 };
 
-// Adapts Container to Accessor
-template < class Impl >
-adapt_f< Impl > adapt(Impl &impl) {
-    return {impl};
+template < class Container >
+adapt_f< Container > adapt(Container &container) {
+    return {container};
 }
 
 // Models Container
@@ -224,6 +306,20 @@ namespace spec {
     struct stages {
         using type = stages;
     };
+
+    /*
+     *  Stages Concept:
+     *
+     *  Stages = stages< Stage..., LastStage >
+     *
+     *  where:
+     *     Stage = stage<Stencil, tmp<StageNo>, args<ArgTags...>>
+     *     LastStage = stage<Stencil, out, args<ArgTags...>>
+     *     ArgsTags = in<InputNo> or tmp<StageNo>
+     *     StageNo = position of the Stage within Stages
+     *     InputNo = position of the input within the invocation of the stencil composition
+     *
+     */
 }
 
 template < class Output, class... Inputs >
@@ -259,8 +355,8 @@ accessor_maker_f< Output, Inputs... > make_accessor_maker(ptrdiff_2d_t &offset,
 };
 
 struct run_stencil_f {
-    template < class Fun, class OutTag, class... ArgTags, class Output, class... Inputs >
-    void operator()(spec::stage< Fun, OutTag, spec::args< ArgTags... > >,
+    template < class Stencil, class OutTag, class... ArgTags, class Output, class... Inputs >
+    void operator()(spec::stage< Stencil, OutTag, spec::args< ArgTags... > >,
         grid_t const &grid,
         std::vector< tmp_storage > &tmp_storages,
         Output &output,
@@ -269,28 +365,28 @@ struct run_stencil_f {
         auto accessor_maker = make_accessor_maker(offset, grid, tmp_storages, output, inputs);
         auto out_acc = accessor_maker(OutTag{});
         auto in_accs = tuple_util::transform(accessor_maker, std::tuple< ArgTags... >{});
-        Fun fun;
+        Stencil stencil;
         for (offset.x = grid.x.begin; offset.x != grid.x.end; ++offset.x)
             for (offset.y = grid.y.begin; offset.y != grid.y.end; ++offset.y)
-                out_acc() = boost::fusion::invoke(fun, in_accs);
+                out_acc() = boost::fusion::invoke(stencil, in_accs);
     }
 };
 
 template < class >
 class intermediate_f;
 
-template < class... Funs, class... Outs, class... Args >
-class intermediate_f< spec::stages< spec::stage< Funs, Outs, Args >... > > {
-    using stages_t = spec::stages< spec::stage< Funs, Outs, Args >... >;
+template < class... Stencils, class... Outs, class... Args >
+class intermediate_f< spec::stages< spec::stage< Stencils, Outs, Args >... > > {
+    using stages_t = spec::stages< spec::stage< Stencils, Outs, Args >... >;
 
-    GRIDTOOLS_STATIC_ASSERT(sizeof...(Funs), "Invalid stencil composition spec.");
+    GRIDTOOLS_STATIC_ASSERT(sizeof...(Stencils), "Invalid stencil composition spec.");
 
     grid_t m_grid;
     std::vector< tmp_storage > m_tmp_storages;
 
   public:
     intermediate_f(grid_t const &grid) : m_grid(grid) {
-        for (size_t i = 0; i != sizeof...(Funs)-1; ++i)
+        for (size_t i = 0; i != sizeof...(Stencils)-1; ++i)
             m_tmp_storages.emplace_back(size_2d(grid));
     }
 
@@ -309,25 +405,26 @@ class intermediate_f< spec::stages< spec::stage< Funs, Outs, Args >... > > {
 namespace meta_detail {
 
     struct graph_eval_f {
-        template < class Fun, class... Args >
-        meta::list< Fun, spec::args< Args... > > operator()(Fun const &, Args const &...) const;
+        template < class Stencil, class... Args >
+        meta::list< Stencil, spec::args< Args... > > operator()(Stencil const &, Args const &...) const;
     };
 
     template < class T >
     GT_META_DEFINE_ALIAS(to_input, spec::in, T::value);
 
     template < size_t Arity,
-        class Spec,
+        class Composition,
         class Indices = GT_META_CALL(meta::make_indices_c, Arity),
         class Inputs = GT_META_CALL(meta::transform, (to_input, Indices)),
         class InputsTuple = GT_META_CALL(meta::rename, (std::tuple, Inputs)),
-        class SpecArgs = GT_META_CALL(meta::push_front, (InputsTuple, graph_eval_f)) >
+        class CompositionArgs = GT_META_CALL(meta::push_front, (InputsTuple, graph_eval_f)) >
 #if GT_BROKEN_TEMPLATE_ALIASES
     struct call_graph {
-        using type = decltype(boost::fusion::invoke(std::declval< Spec >(), std::declval< SpecArgs >()));
+        using type = decltype(boost::fusion::invoke(std::declval< Composition >(), std::declval< CompositionArgs >()));
     };
 #else
-    using call_graph = decltype(boost::fusion::invoke(std::declval< Spec >(), std::declval< SpecArgs >()));
+    using call_graph =
+        decltype(boost::fusion::invoke(std::declval< Composition >(), std::declval< CompositionArgs >()));
 #endif
 
 #if GT_BROKEN_TEMPLATE_ALIASES
@@ -347,11 +444,11 @@ namespace meta_detail {
         using type = meta::list< Leaf >;
     };
 
-    template < class Fun, class ArgList >
-    struct LAZY_FLATTEN_CALL_GRAPH_IMPL< meta::list< Fun, ArgList > > {
+    template < class Stencil, class ArgList >
+    struct LAZY_FLATTEN_CALL_GRAPH_IMPL< meta::list< Stencil, ArgList > > {
         using child_graphs = GT_META_CALL(meta::transform, (flatten_call_graph_impl, ArgList));
         using child_nodes = GT_META_CALL(meta::flatten, child_graphs);
-        using this_node = meta::list< Fun, ArgList >;
+        using this_node = meta::list< Stencil, ArgList >;
         using type = GT_META_CALL(meta::push_back, (child_nodes, this_node));
     };
 
@@ -360,19 +457,6 @@ namespace meta_detail {
         class UniqueNodes = GT_META_CALL(meta::dedup, Nodes) >
     GT_META_DEFINE_ALIAS(
         flatten_call_graph, meta::filter, (meta::is_instantiation_of< meta::list >::apply, UniqueNodes));
-
-    template < class T >
-    GT_META_DEFINE_ALIAS(to_tmp, spec::tmp, T::value);
-
-    template < class Nodes,
-        class T,
-        size_t N = meta::length< Nodes >::value,
-        size_t I = GT_META_CALL(meta::st_position, (Nodes, T))::value >
-    GT_META_DEFINE_ALIAS(node_to_tag2,
-        meta::if_,
-        (bool_constant< I == N >,
-                             T,
-                             GT_META_CALL(meta::if_, (bool_constant< I == N - 1 >, spec::out, spec::tmp< I >))));
 
     template < class Nodes,
         class T,
@@ -393,78 +477,58 @@ namespace meta_detail {
     template < class Nodes >
     struct stage_spec {
         template < class Node,
-            class Fun = GT_META_CALL(meta::first, Node),
+            class Stencil = GT_META_CALL(meta::first, Node),
             class OutTag = GT_META_CALL(node_to_tag_impl, (Nodes, Node)),
             class ArgList = GT_META_CALL(meta::second, Node),
             class ArgTags = GT_META_CALL(meta::transform, (node_to_tag< Nodes >::template apply, ArgList)) >
-        GT_META_DEFINE_ALIAS(apply, spec::stage, (Fun, OutTag, ArgTags));
+        GT_META_DEFINE_ALIAS(apply, spec::stage, (Stencil, OutTag, ArgTags));
     };
 
     template < size_t Arity,
-        class Spec,
-        class CallGraph = GT_META_CALL(call_graph, (Arity, Spec)),
+        class Composition,
+        class CallGraph = GT_META_CALL(call_graph, (Arity, Composition)),
         class Nodes = GT_META_CALL(flatten_call_graph, CallGraph),
         class StageList = GT_META_CALL(meta::transform, (stage_spec< Nodes >::template apply, Nodes)) >
     GT_META_DEFINE_ALIAS(stage_specs, meta::rename, (spec::stages, StageList));
 }
 
+using meta_detail::call_graph;
 using meta_detail::stage_specs;
 
-template < size_t Arity, class Spec, class Stages = GT_META_CALL(stage_specs, (Arity, Spec)) >
-intermediate_f< Stages > make_computation(grid_t const &grid, Spec) {
+template < size_t Arity, class Composition, class Stages = GT_META_CALL(stage_specs, (Arity, Composition)) >
+intermediate_f< Stages > make_computation(grid_t const &grid, Composition const &) {
     return {grid};
 }
 
-template < class Container >
-grid_t make_grid(Container const &container) {
-    auto size = size_2d(container);
-    return {{0, static_cast< ptrdiff_t >(size.x)}, {0, static_cast< ptrdiff_t >(size.y)}};
+/*
+ *  Concept Composability
+ *
+ *  Already done:
+ *    Stencil => LiftedStencil : lift
+ *    LiftedStencil => Stencil : degrade
+ *    StencilComposition => LiftedStencil : fuse
+ *    StencilComposition => Stages : stage_specs
+ *    Container& => (Output)Accessor : adapt
+ *    Stencil => StencilComposition : compose
+ *
+ *  TODO:
+ *    Stages => StencilComposition
+ */
+
+// Models StencilComposition
+template < class Stencil >
+struct single_stencil_composition_f {
+    Stencil m_stencil;
+    template < class Eval, class... Args >
+    auto operator()(Eval const &eval, Args const &... args) GT_AUTO_RETURN(eval(m_stencil, args...));
+};
+template < class Stencil >
+single_stencil_composition_f< Stencil > compose(const Stencil &stencil) {
+    return {stencil};
 }
-
-// Implementation of nested call.
-template < class Fun, class Indices, class... Args >
-struct applied_impl_f;
-
-template < class Fun, template < class... > class L, class... Is, class... Args >
-struct applied_impl_f< Fun, L< Is... >, Args... > {
-    Fun m_fun;
-    std::tuple< Args... > m_args;
-    double operator()(ptrdiff_t i = 0, ptrdiff_t j = 0) const {
-        return m_fun(shift(std::get< Is::value >(m_args), i, j)...);
-    }
-};
-
-template < class Fun, class... Args >
-using applied_f = applied_impl_f< Fun, GT_META_CALL(meta::make_indices_c, sizeof...(Args)), Args... >;
-
-struct fuse_eval_f {
-    template < class Fun, class... Args >
-    applied_f< Fun, Args... > operator()(Fun const &fun, Args const &... args) const {
-        return {fun, {args...}};
-    };
-};
-
-template < class Spec, class... Args >
-auto make_fused(Spec const &spec, Args const &... args) GT_AUTO_RETURN((spec(fuse_eval_f{}, args...)));
-
-const double PI = std::atan(1) * 4;
-
-// input data is taken form interface1 example
-struct input_f {
-    size_t m_x;
-    size_t m_y;
-    double operator()(double i = 0, double j = 0) const {
-        if (i < 0 || i >= m_x || j < 0 || j >= m_y)
-            return NAN;
-        double t = i / m_x + 1.5 * j / m_y;
-        return 5 + 8 * (2 + std::cos(PI * t) + std::sin(2 * PI * t)) / 4;
-    }
-};
-input_f make_input(size_t x, size_t y) { return {x, y}; }
 
 namespace static_tests {
 
-    using meta_detail::call_graph;
     using meta::list;
     using namespace spec;
 
@@ -493,19 +557,43 @@ namespace static_tests {
     static_assert(std::is_same< actual_stages, expected_stages >{}, "");
 }
 
-TEST(toy, diffusion) {
-    constexpr auto size = size_2d_t{9, 10};
-    double output_buf[size.x][size.y];
-    auto output = adapt(output_buf);
-    auto input = make_input(size.x, size.y);
-    auto coeff = [](ptrdiff_t = 0, ptrdiff_t = 0) { return .025; };
-    auto grid = make_grid(output_buf);
-    diffusion_f diffusion;
-    auto expected = make_fused(diffusion, input, coeff);
-    auto testee = make_computation< 2 >(grid, diffusion);
-    testee.run(output, input, coeff);
+namespace runtime_tests {
 
-    for (size_t i = 0; i < size.x; ++i)
-        for (size_t j = 0; j < size.y; ++j)
-            EXPECT_THAT(output(i, j), testing::NanSensitiveDoubleEq(expected(i, j))) << " (" << i << ", " << j << ")";
+    const double PI = std::atan(1) * 4;
+
+    // input data is taken form interface1 example
+    struct input_f {
+        size_t m_x;
+        size_t m_y;
+        double operator()(double i = 0, double j = 0) const {
+            if (i < 0 || i >= m_x || j < 0 || j >= m_y)
+                return NAN;
+            double t = i / m_x + 1.5 * j / m_y;
+            return 5 + 8 * (2 + std::cos(PI * t) + std::sin(2 * PI * t)) / 4;
+        }
+    };
+    input_f make_input(size_t x, size_t y) { return {x, y}; }
+
+    const auto coeff = [](ptrdiff_t = 0, ptrdiff_t = 0) { return .025; };
+
+    constexpr auto diffusion = diffusion_f{};
+    constexpr auto fused_diffusion = fuse(diffusion);
+
+    TEST(toy, diffusion) {
+        constexpr auto size = size_2d_t{9, 10};
+
+        double output_buf[size.x][size.y];
+        auto output = adapt(output_buf);
+        auto input = make_input(size.x, size.y);
+        auto grid = make_grid(output_buf);
+        auto expected = fused_diffusion(input, coeff);
+
+        auto testee = make_computation< 2 >(grid, diffusion);
+        testee.run(output, input, coeff);
+
+        for (size_t i = 0; i < size.x; ++i)
+            for (size_t j = 0; j < size.y; ++j)
+                EXPECT_THAT(output(i, j), testing::NanSensitiveDoubleEq(expected(i, j))) << " (" << i << ", " << j
+                                                                                         << ")";
+    }
 }
