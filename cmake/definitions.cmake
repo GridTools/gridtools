85f7e27476d1e6a1318b5a6894e4eb36ed6560e2
Felix Thaler <thaler@cscs.ch>
2018-07-06 16:34:26 +0200

    Added comment

--- cmake/definitions.cmake
+++ cmake/definitions.cmake
@@ -166,6 +166,7 @@ endif()
 if( USE_MPI )
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GCL_MPI_")
   find_package(MPI)
+  # only test for C++, Fortran MPI is not required
   if (NOT MPI_CXX_FOUND)
     message(FATAL_ERROR "Could not find MPI")
   endif()
