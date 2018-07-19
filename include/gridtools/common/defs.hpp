cf8c2fcd5ce8c8469a1bef951b25b70130e00dba
Hannes Vogt <hannes@havogt.de>
2018-07-19 13:42:59 +0200

    boost::optional workaround wrongly applied to < 9.2 as well

--- include/gridtools/common/defs.hpp
+++ include/gridtools/common/defs.hpp
@@ -156,7 +156,7 @@ namespace gridtools {
 #endif
 
 // check boost::optional workaround for CUDA9.2
-#if (defined(__CUDACC_VER_MAJOR__) && __CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ < 2)
+#if (defined(__CUDACC_VER_MAJOR__) && __CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ == 2)
 #if (not defined(BOOST_OPTIONAL_CONFIG_USE_OLD_IMPLEMENTATION_OF_OPTIONAL) || \
      not defined(BOOST_OPTIONAL_USE_OLD_DEFINITION_OF_NONE))
 #error \
