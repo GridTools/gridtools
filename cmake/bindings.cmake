96286a72e0ad35fb2d6348025293ddcf31db05b1
cosunae <carlos.osuna@meteoswiss.ch>
2018-07-20 08:49:42 +0200

    Cleaner Cmake Code for Bindings (#969)

--- cmake/bindings.cmake
+++ cmake/bindings.cmake
@@ -1,78 +1,49 @@
-if (DEFINED GRIDTOOLS_LIBRARIES_DIR)
-
-    macro(add_bindings_library)
-        add_library(${ARGV})
-        target_link_libraries(${ARGV0}
-            ${GRIDTOOLS_LIBRARIES_DIR}/libc_bindings_generator.a
+macro(add_bindings_library) 
+     
+    if (DEFINED GRIDTOOLS_LIBRARIES_DIR)
+        set(binding_libs ${GRIDTOOLS_LIBRARIES_DIR}/libc_bindings_generator.a
             ${GRIDTOOLS_LIBRARIES_DIR}/libc_bindings_handle.a)
-        add_custom_command(OUTPUT ${ARGV0}_empty.cpp COMMAND touch ${ARGV0}_empty.cpp)
-        add_executable(${ARGV0}_decl_generator
-            ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}_empty.cpp)
-        if (${APPLE})
-            target_link_libraries(${ARGV0}_decl_generator
-                -Wl,-force_load ${ARGV0}
-                ${GRIDTOOLS_LIBRARIES_DIR}/libc_bindings_generator_main.a)
-        else()
-            target_link_libraries(${ARGV0}_decl_generator
-                -Wl,--whole-archive ${ARGV0}
-                -Wl,--no-whole-archive ${GRIDTOOLS_LIBRARIES_DIR}/libc_bindings_generator_main.a)
-        endif()
-        add_custom_command(OUTPUT ${ARGV0}.h ${ARGV0}.f90
-                COMMAND ${ARGV0}_decl_generator ${ARGV0}.h ${ARGV0}.f90 ${ARGV0}
-                DEPENDS $<TARGET_FILE:${ARGV0}_decl_generator>)
-        add_custom_target(${ARGV0}_declarations
-                DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}.h ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}.f90)
-
-        add_library(${ARGV0}_c ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}_empty.cpp)
-        target_link_libraries(${ARGV0}_c ${ARGV0})
-        add_dependencies(${ARGV0}_c ${ARGV0}_declarations)
-        target_include_directories(${ARGV0}_c INTERFACE "${CMAKE_CURRENT_BINARY_DIR}")
-
-        add_library(${ARGV0}_fortran ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}.f90)
-        target_link_libraries(${ARGV0}_fortran ${ARGV0}
-            ${GRIDTOOLS_LIBRARIES_DIR}/libc_bindings_handle_fortran.a
+        set(binding_f90_libs ${GRIDTOOLS_LIBRARIES_DIR}/libc_bindings_handle_fortran.a
             ${GRIDTOOLS_LIBRARIES_DIR}/libarray_descriptor.a)
-        add_dependencies(${ARGV0}_fortran ${ARGV0}_declarations)
-        target_include_directories(${ARGV0}_fortran INTERFACE "${CMAKE_CURRENT_BINARY_DIR}")
+        set(binding_main_lib  ${GRIDTOOLS_LIBRARIES_DIR}/libc_bindings_generator_main.a)
+    else()
+        set(binding_libs c_bindings_generator c_bindings_handle)
+        set(binding_f90_libs c_bindings_handle_fortran array_descriptor)
+        set(binding_main_lib c_bindings_generator_main)
+    endif()
 
-    endmacro()
+    set(target_name ${ARGV0})
 
-else()
-    macro(add_bindings_library)
-        add_library(${ARGV})
-        target_link_libraries(${ARGV0}
-            c_bindings_generator
-            c_bindings_handle)
-        add_custom_command(OUTPUT ${ARGV0}_empty.cpp COMMAND touch ${ARGV0}_empty.cpp)
-        add_executable(${ARGV0}_decl_generator
-            ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}_empty.cpp)
-        if (${APPLE})
-            target_link_libraries(${ARGV0}_decl_generator
-                -Wl,-force_load ${ARGV0}
-                c_bindings_generator_main)
-        else()
-            target_link_libraries(${ARGV0}_decl_generator
-                -Wl,--whole-archive ${ARGV0}
-                -Wl,--no-whole-archive c_bindings_generator_main)
-        endif()
-        add_custom_command(OUTPUT ${ARGV0}.h ${ARGV0}.f90
-                COMMAND ${ARGV0}_decl_generator ${ARGV0}.h ${ARGV0}.f90 ${ARGV0}
-                DEPENDS $<TARGET_FILE:${ARGV0}_decl_generator>)
-        add_custom_target(${ARGV0}_declarations
-                DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}.h ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}.f90)
+    add_library(${ARGV})
 
-        add_library(${ARGV0}_c ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}_empty.cpp)
-        target_link_libraries(${ARGV0}_c ${ARGV0})
-        add_dependencies(${ARGV0}_c ${ARGV0}_declarations)
-        target_include_directories(${ARGV0}_c INTERFACE "${CMAKE_CURRENT_BINARY_DIR}")
+    target_link_libraries(${target_name} ${binding_libs})
+    add_custom_command(OUTPUT ${target_name}_empty.cpp COMMAND touch ${target_name}_empty.cpp)
+    add_executable(${target_name}_decl_generator
+            ${CMAKE_CURRENT_BINARY_DIR}/${target_name}_empty.cpp)
+    if (${APPLE})
+          target_link_libraries(${target_name}_decl_generator
+              -Wl,-force_load ${target_name} ${bindings_main}
+              ${binding_main_lib})
+    else()
+          target_link_libraries(${target_name}_decl_generator
+              -Wl,--whole-archive ${target_name}
+              -Wl,--no-whole-archive ${binding_main_lib})
+    endif()
+    add_custom_command(OUTPUT ${target_name}.h ${target_name}.f90
+            COMMAND ${target_name}_decl_generator ${target_name}.h ${target_name}.f90 ${target_name}
+            DEPENDS $<TARGET_FILE:${target_name}_decl_generator>)
+    add_custom_target(${target_name}_declarations
+            DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${target_name}.h ${CMAKE_CURRENT_BINARY_DIR}/${target_name}.f90)
 
-        add_library(${ARGV0}_fortran ${CMAKE_CURRENT_BINARY_DIR}/${ARGV0}.f90)
-        target_link_libraries(${ARGV0}_fortran ${ARGV0}
-            c_bindings_handle_fortran
-            array_descriptor)
-        add_dependencies(${ARGV0}_fortran ${ARGV0}_declarations)
-        target_include_directories(${ARGV0}_fortran INTERFACE "${CMAKE_CURRENT_BINARY_DIR}")
+    add_library(${target_name}_c ${CMAKE_CURRENT_BINARY_DIR}/${target_name}_empty.cpp)
+    target_link_libraries(${target_name}_c ${target_name})
+    add_dependencies(${target_name}_c ${target_name}_declarations)
+    target_include_directories(${target_name}_c INTERFACE "${CMAKE_CURRENT_BINARY_DIR}")
 
-    endmacro()
+    add_library(${target_name}_fortran ${CMAKE_CURRENT_BINARY_DIR}/${target_name}.f90)
+    target_link_libraries(${target_name}_fortran ${target_name} ${binding_f90_libs})
+    add_dependencies(${target_name}_fortran ${target_name}_declarations)
+    target_include_directories(${target_name}_fortran INTERFACE "${CMAKE_CURRENT_BINARY_DIR}")
 
-endif()
+endmacro()
+
