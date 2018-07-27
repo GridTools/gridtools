d4f7ebec04a530636b63ec6c2bc543944c1eb925
Lukas Mosimann <lmosimann@student.ethz.ch>
2018-07-27 11:39:36 +0200

    Add unittest which fails without fix

--- unit_tests/stencil-composition/structured_grids/test_kparallel.cpp
+++ unit_tests/stencil-composition/structured_grids/test_kparallel.cpp
@@ -51,23 +51,20 @@ using gridtools::uint_t;
 
 using namespace gridtools::enumtype;
 
-using axis = gridtools::axis<2>;
-using axis_lower = axis::get_interval<0>;
-using axis_upper = axis::get_interval<1>;
-
 namespace {
 
+    template <typename Axis>
     struct parallel_functor {
         typedef accessor<0> in;
         typedef accessor<1, inout> out;
         typedef boost::mpl::vector<in, out> arg_list;
 
         template <typename Evaluation>
-        GT_FUNCTION static void Do(Evaluation &eval, axis_lower) {
+        GT_FUNCTION static void Do(Evaluation &eval, typename Axis::template get_interval<0>) {
             eval(out()) = eval(in());
         }
         template <typename Evaluation>
-        GT_FUNCTION static void Do(Evaluation &eval, axis_upper) {
+        GT_FUNCTION static void Do(Evaluation &eval, typename Axis::template get_interval<1>) {
             eval(out()) = 2 * eval(in());
         }
     };
@@ -80,6 +77,7 @@ TEST(structured_grid, kparallel) {
     constexpr uint_t d3_l = 14;
     constexpr uint_t d3_u = 16;
 
+    using axis = gridtools::axis<2>;
     using storage_info_t = typename backend_t::storage_traits_t::storage_info_t<1, 3, gridtools::halo<0, 0, 0>>;
     using storage_t = backend_t::storage_traits_t::data_store_t<double, storage_info_t>;
 
@@ -96,7 +94,50 @@ TEST(structured_grid, kparallel) {
     auto copy = gridtools::make_computation<backend_t>(grid,
         p_in() = in,
         p_out() = out,
-        gridtools::make_multistage(execute<parallel, 20>(), gridtools::make_stage<parallel_functor>(p_in(), p_out())));
+        gridtools::make_multistage(
+            execute<parallel, 20>(), gridtools::make_stage<parallel_functor<axis>>(p_in(), p_out())));
+
+    copy.run();
+
+    copy.sync_bound_data_stores();
+
+    auto outv = make_host_view(out);
+    auto inv = make_host_view(in);
+    for (int i = 0; i < d1; ++i)
+        for (int j = 0; j < d2; ++j) {
+            for (int k = 0; k < d3_l; ++k)
+                EXPECT_EQ(inv(i, j, k), outv(i, j, k));
+            for (int k = d3_l; k < d3_u; ++k)
+                EXPECT_EQ(2 * inv(i, j, k), outv(i, j, k));
+        }
+}
+
+TEST(structured_grid, kparallel_with_extentoffsets_around_interval) {
+
+    constexpr uint_t d1 = 7;
+    constexpr uint_t d2 = 8;
+    constexpr uint_t d3_l = 14;
+    constexpr uint_t d3_u = 16;
+
+    using axis = gridtools::axis<2>::with_offset_limit<5>::with_extra_offsets<3>;
+    using storage_info_t = typename backend_t::storage_traits_t::storage_info_t<1, 3, gridtools::halo<0, 0, 0>>;
+    using storage_t = backend_t::storage_traits_t::data_store_t<double, storage_info_t>;
+
+    storage_info_t storage_info(d1, d2, d3_l + d3_u);
+
+    storage_t in(storage_info, [](int i, int j, int k) { return (double)(i * 1000 + j * 100 + k); });
+    storage_t out(storage_info, (double)1.5);
+
+    typedef arg<0, storage_t> p_in;
+    typedef arg<1, storage_t> p_out;
+
+    auto grid = gridtools::make_grid(d1, d2, axis(d3_l, d3_u));
+
+    auto copy = gridtools::make_computation<backend_t>(grid,
+        p_in() = in,
+        p_out() = out,
+        gridtools::make_multistage(
+            execute<parallel, 20>(), gridtools::make_stage<parallel_functor<axis>>(p_in(), p_out())));
 
     copy.run();
 
