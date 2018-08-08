GridTools/gridtools

commit 0d55c27b5aaa4d8c83baf6273d89ed57158f955e

Tag types for platform, grid and strategy (GC-455, #980)
Description: Replace platform, grid and strategy enums by tag types.

API change:
- platform::cuda, platform::x86 (former host), platform::mc (= many-core or multi-core or modern cpu; former mic)
- grid_type::structured, grid_type::icosahedral
- strategy::naive, strategy::block

include/gridtools/stencil-composition/caches/cache_metafunctions.hpp
@@ -114,12 +114,8 @@ namespace gridtools {
     template <cache_type cacheType>
     struct cache_is_type {
         template <typename Cache>
-        struct apply {
+        struct apply : static_bool<Cache::cacheType == cacheType> {
             GRIDTOOLS_STATIC_ASSERT((is_cache<Cache>::value), GT_INTERNAL_ERROR);
-            typedef
-                typename boost::is_same<enumtype::enum_type<cache_type, cacheType>, typename Cache::cache_type_t>::type
-                    type;
-            BOOST_STATIC_CONSTANT(bool, value = (type::value));
         };
     };
 

commit 472266ea0810660d740e97aa09cea00dfa2a593a

Bugfix: problem with IJ caches in dycore (regression from 0d55c27) (#993)
Description: Fixes #992 (regression of  0d55c27).

For some reason we need `boost::integral_constant` here.

include/gridtools/stencil-composition/caches/cache_metafunctions.hpp
@@ -114,7 +114,7 @@ namespace gridtools {
     template <cache_type cacheType>
     struct cache_is_type {
         template <typename Cache>
-        struct apply : static_bool<Cache::cacheType == cacheType> {
+        struct apply : boost::integral_constant<bool, Cache::cacheType == cacheType> {
             GRIDTOOLS_STATIC_ASSERT((is_cache<Cache>::value), GT_INTERNAL_ERROR);
         };
     };