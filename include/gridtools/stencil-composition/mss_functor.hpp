GridTools/gridtools

commit 0d55c27b5aaa4d8c83baf6273d89ed57158f955e

Tag types for platform, grid and strategy (GC-455, #980)
Description: Replace platform, grid and strategy enums by tag types.

API change:
- platform::cuda, platform::x86 (former host), platform::mc (= many-core or multi-core or modern cpu; former mic)
- grid_type::structured, grid_type::icosahedral
- strategy::naive, strategy::block

include/gridtools/stencil-composition/mss_functor.hpp
@@ -182,7 +182,7 @@ namespace gridtools {
             // Map between interval and actual arguments to pass to Do methods
             typedef typename mss_functor_do_method_lookup_maps<mss_components_t, Grid>::type functors_map_t;
 
-            typedef backend_traits_from_id<BackendIds::s_backend_id> backend_traits_t;
+            typedef backend_traits_from_id<typename BackendIds::backend_id_t> backend_traits_t;
 
             // compute the struct with all the type arguments for the run functor
 
@@ -264,8 +264,8 @@ namespace gridtools {
                 run_functor_args_t;
 
             // now the corresponding backend has to execute all the functors of the mss
-            backend_traits_from_id<BackendIds::s_backend_id>::template mss_loop<run_functor_args_t>::template run(
-                local_domain, m_grid, m_reduction_data, m_execution_info);
+            backend_traits_from_id<typename BackendIds::backend_id_t>::template mss_loop<
+                run_functor_args_t>::template run(local_domain, m_grid, m_reduction_data, m_execution_info);
         }
     };
 } // namespace gridtools

commit 963df1415affb932ef15cd91b5b0e0087acb1845

Bugfix for contains_no_IJ_cache_t (#998)
Description: We compared against an exact type `boost::integral_constant<bool, true>` instead of its contained value. This caused that we wrongly assume that all stencils don't contain IJ caches which implies that we don't do a necessary synchronization. This was fixed already in #993, but the reference was not updated (i.e. performance is worse, but results are fixed; it is a race-condition which we did not catch with our tests). Here we just fix the computation of the contains_no_IJ_cache_t.

Performance changes:
Reference reverted (degradation) to before #988, because the change in reference was due to the incorrect cache behavior introduced in #980, which was again fixed in #993.

include/gridtools/stencil-composition/mss_functor.hpp
@@ -42,12 +42,14 @@
 
 #pragma once
 
+#include "../common/generic_metafunctions/type_traits.hpp"
 #include "caches/cache_metafunctions.hpp"
 #include "hasdo.hpp"
 #include "mss.hpp"
 #include "mss_components_metafunctions.hpp"
 #include "mss_metafunctions.hpp"
 #include "run_functor_arguments.hpp"
+#include <boost/mpl/count_if.hpp>
 
 namespace gridtools {
 
@@ -237,10 +239,8 @@ namespace gridtools {
             // otherwise it could happen that warp A is already in level k+1
             // filling values in the cache while warp B did not consume the
             // cached values (written by A) from level k yet -> race condition)
-            typedef typename boost::mpl::transform<typename mss_components_t::cache_sequence_t, cache_is_type<IJ>>::type
-                cache_types_t;
-            typedef typename boost::mpl::not_<typename boost::mpl::contains<cache_types_t,
-                boost::integral_constant<bool, true>>::type>::type contains_no_IJ_cache_t;
+            using contains_no_IJ_cache_t = bool_constant<
+                boost::mpl::count_if<typename mss_components_t::cache_sequence_t, cache_is_type<IJ>>::type::value == 0>;
 
             typedef typename boost::mpl::insert<async_esf_map_tmp_t,
                 boost::mpl::pair<typename boost::mpl::at_c<functors_list_t, boost::mpl::size<next_thing>::value>::type,