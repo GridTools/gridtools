cc877102623d976b18eefa2db10606cb9bb2d8ce
Hannes Vogt <hannes@havogt.de>
2018-10-12 14:47:43 +0200

    change to CUDA9.2

--- pyutils/perftest/config/daint_gcc.py
+++ pyutils/perftest/config/daint_gcc.py
@@ -3,7 +3,7 @@
 from perftest.config import daint_common as default
 
 modules = default.modules | {'PrgEnv-gnu',
-                             'gcc/5.3.0'}
+                             'gcc/7.3.0'}
 
 env = dict(default.env,
            CXX='g++',
