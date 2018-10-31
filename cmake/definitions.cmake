20829a6e336d72f9159dda3deb526c590d1b8c8b
Mauro Bianco <mbianco@cscs.ch>
2018-10-31 17:10:00 +0100

    enabling 3.12 on kesch

--- cmake/definitions.cmake
+++ cmake/definitions.cmake
@@ -10,6 +10,8 @@ set(GT_CUDA_OPTIMIZATION_FLAGS) # Flags used for optimization
 
 set(GT_C_BUILDING_FLAGS)       # Flags for the C components (driver.c)
 
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wno-missing-include-dirs")
+
 ## set suppress messages ##
 if(SUPPRESS_MESSAGES)
     set( GT_CXX_BUILDING_FLAGS ${GT_CXX_BUILDING_FLAGS}  -DSUPPRESS_MESSAGES )
@@ -190,7 +192,7 @@ if(NOT ENABLE_CUDA)
           endforeach()
           set(OMP_LIBS ${OpenMP_CXX_LIBRARIES})
       else()
-          set( CMAKE_Fortran_FLAGS ${CMAKE_Fortran_FLAGS} ${OpenMP_Fortran_FLAGS} )
+          set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${OpenMP_Fortran_FLAGS}" )
           set( exe_LIBS ${exe_LIBS} ${OpenMP_CXX_LIBRARIES} )
           foreach( tmp ${OpenMP_C_LIBRARIES})
               string(STRIP ${tmp} tmp)
