8ed595844e7fe01b25b822becad0402d4ccfbaf3
Hannes Vogt <hannes@havogt.de>
2018-10-31 18:38:23 +0100

    fix repository

--- cmake/gt_bindings_generate.cmake
+++ cmake/gt_bindings_generate.cmake
@@ -8,8 +8,9 @@ function(check_and_update target_file new_file)
         message(STATUS "${target_file} is up-to-date")
         file(REMOVE ${new_file})
     else()
-        message(STATUS "${target_file} was generated")
-        message(STATUS "  If you ship the generated bindings with your sources, don't forget to ship this updated file (and its variants).")
+        message(WARNING "${target_file} was generated! "
+            "If you ship the generated bindings with your sources, don't forget to ship this updated file (and its variants). "
+            "Otherwise, this warning can be ignored.")
         file(RENAME ${new_file} ${target_file})
     endif()
 endfunction()
@@ -20,6 +21,6 @@ message(STATUS "Generating bindings for library ${FORTRAN_MODULE_NAME}")
 # run generator
 execute_process(COMMAND ${GENERATOR} ${BINDINGS_C_DECL_FILENAME}.new ${BINDINGS_FORTRAN_DECL_FILENAME}.new ${FORTRAN_MODULE_NAME})
 
-# only update the bindings if they changed (no rebuild is triggered if they didn't change)
+# only update the bindings if they changed (file not touched -> no rebuild is triggered)
 check_and_update(${BINDINGS_C_DECL_FILENAME} ${BINDINGS_C_DECL_FILENAME}.new)
 check_and_update(${BINDINGS_FORTRAN_DECL_FILENAME} ${BINDINGS_FORTRAN_DECL_FILENAME}.new)
---
+++ unit_tests/interface/repository/repository_float.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <gridtools/c_bindings/array_descriptor.h>
+#include <gridtools/c_bindings/handle.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+gt_handle* make_exported_repository(int, int, int);
+void set_exported_ijfield(gt_handle*, gt_fortran_array_descriptor*);
+void set_exported_ijkfield(gt_handle*, gt_fortran_array_descriptor*);
+void set_exported_jkfield(gt_handle*, gt_fortran_array_descriptor*);
+void verify_exported_repository(gt_handle*);
+
+#ifdef __cplusplus
+}
+#endif
