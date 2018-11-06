0c43e9bd781bd7dd8dccb57904e794a2c680447b
Anton Afanasyev <anstaf@gmail.com>
2018-11-06 17:25:50 +0100

    fix cuda9.0 comp

--- include/gridtools/meta/zip.hpp
+++ include/gridtools/meta/zip.hpp
@@ -36,7 +36,6 @@
 
 #pragma once
 
-#include "defs.hpp"
 #include "macros.hpp"
 #include "transform.hpp"
 
@@ -45,6 +44,13 @@ namespace gridtools {
      *  Zip lists
      */
     namespace meta {
+#if defined(__CUDACC_VER_MAJOR__) && __CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ < 2
+        GT_META_LAZY_NAMESPASE {
+            template <class... Lists>
+            struct zip : transpose<list<Lists...>> {};
+        }
+        GT_META_DELEGATE_TO_LAZY(zip, class... Lists, Lists...);
+#else
         GT_META_LAZY_NAMESPASE {
             template <class... Lists>
             GT_META_DEFINE_ALIAS(zip, transpose, list<Lists...>);
@@ -52,6 +58,7 @@ namespace gridtools {
 #if !GT_BROKEN_TEMPLATE_ALIASES
         template <class... Lists>
         using zip = typename lazy::transpose<list<Lists...>>::type;
+#endif
 #endif
     } // namespace meta
 } // namespace gridtools
