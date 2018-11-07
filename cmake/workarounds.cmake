c3500dfc9ef2099e3dedbc5d7328d9705acb1c2e
Lukas Mosimann <lukas.mosimann@cscs.ch>
2018-11-07 09:43:08 +0100

    Add TODO

--- cmake/workarounds.cmake
+++ cmake/workarounds.cmake
@@ -1,6 +1,10 @@
 # This function is a workaround for https://gitlab.kitware.com/cmake/cmake/issues/18558
 # For CUDA, the mpi compile options are non-empty, but the flags are invalid with nvcc, so we need
 # to exclude them for targets where COMPILE_LANGUAGE is not CXX
+# TODO: Decide how to do that:
+# - should we disable always if not CXX, or only if CUDA?
+# - should we do it for the other languages too?
+# - also set MPI_CXX_COMPILE_OPTIONS accordingly
 function (_fix_mpi_flags)
 
     if (MPI_FOUND)
