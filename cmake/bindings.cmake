e955438b31ca970c094f0f1c81d0da29b22222d4
Lukas Mosimann <lukas.mosimann@cscs.ch>
2018-12-17 15:44:58 +0100

    Rename BINDINGS_GENERATE_PATH -> BINDINGS_CMAKE_DIR

--- cmake/bindings.cmake
+++ cmake/bindings.cmake
@@ -1,6 +1,6 @@
 function(generate_gt_bindings)
     set(BINDINGS_SOURCE_DIR ${CMAKE_SOURCE_DIR}/src)
-    set(BINDINGS_GENERATE_PATH ${CMAKE_SOURCE_DIR}/cmake/)
+    set(BINDINGS_CMAKE_DIR ${CMAKE_SOURCE_DIR}/cmake/)
     set(BINDINGS_LIBRARIES GridTools)
     configure_file(cmake/gt_bindings.cmake.in
         ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/bindings_for_build/gt_bindings.cmake
--- cmake/export.cmake
+++ cmake/export.cmake
@@ -66,7 +66,7 @@ install(FILES "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/install/GridToolsCo
 # Also consider using a separate directory for source files which are installed, e.g. src_public
 if(COMPONENT_C_BINDINGS)
     set(BINDINGS_SOURCE_DIR "\${GridTools_SOURCES_PATH}")
-    set(BINDINGS_GENERATE_PATH "\${GridTools_MODULE_PATH}")
+    set(BINDINGS_CMAKE_PATH "\${GridTools_MODULE_PATH}")
     set(BINDINGS_LIBRARIES gridtools::GridTools)
     configure_file(cmake/gt_bindings.cmake.in
         ${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/build-install/lib/cmake/gt_bindings.cmake
