44d3fba7303dfe91bd31d1a02562ca5b34560d88
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-01-14 12:19:19 +0100

    Remove cudaDeviceSynchronize

--- include/gridtools/boundary-conditions/apply_gpu.hpp
+++ include/gridtools/boundary-conditions/apply_gpu.hpp
@@ -360,8 +360,8 @@ namespace gridtools {
                 m_conf,
                 m_halo_descriptors,
                 data_field_views...);
-            cudaDeviceSynchronize();
 #ifndef NDEBUG
+            cudaDeviceSynchronize();
             cudaError_t error = cudaGetLastError();
             if (error != cudaSuccess) {
                 fprintf(stderr, "CUDA ERROR: %s in %s at line %dn", cudaGetErrorString(error), __FILE__, __LINE__);
@@ -460,8 +460,8 @@ namespace gridtools {
                     ny,
                     nz,
                     data_field_views...);
-            cudaDeviceSynchronize();
 #ifndef NDEBUG
+            cudaDeviceSynchronize();
             cudaError_t error = cudaGetLastError();
             if (error != cudaSuccess) {
                 fprintf(stderr, "CUDA ERROR: %s in %s at line %dn", cudaGetErrorString(error), __FILE__, __LINE__);
@@ -538,8 +538,6 @@ namespace gridtools {
                 apply_it<direction<plus_, plus_, zero_>>(data_field_views...);
             if (predicate(direction<plus_, plus_, plus_>()))
                 apply_it<direction<plus_, plus_, plus_>>(data_field_views...);
-
-            cudaDeviceSynchronize();
         }
     };
 
