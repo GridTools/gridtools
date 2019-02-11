19fa2daa32a9ac55d2a63f9f9608ba878ac51e94
Hannes Vogt <hannes@havogt.de>
2019-02-11 14:07:13 +0100

    cleanup

--- include/gridtools/stencil-composition/execution_types.hpp
+++ include/gridtools/stencil-composition/execution_types.hpp
@@ -59,13 +59,13 @@ namespace gridtools {
         template <typename T>
         struct is_forward : std::false_type {};
 
-        template <uint_t BlockSize>
+        template <>
         struct is_forward<forward> : std::true_type {};
 
         template <typename T>
         struct is_backward : std::false_type {};
 
-        template <uint_t BlockSize>
+        template <>
         struct is_backward<backward> : std::true_type {};
     } // namespace execute
 
