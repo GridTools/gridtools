07a87a6b2ac42db6ae28787a0c093a0b9538d865
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-02-11 11:39:07 +0100

    improve cbindings exmaple

--- gt_examples/c_bindings/driver_mc.c
+++ gt_examples/c_bindings/driver_mc.c
@@ -35,6 +35,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "copy_stencil.h"
 
@@ -45,8 +46,8 @@ int main() {
     // Note that the order of the indices array here is k, j, i (which is the internal
     // Fortran layout). This is the layout that is expected by the bindings we have
     // written.
-    float in_array[11][10][9];
-    float out_array[11][10][9];
+    float *in_array = (float *)(malloc(11 * 10 * 9 * sizeof(float)));
+    float *out_array = (float *)(malloc(11 * 10 * 9 * sizeof(float)));
 
     // fill some inputs
     float n1 = 0;
@@ -54,8 +55,9 @@ int main() {
     for (int k = 0; k < 11; ++k)
         for (int j = 0; j < 10; ++j)
             for (int i = 0; i < 9; ++i, ++n1, --n2) {
-                in_array[k][j][i] = n1;
-                out_array[k][j][i] = n2;
+                int index = k * 10 * 9 + j * 9 + i;
+                in_array[index] = n1;
+                out_array[index] = n2;
             }
 
     // in the C bindings, the fortran array descriptors need to be filled explicitly
@@ -67,13 +69,15 @@ int main() {
     run_stencil(wrapper_handle, computation_handle, &in_descriptor, &out_descriptor);
 
     // now, the output can be verified
-    for (int k = 1; k < 11; ++k)
+    for (int k = 0; k < 11; ++k)
         for (int j = 0; j < 10; ++j)
-            for (int i = 0; i < 9; ++i)
-                if (in_array[k][j][i] != out_array[k][j][i]) {
+            for (int i = 0; i < 9; ++i) {
+                int idx = k * 10 * 9 + j * 9 + i;
+                if (in_array[idx] != out_array[idx]) {
                     printf("Error at position (k=%i, j=%i, i=%i)\n", k, j, i);
                     return 1;
                 }
+            }
 
     printf("It works!\n");
     return 0;
