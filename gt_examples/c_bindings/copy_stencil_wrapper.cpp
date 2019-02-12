3e631e96fa8c05c851f1680f681aaaf116812674
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-02-12 11:48:41 +0100

    arglist -> paramlist

--- gt_examples/c_bindings/copy_stencil_wrapper.cpp
+++ gt_examples/c_bindings/copy_stencil_wrapper.cpp
@@ -59,7 +59,7 @@ namespace {
     struct copy_functor {
         using in = gt::accessor<0, gt::enumtype::in>;
         using out = gt::accessor<1, gt::enumtype::inout>;
-        using arg_list = gt::make_arg_list<in, out>;
+        using param_list = gt::make_param_list<in, out>;
 
         template <typename Evaluation>
         GT_FUNCTION static void Do(Evaluation &eval) {
