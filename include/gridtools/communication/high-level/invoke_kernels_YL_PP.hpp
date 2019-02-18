e2e1c334ac873399937d0fdb34bd49a9f41476c0
Hannes Vogt <hannes@havogt.de>
2019-02-18 15:29:30 +0100

    update all files

--- include/gridtools/communication/high-level/invoke_kernels_YL_PP.hpp
+++ include/gridtools/communication/high-level/invoke_kernels_YL_PP.hpp
@@ -1,7 +1,10 @@
 /*
  * GridTools Libraries
- * Copyright (c) 2019, ETH Zurich
  *
+ * Copyright (c) 2019, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #define GCL_MACRO_IMPL(z, n, _)                                                                                  \
--- unit_tests/communication/proc_grid_3D.cpp
+++ unit_tests/communication/proc_grid_3D.cpp
@@ -1,7 +1,10 @@
 /*
  * GridTools Libraries
- * Copyright (c) 2019, ETH Zurich
  *
+ * Copyright (c) 2019, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #include <common/boollist.hpp>
