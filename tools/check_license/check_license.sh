afd78977b6d1c580c733394c0e1cd3146e68bd1a
Hannes Vogt <hannes@havogt.de>
2019-02-18 15:20:50 +0100

    update

--- tools/check_license/check_license.sh
+++ tools/check_license/check_license.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # run this script from the root directory
-if [ ! -f $PWD/tools/check_license/GRIDTOOLS_HEADER ] ; then
+if [ ! -f $PWD/LICENSE_HEADER ] ; then
     echo "you have to run the script ./tools/check_license/check_license.sh from the root of the sources"
     exit -1
 fi
@@ -9,17 +9,13 @@ for file in `find . -regextype posix-egrep -regex ".*\.(hpp|cpp|cu)$" `; do
     if ! grep -q "Copyright (c) " $file ; then
         echo $file
         if [ "$1" == "fix" ]; then
-            echo "/*
-$(cat tools/check_license/GRIDTOOLS_HEADER)
-*/
+            echo "$(cat LICENSE_HEADER)
 $(cat $file)" > $file
         fi
         else # if the license matches the string above
         if [ "$1" == "update" ]; then
             sed -i '0,/\*\//d' $file
-            echo "/*
-$(cat tools/check_license/GRIDTOOLS_HEADER)
-*/
+            echo "$(cat LICENSE_HEADER)
 $(cat $file)" > $file
         fi
     fi
