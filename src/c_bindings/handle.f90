4286d261a7fac1e016ac7cebfaa5eee02970cc15
Hannes Vogt <hannes@havogt.de>
2019-02-18 15:38:09 +0100

    manually update Fortran files

--- src/c_bindings/handle.f90
+++ src/c_bindings/handle.f90
@@ -1,3 +1,11 @@
+! GridTools Libraries
+!
+! Copyright (c) 2019, ETH Zurich
+! All rights reserved.
+!
+! Please, refer to the LICENSE file in the root directory.
+! SPDX-License-Identifier: BSD-3-Clause
+
 module gt_handle
     implicit none
     interface
