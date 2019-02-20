dcb8c0b88a2c1aac0bf19872ee4bd9425bfe6360
Hannes Vogt <hannes@havogt.de>
2019-02-20 09:17:46 +0100

    update .h files and comm

--- tools/check_license/check_license.sh
+++ tools/check_license/check_license.sh
@@ -5,18 +5,30 @@ if [ ! -f $PWD/LICENSE_HEADER ] ; then
     exit -1
 fi
 
-for file in `find . -regextype posix-egrep -regex ".*\.(hpp|cpp|cu)$" `; do
-    if ! grep -q "Copyright (c) " $file ; then
-        echo $file
-        if [ "$1" == "fix" ]; then
-            echo "$(cat LICENSE_HEADER)
+execludelist=(\
+    "./unit_tests/interface/repository/repository_double.h" \
+    "./unit_tests/interface/repository/repository_float.h" \
+    "./regression/c_bindings/implementation_float.h" \
+    "./regression/c_bindings/implementation_double.h" \
+    "./regression/c_bindings/implementation_wrapper_double.h" \ 
+    "./regression/c_bindings/implementation_wrapper_float.h" \
+    )
+
+for file in `find . -regextype posix-egrep -regex ".*\.(h|hpp|cpp|cu)$" -not -path "./build*" -not -path "./tools/*" -not -path "./docs/*"`; do
+
+    if [[ ! " ${execludelist[@]} " =~ " ${file} " ]]; then # if not in exclude list
+        if ! grep -q "Copyright (c) " $file ; then
+            echo $file
+            if [ "$1" == "fix" ]; then
+                echo "$(cat LICENSE_HEADER)
 $(cat $file)" > $file
-        fi
-        else # if the license matches the string above
-        if [ "$1" == "update" ]; then
-            sed -i '0,/\*\//d' $file
-            echo "$(cat LICENSE_HEADER)
+            fi
+            else # if the license matches the string above
+            if [ "$1" == "update" ]; then
+                sed -i '0,/\*\//d' $file
+                echo "$(cat LICENSE_HEADER)
 $(cat $file)" > $file
+            fi
         fi
     fi
 done
