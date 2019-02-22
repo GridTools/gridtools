aa0d66aca8b13a8f37a7f4e82dea2c7ab84a89e4
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-02-22 15:43:01 +0100

    Another fix for the pointer sharing constructor

--- include/gridtools/storage/common/storage_info.hpp
+++ include/gridtools/storage/common/storage_info.hpp
@@ -166,7 +166,11 @@ namespace gridtools {
                 else if (strides[i] == 0) {
                     m_padded_lengths[i] = 0;
                 } else {
-                    for (int j = i; j < ndims; ++j)
+                    int i_in_sorted_stride = 0;
+                    for (; i_in_sorted_stride < ndims; ++i_in_sorted_stride)
+                        if (strides[i] == sorted_strides[i_in_sorted_stride])
+                            break;
+                    for (int j = i_in_sorted_stride; j < ndims; ++j)
                         if (strides[i] != sorted_strides[j]) {
                             m_padded_lengths[i] = sorted_strides[j] / strides[i];
                             break;
