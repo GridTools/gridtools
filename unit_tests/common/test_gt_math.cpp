a8f7d299d9d50b5e566e6a6c40f1013ed1f39e8c
Lukas Mosimann <lmosimann@student.ethz.ch>
2019-03-28 08:59:27 +0100

    use cuda test helper for similar tests

--- unit_tests/common/test_gt_math.cpp
+++ unit_tests/common/test_gt_math.cpp
@@ -11,153 +11,150 @@
 #include <gridtools/common/defs.hpp>
 #include <gridtools/common/gt_math.hpp>
 #include <gridtools/tools/verifier.hpp>
+
 using namespace gridtools;
 
-template <typename Value>
-struct test_pow {
-    static bool GT_FUNCTION apply(Value val, Value result) {
-        return expect_with_threshold(math::pow(val, val), result);
-    }
-};
-
-template <typename Value>
-struct test_log {
-    static bool GT_FUNCTION apply(Value val, Value result) { return expect_with_threshold(math::log(val), result); }
-};
-
-template <typename Value>
-struct test_exp {
-    static bool GT_FUNCTION apply(Value val, Value result) { return expect_with_threshold(math::exp(val), result); }
-};
-
-struct test_fabs {
-    static bool GT_FUNCTION apply() {
-        GT_STATIC_ASSERT((std::is_same<decltype(math::fabs(4.0f)), float>::value), "Should return float.");
-        GT_STATIC_ASSERT((std::is_same<decltype(math::fabs(4.0)), double>::value), "Should return double.");
-#ifndef __CUDA_ARCH__
-        GT_STATIC_ASSERT(
-            (std::is_same<decltype(math::fabs((long double)4)), long double>::value), "Should return long double.");
-#endif
-        GT_STATIC_ASSERT((std::is_same<decltype(math::fabs((int)4)), double>::value), "Should return double.");
-
-        if (!expect_with_threshold(math::fabs(5.6), 5.6, 1e-14))
-            return false;
-        else if (!expect_with_threshold(math::fabs(-5.6), 5.6, 1e-14))
-            return false;
-        else if (!expect_with_threshold(math::fabs(-5.6f), 5.6f, 1e-14))
-            return false;
-        else if (!expect_with_threshold(math::fabs(-5), (double)5, 1e-14))
-            return false;
-#ifndef __CUDA_ARCH__
-        else if (!expect_with_threshold(math::fabs((long double)-5), (long double)5., 1e-14))
-            return false;
-#endif
-        else
-            return true;
-    }
-};
-
-struct test_abs {
-    static GT_FUNCTION bool apply() {
-        // float overloads
-        GT_STATIC_ASSERT((std::is_same<decltype(math::abs(4.0f)), float>::value), "Should return float.");
-        GT_STATIC_ASSERT((std::is_same<decltype(math::abs(4.0)), double>::value), "Should return double.");
-#ifndef __CUDA_ARCH__
-        GT_STATIC_ASSERT(
-            (std::is_same<decltype(math::abs((long double)4)), long double>::value), "Should return long double.");
-#endif
-
-        // int overloads
-        GT_STATIC_ASSERT((std::is_same<decltype(math::abs((int)4)), int>::value), "Should return int.");
-        GT_STATIC_ASSERT((std::is_same<decltype(math::abs((long)4)), long>::value), "Should return long.");
-        GT_STATIC_ASSERT(
-            (std::is_same<decltype(math::abs((long long)4)), long long>::value), "Should return long long.");
-
-        if (math::abs(5.6) != 5.6)
-            return false;
-        else if (math::abs(-5.6) != 5.6)
-            return false;
-        else if (math::abs(-5.6f) != 5.6f)
-            return false;
-        else if (math::abs(-5) != 5)
-            return false;
-        else
-            return true;
-    }
-};
-
-TEST(math, test_min) {
-    EXPECT_TRUE(math::min(5, 2, 7) == 2);
-    EXPECT_TRUE(math::min(5, -1) == -1);
-
-    ASSERT_EQ(math::min(5.3, 22.0, 7.7), 5.3);
+template <class Value>
+bool GT_FUNCTION test_pow(Value val, Value result) {
+    return expect_with_threshold(math::pow(val, val), result);
 }
+
+template <typename Value>
+bool GT_FUNCTION test_log(Value val, Value result) {
+    return expect_with_threshold(math::log(val), result);
+}
+
+template <typename Value>
+bool GT_FUNCTION test_exp(Value val, Value result) {
+    return expect_with_threshold(math::exp(val), result);
+}
+
+bool GT_FUNCTION test_fabs() {
+    GT_STATIC_ASSERT((std::is_same<decltype(math::fabs(4.0f)), float>::value), "Should return float.");
+    GT_STATIC_ASSERT((std::is_same<decltype(math::fabs(4.0)), double>::value), "Should return double.");
+#ifndef __CUDA_ARCH__
+    GT_STATIC_ASSERT(
+        (std::is_same<decltype(math::fabs((long double)4)), long double>::value), "Should return long double.");
+#endif
+    GT_STATIC_ASSERT((std::is_same<decltype(math::fabs((int)4)), double>::value), "Should return double.");
+
+    if (!expect_with_threshold(math::fabs(5.6), 5.6, 1e-14))
+        return false;
+    else if (!expect_with_threshold(math::fabs(-5.6), 5.6, 1e-14))
+        return false;
+    else if (!expect_with_threshold(math::fabs(-5.6f), 5.6f, 1e-14))
+        return false;
+    else if (!expect_with_threshold(math::fabs(-5), (double)5, 1e-14))
+        return false;
+#ifndef __CUDA_ARCH__
+    else if (!expect_with_threshold(math::fabs((long double)-5), (long double)5., 1e-14))
+        return false;
+#endif
+    else
+        return true;
+}
+
+GT_FUNCTION bool test_abs() {
+    // float overloads
+    GT_STATIC_ASSERT((std::is_same<decltype(math::abs(4.0f)), float>::value), "Should return float.");
+    GT_STATIC_ASSERT((std::is_same<decltype(math::abs(4.0)), double>::value), "Should return double.");
+#ifndef __CUDA_ARCH__
+    GT_STATIC_ASSERT(
+        (std::is_same<decltype(math::abs((long double)4)), long double>::value), "Should return long double.");
+#endif
+
+    // int overloads
+    GT_STATIC_ASSERT((std::is_same<decltype(math::abs((int)4)), int>::value), "Should return int.");
+    GT_STATIC_ASSERT((std::is_same<decltype(math::abs((long)4)), long>::value), "Should return long.");
+    GT_STATIC_ASSERT((std::is_same<decltype(math::abs((long long)4)), long long>::value), "Should return long long.");
+
+    if (math::abs(5.6) != 5.6)
+        return false;
+    else if (math::abs(-5.6) != 5.6)
+        return false;
+    else if (math::abs(-5.6f) != 5.6f)
+        return false;
+    else if (math::abs(-5) != 5)
+        return false;
+    else
+        return true;
+}
+
+namespace {
+    TEST(math, test_min) {
+        EXPECT_TRUE(math::min(5, 2, 7) == 2);
+        EXPECT_TRUE(math::min(5, -1) == -1);
+
+        ASSERT_EQ(math::min(5.3, 22.0, 7.7), 5.3);
+    }
 
 #ifdef __INTEL_COMPILER
-// test disabled due to Intel compiler bug (see gt_math.hpp)
-TEST(math, DISABLED_test_min_ref) {
+    // test disabled due to Intel compiler bug (see gt_math.hpp)
+    TEST(math, DISABLED_test_min_ref) {
 #else
-TEST(math, test_min_ref) {
+    TEST(math, test_min_ref) {
 #endif
-    // checking returned by const &
-    double a = 3.5;
-    double b = 2.3;
-    double const &min = math::min(a, b);
-    ASSERT_EQ(min, 2.3);
-    b = 8;
-    ASSERT_EQ(min, 8);
-}
+        // checking returned by const &
+        double a = 3.5;
+        double b = 2.3;
+        double const &min = math::min(a, b);
+        ASSERT_EQ(min, 2.3);
+        b = 8;
+        ASSERT_EQ(min, 8);
+    }
 
-TEST(math, test_max) {
-    EXPECT_TRUE(math::max(5, 2, 7) == 7);
-    EXPECT_TRUE(math::max(5, -1) == 5);
+    TEST(math, test_max) {
+        EXPECT_TRUE(math::max(5, 2, 7) == 7);
+        EXPECT_TRUE(math::max(5, -1) == 5);
 
-    ASSERT_EQ(math::max(5.3, 22.0, 7.7), 22.0);
-}
+        ASSERT_EQ(math::max(5.3, 22.0, 7.7), 22.0);
+    }
 
 #ifdef __INTEL_COMPILER
-// test disabled due to Intel compiler bug (see gt_math.hpp)
-TEST(math, DISABLED_test_max_ref) {
+    // test disabled due to Intel compiler bug (see gt_math.hpp)
+    TEST(math, DISABLED_test_max_ref) {
 #else
-TEST(math, test_max_ref) {
+    TEST(math, test_max_ref) {
 #endif
-    // checking returned by const &
-    double a = 3.5;
-    double b = 2.3;
-    double const &max = math::max(a, b);
+        // checking returned by const &
+        double a = 3.5;
+        double b = 2.3;
+        double const &max = math::max(a, b);
 
-    ASSERT_EQ(max, 3.5);
-    a = 8;
-    ASSERT_EQ(max, 8);
-}
+        ASSERT_EQ(max, 3.5);
+        a = 8;
+        ASSERT_EQ(max, 8);
+    }
 
-TEST(math, test_fabs) { EXPECT_TRUE(test_fabs::apply()); }
+    TEST(math, test_fabs) { EXPECT_TRUE(test_fabs()); }
 
-TEST(math, test_abs) { EXPECT_TRUE(test_abs::apply()); }
+    TEST(math, test_abs) { EXPECT_TRUE(test_abs()); }
 
-TEST(math, test_log) {
-    EXPECT_TRUE(test_log<double>::apply(2.3, std::log(2.3)));
-    EXPECT_TRUE(test_log<float>::apply(2.3f, std::log(2.3f)));
-}
+    TEST(math, test_log) {
+        EXPECT_TRUE(test_log<double>(2.3, std::log(2.3)));
+        EXPECT_TRUE(test_log<float>(2.3f, std::log(2.3f)));
+    }
 
-TEST(math, test_exp) {
-    EXPECT_TRUE(test_exp<double>::apply(2.3, std::exp(2.3)));
-    EXPECT_TRUE(test_exp<float>::apply(2.3f, std::exp(2.3f)));
-}
+    TEST(math, test_exp) {
+        EXPECT_TRUE(test_exp<double>(2.3, std::exp(2.3)));
+        EXPECT_TRUE(test_exp<float>(2.3f, std::exp(2.3f)));
+    }
 
-TEST(math, test_pow) {
-    EXPECT_TRUE(test_pow<double>::apply(2.3, std::pow(2.3, 2.3)));
-    EXPECT_TRUE(test_pow<float>::apply(2.3f, std::pow(2.3f, 2.3f)));
-}
+    TEST(math, test_pow) {
+        EXPECT_TRUE(test_pow<double>(2.3, std::pow(2.3, 2.3)));
+        EXPECT_TRUE(test_pow<float>(2.3f, std::pow(2.3f, 2.3f)));
+    }
 
-TEST(math, test_fmod) {
-    EXPECT_FLOAT_EQ(math::fmod(3.7f, 1.2f), std::fmod(3.7f, 1.2f));
-    EXPECT_DOUBLE_EQ(math::fmod(3.7, 1.2), std::fmod(3.7, 1.2));
-    EXPECT_DOUBLE_EQ(math::fmod(3.7l, 1.2l), std::fmod(3.7l, 1.2l));
-}
+    TEST(math, test_fmod) {
+        EXPECT_FLOAT_EQ(math::fmod(3.7f, 1.2f), std::fmod(3.7f, 1.2f));
+        EXPECT_DOUBLE_EQ(math::fmod(3.7, 1.2), std::fmod(3.7, 1.2));
+        EXPECT_DOUBLE_EQ(math::fmod(3.7l, 1.2l), std::fmod(3.7l, 1.2l));
+    }
 
-TEST(math, test_trunc) {
-    EXPECT_FLOAT_EQ(math::trunc(3.7f), std::trunc(3.7f));
-    EXPECT_DOUBLE_EQ(math::trunc(3.7), std::trunc(3.7));
-    EXPECT_DOUBLE_EQ(math::trunc(3.7l), std::trunc(3.7l));
-}
+    TEST(math, test_trunc) {
+        EXPECT_FLOAT_EQ(math::trunc(3.7f), std::trunc(3.7f));
+        EXPECT_DOUBLE_EQ(math::trunc(3.7), std::trunc(3.7));
+        EXPECT_DOUBLE_EQ(math::trunc(3.7l), std::trunc(3.7l));
+    }
+
+} // namespace
--- unit_tests/common/test_gt_math.cu
+++ unit_tests/common/test_gt_math.cu
@@ -11,38 +11,24 @@
 
 #include "../cuda_test_helper.hpp"
 
-TEST(math_cuda, test_fabs) {
-    EXPECT_TRUE(test_fabs::apply());
-    EXPECT_TRUE(cuda_test<test_fabs>());
-}
+namespace {
 
-TEST(math_cuda, test_abs) {
-    EXPECT_TRUE(test_fabs::apply());
-    EXPECT_TRUE(cuda_test<test_fabs>());
-}
+    TEST(math_cuda, test_fabs) { EXPECT_TRUE(on_device::exec(MAKE_CONSTANT(test_fabs))); }
+    TEST(math_cuda, test_abs) { EXPECT_TRUE(on_device::exec(MAKE_CONSTANT(test_fabs))); }
 
-TEST(math_cuda, test_log) {
-    EXPECT_TRUE(test_log<double>::apply(2.3, std::log(2.3)));
-    EXPECT_TRUE(test_log<float>::apply(2.3f, std::log(2.3f)));
+    TEST(math_cuda, test_log) {
+        EXPECT_TRUE(on_device::exec(MAKE_CONSTANT(test_log<double>), 2.3, std::log(2.3)));
+        EXPECT_TRUE(on_device::exec(MAKE_CONSTANT(test_log<float>), 2.3f, std::log(2.3f)));
+    }
 
-    EXPECT_TRUE(cuda_test<test_log<double>>(2.3, std::log(2.3)));
-    EXPECT_TRUE(cuda_test<test_log<float>>(2.3f, std::log(2.3f)));
-}
+    TEST(math_cuda, test_exp) {
+        EXPECT_TRUE(on_device::exec(MAKE_CONSTANT(test_exp<double>), 2.3, std::exp(2.3)));
+        EXPECT_TRUE(on_device::exec(MAKE_CONSTANT(test_exp<float>), 2.3f, std::exp(2.3f)));
+    }
 
-TEST(math_cuda, test_exp) {
+    TEST(math_cuda, test_pow) {
+        EXPECT_TRUE(on_device::exec(MAKE_CONSTANT(test_pow<double>), 2.3, std::pow(2.3, 2.3)));
+        EXPECT_TRUE(on_device::exec(MAKE_CONSTANT(test_pow<float>), 2.3f, std::pow(2.3f, 2.3f)));
+    }
 
-    EXPECT_TRUE(test_exp<double>::apply(2.3, std::exp(2.3)));
-    EXPECT_TRUE(test_exp<float>::apply(2.3f, std::exp(2.3f)));
-
-    EXPECT_TRUE(cuda_test<test_exp<double>>(2.3, std::exp(2.3)));
-    EXPECT_TRUE(cuda_test<test_exp<float>>(2.3f, std::exp(2.3f)));
-}
-
-TEST(math_cuda, test_pow) {
-
-    EXPECT_TRUE(test_pow<double>::apply(2.3, std::pow(2.3, 2.3)));
-    EXPECT_TRUE(test_pow<float>::apply(2.3f, std::pow(2.3f, 2.3f)));
-
-    EXPECT_TRUE(cuda_test<test_pow<double>>(2.3, std::pow(2.3, 2.3)));
-    EXPECT_TRUE(cuda_test<test_pow<float>>(2.3f, std::pow(2.3f, 2.3f)));
-}
+} // namespace
