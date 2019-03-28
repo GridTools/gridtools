58573c6a7b38950fa1ea9c20354ec4a15651f3d0
Felix Thaler <thaler@cscs.ch>
2019-03-28 15:27:47 +0100

    Cleanup

--- include/gridtools/stencil_composition/global_parameter.hpp
+++ include/gridtools/stencil_composition/global_parameter.hpp
@@ -14,25 +14,21 @@
 #include "../storage/storage_facility.hpp"
 
 namespace gridtools {
-    /**
-        Method to retrieve a global parameter
-     */
     template <class Target, class T>
-    static typename storage_traits<Target>::template data_store_t<T,
-        typename storage_traits<Target>::template special_storage_info_t<0, selector<0u>, zero_halo<1>>>
-    make_global_parameter(T const &value) {
-        typename storage_traits<Target>::template special_storage_info_t<0, selector<0u>, zero_halo<1>> si(1);
-        typename storage_traits<Target>::template data_store_t<T, decltype(si)> ds(si);
+    using global_parameter = typename storage_traits<Target>::template data_store_t<T,
+        typename storage_traits<Target>::template special_storage_info_t<0, selector<0>, zero_halo<1>>>;
+
+    template <class Target, class T>
+    global_parameter<Target, T> make_global_parameter(T const &value) {
+        typename global_parameter<Target, T>::storage_info_t si(1);
+        global_parameter<Target, T> ds(si);
         make_host_view(ds)(0) = value;
         ds.sync();
         return ds;
     }
 
-    /**
-        Method to update a global parameter
-     */
     template <class GlobalParameter, class T>
-    static void update_global_parameter(GlobalParameter &gp, T const &value) {
+    void update_global_parameter(GlobalParameter &gp, T const &value) {
         gp.sync();
         auto view = make_host_view(gp);
         assert(check_consistency(gp, view) && "Cannot create a valid view to a global parameter. Properly synced?");
