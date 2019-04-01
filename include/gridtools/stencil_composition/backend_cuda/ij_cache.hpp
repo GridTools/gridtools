61d54c0bdfcd82c4caeddbfd7a06b6dd40148f65
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-04-01 09:31:05 +0200

    icosahedral

--- include/gridtools/stencil_composition/backend_cuda/ij_cache.hpp
+++ include/gridtools/stencil_composition/backend_cuda/ij_cache.hpp
@@ -19,7 +19,6 @@
 
 namespace gridtools {
 #ifndef GT_ICOSAHEDRAL_GRIDS
-
     namespace ij_cache_impl_ {
         template <int_t IStride, int_t JStride>
         using strides_map_t =
@@ -32,7 +31,7 @@ namespace gridtools {
             int_t JZero,
             int_t IStride = 1,
             int_t JStride = IStride *ISize,
-            int_t Size = ISize *JSize,
+            int_t Size = JStride *JSize,
             int_t Offset = IStride *IZero + JStride *JZero>
         auto make_ij_cache_helper(shared_allocator &allocator) GT_AUTO_RETURN(
             sid::synthetic()
@@ -45,32 +44,33 @@ namespace gridtools {
     auto make_ij_cache(shared_allocator &allocator)
         GT_AUTO_RETURN((ij_cache_impl_::make_ij_cache_helper<T, ISize, JSize, IZero, JZero>(allocator)));
 #else
-#endif
+    namespace ij_cache_impl_ {
+        template <int_t IStride, int_t CStride, int_t JStride>
+        using strides_map_t = hymap::keys<dim::i, dim::c, dim::j>::values<integral_constant<int_t, IStride>,
+            integral_constant<int_t, CStride>,
+            integral_constant<int_t, JStride>>;
 
-#ifdef asdfweroji
+        template <class T,
+            int_t ISize,
+            int_t NumColors,
+            int_t JSize,
+            int_t IZero,
+            int_t JZero,
+            int_t IStride = 1,
+            int_t CStride = IStride *NumColors,
+            int_t JStride = CStride *JSize,
+            int_t Size = JStride *JSize,
+            int_t Offset = IStride *IZero + JStride *JZero>
+        auto make_ij_cache_helper(shared_allocator &allocator) GT_AUTO_RETURN(
+            sid::synthetic()
+                .template set<sid::property::origin>(allocator.template allocate<T>(Size) + Offset)
+                .template set<sid::property::strides>(ij_cache_impl_::strides_map_t<IStride, CStride, JStride>{}));
+
+    } // namespace ij_cache_impl_
+
     template <class T, int_t ISize, int_t NumColors, int_t JSize, int_t IZero, int_t JZero>
-    class sid_ij_cache {
-      private:
-        int_t m_allocation; // in bytes
-
-        static constexpr int_t c_stride = i_stride * NumColors;
-        static constexpr int_t j_stride = c_stride * JSize;
-        static constexpr int_t size = j_stride * JSize;
-
-        using StrideMap = hymap::keys<dim::i, dim::c, dim::j>::values<integral_constant<int_t, i_stride>,
-            integral_constant<int_t, c_stride>,
-            integral_constant<int_t, j_stride>>;
-
-      public:
-        template <typename Allocator>
-        sid_ij_cache(Allocator &allocator) : m_allocation(allocator.template allocate<sizeof(T)>(size * sizeof(T))) {}
-
-        friend ptr_holder<T> sid_get_origin(sid_ij_cache &cache) {
-            constexpr int_t offset = IZero * i_stride + JZero * j_stride;
-            return cache.m_allocation + offset * sizeof(T);
-        }
-        friend stride_map_t sid_get_strides(sid_ij_cache const &) { return {}; }
-    };
+    auto make_ij_cache(shared_allocator &allocator)
+        GT_AUTO_RETURN((ij_cache_impl_::make_ij_cache_helper<T, ISize, NumColors, JSize, IZero, JZero>(allocator)));
 #endif
 
 } // namespace gridtools
