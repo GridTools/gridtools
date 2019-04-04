58aa95822441f6d1b37c9bdb4cf173613fbdcfca
Hannes Vogt <hannes@havogt.de>
2019-04-04 15:19:33 +0200

    fix find/replace issue

--- unit_tests/stencil_composition/test_axis.cpp
+++ unit_tests/stencil_composition/test_axis.cpp
@@ -20,7 +20,7 @@ template <uint_t Splitter, int_t Offset>
 using level_t = level<Splitter, Offset, level_offset_limit>;
 
 TEST(test_axis, ctor) {
-    using axis_t = axis<2, gridtools::axis<2, axis_config::offset_limit<level_offset_limit>>>;
+    using axis_t = axis<2, axis_config::offset_limit<level_offset_limit>>;
     auto axis_ = axis_t((uint_t)5, (uint_t)4);
 
     ASSERT_EQ(5, axis_.interval_size(0));
@@ -28,7 +28,7 @@ TEST(test_axis, ctor) {
 }
 
 TEST(test_axis, intervals) {
-    using axis_t = axis<3, gridtools::axis<2, axis_config::offset_limit<level_offset_limit>>>;
+    using axis_t = axis<3, axis_config::offset_limit<level_offset_limit>>;
 
     // full interval
     ASSERT_TYPE_EQ<interval<level_t<0, 1>, level_t<3, -1>>, axis_t::full_interval>();
