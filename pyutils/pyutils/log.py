01b5b0524a168945ba18134d2acafa004051c8f0
Felix Thaler <thaler@cscs.ch>
2019-04-10 09:22:54 +0200

    Fix log

--- pyutils/pyutils/log.py
+++ pyutils/pyutils/log.py
@@ -8,13 +8,14 @@ import textwrap
 
 
 _logger = logging.getLogger('pyutils')
+_logger.setLevel(logging.DEBUG)
 
 _formatter = logging.Formatter('%(levelname)s %(asctime)s: %(message)s',
                                '%Y-%m-%d %H:%M:%S')
 
 logfile = os.path.abspath('pyutils.log')
 
-_filehandler = logging.Filehandler(logfile)
+_filehandler = logging.FileHandler(logfile)
 _filehandler.setFormatter(_formatter)
 _filehandler.setLevel(logging.DEBUG)
 _logger.addHandler(_filehandler)
@@ -25,9 +26,9 @@ _streamhandler.setLevel(logging.WARNING)
 _logger.addHandler(_streamhandler)
 
 def set_verbosity(level):
-    if verbosity == 0:
+    if level <= 0:
         _streamhandler.setLevel(logging.WARNING)
-    elif verbosity == 1:
+    elif level == 1:
         _streamhandler.setLevel(logging.INFO)
     else:
         _streamhandler.setLevel(logging.DEBUG)
@@ -38,8 +39,8 @@ def exception_logging():
     try:
         yield
     except Exception:
-        logger.exception(f'Fatal error: exception was raised, '
-                         f'logfile saved at "{logfile}"')
+        _logger.exception(f'Fatal error: exception was raised, '
+                          f'logfile saved at "{logfile}"')
         sys.exit(1)
 
 
