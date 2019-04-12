2b26621198e9b4723ca2da90188b2756971db10d
Felix Thaler <thaler@cscs.ch>
2019-04-12 17:02:29 +0200

    Updated run_perftest_PR.sh

--- jenkins/run_perftest_PR.sh
+++ jenkins/run_perftest_PR.sh
@@ -1,8 +1,11 @@
 # use the machines python virtualenv with required modules (matplotlib) installed
 source /project/c14/jenkins/python-venvs/${label%%-*}/bin/activate
 
-# get config name, consisting of label without postfix (like the -cn from daint-cn) and compiler name
-config=${label%%-*}_$compiler
+if [[ $label != "kesch" ]]; then
+    export SLURM_ACCOUNT=c14
+    export SBATCH_ACCOUNT=c14
+fi
+
 
 # create directory for temporaries
 if [[ $label == "tave" ]]; then
@@ -15,29 +18,31 @@ fi
 mkdir -p $tmpdir
 export TMPDIR=$tmpdir
 
+grid=structured
+
 # build binaries for performance tests
-./pyutils/build.py -vvv --build-type release --backend $backend --precision $precision --grid $grid --build-dir build --config $config --perftest-targets || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
+./pyutils/builddriver.py -vvv -b release -p $real_type -g $grid -o build -d $target -c $compiler -t perftests || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
 
 for domain in 128 256; do
   # result directory, create if it does not exist yet
-  resultdir=./build/pyutils/results/$config/$grid/$precision/$backend/$domain
+  resultdir=./build/pyutils/perftest/results/$grid/$real_type/$domain/${label%%-*}_${target}_$compiler
   mkdir -p $resultdir
   
   # run performance tests
-  ./build/pyutils/perfdriver.py -vvv run -d $domain $domain 80 --config $config -o $resultdir/result.json || { echo 'Running failed'; rm -rf $tmpdir; exit 1; }
+  ./build/pyutils/perfdriver.py -vvv run -s $domain $domain 80 -o $resultdir/result.json || { echo 'Running failed'; rm -rf $tmpdir; exit 1; }
   
-  # find references for same config-grid-precision-backend combination
-  reference_path=./pyutils/references/$config/$grid/$precision/$backend/$domain
+  # find references for same config-grid-real_type-backend combination
+  reference_path=./pyutils/perftest/references/$grid/$real_type/$domain/${label%%-*}_${target}_$compiler
   if [ -d $reference_path ]; then
   	stella_reference=$(find $reference_path -name stella.json)
-  	gridtools_reference=$(find $reference_path -name result.json)
-  	references="$stella_reference $gridtools_reference"
+  	gridtools_references=$(find $reference_path -name 'result.*.json')
+  	references="$stella_reference $gridtools_references"
   else
   	references=""
   fi
   
   # plot comparison of current result with references
-  ./build/pyutils/perfdriver.py -vvv plot compare -i $references $resultdir/result.json -o plot-$domain.png || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
+  ./build/pyutils/perfdriver.py -vvv plot compare -i $references $resultdir/result.*.json -o plot-$domain.png || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
 done
 
 # clean possible temporary leftovers
