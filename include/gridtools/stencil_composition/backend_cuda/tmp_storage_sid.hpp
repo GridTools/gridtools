56e3fa16c33dff6256be1f9482247848c99bfe0d
Hannes Vogt <hannes@havogt.de>
2019-04-15 09:53:46 +0200

    wip icosahedral

--- include/gridtools/stencil_composition/backend_cuda/tmp_storage_sid.hpp
+++ include/gridtools/stencil_composition/backend_cuda/tmp_storage_sid.hpp
@@ -11,12 +11,14 @@
 
 #include "../../common/hymap.hpp"
 #include "../../common/integral_constant.hpp"
+#include "../color.hpp"
 #include "../dim.hpp"
 #include "../extent.hpp"
 #include "../sid/synthetic.hpp"
 #include <memory>
 
 namespace gridtools {
+#ifndef GT_ICOSAHEDRAL_GRIDS
     namespace tmp_cuda_impl_ {
         template <class Strides, class BlockSizeI, class BlockSizeJ>
         Strides compute_strides(int_t n_blocks_i, int_t n_blocks_j) {
@@ -49,6 +51,10 @@ namespace gridtools {
     /**
      * @brief SID for CUDA temporaries.
      * get_origin() points to first element of compute domain
+     * TODO(havogt): during integration we need to evaluate different types of alignment:
+     *  - no alignment (current implementation)
+     *  - alignment similar to old implementation (first data point in compute domain)
+     *  - align first element of the temporary (i.e. in the redundant computation region)
      */
     template <class T,
         int_t ComputeBlockSizeI,
@@ -85,5 +91,85 @@ namespace gridtools {
                         tmp_cuda_impl_::compute_strides<Strides, BlockSizeI, BlockSizeJ>(n_blocks_i, n_blocks_j))
                     .template set<sid::property::ptr_diff, int_t>()
                     .template set<sid::property::strides_kind, Strides>()));
+
+#else
+#warning "correct branch"
+    namespace tmp_cuda_impl_ {
+        template <class Strides, class BlockSizeI, class BlockSizeJ>
+        Strides compute_strides(int_t n_blocks_i, int_t n_blocks_j) {
+            return {integral_constant<int_t, 1>{}, // TODO support for default init {} in hymap
+                BlockSizeI{},
+                integral_constant<int_t, BlockSizeI{} * BlockSizeJ{}>{},
+                integral_constant<int_t, BlockSizeI{} * BlockSizeJ{} * 1>{}, // TODO 1 -> NColors
+                GT_META_CALL(meta::at_c, (Strides, 2))::value * n_blocks_i,
+                GT_META_CALL(meta::at_c, (Strides, 2))::value * n_blocks_i * n_blocks_j};
+        }
+
+        template <class BlockSizeI, class BlockSizeJ>
+        std::size_t compute_size(int_t n_blocks_i, int_t n_blocks_j, int_t k_size) {
+            return BlockSizeI{} * BlockSizeJ{} * n_blocks_i * n_blocks_j * k_size;
+        }
+
+        template <class Strides>
+        int_t compute_origin_offset(Strides const &strides, int_t shift_i, int_t shift_j) {
+            return shift_i * at_key<dim::i>(strides) + shift_j * at_key<dim::j>(strides);
+        }
+    } // namespace tmp_cuda_impl_
+
+    namespace tmp_cuda {
+        struct block_i;
+        struct block_j;
+
+        template <int_t, int_t>
+        struct blocksize {};
+    } // namespace tmp_cuda
+
+    /**
+     * @brief SID for CUDA temporaries.
+     * get_origin() points to first element of compute domain
+     * TODO(havogt): during integration we need to evaluate different types of alignment:
+     *  - no alignment (current implementation)
+     *  - alignment similar to old implementation (first data point in compute domain)
+     *  - align first element of the temporary (i.e. in the redundant computation region)
+     */
+    template <class T,
+        int_t ComputeBlockSizeI,
+        int_t ComputeBlockSizeJ,
+        int_t ExtentIMinus, // negative by convention
+        int_t ExtentIPlus,
+        int_t ExtentJMinus, // negative by convention
+        int_t ExtentJPlus,
+        uint_t NColors,
+        class Allocator,
+        class BlockSizeI = integral_constant<int_t, ComputeBlockSizeI - ExtentIMinus + ExtentIPlus>,
+        class BlockSizeJ = integral_constant<int_t, ComputeBlockSizeJ - ExtentJMinus + ExtentJPlus>,
+        class Strides = hymap::keys<dim::i, dim::j, dim::c, tmp_cuda::block_i, tmp_cuda::block_j, dim::k>::values<
+            integral_constant<int_t, 1>,
+            BlockSizeI,
+            integral_constant<int_t, BlockSizeI{} * BlockSizeJ{}>,
+            integral_constant<int_t, BlockSizeI{} * BlockSizeJ{} * NColors>,
+            int_t,
+            int_t>>
+    auto make_tmp_storage_cuda(tmp_cuda::blocksize<ComputeBlockSizeI, ComputeBlockSizeJ>,
+        extent<ExtentIMinus, ExtentIPlus, ExtentJMinus, ExtentJPlus>,
+        color_type<NColors>,
+        int_t n_blocks_i,
+        int_t n_blocks_j,
+        int_t k_size,
+        Allocator &&alloc) {
+        return sid::synthetic()
+            .set<sid::property::origin>(
+                alloc.template allocate<T>(
+                    tmp_cuda_impl_::compute_size<BlockSizeI, BlockSizeJ>(n_blocks_i, n_blocks_j, k_size)) +
+                tmp_cuda_impl_::compute_origin_offset(
+                    tmp_cuda_impl_::compute_strides<Strides, BlockSizeI, BlockSizeJ>(n_blocks_i, n_blocks_j),
+                    -ExtentIMinus,
+                    -ExtentJMinus))
+            .template set<sid::property::strides>(
+                tmp_cuda_impl_::compute_strides<Strides, BlockSizeI, BlockSizeJ>(n_blocks_i, n_blocks_j))
+            .template set<sid::property::ptr_diff, int_t>()
+            .template set<sid::property::strides_kind, Strides>();
+    }
+#endif
 
 } // namespace gridtools
---
+++ unit_tests/stencil_composition/icosahedral_grids/test_tmp_storage_sid_cuda.cu
@@ -0,0 +1,83 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2019, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <gridtools/common/tuple_util.hpp>
+#include <gridtools/meta/list.hpp>
+#include <gridtools/stencil_composition/backend_cuda/simple_device_memory_allocator.hpp>
+#include <gridtools/stencil_composition/backend_cuda/tmp_storage_sid.hpp>
+#include <gridtools/stencil_composition/color.hpp>
+#include <gridtools/stencil_composition/sid/concept.hpp>
+#include <gridtools/tools/backend_select.hpp>
+
+#include <type_traits>
+
+#include "../../test_helper.hpp"
+#include <gtest/gtest.h>
+
+namespace gridtools {
+    namespace {
+        namespace tu = tuple_util;
+        using tuple_util::get;
+
+        using data_t = float_type;
+
+        constexpr int_t extent_i_minus = -1;
+        constexpr int_t extent_i_plus = 2;
+        constexpr int_t extent_j_minus = -3;
+        constexpr int_t extent_j_plus = 4;
+
+        constexpr int_t blocksize_i = 32;
+        constexpr int_t blocksize_j = 8;
+
+        constexpr uint_t n_colors = 2;
+
+        TEST(tmp_cuda_storage_sid, maker) {
+            int_t n_blocks_i = 11;
+            int_t n_blocks_j = 12;
+            int_t k_size = 13;
+
+            simple_device_memory_allocator alloc;
+
+            auto testee = make_tmp_storage_cuda<data_t>(tmp_cuda::blocksize<blocksize_i, blocksize_j>{},
+                extent<extent_i_minus, extent_i_plus, extent_j_minus, extent_j_plus>{},
+                color_type<n_colors>{},
+                n_blocks_i,
+                n_blocks_j,
+                k_size,
+                alloc);
+
+            using tmp_cuda_t = decltype(testee);
+            static_assert(is_sid<tmp_cuda_t>(), "");
+            ASSERT_TYPE_EQ<GT_META_CALL(sid::ptr_type, tmp_cuda_t), data_t *>();
+            ASSERT_TYPE_EQ<GT_META_CALL(sid::ptr_diff_type, tmp_cuda_t), int_t>();
+
+            auto strides = sid::get_strides(testee);
+
+            int expected_stride0 = 1;
+            int expected_stride1 = blocksize_i - extent_i_minus + extent_i_plus;
+            int expected_stride2 = expected_stride1 * (blocksize_j - extent_j_minus + extent_j_plus);
+            int expected_stride3 = expected_stride2 * n_colors;
+            int expected_stride4 = expected_stride3 * n_blocks_i;
+            int expected_stride5 = expected_stride4 * n_blocks_j;
+
+            EXPECT_EQ(expected_stride0, at_key<dim::i>(strides));
+            EXPECT_EQ(expected_stride1, at_key<dim::j>(strides));
+            EXPECT_EQ(expected_stride2, at_key<dim::c>(strides));
+            EXPECT_EQ(expected_stride3, at_key<tmp_cuda::block_i>(strides));
+            EXPECT_EQ(expected_stride4, at_key<tmp_cuda::block_j>(strides));
+            EXPECT_EQ(expected_stride5, at_key<dim::k>(strides));
+
+            auto ptr_to_allocation = static_cast<float_type *>(alloc.ptrs()[0].get());
+            auto ptr_to_origin =
+                ptr_to_allocation - expected_stride0 * extent_i_minus - expected_stride1 * extent_j_minus;
+            EXPECT_EQ(ptr_to_origin, sid::get_origin(testee)());
+        }
+    } // namespace
+} // namespace gridtools
