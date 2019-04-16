edfbd67cd2819751a28b4b4b4b7b98779330c1bb
Felix Thaler <thaler@cscs.ch>
2019-04-16 15:08:43 +0200

    Added plot-backend-compare.png output

--- jenkins/run_perftest_PR.sh
+++ jenkins/run_perftest_PR.sh
@@ -17,9 +17,13 @@ for domain in 128 256; do
   # run performance tests
   ./build/pyutils/perfdriver.py -vvv run -s $domain $domain 80 -o $resultdir/result.json || { echo 'Running failed'; rm -rf $tmpdir; exit 1; }
 
+  allresults=''
   for backend in cuda x86 mc; do
     result=$resultdir/result.$backend.json
     if [[ -f $result ]]; then
+      # append result
+      allresults="$allresults $result"
+
       # find references for same configuration
       reference_path=./pyutils/perftest/references/$grid/$real_type/$domain/${label}_${target}_$compiler
       if [[ -d $reference_path ]]; then
@@ -34,4 +38,9 @@ for domain in 128 256; do
       ./build/pyutils/perfdriver.py -vvv plot compare -i $references $resultdir/result.$backend.json -o plot-$backend-$domain.png || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
     fi
   done
+
+  if [[ -n "$allresults" ]]; then
+    # plot comparison of backends
+    ./build/pyutils/perfdriver.py -vvv plot compare -i $allresults -o plot-backend-compare.png || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
+  fi
 done
