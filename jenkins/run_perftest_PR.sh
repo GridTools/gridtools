2d56b357fb72487191ca96d1bdcce7e00fb139a2
Felix Thaler <thaler@cscs.ch>
2019-04-16 12:42:06 +0200

    Updated perftest scripts

--- jenkins/run_perftest_PR.sh
+++ jenkins/run_perftest_PR.sh
@@ -1,11 +1,13 @@
+# remove -cn from label (for daint)
+label=${label%%-*}
+
 # use the machines python virtualenv with required modules (matplotlib) installed
-source /project/c14/jenkins/python-venvs/${label%%-*}/bin/activate
+source /project/c14/jenkins/python-venvs/$label/bin/activate
 
 if [[ $label != "kesch" ]]; then
     export SLURM_ACCOUNT=c14
     export SBATCH_ACCOUNT=c14
 fi
-
 
 # create directory for temporaries
 if [[ $label == "tave" ]]; then
@@ -20,29 +22,36 @@ export TMPDIR=$tmpdir
 
 grid=structured
 
+export GTCMAKE_GT_ENABLE_BACKEND_NAIVE=OFF
+
 # build binaries for performance tests
 ./pyutils/builddriver.py -vvv -b release -p $real_type -g $grid -o build -d $target -c $compiler -t perftests || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
 
 for domain in 128 256; do
   # result directory, create if it does not exist yet
-  resultdir=./build/pyutils/perftest/results/$grid/$real_type/$domain/${label%%-*}_${target}_$compiler
+  resultdir=./build/pyutils/perftest/results/$grid/$real_type/$domain/${label}_${target}_$compiler
   mkdir -p $resultdir
-  
+
   # run performance tests
-  ./build/pyutils/perfdriver.py -vvv run -s $domain $domain 80 -o $resultdir/result.json || { echo 'Running failed'; rm -rf $tmpdir; exit 1; }
-  
-  # find references for same config-grid-real_type-backend combination
-  reference_path=./pyutils/perftest/references/$grid/$real_type/$domain/${label%%-*}_${target}_$compiler
-  if [ -d $reference_path ]; then
-  	stella_reference=$(find $reference_path -name stella.json)
-  	gridtools_references=$(find $reference_path -name 'result.*.json')
-  	references="$stella_reference $gridtools_references"
-  else
-  	references=""
-  fi
-  
-  # plot comparison of current result with references
-  ./build/pyutils/perfdriver.py -vvv plot compare -i $references $resultdir/result.*.json -o plot-$domain.png || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
+  ./build/pyutils/perfdriver.py -vvv run --runs 2 -s $domain $domain 80 -o $resultdir/result.json || { echo 'Running failed'; rm -rf $tmpdir; exit 1; }
+
+  for backend in cuda x86 mc; do
+    result=$resultdir/result.$backend.json
+    if [[ -f $result ]]; then
+      # find references for same configuration
+      reference_path=./pyutils/perftest/references/$grid/$real_type/$domain/${label}_${target}_$compiler
+      if [[ -d $reference_path ]]; then
+        stella_reference=$(find $reference_path -name stella.json)
+        gridtools_reference=$(find $reference_path -name "result.$backend.json")
+        references="$stella_reference $gridtools_reference"
+      else
+        references=""
+      fi
+
+      # plot comparison of current result with references
+      ./build/pyutils/perfdriver.py -vvv plot compare -i $references $resultdir/result.$backend.json -o plot-$backend-$domain.png || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
+    fi
+  done
 done
 
 # clean possible temporary leftovers
--- jenkins/run_perftest_daily.sh
+++ jenkins/run_perftest_daily.sh
@@ -1,11 +1,13 @@
+# remove -cn from label (for daint)
+label=${label%%-*}
+
 # use the machines python virtualenv with required modules (matplotlib) installed
-source /project/c14/jenkins/python-venvs/${label%%-*}/bin/activate
+source /project/c14/jenkins/python-venvs/$label/bin/activate
 
-# print full PATH for debugging purposes
-echo "$PATH"
-
-# get config name, consisting of label without postfix (like the -cn from daint-cn) and compiler name
-config=${label%%-*}_$compiler
+if [[ $label != "kesch" ]]; then
+    export SLURM_ACCOUNT=c14
+    export SBATCH_ACCOUNT=c14
+fi
 
 # create directory for temporaries
 if [[ $label == "tave" ]]; then
@@ -18,26 +20,33 @@ fi
 mkdir -p $tmpdir
 export TMPDIR=$tmpdir
 
+grid=structured
+
+export GTCMAKE_GT_ENABLE_BACKEND_NAIVE=OFF
+
 # build binaries for performance tests
-./pyutils/build.py -vvv --build-type release --backend $backend --precision $precision --grid $grid --build-dir build --config $config --perftest-targets || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
+./pyutils/builddriver.py -vvv -b release -p $real_type -g $grid -o build -d $target -c $compiler -t perftests || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
 
 for domain in 128 256; do
   # result directory, create if it does not exist yet
-  resultdir=/project/c14/jenkins/gridtools-performance-history/$config/$grid/$precision/$backend/$domain
+  resultdir=/project/c14/jenkins/gridtools-performance-history-new/$grid/$real_type/$domain/${label}_${target}_$compiler
   mkdir -p $resultdir
-  
+
   # name result file by date/time
   resultname=$(date +%F-%H-%M-%S).json
-  
+
   # run performance tests
-  ./build/pyutils/perfdriver.py -vvv run -d $domain $domain 80 --config $config -o $resultdir/$resultname || { echo 'Running failed'; rm -rf $tmpdir; exit 1; }
-  
-  # find previous results for history plot
-  results=$(find $resultdir -regex '.*\.json')
-  
-  # plot history
-  ./build/pyutils/perfdriver.py -vvv plot history -i $results -o history-$domain-full.png || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
-  ./build/pyutils/perfdriver.py -vvv plot history -i $results -o history-$domain-last.png --limit=10 || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
+  ./build/pyutils/perfdriver.py -vvv run -s $domain $domain 80 -o $resultdir/$resultname || { echo 'Running failed'; rm -rf $tmpdir; exit 1; }
+
+  for backend in cuda x86 mc; do
+    # find previous results for history plot
+    results=$(find $resultdir -regex ".*\.$backend\.json")
+    if [[ -n "$results" ]]; then
+      # plot history
+      ./build/pyutils/perfdriver.py -vvv plot history -i $results -o history-$backend-$domain-full.png || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
+      ./build/pyutils/perfdriver.py -vvv plot history -i $results -o history-$backend-$domain-last.png --limit=10 || { echo 'Plotting failed'; rm -rf $tmpdir; exit 1; }
+    fi
+  done
 done
 
 # clean possible temporary leftovers
