e0d6293ae8f4a8fae6b441c8e6376af21be4f1f2
Felix Thaler <thaler@cscs.ch>
2019-04-16 12:45:31 +0200

    Use default GCC on Tave

--- pyutils/pyutils/env/tave_cpu_gnu.sh
+++ pyutils/pyutils/env/tave_cpu_gnu.sh
@@ -3,7 +3,6 @@
 source tave.sh
 
 module load PrgEnv-gnu
-module swap gcc/7.3.0
 
 export CXX=$(which g++)
 export CC=$(which gcc)
@@ -13,4 +12,4 @@ export GTCMAKE_CMAKE_CXX_FLAGS='-march=knl -fvect-cost-model=unlimited'
 export GTCMAKE_CMAKE_CXX_FLAGS_RELEASE='-Ofast -DNDEBUG'
 
 export OMP_PLACES='{0,64}:64'
-export OMP_WAIT_POLICY=active
+export OMP_WAIT_POLICY='active'
