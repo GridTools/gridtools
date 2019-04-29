a75ef7e6ff9fa9c5418d330770b19e8d5d5d6a0f
Hannes Vogt <hannes@havogt.de>
2019-04-29 09:58:41 +0200

    re-insert comment

--- unit_tests/tools/multiplet.hpp
+++ unit_tests/tools/multiplet.hpp
@@ -13,6 +13,11 @@
 #include <gridtools/common/array.hpp>
 #include <gridtools/common/array_addons.hpp>
 
+/**
+   @brief Small value type to use in tests where we want to check the
+   values in a fields, for instance to check if layouts works, on in
+   communication tests
+*/
 template <gridtools::uint_t N>
 using multiplet = gridtools::array<int, N>;
 
