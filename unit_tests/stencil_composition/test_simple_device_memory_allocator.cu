8b34cda08961f641edd50dcdc213e43be8410093
Hannes Vogt <hannes@havogt.de>
2019-04-29 14:54:25 +0200

    use on_device::exec

--- unit_tests/stencil_composition/test_simple_device_memory_allocator.cu
+++ unit_tests/stencil_composition/test_simple_device_memory_allocator.cu
@@ -9,33 +9,32 @@
  */
 
 #include <gridtools/stencil_composition/backend_cuda/simple_device_memory_allocator.hpp>
+
+#include "../cuda_test_helper.hpp"
+#include <gridtools/common/integral_constant.hpp>
 #include <gridtools/tools/backend_select.hpp>
 
-#include "../test_helper.hpp"
 #include <gtest/gtest.h>
 
 namespace gridtools {
     namespace {
+
         template <typename PtrHolder>
-        __global__ void test_allocated(PtrHolder testee, bool *result) {
-            auto &ref = *testee();
+        __device__ bool check_allocation(PtrHolder ptr_holder) {
+            auto &ref = *ptr_holder();
             ref = 1.;
-            *result = ref == 1.;
+            return ref == 1.;
         }
+
+        template <typename PtrHolder>
+        __global__ void test_allocated(PtrHolder testee, bool *result) {}
 
         TEST(simple_device_memory_allocator, test) {
             simple_device_memory_allocator alloc;
             auto ptr_holder = alloc.allocate<float_type>(1);
 
-            bool result = false;
-            bool *dev_result;
-            GT_CUDA_CHECK(cudaMalloc(&dev_result, sizeof(bool)));
-            GT_CUDA_CHECK(cudaMemcpy(dev_result, &result, sizeof(bool), cudaMemcpyHostToDevice));
-
-            test_allocated<<<1, 1>>>(ptr_holder, dev_result);
-            GT_CUDA_CHECK(cudaDeviceSynchronize());
-
-            GT_CUDA_CHECK(cudaMemcpy(&result, dev_result, sizeof(bool), cudaMemcpyDeviceToHost));
+            auto result = gridtools::on_device::exec(
+                GT_MAKE_INTEGRAL_CONSTANT_FROM_VALUE(&check_allocation<decltype(ptr_holder)>), ptr_holder);
             ASSERT_TRUE(result);
         }
     } // namespace
