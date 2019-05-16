fe9bf7b42c6c50b7781024af17874608ffe7c3d5
Felix Thaler <thaler@cscs.ch>
2019-05-16 09:47:40 +0200

    Use Multiple Threads in Storage Initialization (#1300)
    Parallel storage initialization if OpenMP is enabled. Parallelization gives huge speedups on manycore processors, e.g. about factor 10 in total run time for performance tests on KNL.

--- include/gridtools/storage/storage_cuda/cuda_storage.hpp
+++ include/gridtools/storage/storage_cuda/cuda_storage.hpp
@@ -95,6 +95,10 @@ namespace gridtools {
         template <typename Fun, uint_t Align = 1>
         cuda_storage(uint_t size, Fun &&initializer, uint_t offset_to_align = 0u, alignment<Align> a = alignment<1u>{})
             : cuda_storage(size, offset_to_align, a) {
+#pragma ivdep
+#ifdef _OPENMP
+#pragma omp parallel for simd
+#endif
             for (uint_t i = 0; i < size; ++i)
                 m_cpu_ptr[i] = initializer(i);
             this->clone_to_device();
