3bfc52d3f7d18f019133517beddf9c0fd5318bd2
Hannes Vogt <hannes@havogt.de>
2019-05-17 15:04:13 +0200

    make running mpi optional

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -4,4 +4,4 @@ source $(dirname "$0")/setup.sh
 
 ./pyutils/driver.py -v -l $logfile build -b $build_type -p $real_type -g $grid_type -e $envfile -o build -i install -t install || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
 
-./build/pyutils/driver.py -v -l $logfile test -m -b || { echo 'Tests failed'; rm -rf $tmpdir; exit 2; }
+./build/pyutils/driver.py -v -l $logfile test $mpi_flag -b || { echo 'Tests failed'; rm -rf $tmpdir; exit 2; }
