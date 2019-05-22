8532410e5e10e15423cc94062078a80f4441de38
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-05-22 07:51:31 +0200

    narrow workaround for specific nvcc versions

--- include/gridtools/meta/macros.hpp
+++ include/gridtools/meta/macros.hpp
@@ -14,9 +14,6 @@
 
 #include <boost/preprocessor.hpp>
 
-// internal
-#define GT_META_INTERNAL_APPLY(fun, args) BOOST_PP_REMOVE_PARENS(fun)<BOOST_PP_REMOVE_PARENS(args)>
-
 #define GT_META_DELEGATE_TO_LAZY(fun, signature, args) \
     template <BOOST_PP_REMOVE_PARENS(signature)>       \
     using fun = typename lazy::fun<BOOST_PP_REMOVE_PARENS(args)>::type
@@ -24,7 +21,8 @@
 /**
  *  NVCC bug workaround: sizeof... works incorrectly within template alias context.
  */
-#ifdef __NVCC__
+#if defined(__CUDACC_VER_MAJOR__) && \
+    (__CUDACC_VER_MAJOR__ < 10 || (__CUDACC_VER_MAJOR__ == 10 && __CUDACC_VER_MINOR__ < 2))
 
 namespace gridtools {
     namespace meta {
