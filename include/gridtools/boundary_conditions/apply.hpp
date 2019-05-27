7ed85534504c11c22902b7b714f142787e8f58fe
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-05-27 10:09:02 +0200

    Remove some unneeded boost stuff

--- include/gridtools/boundary_conditions/apply.hpp
+++ include/gridtools/boundary_conditions/apply.hpp
@@ -13,9 +13,6 @@
 #include "../common/halo_descriptor.hpp"
 #include "direction.hpp"
 #include "predicate.hpp"
-#include <boost/preprocessor/arithmetic/inc.hpp>
-#include <boost/preprocessor/repetition/enum_params.hpp>
-#include <boost/preprocessor/repetition/repeat.hpp>
 
 /**
 @file
