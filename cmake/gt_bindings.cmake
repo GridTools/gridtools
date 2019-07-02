fe9a3abc5e884bb87d9784dfc70b62ad395c0639
Hannes Vogt <hannes@havogt.de>
2019-07-02 15:19:57 +0200

    move to the non-legacy bindgen names

--- cmake/gt_bindings.cmake
+++ cmake/gt_bindings.cmake
@@ -1,3 +1,12 @@
 message(DEPRECATION "Including gt_bindings.cmake is deprecated use find_package(cpp_bindgen)")
 include(cpp_bindgen)
-include(gt_add_bindings_library)
+
+function(gt_add_bindings_library)
+    message(DEPRECATION "gt_add_bindings_library() is deprecated: use cpp_bindgen_add_library().")
+    cpp_bindgen_add_library(${ARGN})
+endfunction()
+
+function(gt_enable_bindings_library_fortran)
+    message(DEPRECATION "gt_enable_bindings_library_fortran() is deprecated: use cpp_bindgen_enable_fortran_library().")
+    cpp_bindgen_enable_fortran_library(${ARGN})
+endfunction()
