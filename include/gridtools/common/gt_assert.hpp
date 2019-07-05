50aa81594618c9b2b52501d647f87f6ebf371842
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-07-05 08:26:55 +0200

    cleanup logic in gt_assert

--- include/gridtools/common/gt_assert.hpp
+++ include/gridtools/common/gt_assert.hpp
@@ -8,6 +8,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #pragma once
+#include <cassert>
 #include <stdexcept>
 
 /** \ingroup common
@@ -15,18 +16,6 @@
     \defgroup assert Assertion
     @{
 */
-
-#ifdef __CUDACC__
-#if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ >= 200)
-// we take the cuda assert for arch greater than 2.x
-#include <assert.h>
-#else
-#undef assert
-#define assert(e)
-#endif
-#else
-#include <cassert>
-#endif
 
 #ifdef __CUDA_ARCH__
 #if __CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ == 2
