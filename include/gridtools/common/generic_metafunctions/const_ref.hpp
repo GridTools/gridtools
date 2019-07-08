0a58a40b9235045d6403bcf4c381a4e062578abc
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-07-08 09:22:44 +0200

    hopefully now correct cuda 8 workaround

--- include/gridtools/common/generic_metafunctions/const_ref.hpp
+++ include/gridtools/common/generic_metafunctions/const_ref.hpp
@@ -16,7 +16,7 @@
 #include "../../meta/type_traits.hpp"
 
 namespace gridtools {
-    namespace lazy {
+    GT_META_LAZY_NAMESPACE {
         template <class T, class = void>
         struct const_ref : std::add_lvalue_reference<add_const_t<T>> {};
 
@@ -24,8 +24,10 @@ namespace gridtools {
         struct const_ref<T,
             enable_if_t<!std::is_reference<T>::value && std::is_trivially_copy_constructible<T>::value &&
                         sizeof(T) <= sizeof(add_pointer_t<T>)>> : std::add_const<T> {};
-    } // namespace lazy
+    }
 
+#if !GT_BROKEN_TEMPLATE_ALIASES
     template <class T>
-    GT_META_DEFINE_ALIAS(const_ref, lazy::const_ref, T);
+    using const_ref = typename lazy::const_ref<T>::type;
+#endif
 } // namespace gridtools
