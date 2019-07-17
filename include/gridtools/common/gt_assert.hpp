c9bc2b20251db4c5b11ab04cdd66b82e97d1c28e
Felix Thaler <thaler@cscs.ch>
2019-07-17 14:04:51 +0200

    Clang-compatible GT_ASSERT_OR_THROW

--- include/gridtools/common/gt_assert.hpp
+++ include/gridtools/common/gt_assert.hpp
@@ -11,12 +11,31 @@
 #include <cassert>
 #include <stdexcept>
 
+#include "hip_wrappers.hpp"
+
 /** \ingroup common
     @{
     \defgroup assert Assertion
     @{
 */
 
+#if defined(__clang__) && defined(__CUDACC__) // Clang CUDA compilation
+namespace gt_assert_impl_ {
+__host__ inline void throw_error(const std::string& msg) {
+    throw std::runtime_error(msg);
+}
+
+__device__ void throw_error(const char*);
+}
+
+#ifdef __CUDA_ARCH__
+#define GT_ASSERT_OR_THROW(cond, msg) assert(cond)
+#else
+#define GT_ASSERT_OR_THROW(cond, msg) \
+    if (!(cond))                      \
+    gt_assert_impl_::throw_error(msg)
+#endif
+#else // NVIDIA CUDA compilation or host-only compilation
 #ifdef __CUDA_ARCH__
 #if __CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ == 2
 // we define this macro to an empty string for CUDA 9.2 because in certain cases, CUDA 9.2 tries to compile device
@@ -29,6 +48,7 @@
 #define GT_ASSERT_OR_THROW(cond, msg) \
     if (!(cond))                      \
     throw std::runtime_error(msg)
+#endif
 #endif
 /** @} */
 /** @} */
