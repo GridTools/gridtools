d5dd0a4e1e537781a06b5dad0bfbbfd560000984
Anton Afanasyev <anstaf@gmail.com>
2019-07-18 10:07:32 +0200

    fix icc comp

--- include/gridtools/stencil_composition/structured_grids/backend_x86/mss_loop_x86.hpp
+++ include/gridtools/stencil_composition/structured_grids/backend_x86/mss_loop_x86.hpp
@@ -15,8 +15,8 @@
 #include "../../../common/defs.hpp"
 #include "../../../common/generic_metafunctions/for_each.hpp"
 #include "../../../common/host_device.hpp"
-#include "../../../common/hymap.hpp"
 #include "../../../common/integral_constant.hpp"
+#include "../../../common/tuple_util.hpp"
 #include "../../../meta.hpp"
 #include "../../dim.hpp"
 #include "../../sid/concept.hpp"
@@ -25,6 +25,26 @@
 
 namespace gridtools {
     namespace x86 {
+
+        template <class KStep, class Intervals>
+        struct k_loop_f {
+            Intervals m_intervals;
+            int_t m_shift_back;
+
+            template <class Ptr, class Strides>
+            GT_FORCE_INLINE void operator()(Ptr &ptr, Strides const &strides) const {
+                tuple_util::for_each(
+                    [&ptr, &strides](auto interval) {
+                        for (int k = 0; k < interval.count(); ++k) {
+                            interval(ptr, strides);
+                            sid::shift(ptr, sid::get_stride<dim::k>(strides), KStep());
+                        }
+                    },
+                    m_intervals);
+                sid::shift(ptr, sid::get_stride<dim::k>(strides), m_shift_back);
+            }
+        };
+
         integral_constant<int_t, 0> k_start(int_t, integral_constant<int_t, 1>) { return {}; }
 
         int_t k_start(int_t total_length, integral_constant<int_t, -1>) { return total_length - 1; }
@@ -39,8 +59,8 @@ namespace gridtools {
 
             return [origin = sid::get_origin(composite) + offset,
                        strides = std::move(strides),
-                       k_shift_back = -k_total_length * KStep(),
-                       loop_intervals](int_t i_block, int_t j_block, int_t i_size, int_t j_size) {
+                       k_loop = k_loop_f<KStep, LoopIntervals>{std::move(loop_intervals), -k_total_length * KStep()}](
+                       int_t i_block, int_t j_block, int_t i_size, int_t j_size) {
                 sid::ptr_diff_type<Composite> offset{};
                 sid::shift(offset, sid::get_stride<dim::thread>(strides), omp_get_thread_num());
                 sid::shift(offset, sid::get_stride<sid::blocked_dim<dim::i>>(strides), i_block);
@@ -49,17 +69,6 @@ namespace gridtools {
 
                 auto i_loop = sid::make_loop<dim::i>(i_size + Extent::iplus::value - Extent::iminus::value);
                 auto j_loop = sid::make_loop<dim::j>(j_size + Extent::jplus::value - Extent::jminus::value);
-                auto k_loop = [&loop_intervals, k_shift_back](auto &ptr, auto const &strides) {
-                    tuple_util::for_each(
-                        [&ptr, &strides, k_shift_back](auto loop_interval) {
-                            for (int k = 0; k < loop_interval.count(); ++k) {
-                                loop_interval(ptr, strides);
-                                sid::shift(ptr, sid::get_stride<dim::k>(strides), KStep());
-                            }
-                        },
-                        loop_intervals);
-                    sid::shift(ptr, sid::get_stride<dim::k>(strides), k_shift_back);
-                };
 
                 i_loop(j_loop(k_loop))(ptr, strides);
             };
