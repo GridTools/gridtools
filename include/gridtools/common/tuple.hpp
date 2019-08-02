f3c5a34ea82b961348357b0162f581b41c8ee09b
Anton Afanasyev <anstaf@gmail.com>
2019-08-02 16:16:22 +0200

    revert some unrelated changes

--- include/gridtools/common/tuple.hpp
+++ include/gridtools/common/tuple.hpp
@@ -75,7 +75,7 @@ namespace gridtools {
             }
 
             template <size_t I, class T>
-            static constexpr GT_FUNCTION T get(tuple_leaf<I, T, true> &&obj) noexcept {
+            static GT_CONSTEXPR GT_FUNCTION T get(tuple_leaf<I, T, true> &&obj) noexcept {
                 return static_cast<T &&>(obj);
             }
         };
@@ -215,7 +215,7 @@ namespace gridtools {
             }
 
             template <size_t I, std::enable_if_t<I == 0, int> = 0>
-            static constexpr GT_FUNCTION T get(tuple &&obj) noexcept {
+            static GT_CONSTEXPR GT_FUNCTION T get(tuple &&obj) noexcept {
                 return static_cast<T &&>(obj.m_value);
             }
         };
