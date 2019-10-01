ad33756aaa30c8faa9bd249b31303a3954d710b1
Felix Thaler <thaler@cscs.ch>
2019-10-01 10:13:16 +0200

    Enable ptr tests for HIP, remove obsolete test

--- unit_tests/common/test_cuda_is_ptr.cu
+++ unit_tests/common/test_cuda_is_ptr.cu
@@ -19,8 +19,6 @@
 using gridtools::is_gpu_ptr;
 using gridtools::cuda_util::cuda_malloc;
 
-#ifndef __HIPCC__
-
 TEST(test_is_gpu_ptr, host_ptr_is_no_cuda_ptr) {
     auto testee = std::unique_ptr<double>(new double);
     EXPECT_FALSE(is_gpu_ptr(testee.get()));
@@ -33,22 +31,8 @@ TEST(test_is_gpu_ptr, cuda_ptr_is_cuda_ptr) {
     EXPECT_EQ(cudaSuccess, cudaGetLastError());
 }
 
-TEST(test_is_gpu_ptr, cudaMallocHost_ptr_is_not_cuda_ptr) {
-    auto testee = std::shared_ptr<double>(
-        []() {
-            double *res;
-            GT_CUDA_CHECK(cudaMallocHost(&res, sizeof(double)));
-            return res;
-        }(),
-        cudaFreeHost);
-    EXPECT_FALSE(is_gpu_ptr(testee.get()));
-    EXPECT_EQ(cudaSuccess, cudaGetLastError());
-}
-
 TEST(test_is_gpu_ptr, cuda_ptr_inner_region_are_cuda_ptr) {
     auto testee = cuda_malloc<double>(2);
     EXPECT_TRUE(is_gpu_ptr(testee.get() + 1));
     EXPECT_EQ(cudaSuccess, cudaGetLastError());
 }
-
-#endif
