da85111eacc7cc7e2e685c6e4be25ddfc77f2713
Felix Thaler <thaler@cscs.ch>
2019-10-01 10:05:20 +0200

    Revert accidentally commited changes

--- unit_tests/interface/repository/repository_double.h
+++ unit_tests/interface/repository/repository_double.h
@@ -8,11 +8,11 @@
 extern "C" {
 #endif
 
-bindgen_handle *make_exported_repository(int, int, int);
-void prefix_set_exported_ijfield(bindgen_handle *, bindgen_fortran_array_descriptor *);
-void prefix_set_exported_ijkfield(bindgen_handle *, bindgen_fortran_array_descriptor *);
-void prefix_set_exported_jkfield(bindgen_handle *, bindgen_fortran_array_descriptor *);
-void verify_exported_repository(bindgen_handle *);
+bindgen_handle* make_exported_repository(int, int, int);
+void prefix_set_exported_ijfield(bindgen_handle*, bindgen_fortran_array_descriptor*);
+void prefix_set_exported_ijkfield(bindgen_handle*, bindgen_fortran_array_descriptor*);
+void prefix_set_exported_jkfield(bindgen_handle*, bindgen_fortran_array_descriptor*);
+void verify_exported_repository(bindgen_handle*);
 
 #ifdef __cplusplus
 }
