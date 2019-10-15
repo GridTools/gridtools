cd52024cd4f6e5f92859700b9c91863bc23c9b7c
Felix Thaler <thaler@cscs.ch>
2019-10-15 15:56:27 +0200

    Possibility to disable example compilation in run_tests.sh

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -4,8 +4,12 @@ source $(dirname "$0")/setup.sh
 
 ./pyutils/driver.py -v -l $logfile build -b $build_type -p $real_type -g $grid_type -e $envfile -o build -i install -t install || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
 
-if [[ -z "${no_mpi}" ]]; then
-    mpi_flag="-m"
+if [[ -z "$no_mpi" ]]; then
+    mpi_flag="--run-mpi-tests"
 fi
 
-./build/pyutils/driver.py -v -l $logfile test $mpi_flag -b || { echo 'Tests failed'; rm -rf $tmpdir; exit 2; }
+if [[ -z "$no_examples" ]]; then
+    examples_flag="--build-examples"
+fi
+
+./build/pyutils/driver.py -v -l $logfile test $mpi_flag $examples_flag || { echo 'Tests failed'; rm -rf $tmpdir; exit 2; }
