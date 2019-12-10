4de26bc62372622a16ce4a605987fdb2a638d86f
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-12-10 15:38:13 +0100

    Environment changes (#1369, #1371, #1373, #1382)

--- jenkins/envs/daint_clang.sh
+++ jenkins/envs/daint_clang.sh
@@ -3,8 +3,10 @@
 source $(dirname "$BASH_SOURCE")/daint.sh
 
 module load /users/vogtha/modules/compilers/clang/7.0.1
+module load gcc
 
 export CXX=$(which clang++)
 export CC=$(which clang)
+export FC=$(which gfortran)
 
 export GTCMAKE_GT_ENABLE_BACKEND_CUDA='OFF'
--- jenkins/envs/daint_clang_nvcc.sh
+++ jenkins/envs/daint_clang_nvcc.sh
@@ -2,18 +2,20 @@
 
 source $(dirname "$BASH_SOURCE")/daint.sh
 
-module swap PrgEnv-cray PrgEnv-gnu
-module swap gcc/6.2.0
-
 export CXX=$(which CC)
 export CC=$(which cc)
 export FC=$(which ftn)
-export GTCMAKE_CMAKE_CUDA_HOST_COMPILER="$CXX"
 
 export CTEST_PARALLEL_LEVEL=1
+
+export CXXFLAGS='-fno-cray-gpu -fno-cray-mallopt -fno-cray'
+export CFLAGS='-fno-cray-gpu -fno-cray-mallopt -fno-cray'
+
+export GTCMAKE_GT_CUDA_COMPILATION_TYPE='Clang-CUDA'
+export GTCMAKE_CMAKE_CXX_FLAGS_RELEASE='-Ofast -DNDEBUG'
 
 export GTCMAKE_GT_ENABLE_BACKEND_CUDA=ON
 export GTCMAKE_GT_ENABLE_BACKEND_X86=OFF
 export GTCMAKE_GT_ENABLE_BACKEND_MC=OFF
 export GTCMAKE_GT_ENABLE_BACKEND_NAIVE=OFF
-export GTCMAKE_GT_EXAMPLES_FORCE_CUDA=ON
+export GTCMAKE_GT_USE_MPI=OFF
