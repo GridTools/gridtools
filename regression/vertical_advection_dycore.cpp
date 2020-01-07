de33199c40b0cdae458e074cd93b0f177bb732e0
Anton Afanasyev <anstaf@gmail.com>
2020-01-07 11:36:09 +0100

    simplify verticval advection

--- regression/vertical_advection_dycore.cpp
+++ regression/vertical_advection_dycore.cpp
@@ -28,97 +28,66 @@ using namespace cartesian;
 using axis_t = axis<1, axis_config::offset_limit<3>>;
 using full_t = axis_t::full_interval;
 
-struct u_forward_function {
+class u_forward_function {
     using utens_stage = in_accessor<0>;
     using wcon = in_accessor<1, extent<0, 1, 0, 0, 0, 1>>;
     using u_stage = in_accessor<2, extent<0, 0, 0, 0, -1, 1>>;
     using u_pos = in_accessor<3>;
     using utens = in_accessor<4>;
     using dtr_stage = in_accessor<5>;
-    using acol = inout_accessor<6>;
-    using bcol = inout_accessor<7>;
-    using ccol = inout_accessor<8, extent<0, 0, 0, 0, -1, 0>>;
-    using dcol = inout_accessor<9, extent<0, 0, 0, 0, -1, 0>>;
+    using ccol = inout_accessor<6, extent<0, 0, 0, 0, -1, 0>>;
+    using dcol = inout_accessor<7, extent<0, 0, 0, 0, -1, 0>>;
 
-    using param_list = make_param_list<utens_stage, wcon, u_stage, u_pos, utens, dtr_stage, acol, bcol, ccol, dcol>;
+    template <class Eval>
+    GT_FUNCTION static float_type compute_d_column(Eval &&eval, float_type correction) {
+        return eval(dtr_stage()) * eval(u_pos()) + eval(utens()) + eval(utens_stage()) + correction;
+    }
 
-    template <typename Evaluation>
-    GT_FUNCTION static void apply(Evaluation eval, full_t::modify<1, -1> interval) {
-        // TODO use Average function here
+  public:
+    using param_list = make_param_list<utens_stage, wcon, u_stage, u_pos, utens, dtr_stage, ccol, dcol>;
+
+    template <class Eval>
+    GT_FUNCTION static void apply(Eval &&eval, full_t::modify<1, -1>) {
         float_type gav = -float_type{.25} * (eval(wcon(1, 0, 0)) + eval(wcon(0, 0, 0)));
         float_type gcv = float_type{.25} * (eval(wcon(1, 0, 1)) + eval(wcon(0, 0, 1)));
-
         float_type as = gav * BET_M;
         float_type cs = gcv * BET_M;
-
-        eval(acol()) = gav * BET_P;
-        eval(ccol()) = gcv * BET_P;
-        eval(bcol()) = eval(dtr_stage()) - eval(acol()) - eval(ccol());
-
-        float_type correctionTerm =
+        float_type a = gav * BET_P;
+        float_type c = gcv * BET_P;
+        float_type b = eval(dtr_stage()) - a - c;
+        float_type correction =
             -as * (eval(u_stage(0, 0, -1)) - eval(u_stage())) - cs * (eval(u_stage(0, 0, 1)) - eval(u_stage()));
-        // update the d column
-        compute_d_column(eval, correctionTerm);
-        thomas_forward(eval, interval);
+        float_type divided = float_type{1} / (b - (eval(ccol(0, 0, -1)) * a));
+
+        eval(ccol()) = c * divided;
+        eval(dcol()) = (compute_d_column(eval, correction) - eval(dcol(0, 0, -1)) * a) * divided;
     }
 
-    template <typename Evaluation>
-    GT_FUNCTION static void apply(Evaluation eval, full_t::last_level interval) {
+    template <class Eval>
+    GT_FUNCTION static void apply(Eval &&eval, full_t::last_level) {
         float_type gav = -float_type{.25} * (eval(wcon(1, 0, 0)) + eval(wcon()));
         float_type as = gav * BET_M;
+        float_type a = gav * BET_P;
+        float_type b = eval(dtr_stage()) - a;
+        float_type correction = -as * (eval(u_stage(0, 0, -1)) - eval(u_stage()));
 
-        eval(acol()) = gav * BET_P;
-        eval(bcol()) = eval(dtr_stage()) - eval(acol());
-
-        float_type correctionTerm = -as * (eval(u_stage(0, 0, -1)) - eval(u_stage()));
-
-        // update the d column
-        compute_d_column(eval, correctionTerm);
-        thomas_forward(eval, interval);
+        eval(dcol()) = (compute_d_column(eval, correction) - eval(dcol(0, 0, -1)) * a) / (b - eval(ccol(0, 0, -1)) * a);
     }
 
-    template <typename Evaluation>
-    GT_FUNCTION static void apply(Evaluation eval, full_t::first_level interval) {
+    template <class Eval>
+    GT_FUNCTION static void apply(Eval &&eval, full_t::first_level) {
         float_type gcv = float_type{.25} * (eval(wcon(1, 0, 1)) + eval(wcon(0, 0, 1)));
         float_type cs = gcv * BET_M;
+        float_type c = gcv * BET_P;
+        float_type b = eval(dtr_stage()) - c;
+        float_type correction = -cs * (eval(u_stage(0, 0, 1)) - eval(u_stage()));
 
-        eval(ccol()) = gcv * BET_P;
-        eval(bcol()) = eval(dtr_stage()) - eval(ccol());
-
-        float_type correctionTerm = -cs * (eval(u_stage(0, 0, 1)) - eval(u_stage()));
-        // update the d column
-        compute_d_column(eval, correctionTerm);
-        thomas_forward(eval, interval);
-    }
-
-  private:
-    template <typename Evaluation>
-    GT_FUNCTION static void compute_d_column(Evaluation &eval, float_type correctionTerm) {
-        eval(dcol()) = eval(dtr_stage()) * eval(u_pos()) + eval(utens()) + eval(utens_stage()) + correctionTerm;
-    }
-
-    template <typename Evaluation>
-    GT_FUNCTION static void thomas_forward(Evaluation &eval, full_t::modify<1, -1>) {
-        float_type divided = float_type{1} / (eval(bcol()) - (eval(ccol(0, 0, -1)) * eval(acol())));
-        eval(ccol()) = eval(ccol()) * divided;
-        eval(dcol()) = (eval(dcol()) - (eval(dcol(0, 0, -1)) * eval(acol()))) * divided;
-    }
-
-    template <typename Evaluation>
-    GT_FUNCTION static void thomas_forward(Evaluation &eval, full_t::last_level) {
-        float_type divided = float_type{1} / (eval(bcol()) - eval(ccol(0, 0, -1)) * eval(acol()));
-        eval(dcol()) = (eval(dcol()) - eval(dcol(0, 0, -1)) * eval(acol())) * divided;
-    }
-
-    template <typename Evaluation>
-    GT_FUNCTION static void thomas_forward(Evaluation &eval, full_t::first_level) {
-        float_type divided = float_type{1} / eval(bcol());
-        eval(ccol()) = eval(ccol()) * divided;
-        eval(dcol()) = eval(dcol()) * divided;
+        eval(ccol()) = c / b;
+        eval(dcol()) = compute_d_column(eval, correction) / b;
     }
 };
 
-struct u_backward_function {
+class u_backward_function {
     using utens_stage = inout_accessor<0>;
     using u_pos = in_accessor<1>;
     using dtr_stage = in_accessor<2>;
@@ -126,42 +95,30 @@ struct u_backward_function {
     using dcol = in_accessor<4>;
     using data_col = inout_accessor<5, extent<0, 0, 0, 0, 0, 1>>;
 
+  public:
     using param_list = make_param_list<utens_stage, u_pos, dtr_stage, ccol, dcol, data_col>;
 
-    template <typename Evaluation>
-    GT_FUNCTION static void apply(Evaluation &eval, full_t::modify<0, -1> interval) {
-        eval(utens_stage()) = eval(dtr_stage()) * (thomas_backward(eval, interval) - eval(u_pos()));
+    template <class Eval>
+    GT_FUNCTION static void apply(Eval &&eval, full_t::modify<0, -1> interval) {
+        float_type data = eval(dcol()) - eval(ccol()) * eval(data_col(0, 0, 1));
+        eval(utens_stage()) = eval(dtr_stage()) * (data - eval(u_pos()));
+        eval(data_col()) = data;
     }
 
-    template <typename Evaluation>
-    GT_FUNCTION static void apply(Evaluation &eval, full_t::last_level interval) {
-        eval(utens_stage()) = eval(dtr_stage()) * (thomas_backward(eval, interval) - eval(u_pos()));
-    }
-
-  private:
-    template <typename Evaluation>
-    GT_FUNCTION static float_type thomas_backward(Evaluation &eval, full_t::modify<0, -1>) {
-        float_type datacol = eval(dcol()) - eval(ccol()) * eval(data_col(0, 0, 1));
-        eval(data_col()) = datacol;
-        return datacol;
-    }
-
-    template <typename Evaluation>
-    GT_FUNCTION static float_type thomas_backward(Evaluation &eval, full_t::last_level) {
-        float_type datacol = eval(dcol());
-        eval(data_col()) = datacol;
-        return datacol;
+    template <class Eval>
+    GT_FUNCTION static void apply(Eval &&eval, full_t::last_level interval) {
+        float_type data = eval(dcol());
+        eval(utens_stage()) = eval(dtr_stage()) * (data - eval(u_pos()));
+        eval(data_col()) = data;
     }
 };
 
 const auto vertical_advection = [](auto utens_stage, auto u_stage, auto wcon, auto u_pos, auto utens, auto dtr_stage) {
-    GT_DECLARE_TMP(float_type, acol, bcol, ccol, dcol, data_col);
-    return multi_pass(
-        execute_forward()
-            .k_cached(acol, bcol)
-            .k_cached(cache_io_policy::flush(), ccol, dcol)
-            .k_cached(cache_io_policy::fill(), u_stage)
-            .stage(u_forward_function(), utens_stage, wcon, u_stage, u_pos, utens, dtr_stage, acol, bcol, ccol, dcol),
+    GT_DECLARE_TMP(float_type, ccol, dcol, data_col);
+    return multi_pass(execute_forward()
+                          .k_cached(cache_io_policy::flush(), ccol, dcol)
+                          .k_cached(cache_io_policy::fill(), u_stage)
+                          .stage(u_forward_function(), utens_stage, wcon, u_stage, u_pos, utens, dtr_stage, ccol, dcol),
         execute_backward().k_cached(data_col).stage(
             u_backward_function(), utens_stage, u_pos, dtr_stage, ccol, dcol, data_col));
 };
