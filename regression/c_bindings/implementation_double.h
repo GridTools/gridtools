8ca8d406e40a783db9c24eac920efd0f0adc757e
Anton Afanasyev <anstaf@gmail.com>
2020-01-23 18:19:41 +0100

    fix generated files

--- regression/c_bindings/implementation_double.h
+++ regression/c_bindings/implementation_double.h
@@ -8,10 +8,10 @@
 extern "C" {
 #endif
 
-void copy_from_data_store(bindgen_handle *, double *);
-void copy_to_data_store(bindgen_handle *, double *);
+void copy_from_data_store(bindgen_handle*, double*);
+void copy_to_data_store(bindgen_handle*, double*);
 bindgen_handle *create_data_store(unsigned int, unsigned int, unsigned int);
-void run_copy_stencil(bindgen_handle *, bindgen_handle *);
+void run_copy_stencil(bindgen_handle*, bindgen_handle*);
 
 #ifdef __cplusplus
 }
