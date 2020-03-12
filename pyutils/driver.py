613c6040227169b89dec04b6a08938e116f8768e
Anton Afanasyev <anstaf@gmail.com>
2020-03-12 11:37:20 +0100

    finally fix driver

--- pyutils/driver.py
+++ pyutils/driver.py
@@ -22,6 +22,8 @@ def driver(verbose, logfile):
 @driver.command(description='build GridTools')
 @args.arg('--build-type', '-b', choices=['release', 'debug'], required=True)
 @args.arg('--precision', '-p', choices=['float', 'double'], required=True)
+@args.arg('--grid', '-g', choices=['structured', 'icosahedral'],
+          default='structured')
 @args.arg('--environment', '-e', help='path to environment file')
 @args.arg('--target', '-t', nargs='+', help='make targets to build')
 @args.arg('--source-dir', help='GridTools source directory')
@@ -29,7 +31,7 @@ def driver(verbose, logfile):
 @args.arg('--install-dir', '-i', help='install directory')
 @args.arg('--cmake-only', action='store_true',
           help='only execute CMake but do not build')
-def build(build_type, precision, environment, target, source_dir,
+def build(build_type, grid, precision, environment, target, source_dir,
           build_dir, install_dir, cmake_only):
     import build
 
