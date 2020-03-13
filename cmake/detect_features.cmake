71e48449a9102910ad30322ecc12171966af6261
Hannes Vogt <hannes@havogt.de>
2020-03-13 23:34:18 +0100

    cleanup cuda detection

--- cmake/detect_features.cmake
+++ cmake/detect_features.cmake
@@ -6,16 +6,11 @@ function(detect_cuda_type dst prefer_clang)
         set(${dst} HIPCC-AMDGPU PARENT_SCOPE)
         return()
     endif()
+    
     include(CheckLanguage)
     check_language(CUDA)
-    find_package(CUDA QUIET)
-    if (CMAKE_CUDA_COMPILER AND NOT (CMAKE_CXX_COMPILER_ID STREQUAL Clang AND prefer_clang))
-        enable_language(CUDA)
-    endif()
-
     if (CMAKE_CUDA_COMPILER AND
-            NOT (CMAKE_CXX_COMPILER_ID STREQUAL Clang AND prefer_clang) AND
-            CMAKE_CUDA_COMPILER_VERSION VERSION_GREATER_EQUAL 9.0)
+            NOT (CMAKE_CXX_COMPILER_ID STREQUAL Clang AND prefer_clang))
         set(${dst} NVCC-CUDA PARENT_SCOPE)
     elseif (CMAKE_CXX_COMPILER_ID STREQUAL Clang AND (CMAKE_CUDA_COMPILER OR CUDA_FOUND))
         set(${dst} Clang-CUDA PARENT_SCOPE)
--- jenkins/envs/daint_clang.sh
+++ jenkins/envs/daint_clang.sh
@@ -5,6 +5,8 @@ source $(dirname "$BASH_SOURCE")/daint.sh
 module load /users/vogtha/modules/compilers/clang/7.0.1
 module load gcc
 
+export GTCMAKE_GT_PREFER_CLANG_CUDA_OVER_NVCC_CUDA=OFF
+
 export CXX=$(which clang++)
 export CC=$(which clang)
 export FC=$(which gfortran)
