c26fded31406eb9bd1b9600a6638ff98dffc21c3
Hannes Vogt <hannes@havogt.de>
2020-03-16 17:36:49 +0000

    Add hip check (not tested)

---
+++ cmake/try_compile_hip.cmake
@@ -0,0 +1,33 @@
+# Tries HIP compilation
+#
+# Usage of this function:
+#
+#  try_compile_hip()
+#
+# This function defines:
+#  GT_HIP_WORKS         Test file was successfully compiled with HIP
+#
+function(try_compile_hip)
+    set(HIP_TEST_SOURCE
+    "
+    #include <hip/hip_runtime.h>
+    __global__ void helloworld(int* in, int* out) {
+        *out = *in;
+    }
+    int main(int argc, char* argv[]) {
+        int* in;
+        int* out;
+        hipMalloc((void**)&in, sizeof(int));
+        hipMalloc((void**)&out, sizeof(int));
+        helloworld<<<1,1>>>(in, out);
+        hipFree(in);
+        hipFree(out);
+    }
+    ")
+
+    set(SRC_FILE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/TryHip.cpp)
+    file(WRITE "${SRC_FILE}" "${HIP_TEST_SOURCE}")
+
+    try_compile(hip_works ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY} ${SRC_FILE})
+    set(GT_HIP_WORKS ${hip_works} PARENT_SCOPE)
+endfunction()
