bba68da33f4fc7e4705c85b40cd17cdc0d16ae85
Hannes Vogt <hannes@havogt.de>
2020-03-20 11:04:53 +0100

    strategy for enabling tests

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -1,17 +1,15 @@
-
 set(CMAKE_EXPORT_NO_PACKAGE_REGISTRY ON CACHE BOOL "")
 mark_as_advanced(CMAKE_EXPORT_NO_PACKAGE_REGISTRY)
 
 option(GT_ENABLE_PYUTILS "If on, Python utility scripts will be configured" OFF)
 
+# GT_CLANG_CUDA_MODE: decide if Clang-CUDA or NVCC
 if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     set(GT_CLANG_CUDA_MODE "AUTO" CACHE STRING
         "AUTO, Clang-CUDA or NVCC-CUDA; \
         AUTO = Use NVCC if language CUDA is enabled, else prefer Clang-CUDA.")
     set_property(CACHE GT_CLANG_CUDA_MODE PROPERTY STRINGS "AUTO;Clang-CUDA;NVCC-CUDA")
 endif()
-
-option(GT_ENABLE_EXPERIMENTAL_REPOSITORY "Enables downloading the gridtools_experimental repository" OFF )
 
 #if we are pointing to the default install path (usually system) we will disable installation of examples by default
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
