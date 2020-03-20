9d8641ba02e4efb66b9adec75338823826eccedc
Hannes Vogt <hannes@havogt.de>
2020-03-20 15:55:25 +0100

    simple GridToolsConfig

--- cmake/internal/export.cmake
+++ cmake/internal/export.cmake
@@ -1,6 +1,3 @@
-# this registers the build-tree with a global CMake-registry
-export(PACKAGE GridTools)
-
 # for install tree
 set(GRIDTOOLS_MODULE_PATH lib/cmake)
 
@@ -15,35 +12,24 @@ write_basic_package_version_file(
   ${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/install/GridToolsConfigVersion.cmake
   COMPATIBILITY SameMajorVersion )
 
-# for build tree
-set(GRIDTOOLS_MODULE_PATH ${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/build-install/lib/cmake)
-set(GRIDTOOLS_INCLUDE_PATH ${PROJECT_SOURCE_DIR}/include)
-configure_package_config_file(cmake/internal/GridToolsConfig.cmake.in
-  ${PROJECT_BINARY_DIR}/GridToolsConfig.cmake
-  PATH_VARS GRIDTOOLS_MODULE_PATH GRIDTOOLS_INCLUDE_PATH
-  INSTALL_DESTINATION ${PROJECT_BINARY_DIR})
-write_basic_package_version_file(
-  ${PROJECT_BINARY_DIR}/GridToolsConfigVersion.cmake
-  COMPATIBILITY SameMajorVersion )
-
-# export(EXPORT GridToolsTargets
-#     FILE ${PROJECT_BINARY_DIR}/GridToolsTargets.cmake
-#     NAMESPACE GridTools::
-# )
-
-# install(EXPORT GridToolsTargets
-#   FILE GridToolsTargets.cmake
-#   NAMESPACE GridTools::
-#   DESTINATION lib/cmake
-# )
-
 install(DIRECTORY include/gridtools/ DESTINATION include/gridtools)
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/cmake/public/ DESTINATION "lib/cmake/${PROJECT_NAME}")
+
+# # for build tree
+# set(GRIDTOOLS_MODULE_PATH ${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/build-install/lib/cmake)
+# set(GRIDTOOLS_INCLUDE_PATH ${PROJECT_SOURCE_DIR}/include)
+# configure_package_config_file(cmake/internal/GridToolsConfig.cmake.in
+#   ${PROJECT_BINARY_DIR}/GridToolsConfig.cmake
+#   PATH_VARS GRIDTOOLS_MODULE_PATH GRIDTOOLS_INCLUDE_PATH
+#   INSTALL_DESTINATION ${PROJECT_BINARY_DIR})
+# write_basic_package_version_file(
+#   ${PROJECT_BINARY_DIR}/GridToolsConfigVersion.cmake
+#   COMPATIBILITY SameMajorVersion )
 
 # Install the GridToolsConfig.cmake and GridToolsConfigVersion.cmake
 install(FILES "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/install/GridToolsConfig.cmake"
     "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/install/GridToolsConfigVersion.cmake"
   DESTINATION "lib/cmake/${PROJECT_NAME}")
 
-install(DIRECTORY ${PROJECT_SOURCE_DIR}/cmake/public/ DESTINATION "lib/cmake/${PROJECT_NAME}")
 
-file(COPY ${PROJECT_SOURCE_DIR}/cmake/public/ DESTINATION "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/build-install/lib/cmake")
+# file(COPY ${PROJECT_SOURCE_DIR}/cmake/public/ DESTINATION "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/build-install/lib/cmake")
