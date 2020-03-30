804db42ece11973471ec7defde2546be43c6204f
Hannes Vogt <hannes@havogt.de>
2020-03-30 17:54:26 +0200

    validate copies

--- tests/cmake/storage_cuda/main.cpp
+++ tests/cmake/storage_cuda/main.cpp
@@ -1,9 +1,21 @@
+#include <cuda_runtime.h>
 #include <gridtools/storage/builder.hpp>
 #include <gridtools/storage/cuda.hpp>
 
 int main() {
+    int result_value = 42;
+
     auto builder = gridtools::storage::builder<gridtools::storage::cuda>.type<int>().dimensions(2);
     auto my_storage = builder();
-    my_storage->host_view()(0) = 10;
-    my_storage->get_target_ptr();
+
+    my_storage->host_view()(0) = result_value;
+    auto dev_ptr = my_storage->get_target_ptr();
+
+    int host_buffer;
+    cudaMemcpy(&host_buffer, dev_ptr, sizeof(int), cudaMemcpyDeviceToHost);
+
+    if (host_buffer == result_value)
+        exit(0);
+    else
+        exit(1);
 }
