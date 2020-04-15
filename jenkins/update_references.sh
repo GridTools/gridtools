ce4bd8f163f27561dc1d003b49c5943266aead40
Felix Thaler <thaler@cscs.ch>
2020-04-15 09:23:18 +0000

    Perftests Revival (#1478)
    - Re-enables perftests after recent CMake and perftest refactorings.
    - Includes old references converted to new format.
    - Generates HTML reports now.
    - Includes statistical estimates of performance changes.

--- jenkins/update_references.sh
+++ jenkins/update_references.sh
@@ -7,28 +7,23 @@ fi
 
 refpath=$(dirname "$BASH_SOURCE")/../pyutils/perftest/references
 
-grid=structured
-for real_type in float double; do
-    for domain in 128 256; do
-        for label in tave kesch daint-cn; do
-            for env in gcc clang nvcc_gcc nvcc_clang clang_nvcc; do
-                for backend in x86 mc cuda; do
-                    current="$grid/$real_type/$domain/${label%-*}_$env/result.$backend.json"
-                    src="http://jenkins-mch.cscs.ch/view/GridTools/job/GridTools_perftest_PR/$1/env=$env,label=$label,real_type=$real_type/artifact/build/pyutils/perftest/results/$current"
+for domain in 128 256; do
+    for label in tave kesch daint-cn; do
+        for env in gcc clang nvcc_gcc clang_nvcc; do
+            current="${label%-*}_$env/$domain.json"
+            src="http://jenkins-mch.cscs.ch/view/GridTools/job/GridTools_perftest_PR/$1/env=$env,label=$label/artifact/build/pyutils/perftest/results/$current"
 
-                    tmp=$(mktemp)
-                    wget -q -O "$tmp" "$src"
+            tmp=$(mktemp)
+            wget -q -O "$tmp" "$src"
 
-                    if [[ $? == 0 ]]; then
-                        dst="$refpath/$current"
-                        mkdir -p "$(dirname "$dst")"
-                        mv "$tmp" "$dst"
-                        echo "Updated $current"
-                    else
-                        rm "$tmp"
-                    fi
-                done
-            done
+            if [[ $? == 0 ]]; then
+                dst="$refpath/$current"
+                mkdir -p "$(dirname "$dst")"
+                mv "$tmp" "$dst"
+                echo "Updated $current"
+            else
+                rm "$tmp"
+            fi
         done
     done
 done
