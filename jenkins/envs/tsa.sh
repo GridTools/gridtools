15c33d5d7b8e677d0a4eb6a04e435c53bde584da
Hannes Vogt <hannes@havogt.de>
2020-05-06 16:02:21 +0200

    add ucx vars from buildenv

--- jenkins/envs/tsa.sh
+++ jenkins/envs/tsa.sh
@@ -18,8 +18,13 @@ export GTRUN_SBATCH_NTASKS_PER_NODE=1
 export GTRUN_SBATCH_GRES='gpu:1'
 export GTRUNMPI_SBATCH_PARTITION='debug'
 export GTRUNMPI_SBATCH_NODES=1
+export GTRUNMPI_SBATCH_NTASKS_PER_NODE=4
 export GTRUNMPI_SBATCH_GRES='gpu:4'
 
 export CUDA_AUTO_BOOST=0
 export GCLOCK=1530
 export OMP_NUM_THREADS=16
+
+export UCX_MEMTYPE_CACHE=n
+export UCX_TLS=rc_x,ud_x,mm,shm,cuda_copy,cuda_ipc,cma
+
