53910ee405bea59b65b5f94ce2ef58d9ee03066e
Felix Thaler <thaler@cscs.ch>
2020-06-12 12:20:57 +0000

    Rename inconsistent environments (#1507)

--- jenkins/update_references.sh
+++ jenkins/update_references.sh
@@ -9,7 +9,7 @@ refpath=$(dirname "$BASH_SOURCE")/../pyutils/perftest/references
 
 for domain in 128 256; do
     for label in tave kesch daint-cn; do
-        for env in gcc clang nvcc_gcc clang_nvcc; do
+        for env in gcc nvcc_clang nvcc_gcc cray; do
             current="${label%-*}_$env/$domain.json"
             src="http://jenkins-mch.cscs.ch/view/GridTools/job/GridTools_perftest_PR/$1/env=$env,label=$label/artifact/build/pyutils/perftest/results/$current"
 
