32476fed9456e97fa71127f023289100623c6537
Hannes Vogt <hannes@havogt.de>
2020-06-12 12:33:15 +0200

    Defaults for gcl::init() (#1513)

--- include/gridtools/gcl/GCL.hpp
+++ include/gridtools/gcl/GCL.hpp
@@ -13,6 +13,8 @@
 
 namespace gridtools {
     namespace gcl {
+        inline auto world() { return MPI_COMM_WORLD; }
+
         namespace impl_ {
             inline int &pid_holder() {
                 static int res;
@@ -23,22 +25,24 @@ namespace gridtools {
                 static int res;
                 return res;
             }
-        } // namespace impl_
 
-        inline auto world() { return MPI_COMM_WORLD; }
+            inline void init(int *argc, char ***argv) {
+                int ready;
+                MPI_Initialized(&ready);
+                if (!ready)
+                    MPI_Init(argc, argv);
+                MPI_Comm_rank(world(), &pid_holder());
+                MPI_Comm_size(world(), &procs_holder());
+            }
+        } // namespace impl_
 
         inline int pid() { return impl_::pid_holder(); }
 
         inline int procs() { return impl_::procs_holder(); }
 
-        inline void init(int argc, char **argv) {
-            int ready;
-            MPI_Initialized(&ready);
-            if (!ready)
-                MPI_Init(&argc, &argv);
-            MPI_Comm_rank(world(), &impl_::pid_holder());
-            MPI_Comm_size(world(), &impl_::procs_holder());
-        }
+        inline void init(int argc, char **argv) { impl_::init(&argc, &argv); }
+
+        inline void init() { impl_::init(nullptr, nullptr); }
 
         inline void finalize() { MPI_Finalize(); }
     } // namespace gcl
