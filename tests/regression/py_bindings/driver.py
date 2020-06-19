fa60a38a068b8e884aaf47380f004a8cb6c5164b
Anton Afanasyev <anstaf@gmail.com>
2020-06-19 16:39:14 +0200

    add python sid adapter

--- tests/regression/py_bindings/driver.py
+++ tests/regression/py_bindings/driver.py
@@ -1,3 +1,31 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+import numpy
 import py_implementation
 
-assert py_implementation.add(1, 2) == 3
+def test_3d():
+    src = numpy.fromfunction(lambda i, j, k : i + j + k, (3, 4, 5), dtype=numpy.double)
+    dst = numpy.empty_like(src)
+    py_implementation.copy_from_3D(src, dst)
+    assert numpy.all(dst == src)
+
+def test_1d():
+    shape = (3, 4, 5)
+    src = numpy.arange(shape[0], dtype=numpy.double)
+    dst = numpy.empty(shape, dtype=numpy.double)
+    py_implementation.copy_from_1D(src, dst)
+    expected = numpy.fromfunction(lambda i, j, k : i, shape, dtype=numpy.double)
+    assert numpy.all(dst == expected)
+
+def test_scalar():
+    shape = (3, 4, 5)
+    dst = numpy.empty(shape, dtype=numpy.double)
+    py_implementation.copy_from_scalar(42, dst)
+    assert numpy.all(dst == 42)
+
+test_3d()
+test_1d()
+test_scalar()
--- tests/regression/py_bindings/implementation.cpp
+++ tests/regression/py_bindings/implementation.cpp
@@ -10,9 +10,50 @@
 
 #include <pybind11/pybind11.h>
 
-int add(int i, int j) { return i + j; }
+#include <gridtools/stencil/cartesian.hpp>
+#include <gridtools/stencil/global_parameter.hpp>
+#include <gridtools/stencil/naive.hpp>
+#include <gridtools/storage/adapter/python_sid_adapter.hpp>
 
-PYBIND11_MODULE(example, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
-    m.def("add", &add, "A function which adds two numbers");
+namespace py = pybind11;
+
+using namespace gridtools;
+using namespace stencil;
+using namespace cartesian;
+
+struct copy_functor {
+    using in = in_accessor<0>;
+    using out = inout_accessor<1>;
+    using param_list = make_param_list<in, out>;
+
+    template <typename Evaluation>
+    GT_FUNCTION static void apply(Evaluation &&eval) {
+        eval(out{}) = eval(in{});
+    }
+};
+
+template <class From, class To>
+void copy(From from, To to) {
+    static_assert(is_sid<From>(), "");
+    static_assert(is_sid<To>(), "");
+    auto &&size = sid::get_upper_bounds(to);
+    run_single_stage(copy_functor(),
+        naive(),
+        make_grid(at_key<dim::i>(size), at_key<dim::j>(size), at_key<dim::k>(size)),
+        std::move(from),
+        std::move(to));
+}
+
+PYBIND11_MODULE(py_implementation, m) {
+    m.def("copy_from_3D",
+        [](py::buffer from, py::buffer to) { copy(as_sid<double const, 3>(from), as_sid<double, 3>(to)); },
+        "Copy from one buffer 3D buffer of doubles to another.");
+
+    m.def("copy_from_1D",
+        [](py::buffer from, py::buffer to) { copy(as_sid<double const, 1>(from), as_sid<double, 3>(to)); },
+        "Copy from the 1D double buffer to a 3D one.");
+
+    m.def("copy_from_scalar",
+        [](double from, py::buffer to) { copy(make_global_parameter(from), as_sid<double, 3>(to)); },
+        "Copy from the 1D double buffer to a 3D one.");
 }
