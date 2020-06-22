ec8a8d2c948c11c5c99f730fb328764e01168cc6
Anton Afanasyev <anstaf@gmail.com>
2020-06-22 14:31:10 +0200

    export numpy as np

--- tests/regression/py_bindings/driver.py
+++ tests/regression/py_bindings/driver.py
@@ -3,27 +3,27 @@ import sys
 
 sys.path.append(os.getcwd())
 
-import numpy
-import py_implementation
+import numpy as np
+import py_implementation as testee
 
 def test_3d():
-    src = numpy.fromfunction(lambda i, j, k : i + j + k, (3, 4, 5), dtype=numpy.double)
-    dst = numpy.empty_like(src)
-    py_implementation.copy_from_3D(src, dst)
-    assert numpy.all(dst == src)
+    src = np.fromfunction(lambda i, j, k : i + j + k, (3, 4, 5), dtype=np.double)
+    dst = np.empty_like(src)
+    testee.copy_from_3D(src, dst)
+    assert np.all(dst == src)
 
 def test_1d():
     shape = (3, 4, 5)
-    src = numpy.arange(shape[0], dtype=numpy.double)
-    dst = numpy.empty(shape, dtype=numpy.double)
-    py_implementation.copy_from_1D(src, dst)
-    expected = numpy.fromfunction(lambda i, j, k : i, shape, dtype=numpy.double)
-    assert numpy.all(dst == expected)
+    src = np.arange(shape[0], dtype=np.double)
+    dst = np.empty(shape, dtype=np.double)
+    testee.copy_from_1D(src, dst)
+    expected = np.fromfunction(lambda i, j, k : i, shape, dtype=np.double)
+    assert np.all(dst == expected)
 
 def test_scalar():
-    dst = numpy.empty((3, 4, 5), dtype=numpy.double)
-    py_implementation.copy_from_scalar(42, dst)
-    assert numpy.all(dst == 42)
+    dst = np.empty((3, 4, 5), dtype=np.double)
+    testee.copy_from_scalar(42, dst)
+    assert np.all(dst == 42)
 
 test_3d()
 test_1d()
