dbe4acbb832881d3674fd49d408150c2f1b7c4ee
Anton Afanasyev <anstaf@gmail.com>
2020-07-08 14:16:15 +0200

    address review comments

--- include/gridtools/sid/delegate.hpp
+++ include/gridtools/sid/delegate.hpp
@@ -23,6 +23,34 @@ namespace gridtools {
          *  Typically the user template class should inherit from `delegate`
          *  For example please look into `test_sid_delegate.cpp`
          *
+         *  The typical usage looks like:
+         *
+         *  namespace foo_impl_ {
+         *    template <class Sid>
+         *    struct foo : delegate<Sid> { using delegate<Sid>::delegate; };
+         *
+         *    // some SID concept overloads for `foo` here
+         *
+         *    template <class Sid> foo<Sid> make_foo(Sid&& sid) { return {std::forward<Sid>(sid)}; }
+         *  }
+         *  using foo_impl_::make_foo;
+         *
+         *  Please be aware that in this example `delegate` could is instantiated with the reference if the L-value is
+         *  passed to the `make_foo`. If R-value is passed, delegate is instantiated with the non-reference type.
+         *  This affects the ownership semantic of the `foo` proxy. Example:
+         *
+         *  my_sid_t a = make_my_sid();
+         *  auto foo_a = make_foo(a);  // `foo_a` contains the reference on `a`.
+         *
+         *  auto foo_b = make_foo(make_my_sid()); // `foo_b` contains the object of `my_sid`
+         *
+         *  Note also that the ctor of `delegate` is propagated to `foo` with C++11 `using` syntax.
+         *  Please don't propagate `delagate` ctor using perfect forwarding like:
+         *  template <class T> foo(T&& obj) : delegate<Sid>(std::forward<T>(obj)) {}
+         *  It's a well known trap -- copy/move `foo` constructor could be shadowed that way.
+         *  Although it is fine to do perfect forwarding with non unaray constructors. Like:
+         *  template <class T> foo(T&& obj, int arg) : delegate<Sid>(std::forward<T>(obj)) { use_arg(arg); }
+         *
          * @tparam Sid a object that models `SID` concept.
          */
         template <class Sid>
@@ -36,6 +64,9 @@ namespace gridtools {
             template <bool IsRef = std::is_reference<Sid>::value, std::enable_if_t<IsRef, int> = 0>
             delegate(Sid impl) : m_impl(impl) {}
         };
+
+        // Here and below SFINAE principal is used to ensure that the concept functions for the `delegate<Sid>` are
+        // defined only if the corespondent functions are defined for `Sid`.
 
         template <class Sid>
         decltype(sid_get_origin(std::declval<Sid &>())) sid_get_origin(delegate<Sid> &obj) {
