fe39a0028bd211d21d6c94a1f1a13e3e6bebee5c
Felix Thaler <thaler@cscs.ch>
2020-09-29 11:31:06 +0200

    Support for explicit and transparent huge pages

--- include/gridtools/common/hugepage_alloc.hpp
+++ include/gridtools/common/hugepage_alloc.hpp
@@ -15,13 +15,14 @@
 #include <cstdlib>
 #include <fstream>
 #include <new>
+#include <stdexcept>
 #if __cpp_lib_int_pow2 >= 202002L
 #include <bit>
 #endif
 #ifdef __linux__
+#include <iostream>
 #include <regex>
 
-#include <errno.h>
 #include <sys/mman.h>
 #endif
 
@@ -58,22 +59,37 @@ namespace gridtools {
             return value;
         }
 
-        inline std::size_t hugepage_size() {
-            std::size_t value = 2 * 1024 * 1024; // 2MB default on most systems
 #ifdef __linux__
+        inline std::size_t parse_proc_meminfo(std::string const &regex) {
             std::ifstream file("/proc/meminfo");
             if (file.is_open()) {
-                std::regex re("^Hugepagesize: *([0-9]+) *kB$");
+                std::regex re(regex);
                 std::string line;
                 while (std::getline(file, line)) {
                     std::smatch match;
                     if (std::regex_match(line, match, re))
-                        return std::stoll(match[1].str()) * 1024;
+                        return std::stoll(match[1].str());
                 }
             }
-#endif
-            return value;
+            throw std::runtime_error("regex '" + regex + "' did not match /proc/meminfo");
         }
+#endif
+
+        inline std::size_t hugepage_size() {
+#ifdef GT_NO_HUGETLB
+            return 1;
+#else
+#ifdef __linux__
+            return parse_proc_meminfo("^Hugepagesize: *([0-9]+) *kB$") * 1024;
+#else
+            return 2 * 1024 * 1024; // 2MB is the default on most systems
+#endif
+#endif
+        }
+
+#ifdef __linux__
+        inline std::size_t free_hugepages() { return parse_proc_meminfo("^HugePages_Free: *([0-9]+) *$"); }
+#endif
     } // namespace hugepage_alloc_impl_
 
     /**
@@ -91,18 +107,35 @@ namespace gridtools {
             ((s_offset++ % cache_sets) << hugepage_alloc_impl_::ilog2(cache_line_size)) + cache_line_size;
         std::size_t full_size = ((size + offset + hugepage_size - 1) / hugepage_size) * hugepage_size;
 
-        void *ptr;
+        void *ptr = nullptr;
 #if defined(__linux__) && !defined(GT_NO_HUGETLB)
-        ptr = mmap(nullptr,
-            full_size,
-            PROT_READ | PROT_WRITE,
-            MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_NORESERVE,
-            -1,
-            0);
-        if (ptr == MAP_FAILED)
+        // we test if explicit huge pages are available to make sure that mmap does not trigger a bus error there is
+        // probably no full-system thread-safe way to do so, so we might still run into one, but the main functionality
+        // is to avoid bus errors if explicit huge pages are not enabled on the system and thus free_hugepages() returns
+        // zero always
+        if (hugepage_alloc_impl_::free_hugepages() * hugepage_size >= full_size) {
+            ptr = mmap(nullptr,
+                full_size,
+                PROT_READ | PROT_WRITE,
+                MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_NORESERVE,
+                -1,
+                0);
+            if (ptr == MAP_FAILED)
+                throw std::bad_alloc();
+        } else {
+            // here we try to get transparent huge pages
+            ptr = mmap(nullptr, full_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
+            if (ptr == MAP_FAILED)
+                throw std::bad_alloc();
+            madvise(ptr, full_size, MADV_HUGEPAGE);
+        }
+#elif !defined(GT_NO_HUGETLB)
+        // here we hope that aligning to hugepage_size will return transparent huge pages
+        if (posix_memalign(&ptr, hugepage_size, full_size))
             throw std::bad_alloc();
 #else
-        if (posix_memalign(&ptr, hugepage_size, full_size))
+        // here we just align to cache line size
+        if (posix_memalign(&ptr, cache_line_size, full_size))
             throw std::bad_alloc();
 #endif
 
