d67facb32b73a052a77e67810ee00f73884c7bd7
Anton Afanasyev <anstaf@gmail.com>
2020-10-05 14:06:29 +0200

    add topo.hpp

---
+++ tests/unit_tests/test_topo.cpp
@@ -0,0 +1,89 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2019, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <gridtools/topo.hpp>
+
+#include <type_traits>
+
+#include <gridtools/meta.hpp>
+
+namespace gridtools::topo {
+    namespace {
+        // locations
+        static_assert(meta::is_set_fast<locations>());
+        static_assert(meta::length<locations>() == 3);
+        static_assert(meta::st_contains<locations, vertex>());
+        static_assert(meta::st_contains<locations, edge>());
+        static_assert(meta::st_contains<locations, cell>());
+
+        // is_location
+        static_assert(!is_location<int>());
+        static_assert(is_location<vertex>());
+        static_assert(is_location<edge>());
+        static_assert(is_location<cell>());
+        static_assert(!is_location<vertex::edge>());
+
+        // is_chain
+        static_assert(!is_chain<int>());
+        static_assert(!is_chain<vertex>());
+        static_assert(is_chain<vertex::edge>());
+        static_assert(is_chain<vertex::cell::edge>());
+
+        // is_link
+        static_assert(!is_link<int>());
+        static_assert(!is_link<vertex>());
+        static_assert(is_link<vertex::edge>());
+        static_assert(!is_link<vertex::cell::edge>());
+        static_assert(!is_link<vertex::cell>());
+
+        // invalid chains. should not compile.
+        // TODO(anstaf): Is there the way to check it? run compilation and verify that it fails?
+        // using same_location_t = vertex::vertex;
+
+        // chain canonicalization
+        static_assert(std::is_same_v<vertex::cell, vertex::edge::cell>);
+        static_assert(std::is_same_v<cell::vertex, cell::edge::vertex>);
+
+        // reverse
+        static_assert(std::is_same_v<reverse<vertex::edge>, edge::vertex>);
+        static_assert(std::is_same_v<reverse<vertex::cell::edge::vertex::cell>, cell::vertex::edge::cell::vertex>);
+
+        // from
+        static_assert(std::is_same_v<from<vertex::cell::edge::vertex::cell>, vertex>);
+
+        // to
+        static_assert(std::is_same_v<to<vertex::cell::edge::vertex::cell>, cell>);
+
+        // join
+        static_assert(std::is_same_v<join<meta::list<vertex::cell, cell::edge, edge::vertex, vertex::cell>>,
+            vertex::cell::edge::vertex::cell>);
+
+        // links
+        namespace links_test {
+            using src_t = vertex::cell::edge::vertex::cell;
+            using testee_t = links<src_t>;
+            static_assert(meta::length<testee_t>() == 6);
+            static_assert(std::is_same_v<join<testee_t>, src_t>);
+        } // namespace links_test
+
+        // splits
+        namespace splits_test {
+            using src_t = vertex::cell::edge::vertex::cell;
+            using testee_t = splits<src_t>;
+            static_assert(meta::length<testee_t>() == 32);
+            static_assert(meta::is_set_fast<testee_t>());
+
+            template <class T>
+            using check = std::is_same<join<T>, src_t>;
+
+            static_assert(meta::all<meta::transform<check, testee_t>>());
+        } // namespace splits_test
+    }     // namespace
+} // namespace gridtools::topo
