3d4bbfa898d786d9c67fac19ab0a9a75c38f1c70
Hannes Vogt <hannes@havogt.de>
2020-10-07 13:24:31 +0200

    remove module wrapper

--- jenkins/envs/dom_nvcc_gcc.sh
+++ jenkins/envs/dom_nvcc_gcc.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-source $(dirname "$BASH_SOURCE")/daint_nvcc_gcc.sh # daint on purpose until it breaks
+source $(dirname "$BASH_SOURCE")/dom.sh
+
+module swap PrgEnv-cray PrgEnv-gnu
+
+if [ "$build_type" != "debug" ]; then
+  module load HPX/1.5.0-CrayGNU-20.08-cuda
+fi
+
+export CXX=$(which CC)
+export CC=$(which cc)
+export FC=$(which ftn)
+export CUDAHOSTCXX="$CXX"
+
+export GTCMAKE_CMAKE_CXX_FLAGS='-march=haswell'
+export GTCMAKE_CMAKE_CXX_FLAGS_RELEASE='-Ofast -DNDEBUG'
+
+export CTEST_PARALLEL_LEVEL=1
