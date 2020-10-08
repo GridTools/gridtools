089f9019e1936a380e993a4ffdc8387448e83b9c
Hannes Vogt <hannes@havogt.de>
2020-10-08 10:51:27 +0200

    set account

--- jenkins/envs/dom.sh
+++ jenkins/envs/dom.sh
@@ -11,7 +11,8 @@ export BOOST_ROOT=/project/c14/install/daint/boost/boost_1_67_0/
 export CUDATOOLKIT_HOME=$CUDA_PATH
 export CUDA_ARCH=sm_60
 
-export GTRUN_BUILD_COMMAND='srun -C gpu -p cscsci --time=00:20:00 make -j 24'
+export GTRUN_BUILD_COMMAND='srun --account c14 -C gpu -p cscsci --time=00:20:00 make -j 24'
+export GTRUN_SBATCH_ACCOUNT='c14'
 export GTRUN_SBATCH_PARTITION='cscsci'
 export GTRUN_SBATCH_NODES=1
 export GTRUN_SBATCH_NTASKS_PER_CORE=2
