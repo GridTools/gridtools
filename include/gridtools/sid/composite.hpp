847c5b2a60bb926e0366fdb3ce031f53c6a3687b
Anton Afanasyev <31539054+anstaf@users.noreply.github.com>
2020-10-23 19:06:46 +0200

    fix an empty composite and add composite::make helper (#1583)

--- include/gridtools/sid/composite.hpp
+++ include/gridtools/sid/composite.hpp
@@ -346,10 +346,16 @@ namespace gridtools {
                             [](auto obj) GT_FORCE_INLINE_LAMBDA { return get_origin(obj); }, obj.m_sids);
                     }
 
+                    template <class U = strides_t, std::enable_if_t<!meta::is_empty<U>::value, int> = 0>
                     friend strides_t sid_get_strides(values const &obj) {
                         return tuple_util::transform(typename compressed_t::convert_f(),
                             tuple_util::transpose(
                                 tuple_util::transform(impl_::normalize_strides_f<stride_keys_t>(), obj.m_sids)));
+                    }
+
+                    template <class U = strides_t, std::enable_if_t<meta::is_empty<U>::value, int> = 0>
+                    friend strides_t sid_get_strides(values const &) {
+                        return {};
                     }
 
                     friend ptr_diff_t sid_get_ptr_diff(values const &) { return {}; }
@@ -388,6 +394,17 @@ namespace gridtools {
                     friend keys hymap_get_keys(values const &) { return {}; }
                 };
             };
+
+            template <class... Keys>
+            struct make_f {
+                template <class... Sids>
+                constexpr auto operator()(Sids &&... sids) const {
+                    return tuple_util::make<keys<Keys...>::template values>(std::forward<Sids>(sids)...);
+                }
+            };
+
+            template <class... Keys>
+            constexpr make_f<Keys...> make = {};
         } // namespace composite
     }     // namespace sid
 } // namespace gridtools
