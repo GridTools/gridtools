25ecc0491ef503dbb3c4c12bada4f8c7b9182126
Felix Thaler <thaler@cscs.ch>
2020-12-15 09:05:40 +0000

    More stable ROCm Environment on Ault (#1588)
    This enables all AMD GPU nodes on Ault for possible execution of the tests and fixes ROCm 3.10 issues.

--- jenkins/envs/ault_hip.sh
+++ jenkins/envs/ault_hip.sh
@@ -15,15 +15,15 @@ export CXX=$(which hipcc)
 export CC=$(which gcc)
 export FC=$(which gfortran)
 
-export GTRUN_BUILD_COMMAND='srun -w ault20 --time=03:00:00 make -j 64'
+export GTRUN_BUILD_COMMAND='srun -p amdvega --time=03:00:00 make -j 64'
 export GTRUN_SBATCH_NTASKS=1
 export GTRUN_SBATCH_CPUS_PER_TASK=128
 export GTRUN_SBATCH_MEM_BIND=local
-export GTRUN_SBATCH_NODELIST=ault20
+export GTRUN_SBATCH_PARTITION=amdvega
 export GTRUN_SBATCH_TIME='00:30:00'
 export GTCMAKE_CMAKE_CXX_FLAGS_RELEASE='-O3 -DNDEBUG -march=znver1'
 
-export HIP_VISIBLE_DEVICES=0
+export HIP_VISIBLE_DEVICES=1
 export OMP_NUM_THREADS=64
 export OMP_PLACES='{0}:64'
 export HCC_AMDGPU_TARGET=gfx906
