56630769a366b573934497fae4b2297873222a48
Hannes Vogt <vogt@hey.com>
2021-03-23 14:05:01 +0100

    update more envs

--- jenkins/envs/ault.sh
+++ jenkins/envs/ault.sh
@@ -5,7 +5,7 @@ source $(dirname "$BASH_SOURCE")/base.sh
 source /users/fthaler/public/jenkins/spack/share/spack/setup-env.sh
 
 spack load boost
-spack load cmake
+module load cmake/3.18.2
 
 export GTRUN_BUILD_COMMAND='make -j 8'
 export GTRUN_SBATCH_NODES=1
