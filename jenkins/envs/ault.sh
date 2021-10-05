96786b7a20349748087b770382d1ebd909dd85f3
Felix Thaler <thaler@cscs.ch>
2021-10-05 11:58:22 +0200

    Use spack-installed CMake

--- jenkins/envs/ault.sh
+++ jenkins/envs/ault.sh
@@ -5,7 +5,7 @@ source $(dirname "$BASH_SOURCE")/base.sh
 source /users/fthaler/public/jenkins/spack/share/spack/setup-env.sh
 
 spack load boost
-module load cmake/3.18.2
+spack load cmake
 
 export GTRUN_BUILD_COMMAND='make -j 8'
 export GTRUN_SBATCH_NODES=1
