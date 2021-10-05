2fa0d2d3760163fb0c031bf59524d34aa76346cc
Hannes Vogt <hannes@havogt.de>
2021-10-05 15:54:29 +0200

    boost 1.77

--- jenkins/envs/daint.sh
+++ jenkins/envs/daint.sh
@@ -20,7 +20,7 @@ module load cudatoolkit
 module load CMake
 
 
-export BOOST_ROOT=/apps/daint/UES/jenkins/7.0.UP02-20.11/gpu/easybuild/software/Boost/1.75.0-CrayGNU-20.11
+export BOOST_ROOT=/apps/daint/SSL/gridtools/jenkins/boost_1_77_0
 export CUDATOOLKIT_HOME=$CUDA_PATH
 export CUDA_ARCH=sm_60
 
