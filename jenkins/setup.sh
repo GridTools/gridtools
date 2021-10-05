ebac8f5763ba8930c6d5f8869281de0f588d4595
Hannes Vogt <hannes@havogt.de>
2021-10-05 16:02:56 +0200

    redirect dom to daint

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -10,8 +10,8 @@ envfile=./jenkins/envs/${label}_$env.sh
 # use the machines python virtualenv with required modules installed
 if [[ $label = ault ]]; then
     venv_dir=/users/fthaler/public/jenkins/gridtools-venv
-else
-    venv_dir=/apps/$label/SSL/gridtools/jenkins/venv
+elif [[ $label = dom ]]; then
+    venv_dir=/apps/daint/SSL/gridtools/jenkins/venv
 fi
 source $venv_dir/bin/activate
 
