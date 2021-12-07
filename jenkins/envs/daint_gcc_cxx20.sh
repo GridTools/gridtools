e4c1857c76c50149da824614475f2d208fba6d30
Hannes Vogt <hannes@havogt.de>
2021-12-07 10:15:38 +0100

    more disable CUDA

--- jenkins/envs/daint_gcc_cxx20.sh
+++ jenkins/envs/daint_gcc_cxx20.sh
@@ -4,4 +4,6 @@ source $(dirname "$BASH_SOURCE")/daint_nvcc_gcc.sh
 module switch gcc/10.3.0
 module unload cudatoolkit
 
+export GTCMAKE_GT_TESTS_REQUIRE_GPU=OFF
 export GTCMAKE_GT_TESTS_CXX_STANDARD=20
+
