580472ccbeb29354c2b963d4cdc7e69cee494fca
Hannes Vogt <hannes@havogt.de>
2021-12-07 14:28:33 +0100

    back to clang

--- jenkins/envs/daint_gcc_cxx20.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source $(dirname "$BASH_SOURCE")/daint_nvcc_gcc.sh
-module switch gcc/10.3.0
-module unload cudatoolkit
-
-export GTCMAKE_GT_TESTS_REQUIRE_GPU=OFF
-export GTCMAKE_GT_TESTS_CXX_STANDARD=20
-
