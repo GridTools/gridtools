bb63e698477994dc849087ca7cabbf117fc2d8c4
Hannes Vogt <vogt@hey.com>
2021-12-10 13:28:45 +0100

    cleanup

--- jenkins/plot_full_comparison.sh
+++ jenkins/plot_full_comparison.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source $(dirname "$0")/config.sh
+source $(dirname "$0")/setup.sh
 
 # create directory for reports
 mkdir reports
--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-source $(dirname "$0")/config.sh
+echo "Running on host $(hostname)"
+
+# remove -cn from label (for daint)
+label=${label%%-*}
+
+envfile=./jenkins/envs/${label}_$env.sh
+
+export GT_PERFORMANCE_HISTORY_PATH_PREFIX=/apps/${label}/SSL/gridtools/jenkins/gridtools-performance-history-gt2
+
+# use the machines python virtualenv with required modules installed
+if [[ $label = ault ]]; then
+    venv_dir=/users/fthaler/public/jenkins/gridtools-venv
+elif [[ $label = dom ]]; then
+    venv_dir=/apps/daint/SSL/gridtools/jenkins/venv
+else
+    venv_dir=/apps/$label/SSL/gridtools/jenkins/venv
+fi
+source $venv_dir/bin/activate
 
 if [[ $label != "tsa" ]]; then
     export SLURM_ACCOUNT=d75
