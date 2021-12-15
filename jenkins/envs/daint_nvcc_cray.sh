a250a70945ce7986e2b1ef6612519cbd8a5e75b0
Hannes Vogt <hannes@havogt.de>
2021-12-15 08:48:23 +0100

    switch to cuda 11.2

--- jenkins/envs/daint_nvcc_cray.sh
+++ jenkins/envs/daint_nvcc_cray.sh
@@ -2,6 +2,9 @@
 
 source $(dirname "$BASH_SOURCE")/daint_cray.sh
 
+module switch cudatoolkit/11.0.2_3.38-8.1__g5b73779 cudatoolkit/11.2.0_3.39-2.1__gf93aa1c
+
 export GTCMAKE_GT_CLANG_CUDA_MODE=NVCC-CUDA
 
 export CTEST_PARALLEL_LEVEL=1
+
