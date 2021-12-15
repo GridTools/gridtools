2e39329c94e428c5964166bc0c2c448b849b961b
Hannes Vogt <hannes@havogt.de>
2021-12-15 09:22:23 +0100

    Update daint_nvcc_cray.sh

--- jenkins/envs/daint_nvcc_cray.sh
+++ jenkins/envs/daint_nvcc_cray.sh
@@ -2,7 +2,9 @@
 
 source $(dirname "$BASH_SOURCE")/daint_cray.sh
 
-module switch cudatoolkit/11.0.2_3.38-8.1__g5b73779 cudatoolkit/11.2.0_3.39-2.1__gf93aa1c
+# only supported configuration on daint for NVCC-CUDA mode
+module switch cudatoolkit/11.2.0_3.39-2.1__gf93aa1c
+module switch cce/10.0.2
 
 export GTCMAKE_GT_CLANG_CUDA_MODE=NVCC-CUDA
 
