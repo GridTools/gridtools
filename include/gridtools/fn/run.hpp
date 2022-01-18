da12d63f29e94a9da81cc119a5d0bd417bfafc9c
Felix Thaler <thaler@cscs.ch>
2022-01-18 14:15:18 +0100

    Added run function for stencils

---
+++ include/gridtools/fn/run.hpp
@@ -0,0 +1,38 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2021, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#pragma once
+
+#include "gridtools/meta/debug.hpp"
+#include <gridtools/common/tuple_util.hpp>
+#include <gridtools/meta.hpp>
+#include <gridtools/sid/composite.hpp>
+
+namespace gridtools::fn {
+    namespace run_impl_ {
+        template <class Sids>
+        auto make_composite(Sids &&sids) {
+            using keys_t = meta::iseq_to_list<std::make_integer_sequence<int, std::tuple_size_v<Sids>>,
+                sid::composite::keys,
+                integral_constant>;
+            return tuple_util::convert_to<keys_t::template values>(std::forward<Sids>(sids));
+        }
+
+        template <class Backend, class StageSpecs, class Domain, class Sids>
+        void run(Backend, StageSpecs, Domain const &domain, Sids &&sids) {
+            auto composite = make_composite(std::forward<Sids>(sids));
+            auto ptr = sid::get_origin(composite)();
+            tuple_util::for_each(
+                [&](auto stage) { apply_stencil_stage(Backend(), domain, std::move(stage), composite); },
+                meta::rename<std::tuple, StageSpecs>());
+        }
+    } // namespace run_impl_
+
+    using run_impl_::run;
+} // namespace gridtools::fn
---
+++ tests/unit_tests/fn/test_fn_run.cpp
@@ -0,0 +1,83 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2021, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <gridtools/fn/run.hpp>
+
+#include <gtest/gtest.h>
+
+#include <gridtools/fn/backend2/naive.hpp>
+#include <gridtools/fn/scan.hpp>
+#include <gridtools/fn/stencil_stage.hpp>
+#include <gridtools/sid/concept.hpp>
+
+namespace gridtools::fn {
+    namespace {
+        using namespace literals;
+        using sid::property;
+
+        template <int I>
+        using int_t = integral_constant<int, I>;
+
+        struct stencil {
+            GT_FUNCTION constexpr auto operator()() const {
+                return [](auto const &iter) { return 2 * *iter; };
+            }
+        };
+
+        struct sum_scan : fwd {
+            static GT_FUNCTION constexpr auto body() {
+                return scan_pass(
+                    [](auto acc, auto const &iter) { return tuple(get<0>(acc) + *iter, get<1>(acc) * *iter); },
+                    [](auto acc) { return get<0>(acc); });
+            }
+        };
+
+        struct make_iterator_mock {
+            GT_FUNCTION auto operator()() const {
+                return [](auto tag, auto const &ptr, auto const &strides) { return at_key<decltype(tag)>(ptr); };
+            }
+        };
+
+        TEST(run, stencils) {
+            using vdim_t = int_t<1>;
+            using backend_t = backend::naive;
+            using stages_specs_t = meta::list<stencil_stage<stencil, make_iterator_mock, 1, 2>,
+                stencil_stage<stencil, make_iterator_mock, 0, 1>>;
+            auto domain = hymap::keys<int_t<0>, int_t<1>>::values(2_c, 3_c);
+
+            auto alloc = backend::tmp_allocator(backend_t());
+            auto a = backend::allocate_global_tmp<int>(alloc, domain);
+            auto b = backend::allocate_global_tmp<int>(alloc, domain);
+            auto c = backend::allocate_global_tmp<int>(alloc, domain);
+
+            auto loop = [](auto x, auto f) {
+                auto ptr = sid::get_origin(x)();
+                auto strides = sid::get_strides(x);
+                for (int i = 0; i < 2; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        f(ptr, i, j);
+                        sid::shift(ptr, sid::get_stride<int_t<1>>(strides), 1_c);
+                    }
+                    sid::shift(ptr, sid::get_stride<int_t<1>>(strides), -3_c);
+                    sid::shift(ptr, sid::get_stride<int_t<0>>(strides), 1_c);
+                }
+            };
+            loop(a, [](auto &ptr, int i, int j) { *ptr = 0; });
+            loop(b, [](auto &ptr, int i, int j) { *ptr = 0; });
+            loop(c, [](auto &ptr, int i, int j) { *ptr = 3 * i + j; });
+
+            run(backend_t(), stages_specs_t(), domain, std::forward_as_tuple(a, b, c));
+
+            loop(a, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 4); });
+            loop(b, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 2); });
+            loop(c, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 1); });
+        }
+
+    } // namespace
+} // namespace gridtools::fn
