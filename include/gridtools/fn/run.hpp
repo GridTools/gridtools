afe30b5e22dcd6445c54a9e38f296a42d23886a5
Felix Thaler <thaler@cscs.ch>
2022-01-18 15:16:55 +0100

    Support for column_stage in run()

--- include/gridtools/fn/run.hpp
+++ include/gridtools/fn/run.hpp
@@ -9,10 +9,10 @@
  */
 #pragma once
 
-#include "gridtools/meta/debug.hpp"
-#include <gridtools/common/tuple_util.hpp>
-#include <gridtools/meta.hpp>
-#include <gridtools/sid/composite.hpp>
+#include "../common/tuple_util.hpp"
+#include "../meta.hpp"
+#include "../sid/composite.hpp"
+#include "./stencil_stage.hpp"
 
 namespace gridtools::fn {
     namespace run_impl_ {
@@ -24,12 +24,28 @@ namespace gridtools::fn {
             return tuple_util::convert_to<keys_t::template values>(std::forward<Sids>(sids));
         }
 
-        template <class Backend, class StageSpecs, class Domain, class Sids>
-        void run(Backend, StageSpecs, Domain const &domain, Sids &&sids) {
+        template <class T>
+        struct is_stencil_stage : std::false_type {};
+
+        template <class Stencil, class MakeIterator, int Out, int... Ins>
+        struct is_stencil_stage<stencil_stage<Stencil, MakeIterator, Out, Ins...>> : std::true_type {};
+
+        template <class... Stages>
+        struct is_stencil_stage<merged_stencil_stage<Stages...>> : std::true_type {};
+
+        template <class Backend, class Vertical, class StageSpecs, class Domain, class Sids, class Seeds>
+        void run(Backend, Vertical, StageSpecs, Domain const &domain, Sids &&sids, Seeds &&seeds) {
             auto composite = make_composite(std::forward<Sids>(sids));
             tuple_util::for_each(
-                [&](auto stage) { apply_stencil_stage(Backend(), domain, std::move(stage), composite); },
-                meta::rename<std::tuple, StageSpecs>());
+                [&](auto stage, auto seed) {
+                    if constexpr (is_stencil_stage<decltype(stage)>()) {
+                        apply_stencil_stage(Backend(), domain, std::move(stage), composite);
+                    } else {
+                        apply_column_stage(Backend(), domain, std::move(stage), composite, Vertical(), std::move(seed));
+                    }
+                },
+                meta::rename<std::tuple, StageSpecs>(),
+                std::forward<Seeds>(seeds));
         }
     } // namespace run_impl_
 
--- tests/unit_tests/fn/test_fn_run.cpp
+++ tests/unit_tests/fn/test_fn_run.cpp
@@ -9,6 +9,9 @@
  */
 #include <gridtools/fn/run.hpp>
 
+#include <tuple>
+#include <variant>
+
 #include <gtest/gtest.h>
 
 #include <gridtools/fn/backend2/naive.hpp>
@@ -32,9 +35,7 @@ namespace gridtools::fn {
 
         struct sum_scan : fwd {
             static GT_FUNCTION constexpr auto body() {
-                return scan_pass(
-                    [](auto acc, auto const &iter) { return tuple(get<0>(acc) + *iter, get<1>(acc) * *iter); },
-                    [](auto acc) { return get<0>(acc); });
+                return scan_pass([](auto acc, auto const &iter) { return acc + *iter; }, [](auto acc) { return acc; });
             }
         };
 
@@ -45,7 +46,6 @@ namespace gridtools::fn {
         };
 
         TEST(run, stencils) {
-            using vdim_t = int_t<1>;
             using backend_t = backend::naive;
             using stages_specs_t = meta::list<stencil_stage<stencil, make_iterator_mock, 1, 2>,
                 stencil_stage<stencil, make_iterator_mock, 0, 1>>;
@@ -72,11 +72,56 @@ namespace gridtools::fn {
             loop(b, [](auto &ptr, int i, int j) { *ptr = 0; });
             loop(c, [](auto &ptr, int i, int j) { *ptr = 3 * i + j; });
 
-            run(backend_t(), stages_specs_t(), domain, std::forward_as_tuple(a, b, c));
+            run(backend_t(),
+                int_t<1>(),
+                stages_specs_t(),
+                domain,
+                std::forward_as_tuple(a, b, c),
+                std::forward_as_tuple(std::monostate(), std::monostate()));
 
             loop(a, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 4); });
             loop(b, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 2); });
             loop(c, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 1); });
+        }
+
+        TEST(run, scan_and_stencil) {
+            using backend_t = backend::naive;
+            using stages_specs_t = meta::list<column_stage<int_t<1>, sum_scan, make_iterator_mock, 1, 2>,
+                stencil_stage<stencil, make_iterator_mock, 0, 1>>;
+            auto domain = hymap::keys<int_t<0>, int_t<1>>::values(2_c, 3_c);
+
+            auto alloc = backend::tmp_allocator(backend_t());
+            auto a = backend::allocate_global_tmp<int>(alloc, domain);
+            auto b = backend::allocate_global_tmp<int>(alloc, domain);
+            auto c = backend::allocate_global_tmp<int>(alloc, domain);
+
+            auto loop = [](auto x, auto f) {
+                auto ptr = sid::get_origin(x)();
+                auto strides = sid::get_strides(x);
+                for (int i = 0; i < 2; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        f(ptr, i, j);
+                        sid::shift(ptr, sid::get_stride<int_t<1>>(strides), 1_c);
+                    }
+                    sid::shift(ptr, sid::get_stride<int_t<1>>(strides), -3_c);
+                    sid::shift(ptr, sid::get_stride<int_t<0>>(strides), 1_c);
+                }
+            };
+            loop(a, [](auto &ptr, int i, int j) { *ptr = 0; });
+            loop(b, [](auto &ptr, int i, int j) { *ptr = 0; });
+            loop(c, [](auto &ptr, int i, int j) { *ptr = 3 * i + j; });
+
+            run(backend_t(),
+                int_t<1>(),
+                stages_specs_t(),
+                domain,
+                std::forward_as_tuple(a, b, c),
+                std::forward_as_tuple(42, std::monostate()));
+
+            loop(a,
+                [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (42 + (3 * i * (j + 1) + j * (j + 1) / 2)) * 2); });
+            loop(b, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, 42 + (3 * i * (j + 1) + j * (j + 1) / 2)); });
+            loop(c, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, 3 * i + j); });
         }
 
     } // namespace
