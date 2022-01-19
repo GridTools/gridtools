82d2141230a08eb875cef3f1cc8822f2c11eb9e5
Felix Thaler <thaler@cscs.ch>
2022-01-19 09:43:05 +0100

    Added vertical_executor

--- include/gridtools/fn/executor.hpp
+++ include/gridtools/fn/executor.hpp
@@ -14,6 +14,8 @@
 #include "../common/tuple_util.hpp"
 #include "../meta.hpp"
 #include "./run.hpp"
+#include "./scan.hpp"
+#include "./stencil_stage.hpp"
 
 namespace gridtools::fn {
 
@@ -40,6 +42,44 @@ namespace gridtools::fn {
             using stages_t = meta::push_back<Stages, stencil_stage<Stencil, MakeIterator, Out::value, Ins::value...>>;
             return stencil_executor<Backend, MakeIterator, Domain, Args, stages_t>{
                 std::move(m_domain), std::move(m_args)};
+        }
+    };
+
+    template <class Backend,
+        class MakeIterator,
+        class Domain,
+        class Vertical,
+        class Args = std::tuple<>,
+        class Stages = meta::list<>,
+        class Seeds = std::tuple<>>
+    class vertical_executor {
+        Domain m_domain;
+        Args m_args;
+        Seeds m_seeds;
+
+      public:
+        vertical_executor(Backend, MakeIterator, Domain const &domain, Vertical) : m_domain(domain) {}
+        vertical_executor(Domain &&domain, Args &&args, Seeds &&seeds)
+            : m_domain(std::move(domain)), m_args(std::move(args)), m_seeds(std::move(seeds)) {}
+        vertical_executor(vertical_executor const &) = delete;
+        vertical_executor(vertical_executor &&) = default;
+        ~vertical_executor() {
+            run_vertical(Backend(), Stages(), m_domain, Vertical(), std::move(m_args), std::move(m_seeds));
+        }
+
+        template <class Arg>
+        auto arg(Arg &&arg) && {
+            auto args = tuple_util::push_back(std::move(m_args), std::forward<Arg>(arg));
+            return vertical_executor<Backend, MakeIterator, Domain, Vertical, decltype(args), Stages, Seeds>{
+                std::move(m_domain), std::move(args), std::move(m_seeds)};
+        }
+        template <class Out, class Stencil, class Seed, class... Ins>
+        auto assign(Out, Stencil, Seed &&seed, Ins...) && {
+            using stages_t =
+                meta::push_back<Stages, column_stage<Vertical, Stencil, MakeIterator, Out::value, Ins::value...>>;
+            auto seeds = tuple_util::push_back(std::move(m_seeds), std::forward<Seed>(seed));
+            return vertical_executor<Backend, MakeIterator, Domain, Vertical, Args, stages_t, decltype(seeds)>{
+                std::move(m_domain), std::move(m_args), std::move(seeds)};
         }
     };
 
--- tests/unit_tests/fn/test_fn_executor.cpp
+++ tests/unit_tests/fn/test_fn_executor.cpp
@@ -12,6 +12,7 @@
 #include <gtest/gtest.h>
 
 #include <gridtools/fn/backend2/naive.hpp>
+#include <gridtools/fn/scan.hpp>
 
 namespace gridtools::fn {
     namespace {
@@ -27,13 +28,25 @@ namespace gridtools::fn {
             }
         };
 
+        struct fwd_sum_scan : fwd {
+            static GT_FUNCTION constexpr auto body() {
+                return scan_pass([](auto acc, auto const &iter) { return acc + *iter; }, [](auto acc) { return acc; });
+            }
+        };
+
+        struct bwd_sum_scan : bwd {
+            static GT_FUNCTION constexpr auto body() {
+                return scan_pass([](auto acc, auto const &iter) { return acc + *iter; }, [](auto acc) { return acc; });
+            }
+        };
+
         struct make_iterator_mock {
             GT_FUNCTION auto operator()() const {
                 return [](auto tag, auto const &ptr, auto const &strides) { return at_key<decltype(tag)>(ptr); };
             }
         };
 
-        TEST(executor, args) {
+        TEST(stencil_executor, smoke) {
             using backend_t = backend::naive;
             auto domain = hymap::keys<int_t<0>, int_t<1>>::values(2_c, 3_c);
 
@@ -70,6 +83,49 @@ namespace gridtools::fn {
             loop(a, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 4); });
             loop(b, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 2); });
             loop(c, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, (3 * i + j) * 1); });
+        }
+
+        TEST(vertical_executor, smoke) {
+            using backend_t = backend::naive;
+            using stages_specs_t = meta::list<column_stage<int_t<1>, fwd_sum_scan, make_iterator_mock, 1, 2>,
+                column_stage<int_t<1>, bwd_sum_scan, make_iterator_mock, 0, 1>>;
+            auto domain = hymap::keys<int_t<0>, int_t<1>>::values(2_c, 3_c);
+
+            auto alloc = backend::tmp_allocator(backend_t());
+            auto a = backend::allocate_global_tmp<int>(alloc, domain);
+            auto b = backend::allocate_global_tmp<int>(alloc, domain);
+            auto c = backend::allocate_global_tmp<int>(alloc, domain);
+
+            auto loop = [](auto x, auto f) {
+                auto ptr = sid::get_origin(x)();
+                auto strides = sid::get_strides(x);
+                for (int i = 0; i < 2; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        f(ptr, i, j);
+                        sid::shift(ptr, sid::get_stride<int_t<1>>(strides), 1_c);
+                    }
+                    sid::shift(ptr, sid::get_stride<int_t<1>>(strides), -3_c);
+                    sid::shift(ptr, sid::get_stride<int_t<0>>(strides), 1_c);
+                }
+            };
+            loop(a, [](auto &ptr, int i, int j) { *ptr = 0; });
+            loop(b, [](auto &ptr, int i, int j) { *ptr = 0; });
+            loop(c, [](auto &ptr, int i, int j) { *ptr = 3 * i + j; });
+
+            {
+                vertical_executor(backend_t(), make_iterator_mock(), domain, int_t<1>())
+                    .arg(a)
+                    .arg(b)
+                    .arg(c)
+                    .assign(1_c, fwd_sum_scan(), 42, 2_c)
+                    .assign(0_c, bwd_sum_scan(), 8, 1_c);
+            }
+
+            loop(a, [](auto const &ptr, int i, int j) {
+                EXPECT_EQ(*ptr, -(9 * i * j * (j + 1) - 108 * i + j * j * j + 251 * j - 828) / 6);
+            });
+            loop(b, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, 42 + 3 * i * (j + 1) + j * (j + 1) / 2); });
+            loop(c, [](auto const &ptr, int i, int j) { EXPECT_EQ(*ptr, 3 * i + j); });
         }
     } // namespace
 } // namespace gridtools::fn
