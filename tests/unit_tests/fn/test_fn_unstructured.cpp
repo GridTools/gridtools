1b89fac71a32073e06c26f6a360c3d56b06d3c46
Felix Thaler <thaler@cscs.ch>
2022-01-21 13:53:20 +0100

    Added v2e test

--- tests/unit_tests/fn/test_fn_unstructured.cpp
+++ tests/unit_tests/fn/test_fn_unstructured.cpp
@@ -20,28 +20,30 @@ namespace gridtools::fn {
         using namespace literals;
         using sid::property;
 
-        template <class V2V>
+        template <class C, int MaxNeighbors>
         struct stencil {
             constexpr auto operator()() const {
                 return [](auto const &in) {
-                    int sum = deref(in);
-                    if (can_deref(shift(in, V2V(), 0)))
-                        sum += deref(shift(in, V2V(), 0));
-                    if (can_deref(shift(in, V2V(), 1)))
-                        sum += deref(shift(in, V2V(), 1));
-                    if (can_deref(shift(in, V2V(), 2)))
-                        sum += deref(shift(in, V2V(), 2));
+                    int sum = 0;
+                    tuple_util::for_each(
+                        [&](auto offset) {
+                            if (can_deref(shift(in, C(), offset)))
+                                sum += deref(shift(in, C(), offset));
+                        },
+                        meta::rename<tuple, meta::make_indices_c<MaxNeighbors>>());
                     return sum;
                 };
             }
         };
 
         struct vertex {};
+        struct edge {};
         struct v2v {};
+        struct v2e {};
 
-        TEST(unstructured, stencil) {
+        TEST(unstructured, v2v_sum) {
             auto apply_stencil = [](auto executor, auto &out, auto const &in, auto const &vidx) {
-                executor().arg(out).arg(in).arg(vidx).assign(0_c, stencil<v2v>(), 1_c);
+                executor().arg(out).arg(in).arg(vidx).assign(0_c, stencil<v2v, 3>(), 1_c);
             };
             auto fencil =
                 [&](auto const &v2v_table, int nvertices, int nlevels, auto &out, auto const &in, auto const &vidx) {
@@ -76,11 +78,57 @@ namespace gridtools::fn {
 
             for (int v = 0; v < 3; ++v)
                 for (int k = 0; k < 5; ++k) {
-                    int nbsum = in[v][k];
+                    int nbsum = 0;
                     for (int i = 0; i < 3; ++i) {
                         int nb = v2v_table[v][i];
                         if (nb != -1)
                             nbsum += in[nb][k];
+                    }
+                    EXPECT_EQ(out[v][k], nbsum);
+                }
+        }
+
+        TEST(unstructured, v2e_sum) {
+            auto apply_stencil = [](auto executor, auto &out, auto const &in, auto const &vidx) {
+                executor().arg(out).arg(in).arg(vidx).assign(0_c, stencil<v2e, 2>(), 1_c);
+            };
+            auto fencil =
+                [&](auto const &v2e_table, int nvertices, int nlevels, auto &out, auto const &in, auto const &vidx) {
+                    auto v2e_conn = connectivity<v2e, vertex, edge>(v2e_table, 3_c);
+                    auto domain = unstructured_domain<vertex>(nvertices, nlevels, v2e_conn);
+                    auto backend = make_backend(backend::naive(), domain);
+                    apply_stencil(backend.stencil_executor(), out, in, vidx);
+                };
+
+            int v2e_table[3][2] = {{0, 2}, {0, 1}, {1, 2}};
+            auto v2e_conn =
+                sid::synthetic()
+                    .set<property::origin>(sid::host::make_simple_ptr_holder(&v2e_table[0][0]))
+                    .set<property::strides>(hymap::keys<vertex, unstructured::dim::neighbor>::values(2_c, 1_c));
+
+            int in[3][5], out[3][5] = {};
+            for (int e = 0; e < 3; ++e)
+                for (int k = 0; k < 5; ++k)
+                    in[e][k] = 5 * e + k;
+
+            auto as_synthetic = [](int x[3][5], auto loc) {
+                return sid::synthetic()
+                    .set<property::origin>(sid::host::make_simple_ptr_holder(&x[0][0]))
+                    .set<property::strides>(hymap::keys<decltype(loc), unstructured::dim::k>::make_values(5_c, 1_c));
+            };
+            auto in_s = as_synthetic(in, edge());
+            auto out_s = as_synthetic(out, vertex());
+
+            auto vidx = gridtools::stencil::positional<vertex>();
+
+            fencil(v2e_conn, 3, 5, out_s, in_s, vidx);
+
+            for (int v = 0; v < 3; ++v)
+                for (int k = 0; k < 5; ++k) {
+                    int nbsum = 0;
+                    for (int i = 0; i < 2; ++i) {
+                        int nb = v2e_table[v][i];
+                        nbsum += in[nb][k];
                     }
                     EXPECT_EQ(out[v][k], nbsum);
                 }
