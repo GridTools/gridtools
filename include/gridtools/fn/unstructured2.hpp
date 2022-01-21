d740317fa6cf5f049fcee33b4201715f09aad7b9
Felix Thaler <thaler@cscs.ch>
2022-01-21 13:36:09 +0100

    WIP unstructured frontend

---
+++ include/gridtools/fn/unstructured2.hpp
@@ -0,0 +1,149 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2021, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include "../common/hymap.hpp"
+#include "../sid/concept.hpp"
+#include "./executor.hpp"
+
+namespace gridtools::fn {
+    namespace unstructured::dim {
+        struct neighbor {};
+        struct k {};
+    } // namespace unstructured::dim
+
+    namespace unstructured_impl_ {
+        namespace dim = unstructured::dim;
+
+        template <class Tag, class From, class To, class PtrHolder, class Strides, int MaxNeighbors>
+        struct connectivity_table {
+            PtrHolder m_ptr_holder;
+            Strides m_strides;
+            using from_t = From;
+            using to_t = To;
+            using tag_t = Tag;
+            using max_neighbors_t = integral_constant<int, MaxNeighbors>;
+        };
+
+        template <class Tables, class Sizes>
+        struct domain {
+            Tables m_tables;
+            Sizes m_sizes;
+        };
+
+        template <class Tag, class From, class To, class Sid, int I>
+        auto connectivity(Sid const &s, integral_constant<int, I>) {
+            auto ptr_holder = sid::get_origin(s);
+            auto strides = sid::get_strides(s);
+            return connectivity_table<Tag, From, To, decltype(ptr_holder), decltype(strides), I>{
+                std::move(ptr_holder), std::move(strides)};
+        }
+
+        template <class Horizontal, class... Connectivities>
+        auto unstructured_domain(
+            std::size_t horizontal_size, std::size_t vertical_size, Connectivities const &...conns) {
+            auto table_map = hymap::keys<typename Connectivities::tag_t...>::make_values(conns...);
+            auto sizes = hymap::keys<Horizontal, dim::k>::make_values(horizontal_size, vertical_size);
+            return domain<decltype(table_map), decltype(sizes)>{std::move(table_map), std::move(sizes)};
+        };
+
+        template <class Tag, class Ptr, class Strides, class Domain>
+        struct iterator {
+            Ptr m_ptr;
+            Strides const &m_strides;
+            Domain const &m_domain;
+            int m_index;
+        };
+
+        template <class Tag, class Ptr, class Strides, class Domain>
+        bool can_deref(iterator<Tag, Ptr, Strides, Domain> const &it) {
+            return it.m_index != -1;
+        }
+
+        template <class Tag, class Ptr, class Strides, class Domain>
+        auto deref(iterator<Tag, Ptr, Strides, Domain> const &it) {
+            return *it.m_ptr;
+        }
+
+        template <class Tag, class Ptr, class Strides, class Domain, class Conn, class Offset>
+        GT_FUNCTION auto horizontal_shift(iterator<Tag, Ptr, Strides, Domain> const &it, Conn, Offset offset) {
+            auto const &table = at_key<Conn>(it.m_domain.m_tables);
+            using from_t = typename std::remove_reference_t<decltype(table)>::from_t;
+            using to_t = typename std::remove_reference_t<decltype(table)>::to_t;
+            auto table_ptr = table.m_ptr_holder();
+            auto hori_stride = sid::get_stride<from_t>(table.m_strides);
+            auto nb_stride = sid::get_stride<dim::neighbor>(table.m_strides);
+            sid::shift(table_ptr, hori_stride, it.m_index);
+            sid::shift(table_ptr, nb_stride, offset);
+            auto table_value = *table_ptr;
+            auto shifted = it;
+            auto from_stride = at_key<Tag>(sid::get_stride<from_t>(shifted.m_strides));
+            sid::shift(shifted.m_ptr, from_stride, -shifted.m_index);
+            auto to_stride = at_key<Tag>(sid::get_stride<to_t>(shifted.m_strides));
+            sid::shift(shifted.m_ptr, to_stride, table_value);
+            shifted.m_index = table_value;
+            return shifted;
+        }
+
+        template <class Tag, class Ptr, class Strides, class Domain, class Dim, class Offset>
+        GT_FUNCTION auto non_horizontal_shift(iterator<Tag, Ptr, Strides, Domain> const &it, Dim, Offset offset) {
+            auto shifted = it;
+            sid::shift(shifted.m_ptr, at_key<Tag>(sid::get_stride<Dim>(shifted.m_strides)), offset);
+            return shifted;
+        }
+
+        template <class Tag, class Ptr, class Strides, class Domain, class Dim, class Offset, class... Offsets>
+        GT_FUNCTION auto shift(iterator<Tag, Ptr, Strides, Domain> const &it, Dim, Offset offset, Offsets... offsets) {
+            if constexpr (has_key<decltype(it.m_domain.m_tables), Dim>()) {
+                return shift(horizontal_shift(it, Dim(), offset), offsets...);
+            } else {
+                return shift(non_horizontal_shift(it, Dim(), offset), offsets...);
+            }
+        }
+        template <class Tag, class Ptr, class Strides, class Domain>
+        GT_FUNCTION auto shift(iterator<Tag, Ptr, Strides, Domain> const &it) {
+            return it;
+        }
+
+        template <class Domain>
+        struct make_iterator {
+            Domain m_domain;
+
+            GT_FUNCTION auto operator()() const {
+                return [&](auto tag, auto const &ptr, auto const &strides) {
+                    auto tptr = host_device::at_key<decltype(tag)>(ptr);
+                    int index = *host_device::at_key<integral_constant<int, 2>>(ptr);
+                    return iterator<decltype(tag), decltype(tptr), decltype(strides), Domain>{
+                        std::move(tptr), strides, m_domain, index};
+                };
+            }
+        };
+
+        template <class Backend, class Domain>
+        using stencil_exec_t = stencil_executor<Backend, make_iterator<Domain>, decltype(Domain::m_sizes)>;
+
+        template <class Backend, class Domain>
+        struct backend {
+            Domain m_domain;
+
+            auto stencil_executor() const {
+                return [&] {
+                    return stencil_exec_t<Backend, Domain>{m_domain.m_sizes, make_iterator<Domain>{m_domain}};
+                };
+            }
+        };
+
+        template <class Backend, class Tables, class Sizes>
+        auto make_backend(Backend, domain<Tables, Sizes> const &d) {
+            return backend<Backend, domain<Tables, Sizes>>{d};
+        }
+    } // namespace unstructured_impl_
+
+    using unstructured_impl_::connectivity;
+    using unstructured_impl_::unstructured_domain;
+} // namespace gridtools::fn
---
+++ tests/unit_tests/fn/test_fn_unstructured.cpp
@@ -0,0 +1,90 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2021, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <gridtools/fn/unstructured2.hpp>
+
+#include <gtest/gtest.h>
+
+#include <gridtools/fn/backend2/naive.hpp>
+#include <gridtools/sid/synthetic.hpp>
+#include <gridtools/stencil/positional.hpp>
+
+namespace gridtools::fn {
+    namespace {
+        using namespace literals;
+        using sid::property;
+
+        template <class V2V>
+        struct stencil {
+            constexpr auto operator()() const {
+                return [](auto const &in) { /*return reduce(std::plus(), 0, shift(in, V2V()));*/
+                    int sum = deref(in);
+                    if (can_deref(shift(in, V2V(), 0)))
+                        sum += deref(shift(in, V2V(), 0));
+                    if (can_deref(shift(in, V2V(), 1)))
+                        sum += deref(shift(in, V2V(), 1));
+                    if (can_deref(shift(in, V2V(), 2)))
+                        sum += deref(shift(in, V2V(), 2));
+                    return sum;
+                };
+            }
+        };
+
+        struct vertex {};
+        struct v2v {};
+
+        TEST(unstructured, stencil) {
+            auto apply_stencil = [](auto executor, auto &out, auto const &in, auto const &vidx) {
+                executor().arg(out).arg(in).arg(vidx).assign(0_c, stencil<v2v>(), 1_c);
+            };
+            auto fencil =
+                [&](auto const &v2v_table, int nvertices, int nlevels, auto &out, auto const &in, auto const &vidx) {
+                    auto v2v_conn = connectivity<v2v, vertex, vertex>(v2v_table, 3_c);
+                    auto domain = unstructured_domain<vertex>(nvertices, nlevels, v2v_conn);
+                    auto backend = make_backend(backend::naive(), domain);
+                    apply_stencil(backend.stencil_executor(), out, in, vidx);
+                };
+
+            int v2v_table[3][3] = {{1, 2, -1}, {0, 2, -1}, {0, 1, -1}};
+            auto v2v_conn =
+                sid::synthetic()
+                    .set<property::origin>(sid::host::make_simple_ptr_holder(&v2v_table[0][0]))
+                    .set<property::strides>(hymap::keys<vertex, unstructured::dim::neighbor>::values(3_c, 1_c));
+
+            int in[3][5], out[3][5] = {};
+            for (int v = 0; v < 3; ++v)
+                for (int k = 0; k < 5; ++k)
+                    in[v][k] = 5 * v + k;
+
+            auto as_synthetic = [](int x[3][5]) {
+                return sid::synthetic()
+                    .set<property::origin>(sid::host::make_simple_ptr_holder(&x[0][0]))
+                    .set<property::strides>(hymap::keys<vertex, unstructured::dim::k>::values(5_c, 1_c));
+            };
+            auto in_s = as_synthetic(in);
+            auto out_s = as_synthetic(out);
+
+            auto vidx = gridtools::stencil::positional<vertex>();
+
+            fencil(v2v_conn, 3, 5, out_s, in_s, vidx);
+
+            for (int v = 0; v < 3; ++v)
+                for (int k = 0; k < 5; ++k) {
+                    int nbsum = in[v][k];
+                    for (int i = 0; i < 3; ++i) {
+                        int nb = v2v_table[v][i];
+                        if (nb != -1)
+                            nbsum += in[nb][k];
+                    }
+                    EXPECT_EQ(out[v][k], nbsum);
+                }
+        }
+
+    } // namespace
+} // namespace gridtools::fn
