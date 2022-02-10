77020beec229cf56667ac07ec8fd509f597fc7c7
Hannes Vogt <vogt@hey.com>
2022-02-10 10:51:52 +0100

    default ctor for tests...

--- include/gridtools/sid/simple_ptr_holder.hpp
+++ include/gridtools/sid/simple_ptr_holder.hpp
@@ -36,6 +36,7 @@ namespace gridtools {
 #else
                 // Enables CTAD in C++17.
                 GT_TARGET GT_FORCE_INLINE constexpr simple_ptr_holder(T const &ptr) : m_val{ptr} {}
+                GT_TARGET GT_FORCE_INLINE constexpr simple_ptr_holder() = default;
 #endif
                 GT_TARGET GT_FORCE_INLINE constexpr T const &operator()() const { return m_val; }
             };
