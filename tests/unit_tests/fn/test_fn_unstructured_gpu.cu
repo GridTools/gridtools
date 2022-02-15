988b89bfece5dc47d3683b15b4ca8b34a92c213d
Felix Thaler <thaler@cscs.ch>
2022-02-15 08:47:51 +0100

    Updated unstructured GPU example

--- tests/unit_tests/fn/test_fn_unstructured_gpu.cu
+++ tests/unit_tests/fn/test_fn_unstructured_gpu.cu
@@ -7,6 +7,7 @@
  * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
  */
+#include <gridtools/common/array.hpp>
 #include <gridtools/fn/unstructured2.hpp>
 
 #include <gtest/gtest.h>
@@ -22,42 +23,43 @@ namespace gridtools::fn {
         template <int I>
         using int_t = integral_constant<int, I>;
 
-        template <class C>
+        template <class C, int MaxNeighbors>
         struct stencil {
-            constexpr auto operator()() const {
-                return [](auto const &in) { return reduce(C(), std::plus(), 0, in); };
+            GT_FUNCTION constexpr auto operator()() const {
+                return [](auto const &in) {
+                    int tmp = 0;
+                    tuple_util::host_device::for_each(
+                        [&](auto i) {
+                            auto shifted = shift(in, C(), i);
+                            if (can_deref(shifted))
+                                tmp += deref(shifted);
+                        },
+                        meta::rename<tuple, meta::make_indices_c<MaxNeighbors>>());
+                    return tmp;
+                };
             }
         };
 
-        struct vertex {};
-        struct edge {};
-        struct v2v {
-            static constexpr int max_neighbors = 3;
-        };
-        struct v2e {
-            static constexpr int max_neighbors = 2;
-        };
+        struct v2v {};
+        struct v2e {};
 
-        using block_sizes_t = meta::list<meta::list<vertex, int_t<32>>, meta::list<unstructured::dim::k, int_t<1>>>;
+        using block_sizes_t = meta::list<meta::list<unstructured::dim::horizontal, int_t<32>>,
+            meta::list<unstructured::dim::k, int_t<1>>>;
 
         TEST(unstructured, v2v_sum) {
             auto apply_stencil = [](auto executor, auto &out, auto const &in) {
-                executor().arg(out).arg(in).assign(0_c, stencil<v2v>(), 1_c);
+                executor().arg(out).arg(in).assign(0_c, stencil<v2v, 3>(), 1_c);
             };
             auto fencil = [&](auto const &v2v_table, int nvertices, int nlevels, auto &out, auto const &in) {
-                auto v2v_conn = connectivity<v2v, vertex, vertex>(v2v_table);
-                auto domain = unstructured_domain<vertex>(nvertices, nlevels, v2v_conn);
+                auto v2v_conn = connectivity<v2v>(v2v_table);
+                auto domain = unstructured_domain(nvertices, nlevels, v2v_conn);
                 auto backend = make_backend(backend::gpu<block_sizes_t>(), domain);
                 apply_stencil(backend.stencil_executor(), out, in);
             };
 
-            auto v2v_table = cuda_util::cuda_malloc<int>(3 * 3);
+            auto v2v_table = cuda_util::cuda_malloc<array<int, 3>>(3);
             int v2v_tableh[3][3] = {{1, 2, -1}, {0, 2, -1}, {0, 1, -1}};
-            cudaMemcpy(v2v_table.get(), v2v_tableh, 3 * 3 * sizeof(int), cudaMemcpyHostToDevice);
-            auto v2v_conn =
-                sid::synthetic()
-                    .set<property::origin>(sid::host_device::make_simple_ptr_holder(v2v_table.get()))
-                    .set<property::strides>(hymap::keys<vertex, unstructured::dim::neighbor>::make_values(3_c, 1_c));
+            cudaMemcpy(v2v_table.get(), v2v_tableh, 3 * sizeof(array<int, 3>), cudaMemcpyHostToDevice);
 
             auto in = cuda_util::cuda_malloc<int>(3 * 5);
             auto out = cuda_util::cuda_malloc<int>(3 * 5);
@@ -70,12 +72,15 @@ namespace gridtools::fn {
             auto as_synthetic = [](int *x) {
                 return sid::synthetic()
                     .set<property::origin>(sid::host_device::make_simple_ptr_holder(x))
-                    .set<property::strides>(hymap::keys<vertex, unstructured::dim::k>::make_values(5_c, 1_c));
+                    .set<property::strides>(
+                        hymap::keys<unstructured::dim::horizontal, unstructured::dim::k>::make_values(5_c, 1_c));
             };
             auto in_s = as_synthetic(in.get());
             auto out_s = as_synthetic(out.get());
 
-            fencil(v2v_conn, 3, 5, out_s, in_s);
+            GT_CUDA_CHECK(cudaDeviceSynchronize());
+            fencil(v2v_table.get(), 3, 5, out_s, in_s);
+            GT_CUDA_CHECK(cudaDeviceSynchronize());
             cudaMemcpy(outh, out.get(), 3 * 5 * sizeof(int), cudaMemcpyDeviceToHost);
 
             for (int v = 0; v < 3; ++v)
@@ -92,22 +97,18 @@ namespace gridtools::fn {
 
         TEST(unstructured, v2e_sum) {
             auto apply_stencil = [](auto executor, auto &out, auto const &in) {
-                executor().arg(out).arg(in).assign(0_c, stencil<v2e>(), 1_c);
+                executor().arg(out).arg(in).assign(0_c, stencil<v2e, 2>(), 1_c);
             };
             auto fencil = [&](auto const &v2e_table, int nvertices, int nlevels, auto &out, auto const &in) {
-                auto v2e_conn = connectivity<v2e, vertex, edge>(v2e_table);
-                auto domain = unstructured_domain<vertex>(nvertices, nlevels, v2e_conn);
+                auto v2e_conn = connectivity<v2e>(v2e_table);
+                auto domain = unstructured_domain(nvertices, nlevels, v2e_conn);
                 auto backend = make_backend(backend::gpu<block_sizes_t>(), domain);
                 apply_stencil(backend.stencil_executor(), out, in);
             };
 
-            auto v2e_table = cuda_util::cuda_malloc<int>(3 * 2);
+            auto v2e_table = cuda_util::cuda_malloc<array<int, 2>>(3);
             int v2e_tableh[3][2] = {{0, 2}, {0, 1}, {1, 2}};
-            cudaMemcpy(v2e_table.get(), v2e_tableh, 3 * 2 * sizeof(int), cudaMemcpyHostToDevice);
-            auto v2e_conn =
-                sid::synthetic()
-                    .set<property::origin>(sid::host_device::make_simple_ptr_holder(v2e_table.get()))
-                    .set<property::strides>(hymap::keys<vertex, unstructured::dim::neighbor>::make_values(2_c, 1_c));
+            cudaMemcpy(v2e_table.get(), v2e_tableh, 3 * sizeof(array<int, 2>), cudaMemcpyHostToDevice);
 
             auto in = cuda_util::cuda_malloc<int>(3 * 5);
             auto out = cuda_util::cuda_malloc<int>(3 * 5);
@@ -117,16 +118,17 @@ namespace gridtools::fn {
                     inh[e][k] = 5 * e + k;
             cudaMemcpy(in.get(), inh, 3 * 5 * sizeof(int), cudaMemcpyHostToDevice);
 
-            auto as_synthetic = [](int *x, auto loc) {
+            auto as_synthetic = [](int *x) {
                 return sid::synthetic()
                     .set<property::origin>(sid::host_device::make_simple_ptr_holder(x))
-                    .set<property::strides>(hymap::keys<decltype(loc), unstructured::dim::k>::make_values(5_c, 1_c));
+                    .set<property::strides>(
+                        hymap::keys<unstructured::dim::horizontal, unstructured::dim::k>::make_values(5_c, 1_c));
             };
-            auto in_s = as_synthetic(in.get(), edge());
-            auto out_s = as_synthetic(out.get(), vertex());
+            auto in_s = as_synthetic(in.get());
+            auto out_s = as_synthetic(out.get());
 
             GT_CUDA_CHECK(cudaDeviceSynchronize());
-            fencil(v2e_conn, 3, 5, out_s, in_s);
+            fencil(v2e_table.get(), 3, 5, out_s, in_s);
             GT_CUDA_CHECK(cudaDeviceSynchronize());
             cudaMemcpy(outh, out.get(), 3 * 5 * sizeof(int), cudaMemcpyDeviceToHost);
 
