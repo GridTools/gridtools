7c91d4908a8dfccf85f0af811fe4fd81d6786322
Felix Thaler <thaler@cscs.ch>
2022-02-18 12:16:23 +0100

    Cleanup nabla

--- tests/regression/fn/fn_unstructured_nabla.cpp
+++ tests/regression/fn/fn_unstructured_nabla.cpp
@@ -59,34 +59,25 @@ namespace {
         }
     };
 
-    GT_REGRESSION_TEST(fn_unstructured_nabla, test_environment<>, fn_backend_t) {
-        using float_t = typename TypeParam::float_t;
-
-        auto mesh = TypeParam::fn_unstructured_mesh();
-
-        auto v2e_table = mesh.v2e_table();
-        auto e2v_table = mesh.e2v_table();
-
-        auto v2e_view = v2e_table->const_host_view();
-        auto e2v_view = e2v_table->const_host_view();
-
-        auto pp = [](int vertex, int k) -> float_t { return (vertex + k) % 19; };
-        auto sign = [](int vertex) -> array<float_t, 6> {
-            return {0, 1, float_t(vertex % 2), 1, float_t((vertex + 1) % 2), 0};
-        };
-        auto vol = [](int vertex) -> float_t { return vertex % 13 + 1; };
-        auto s = [](int edge, int k) -> tuple<float_t, float_t> { return {(edge + k) % 17, (edge + k) % 7}; };
-        auto zavg = [&](int edge, int k) -> tuple<float_t, float_t> {
-            float_t tmp = 0;
+    constexpr inline auto pp = [](int vertex, int k) { return (vertex + k) % 19; };
+    constexpr inline auto sign = [](int vertex) { return array{0, 1, vertex % 2, 1, (vertex + 1) % 2, 0}; };
+    constexpr inline auto vol = [](int vertex) { return vertex % 13 + 1; };
+    constexpr inline auto s = [](int edge, int k) { return tuple((edge + k) % 17, (edge + k) % 7); };
+    constexpr inline auto zavg = [](auto const &e2v) {
+        return [&e2v](int edge, int k) {
+            double tmp = 0.0;
             for (int neighbor = 0; neighbor < 2; ++neighbor)
-                tmp += pp(e2v_view(edge, neighbor), k);
-            tmp /= 2;
-            return {tmp * get<0>(s(edge, k)), tmp * get<1>(s(edge, k))};
+                tmp += pp(e2v(edge, neighbor), k);
+            tmp /= 2.0;
+            return tuple{tmp * get<0>(s(edge, k)), tmp * get<1>(s(edge, k))};
         };
-        auto expected = [&](int vertex, int k) {
-            auto res = tuple(float_t(0), float_t(0));
+    };
+
+    constexpr inline auto expected = [](auto const &v2e, auto const &e2v) {
+        return [&v2e, zavg = zavg(e2v)](int vertex, int k) {
+            auto res = tuple(0.0, 0.0);
             for (int neighbor = 0; neighbor < 6; ++neighbor) {
-                int edge = v2e_view(vertex, neighbor);
+                int edge = v2e(vertex, neighbor);
                 if (edge != -1) {
                     get<0>(res) += get<0>(zavg(edge, k)) * sign(vertex)[neighbor];
                     get<1>(res) += get<1>(zavg(edge, k)) * sign(vertex)[neighbor];
@@ -96,58 +87,86 @@ namespace {
             get<1>(res) /= vol(vertex);
             return res;
         };
+    };
 
-        auto apply_zavg = [](auto executor, auto &zavg, auto const &pp, auto const &s) {
-            executor().arg(zavg).arg(pp).arg(s).assign(0_c, zavg_stencil(), 1_c, 2_c);
-        };
-        auto apply_nabla = [](auto executor, auto &nabla, auto const &zavg, auto const &sign, auto const &vol) {
+    constexpr inline auto apply_zavg = [](auto executor, auto &zavg, auto const &pp, auto const &s) {
+        executor().arg(zavg).arg(pp).arg(s).assign(0_c, zavg_stencil(), 1_c, 2_c);
+    };
+    constexpr inline auto apply_nabla =
+        [](auto executor, auto &nabla, auto const &zavg, auto const &sign, auto const &vol) {
             executor().arg(nabla).arg(zavg).arg(sign).arg(vol).assign(0_c, nabla_stencil(), 1_c, 2_c, 3_c);
         };
-        auto fencil = [&](int nvertices,
-                          int nedges,
-                          int nlevels,
-                          auto const &v2e_table,
-                          auto const &e2v_table,
-                          auto &nabla,
-                          auto const &pp,
-                          auto const &s,
-                          auto const &sign,
-                          auto const &vol) {
-            auto v2e_conn = connectivity<v2e>(v2e_table);
-            auto e2v_conn = connectivity<e2v>(e2v_table);
-            auto edge_domain = unstructured_domain(nedges, nlevels, e2v_conn);
-            auto vertex_domain = unstructured_domain(nvertices, nlevels, v2e_conn);
-            auto edge_backend = make_backend(backend::naive(), edge_domain);
-            auto vertex_backend = make_backend(backend::naive(), vertex_domain);
-            auto zavg = edge_backend.make_tmp_like(nabla);
-            apply_zavg(edge_backend.stencil_executor(), zavg, pp, s);
-            apply_nabla(vertex_backend.stencil_executor(), nabla, zavg, sign, vol);
-        };
 
-        auto comp = [&,
-                        v2e_table = reinterpret_cast<array<int, 6> const *>(v2e_table->get_const_target_ptr()),
-                        e2v_table = reinterpret_cast<array<int, 2> const *>(e2v_table->get_const_target_ptr()),
-                        pp = mesh.make_const_storage(pp, mesh.nvertices(), mesh.nlevels()),
-                        sign = mesh.template make_const_storage<array<float_t, 6>>(sign, mesh.nvertices()),
-                        vol = mesh.make_const_storage(vol, mesh.nvertices()),
-                        s = mesh.template make_const_storage<tuple<float_t, float_t>>(
-                            s, mesh.nedges(), mesh.nlevels())](auto &nabla) {
-            fencil(mesh.nvertices(), mesh.nedges(), mesh.nlevels(), v2e_table, e2v_table, nabla, pp, s, sign, vol);
+    constexpr inline auto fencil = [](int nvertices,
+                                       int nedges,
+                                       int nlevels,
+                                       auto const &v2e_table,
+                                       auto const &e2v_table,
+                                       auto &nabla,
+                                       auto const &pp,
+                                       auto const &s,
+                                       auto const &sign,
+                                       auto const &vol) {
+        auto v2e_conn = connectivity<v2e>(v2e_table);
+        auto e2v_conn = connectivity<e2v>(e2v_table);
+        auto edge_domain = unstructured_domain(nedges, nlevels, e2v_conn);
+        auto vertex_domain = unstructured_domain(nvertices, nlevels, v2e_conn);
+        auto edge_backend = make_backend(backend::naive(), edge_domain);
+        auto vertex_backend = make_backend(backend::naive(), vertex_domain);
+        auto zavg = edge_backend.make_tmp_like(nabla);
+        apply_zavg(edge_backend.stencil_executor(), zavg, pp, s);
+        apply_nabla(vertex_backend.stencil_executor(), nabla, zavg, sign, vol);
+    };
+
+    constexpr inline auto make_comp = [](auto const &mesh, auto &nabla) {
+        return [&nabla,
+                   nvertices = mesh.nvertices(),
+                   nedges = mesh.nedges(),
+                   nlevels = mesh.nlevels(),
+                   v2e_table = mesh.v2e_table(),
+                   e2v_table = mesh.e2v_table(),
+                   pp = mesh.make_const_storage(pp, mesh.nvertices(), mesh.nlevels()),
+                   sign = mesh.template make_const_storage<array<float_t, 6>>(sign, mesh.nvertices()),
+                   vol = mesh.make_const_storage(vol, mesh.nvertices()),
+                   s = mesh.template make_const_storage<tuple<float_t, float_t>>(s, mesh.nedges(), mesh.nlevels())] {
+            auto v2e_ptr = reinterpret_cast<array<int, 6> const *>(v2e_table->get_const_target_ptr());
+            auto e2v_ptr = reinterpret_cast<array<int, 2> const *>(e2v_table->get_const_target_ptr());
+            fencil(nvertices, nedges, nlevels, v2e_ptr, e2v_ptr, nabla, pp, s, sign, vol);
         };
-        {
-            auto nabla = mesh.template make_storage<tuple<float_t, float_t>>(mesh.nvertices(), mesh.nlevels());
-            comp(nabla);
-            TypeParam::verify(expected, nabla);
-            TypeParam::benchmark("fn_unstructured_nabla", std::bind(comp, nabla));
-        }
-        {
-            auto nabla0 = mesh.make_storage(mesh.nvertices(), mesh.nlevels());
-            auto nabla1 = mesh.make_storage(mesh.nvertices(), mesh.nlevels());
-            auto nabla =
-                sid::composite::keys<integral_constant<int, 0>, integral_constant<int, 1>>::make_values(nabla0, nabla1);
-            comp(nabla);
-            TypeParam::verify([&](int vertex, int k) { return get<0>(expected(vertex, k)); }, nabla0);
-            TypeParam::verify([&](int vertex, int k) { return get<1>(expected(vertex, k)); }, nabla1);
-        }
+    };
+
+    constexpr inline auto make_expected = [](auto const &mesh) {
+        return [v2e_table = mesh.v2e_table(), e2v_table = mesh.e2v_table()](int vertex, int k) {
+            auto v2e = v2e_table->const_host_view();
+            auto e2v = e2v_table->const_host_view();
+            return expected(v2e, e2v)(vertex, k);
+        };
+    };
+
+    GT_REGRESSION_TEST(fn_unstructured_nabla_field_of_tuples, test_environment<>, fn_backend_t) {
+        using float_t = typename TypeParam::float_t;
+
+        auto mesh = TypeParam::fn_unstructured_mesh();
+        auto nabla = mesh.template make_storage<tuple<float_t, float_t>>(mesh.nvertices(), mesh.nlevels());
+        auto comp = make_comp(mesh, nabla);
+        comp();
+        auto expected = make_expected(mesh);
+        TypeParam::verify(expected, nabla);
+        TypeParam::benchmark("fn_unstructured_nabla_field_of_tuples", comp);
+    }
+
+    GT_REGRESSION_TEST(fn_unstructured_nabla_tuple_of_fields, test_environment<>, fn_backend_t) {
+        auto mesh = TypeParam::fn_unstructured_mesh();
+        auto nabla0 = mesh.make_storage(mesh.nvertices(), mesh.nlevels());
+        auto nabla1 = mesh.make_storage(mesh.nvertices(), mesh.nlevels());
+        auto nabla =
+            sid::composite::keys<integral_constant<int, 0>, integral_constant<int, 1>>::make_values(nabla0, nabla1);
+
+        auto comp = make_comp(mesh, nabla);
+        comp();
+        auto expected = make_expected(mesh);
+        TypeParam::verify([&](int vertex, int k) { return get<0>(expected(vertex, k)); }, nabla0);
+        TypeParam::verify([&](int vertex, int k) { return get<1>(expected(vertex, k)); }, nabla1);
+        TypeParam::benchmark("fn_unstructured_nabla_tuple_of_fields", comp);
     }
 } // namespace
