447c2c1d38156fe1d6c58643a44f64790a2abeda
Felix Thaler <thaler@cscs.ch>
2022-02-18 09:31:23 +0100

    Add unstructured tridiagonal solver test

--- tests/regression/fn/fn_tridiagonal_solve.cpp
+++ tests/regression/fn/fn_tridiagonal_solve.cpp
@@ -61,14 +61,20 @@ namespace {
         }
     };
 
-    GT_REGRESSION_TEST(fn_cartesian_tridiagonal_solve, vertical_test_environment<>, fn_backend_t) {
+    GT_REGRESSION_TEST(fn_tridiagonal_solve, vertical_test_environment<>, fn_backend_t) {
         using float_t = typename TypeParam::float_t;
 
-        auto a = [](int, int, int) -> float_t { return -1; };
-        auto b = [](int, int, int) -> float_t { return 3; };
-        auto c = [](int, int, int) -> float_t { return 1; };
-        auto d = [kmax = TypeParam::d(2) - 1](int, int, int k) -> float_t { return k == 0 ? 4 : k == kmax ? 2 : 3; };
-        auto expected = [](int, int, int) -> float_t { return 1; };
+        auto a = [](auto...) -> float_t { return -1; };
+        auto b = [](auto...) -> float_t { return 3; };
+        auto c = [](auto...) -> float_t { return 1; };
+        auto d = [kmax = TypeParam::d(2) - 1](auto... indices) -> float_t {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-value"
+            int k = (..., indices);
+#pragma GCC diagnostic pop
+            return k == 0 ? 4 : k == kmax ? 2 : 3;
+        };
+        auto expected = [](auto...) -> float_t { return 1; };
 
         auto tridiagonal_solve =
             [](auto executor, auto const &a, auto const &b, auto const &c, auto const &d, auto &cpdp, auto &x) {
@@ -83,23 +89,46 @@ namespace {
                     .assign(5_c, backward_scan(), float_t(0), 4_c);
             };
 
-        auto tridiagonal_solve_fencil =
-            [&](auto sizes, auto const &a, auto const &b, auto const &c, auto const &d, auto &x) {
+        {
+            auto fencil = [&](auto sizes, auto const &a, auto const &b, auto const &c, auto const &d, auto &x) {
                 auto domain = cartesian_domain(sizes);
                 auto backend = make_backend(fn_backend_t(), domain);
                 auto cpdp = backend.template make_tmp<tuple<float_t, float_t>>();
                 tridiagonal_solve(backend.vertical_executor(), a, b, c, d, cpdp, x);
             };
 
-        auto x = TypeParam::make_storage();
-        auto comp = [&,
-                        a = TypeParam::make_const_storage(a),
-                        b = TypeParam::make_const_storage(b),
-                        c = TypeParam::make_const_storage(c),
-                        d = TypeParam::make_const_storage(d)] {
-            tridiagonal_solve_fencil(TypeParam::fn_cartesian_sizes(), a, b, c, d, x);
-        };
-        comp();
-        TypeParam::verify(expected, x);
+            auto x = TypeParam::make_storage();
+            auto comp = [&,
+                            a = TypeParam::make_const_storage(a),
+                            b = TypeParam::make_const_storage(b),
+                            c = TypeParam::make_const_storage(c),
+                            d = TypeParam::make_const_storage(d)] {
+                fencil(TypeParam::fn_cartesian_sizes(), a, b, c, d, x);
+            };
+            comp();
+            TypeParam::verify(expected, x);
+        }
+        {
+            auto mesh = TypeParam::fn_unstructured_mesh();
+
+            auto fencil =
+                [&](int nvertices, int nlevels, auto const &a, auto const &b, auto const &c, auto const &d, auto &x) {
+                    auto domain = unstructured_domain(nvertices, nlevels);
+                    auto backend = make_backend(fn_backend_t(), domain);
+                    auto cpdp = backend.template make_tmp<tuple<float_t, float_t>>();
+                    tridiagonal_solve(backend.vertical_executor(), a, b, c, d, cpdp, x);
+                };
+
+            auto x = mesh.make_storage(mesh.nvertices(), mesh.nlevels());
+            auto comp = [&,
+                            a = mesh.make_const_storage(a, mesh.nvertices(), mesh.nlevels()),
+                            b = mesh.make_const_storage(b, mesh.nvertices(), mesh.nlevels()),
+                            c = mesh.make_const_storage(c, mesh.nvertices(), mesh.nlevels()),
+                            d = mesh.make_const_storage(d, mesh.nvertices(), mesh.nlevels())] {
+                fencil(mesh.nvertices(), mesh.nlevels(), a, b, c, d, x);
+            };
+            comp();
+            TypeParam::verify(expected, x);
+        }
     }
 } // namespace
