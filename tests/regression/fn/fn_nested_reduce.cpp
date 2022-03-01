ca6b9ca01d38a8df2a9cc22ae93d6ca03f97d561
Felix Thaler <thaler@cscs.ch>
2022-03-01 16:18:50 +0100

    Use mixed connectivities

--- tests/regression/fn/fn_nested_reduce.cpp
+++ tests/regression/fn/fn_nested_reduce.cpp
@@ -22,18 +22,19 @@ using namespace literals;
 constexpr auto zero = []<class T>(T) { return T{}; };
 constexpr auto sum = reduce<plus, zero>;
 
-template <auto V2V>
-constexpr auto nb_sum = [](auto const &x) { return sum(shift<V2V>(x)); };
+template <auto V2E>
+constexpr auto nb_sum = [](auto const &x) { return sum(shift<V2E>(x)); };
 
-template <auto V2V, bool UseTmp>
-constexpr auto nb_nb_sum = [](auto const &x) { return sum(shift<V2V>(lift<sum, UseTmp>(shift<V2V>(x)))); };
+template <auto V2V, auto V2E, bool UseTmp>
+constexpr auto nb_nb_sum = [](auto const &x) { return sum(shift<V2V>(lift<sum, UseTmp>(shift<V2E>(x)))); };
 
-template <auto V2V, bool UseTmp>
+template <auto V2V, auto V2E, bool UseTmp>
 constexpr auto nb_nb_nb_sum =
-    [](auto const &x) { return sum(shift<V2V>(lift<sum, UseTmp>(shift<V2V>(lift<sum, UseTmp>(shift<V2V>(x)))))); };
+    [](auto const &x) { return sum(shift<V2V>(lift<sum, UseTmp>(shift<V2V>(lift<sum, UseTmp>(shift<V2E>(x)))))); };
 
 using namespace simple_mesh;
 constexpr auto K = 3_c;
+constexpr auto n_v2e = neighbours_num<decltype(v2e)>::value;
 constexpr auto n_v2v = neighbours_num<decltype(v2v)>::value;
 
 using params_t = testing::Types<std::false_type, std::true_type>;
@@ -44,24 +45,24 @@ using lift_test = testing::Test;
 TYPED_TEST_SUITE(lift_test, params_t);
 
 TYPED_TEST(lift_test, nb_sum) {
-    double x[n_vertices][K];
+    double x[n_edges][K];
     for (auto &xx : x)
         for (auto &xxx : xx)
             xxx = rand() % 100;
 
     auto expected = [&](int h, int v) {
         double res = 0.0;
-        for (int i = 0; i < n_v2v; ++i) {
-            auto vi = v2v[h][i];
-            if (vi != -1)
-                res += x[vi][v];
+        for (int i = 0; i < n_v2e; ++i) {
+            auto ei = v2e[h][i];
+            if (ei != -1)
+                res += x[ei][v];
         }
         return res;
     };
 
     double actual[n_vertices][K] = {};
 
-    using stage_t = make_stage<nb_sum<v2v>, std::identity{}, 0, 1>;
+    using stage_t = make_stage<nb_sum<v2e>, std::identity{}, 0, 1>;
     constexpr auto testee = fencil<naive, stage_t>;
     constexpr auto domain = unstructured(std::tuple(n_vertices, K));
     testee(domain, actual, x);
@@ -72,7 +73,7 @@ TYPED_TEST(lift_test, nb_sum) {
 }
 
 TYPED_TEST(lift_test, nb_nb_sum) {
-    double x[n_vertices][K];
+    double x[n_edges][K];
     for (auto &xx : x)
         for (auto &xxx : xx)
             xxx = rand() % 100;
@@ -82,10 +83,10 @@ TYPED_TEST(lift_test, nb_nb_sum) {
         for (int i = 0; i < n_v2v; ++i) {
             auto vi = v2v[h][i];
             if (vi != -1) {
-                for (int j = 0; j < n_v2v; ++j) {
-                    auto vj = v2v[vi][j];
-                    if (vj != -1)
-                        res += x[vj][v];
+                for (int j = 0; j < n_v2e; ++j) {
+                    auto ej = v2e[vi][j];
+                    if (ej != -1)
+                        res += x[ej][v];
                 }
             }
         }
@@ -94,7 +95,7 @@ TYPED_TEST(lift_test, nb_nb_sum) {
 
     double actual[n_vertices][K] = {};
 
-    using stage_t = make_stage<nb_nb_sum<v2v, TypeParam::value>, std::identity{}, 0, 1>;
+    using stage_t = make_stage<nb_nb_sum<v2v, v2e, TypeParam::value>, std::identity{}, 0, 1>;
     constexpr auto testee = fencil<naive, stage_t>;
     constexpr auto domain = unstructured(std::tuple(n_vertices, K));
     testee(domain, actual, x);
@@ -105,7 +106,7 @@ TYPED_TEST(lift_test, nb_nb_sum) {
 }
 
 TYPED_TEST(lift_test, nb_nb_nb_sum) {
-    double x[n_vertices][K];
+    double x[n_edges][K];
     for (auto &xx : x)
         for (auto &xxx : xx)
             xxx = rand() % 100;
@@ -118,10 +119,10 @@ TYPED_TEST(lift_test, nb_nb_nb_sum) {
                 for (int j = 0; j < n_v2v; ++j) {
                     auto vj = v2v[vi][j];
                     if (vj != -1) {
-                        for (int k = 0; k < n_v2v; ++k) {
-                            auto vk = v2v[vj][k];
-                            if (vk != -1)
-                                res += x[vk][v];
+                        for (int k = 0; k < n_v2e; ++k) {
+                            auto ek = v2e[vj][k];
+                            if (ek != -1)
+                                res += x[ek][v];
                         }
                     }
                 }
@@ -132,7 +133,7 @@ TYPED_TEST(lift_test, nb_nb_nb_sum) {
 
     double actual[n_vertices][K] = {};
 
-    using stage_t = make_stage<nb_nb_nb_sum<v2v, TypeParam::value>, std::identity{}, 0, 1>;
+    using stage_t = make_stage<nb_nb_nb_sum<v2v, v2e, TypeParam::value>, std::identity{}, 0, 1>;
     constexpr auto testee = fencil<naive, stage_t>;
     constexpr auto domain = unstructured(std::tuple(n_vertices, K));
     testee(domain, actual, x);
