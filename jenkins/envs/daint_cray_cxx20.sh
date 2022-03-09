d8e94571670a9bc56fc58e51ee6e619c9b1efa3a
Hannes Vogt <hannes@havogt.de>
2022-03-09 10:55:47 +0100

    Update daint_cray_cxx20.sh

--- jenkins/envs/daint_cray_cxx20.sh
+++ jenkins/envs/daint_cray_cxx20.sh
@@ -2,8 +2,6 @@
 
 source $(dirname "$BASH_SOURCE")/daint_cray.sh
 
-module switch cce/12.0.2
-
 export GCC_X86_64=/opt/gcc/10.3.0/snos
 export GTCMAKE_GT_TESTS_CXX_STANDARD=20
 # some problem with C++20 math library included by Python (std::lerp not defined)
