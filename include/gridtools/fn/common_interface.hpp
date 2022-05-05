1ed3f070edf6b6a4aee015160efc58223176f74b
Felix Thaler <thaler@cscs.ch>
2022-05-05 14:17:21 +0200

    Added fn::tuple_get and fn::make_tuple builtins

--- include/gridtools/fn/common_interface.hpp
+++ include/gridtools/fn/common_interface.hpp
@@ -9,6 +9,8 @@
  */
 #pragma once
 
+#include "../common/tuple.hpp"
+#include "../common/tuple_util.hpp"
 #include "./backend/common.hpp"
 
 namespace gridtools::fn {
@@ -16,6 +18,16 @@ namespace gridtools::fn {
     template <class T, class Allocator, class Sizes>
     auto allocate_global_tmp(Allocator &alloc, Sizes const &sizes) {
         return allocate_global_tmp(alloc, sizes, backend::data_type<T>());
+    }
+
+    template <int I, class Tuple>
+    GT_FUNCTION auto &&tuple_get(integral_constant<int, I>, Tuple &&t) {
+        return tuple_util::get<I>(std::forward<Tuple>(t));
+    }
+
+    template <class... Args>
+    GT_FUNCTION tuple<std::decay_t<Args>...> make_tuple(Args &&...args) {
+        return {std::forward<Args>(args)...};
     }
 
 } // namespace gridtools::fn
