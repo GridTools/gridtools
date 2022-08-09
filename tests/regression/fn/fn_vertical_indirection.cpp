93e11dc33c1338342122b9672f0acb06a2d7e5c5
Péter Kardos <kardospeter1994@hotmail.com>
2022-08-09 18:43:28 +0200

    fixes

--- tests/regression/fn/fn_vertical_indirection.cpp
+++ tests/regression/fn/fn_vertical_indirection.cpp
@@ -8,10 +8,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <cstddef>
 #include <gtest/gtest.h>
 
-#include <gridtools/fn/cartesian.hpp>
 #include <gridtools/fn/unstructured.hpp>
 
 #include <fn_select.hpp>
@@ -23,7 +21,7 @@ namespace {
     using namespace literals;
 
     struct derivative_stencil {
-        auto operator()() const {
+        GT_FUNCTION auto operator()() const {
             return [](auto const &field, auto const &offset) {
                 const auto shifted = shift(field, unstructured::dim::vertical{}, deref(offset));
                 return deref(field) - deref(shifted);
@@ -31,30 +29,22 @@ namespace {
         }
     };
 
-    constexpr auto parabolic = []([[maybe_unused]] auto horizontal_idx, auto vertical_idx) -> float {
-        const float x = float(vertical_idx);
-        const float xp = x + 0.5f;
-        const float y = 0.5f * xp * xp;
-        return y;
+    constexpr auto parabolic = [](auto /* vertex */, auto k) -> float {
+        const float x_shifted = float(k) + 0.5f;
+        return 0.5f * x_shifted * x_shifted;
     };
-    constexpr auto offsets = []([[maybe_unused]] auto horizontal_idx, auto vertical_idx) -> int {
-        return vertical_idx > 0 ? -1 : 0;
-    };
-    constexpr auto linear = []([[maybe_unused]] auto horizontal_idx, auto vertical_idx) { return vertical_idx; };
+    constexpr auto offsets = [](auto /* vertex */, auto k) -> int { return k > 0 ? -1 : 0; };
+    constexpr auto linear = [](auto /* vertex */, auto k) { return k; };
 
     constexpr auto compute_derivative = [](auto executor, auto const &input, auto const &offsets, auto &output) {
-        using float_t = sid::element_type<decltype(input)>;
         executor().arg(input).arg(offsets).arg(output).assign(2_c, derivative_stencil(), 0_c, 1_c).execute();
     };
 
     GT_REGRESSION_TEST(fn_vertical_indirection, test_environment<>, fn_backend_t) {
-        using float_t = typename TypeParam::float_t;
-
         auto fencil = [](int nvertices, int nlevels, auto const &field, auto &offets, auto &output) {
             auto be = fn_backend_t();
             auto domain = unstructured_domain({nvertices, nlevels}, tuple{0, 0});
             auto backend = make_backend(be, domain);
-            auto alloc = tmp_allocator(be);
             compute_derivative(backend.stencil_executor(), field, offets, output);
         };
 
