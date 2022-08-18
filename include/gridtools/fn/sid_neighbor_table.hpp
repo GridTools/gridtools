9c5a26b2de2784cc68fd18852b88efb69f213ca3
Péter Kardos <kardospeter1994@hotmail.com>
2022-08-18 11:23:54 +0200

    proper reading of elements

--- include/gridtools/fn/sid_neighbor_table.hpp
+++ include/gridtools/fn/sid_neighbor_table.hpp
@@ -9,31 +9,46 @@
  */
 #pragma once
 
+#include <cstdint>
+#include <gridtools/common/array.hpp>
+#include <gridtools/fn/unstructured.hpp>
 #include <gridtools/sid/concept.hpp>
 #include <type_traits>
 
 namespace gridtools::fn::sid_neighbor_table {
     namespace sid_neighbor_table_impl_ {
-        template <std::size_t MaxNumNeighbors, class Sid>
+        template <class IndexDimension, class NeighborDimension, int32_t MaxNumNeighbors, class Sid>
         struct sid_neighbor_table {
             Sid sid;
         };
 
-        template <std::size_t MaxNumNeighbors, class Sid>
-        auto neighbor_table_neighbors(sid_neighbor_table<MaxNumNeighbors, Sid> const &table, size_t index) {
+        template <class IndexDimension, class NeighborDimension, int32_t MaxNumNeighbors, class Sid>
+        auto neighbor_table_neighbors(
+            sid_neighbor_table<IndexDimension, NeighborDimension, MaxNumNeighbors, Sid> const &table, size_t index) {
             using element_type = sid::element_type<Sid>;
+
             const auto ptr = sid_get_origin(table.sid);
-            const auto lower_bounds = sid_get_lower_bounds(table.sid);
-            const auto upper_bounds = sid_get_upper_bounds(table.sid);
             const auto strides = sid_get_strides(table.sid);
-            std::array<element_type, MaxNumNeighbors> neighbors;
-            std::fill(std::begin(neighbors), std::end(neighbors), element_type{-1});
+
+            const auto index_stride = at_key<IndexDimension>(strides);
+            const auto neighbour_stride = at_key<NeighborDimension>(strides);
+
+            gridtools::array<element_type, MaxNumNeighbors> neighbors;
+            for (int32_t elementIdx = 0; elementIdx < MaxNumNeighbors; ++elementIdx) {
+                const auto element_ptr = ptr + index * index_stride + elementIdx * neighbour_stride;
+                neighbors[elementIdx] = *element_ptr();
+            }
             return neighbors;
         }
 
-        template <std::size_t MaxNumNeighbors, class Sid>
+        template <class IndexDimension, class NeighborDimension, int32_t MaxNumNeighbors, class Sid>
         auto as_neighbor_table(Sid sid) {
-            return sid_neighbor_table<MaxNumNeighbors, Sid>{std::move(sid)};
+            static_assert(gridtools::tuple_util::size<decltype(sid_get_strides(std::declval<Sid>()))>::value == 2,
+                "Neighbor tables must have exactly two dimensions: the index dimension and the neighbor dimension");
+            static_assert(!std::is_same_v<IndexDimension, NeighborDimension>,
+                "The index dimension and the neighbor dimension must be different.");
+
+            return sid_neighbor_table<IndexDimension, NeighborDimension, MaxNumNeighbors, Sid>{std::move(sid)};
         }
     } // namespace sid_neighbor_table_impl_
 
--- tests/unit_tests/fn/test_fn_sid_neighbor_table.cpp
+++ tests/unit_tests/fn/test_fn_sid_neighbor_table.cpp
@@ -24,18 +24,18 @@ using namespace gridtools;
 using namespace fn;
 using sid_neighbor_table::as_neighbor_table;
 
+using edge_dim_t = unstructured::dim::horizontal;
+using edge_to_cell_dim_t = struct {};
+
 TEST(sid_neighbor_table, correctness) {
     constexpr size_t numElements = 3;
     constexpr size_t numNeighbors = 2;
     std::array<int32_t, numElements *numNeighbors> data = {0, 1, 10, 11, 20, 21};
-    using dim_hymap_t = hymap::keys<unstructured::dim::horizontal, unstructured::dim::vertical>;
+    using dim_hymap_t = hymap::keys<edge_dim_t, edge_to_cell_dim_t>;
     auto contents = sid::synthetic()
                         .set<sid::property::origin>(sid::host_device::simple_ptr_holder(data.data()))
-                        .set<sid::property::lower_bounds>(dim_hymap_t::make_values(0, 0))
-                        .set<sid::property::upper_bounds>(dim_hymap_t::make_values(numElements, numNeighbors))
-                        .set<sid::property::strides>(dim_hymap_t::make_values(3, 1));
-    const auto table = as_neighbor_table<numNeighbors>(contents);
-    sid_get_origin(contents);
+                        .set<sid::property::strides>(dim_hymap_t::make_values(numNeighbors, 1));
+    const auto table = as_neighbor_table<edge_dim_t, edge_to_cell_dim_t, numNeighbors>(contents);
 
     auto [n00, n01] = neighbor_table_neighbors(table, 0);
     auto [n10, n11] = neighbor_table_neighbors(table, 1);
