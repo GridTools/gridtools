eb9f344a07d668499edffadd971a51de9a135af0
Rico Häuselmann <ricoh@cscs.ch>
2022-11-25 14:37:32 +0100

    update tests with new package name

--- .python_package/tests/test_dirs.py
+++ .python_package/tests/test_dirs.py
@@ -6,16 +6,16 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import gridtools
+import gridtools_cpp
 
 
 def test_cmake_dir_contains_gridtools_cmake():
-    main_config_file = gridtools.get_cmake_dir() / "GridToolsConfig.cmake"
+    main_config_file = gridtools_cpp.get_cmake_dir() / "GridToolsConfig.cmake"
     assert main_config_file.exists()
     assert main_config_file.read_text()
 
 
 def test_include_dir_contains_headers():
-    include_path = gridtools.get_include_dir()
+    include_path = gridtools_cpp.get_include_dir()
     assert include_path.exists()
     assert len(list(include_path.rglob("*.hpp"))) > 0
