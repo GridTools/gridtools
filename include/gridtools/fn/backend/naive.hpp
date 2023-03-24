4fd8ce20a48173721b0799448f67296fa714ccf5
Felix Thaler <thaler@cscs.ch>
2023-03-24 11:02:21 +0100

    Parallel fn::backend::naive (#1746)
    Introduces parallel omp loops to fn::backend::naive without additional optimizations. Falls back to serial loops in case omp is not available.

--- include/gridtools/fn/backend/naive.hpp
+++ include/gridtools/fn/backend/naive.hpp
@@ -11,28 +11,68 @@
 
 #include "../../common/functional.hpp"
 #include "../../common/hymap.hpp"
+#include "../../common/tuple_util.hpp"
 #include "../../sid/allocator.hpp"
 #include "../../sid/concept.hpp"
 #include "../../sid/contiguous.hpp"
+#include "../../sid/multi_shift.hpp"
 #include "../../sid/unknown_kind.hpp"
+#include "../../thread_pool/concept.hpp"
+#include "../../thread_pool/dummy.hpp"
+#include "../../thread_pool/omp.hpp"
 #include "./common.hpp"
 
 namespace gridtools::fn::backend {
     namespace naive_impl_ {
-        struct naive {};
+        template <class ThreadPool>
+        struct naive_with_threadpool {};
 
-        template <class Sizes, class StencilStage, class MakeIterator, class Composite>
-        void apply_stencil_stage(
-            naive, Sizes const &sizes, StencilStage, MakeIterator &&make_iterator, Composite &&composite) {
+        using naive = naive_with_threadpool<
+#if defined(_OPENMP) || defined(GT_HIP_OPENMP_WORKAROUND)
+            thread_pool::omp
+#else
+            thread_pool::dummy
+#endif
+            >;
+
+        template <class ThreadPool, class Sizes, class Dims = meta::rename<hymap::keys, get_keys<Sizes>>>
+        auto make_parallel_loops(ThreadPool, Sizes const &sizes) {
+            return [=](auto f) {
+                return [=](auto ptr, auto const &strides) {
+                    auto loop_f = [&](auto... indices) {
+                        auto local_ptr = ptr;
+                        sid::multi_shift(local_ptr, strides, Dims::make_values(indices...));
+                        f(local_ptr, strides);
+                    };
+
+                    tuple_util::apply(
+                        [&](auto... sizes) { thread_pool::parallel_for_loop(ThreadPool(), loop_f, int(sizes)...); },
+                        sizes);
+                };
+            };
+        }
+
+        template <class ThreadPool, class Sizes, class StencilStage, class MakeIterator, class Composite>
+        void apply_stencil_stage(naive_with_threadpool<ThreadPool>,
+            Sizes const &sizes,
+            StencilStage,
+            MakeIterator &&make_iterator,
+            Composite &&composite) {
             auto ptr = sid::get_origin(std::forward<Composite>(composite))();
             auto strides = sid::get_strides(std::forward<Composite>(composite));
-            common::make_loops(sizes)([make_iterator = make_iterator()](auto ptr, auto const &strides) {
+            make_parallel_loops(ThreadPool(), sizes)([make_iterator = make_iterator()](auto ptr, auto const &strides) {
                 StencilStage()(make_iterator, ptr, strides);
             })(ptr, strides);
         }
 
-        template <class Sizes, class ColumnStage, class MakeIterator, class Composite, class Vertical, class Seed>
-        void apply_column_stage(naive,
+        template <class ThreadPool,
+            class Sizes,
+            class ColumnStage,
+            class MakeIterator,
+            class Composite,
+            class Vertical,
+            class Seed>
+        void apply_column_stage(naive_with_threadpool<ThreadPool>,
             Sizes const &sizes,
             ColumnStage,
             MakeIterator &&make_iterator,
@@ -42,21 +82,26 @@ namespace gridtools::fn::backend {
             auto ptr = sid::get_origin(std::forward<Composite>(composite))();
             auto strides = sid::get_strides(std::forward<Composite>(composite));
             auto v_size = at_key<Vertical>(sizes);
-            common::make_loops(hymap::canonicalize_and_remove_key<Vertical>(sizes))(
+            make_parallel_loops(ThreadPool(), hymap::canonicalize_and_remove_key<Vertical>(sizes))(
                 [v_size = std::move(v_size), make_iterator = make_iterator(), seed = std::move(seed)](auto ptr,
                     auto const &strides) { ColumnStage()(seed, v_size, make_iterator, std::move(ptr), strides); })(
                 ptr, strides);
         }
 
-        inline auto tmp_allocator(naive be) { return std::tuple(be, sid::allocator(&std::make_unique<char[]>)); }
+        template <class ThreadPool>
+        inline auto tmp_allocator(naive_with_threadpool<ThreadPool> be) {
+            return std::make_tuple(be, sid::allocator(&std::make_unique<char[]>));
+        }
 
-        template <class Allocator, class Sizes, class T>
-        auto allocate_global_tmp(std::tuple<naive, Allocator> &alloc, Sizes const &sizes, data_type<T>) {
+        template <class ThreadPool, class Allocator, class Sizes, class T>
+        auto allocate_global_tmp(
+            std::tuple<naive_with_threadpool<ThreadPool>, Allocator> &alloc, Sizes const &sizes, data_type<T>) {
             return sid::make_contiguous<T, int_t, sid::unknown_kind>(std::get<1>(alloc), sizes);
         }
     } // namespace naive_impl_
 
     using naive_impl_::naive;
+    using naive_impl_::naive_with_threadpool;
 
     using naive_impl_::apply_column_stage;
     using naive_impl_::apply_stencil_stage;
