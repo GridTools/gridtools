963aca2f9a16fb3b8508657271305d1abc4ecf04
Hannes Vogt <hannes@havogt.de>
2023-04-12 08:40:41 +0000

    make it a tuple of size 5 to destroy float4 aos behavior

--- tests/regression/copy_stencil_tuple.cpp
+++ tests/regression/copy_stencil_tuple.cpp
@@ -27,12 +27,14 @@ namespace {
         using in1 = in_accessor<1>;
         using in2 = in_accessor<2>;
         using in3 = in_accessor<3>;
-        using out0 = inout_accessor<4>;
-        using out1 = inout_accessor<5>;
-        using out2 = inout_accessor<6>;
-        using out3 = inout_accessor<7>;
+        using in4 = in_accessor<4>;
+        using out0 = inout_accessor<5>;
+        using out1 = inout_accessor<6>;
+        using out2 = inout_accessor<7>;
+        using out3 = inout_accessor<8>;
+        using out4 = inout_accessor<9>;
 
-        using param_list = make_param_list<in0, in1, in2, in3, out0, out1, out2, out3>;
+        using param_list = make_param_list<in0, in1, in2, in3, in4, out0, out1, out2, out3, out4>;
 
         template <class Eval>
         GT_FUNCTION static void apply(Eval &&eval) {
@@ -40,6 +42,7 @@ namespace {
             eval(out1()) = eval(in1());
             eval(out2()) = eval(in2());
             eval(out3()) = eval(in3());
+            eval(out4()) = eval(in4());
         }
     };
 
@@ -67,6 +70,7 @@ namespace {
             tuple_util::host_device::get<1>(eval(out())) = tuple_util::host_device::get<1>(eval(in()));
             tuple_util::host_device::get<2>(eval(out())) = tuple_util::host_device::get<2>(eval(in()));
             tuple_util::host_device::get<3>(eval(out())) = tuple_util::host_device::get<3>(eval(in()));
+            tuple_util::host_device::get<4>(eval(out())) = tuple_util::host_device::get<4>(eval(in()));
         }
     };
 
@@ -82,6 +86,7 @@ namespace {
             eval(out(0, 0, 0, 1)) = eval(in(0, 0, 0, 1));
             eval(out(0, 0, 0, 2)) = eval(in(0, 0, 0, 2));
             eval(out(0, 0, 0, 3)) = eval(in(0, 0, 0, 3));
+            eval(out(0, 0, 0, 4)) = eval(in(0, 0, 0, 4));
         }
     };
 
@@ -92,35 +97,54 @@ namespace {
         auto out1 = TypeParam::make_storage();
         auto out2 = TypeParam::make_storage();
         auto out3 = TypeParam::make_storage();
+        auto out4 = TypeParam::make_storage();
         auto comp = [&out0,
                         &out1,
                         &out2,
                         &out3,
+                        &out4,
                         grid = TypeParam::make_grid(),
                         in0 = TypeParam::make_const_storage(in),
                         in1 = TypeParam::make_const_storage(in),
                         in2 = TypeParam::make_const_storage(in),
-                        in3 = TypeParam::make_const_storage(in)] {
-            run_single_stage(
-                copy_functor_unrolled(), stencil_backend_t(), grid, in0, in1, in2, in3, out0, out1, out2, out3);
+                        in3 = TypeParam::make_const_storage(in),
+                        in4 = TypeParam::make_const_storage(in)] {
+            run_single_stage(copy_functor_unrolled(),
+                stencil_backend_t(),
+                grid,
+                in0,
+                in1,
+                in2,
+                in3,
+                in4,
+                out0,
+                out1,
+                out2,
+                out3,
+                out4);
         };
         comp();
         TypeParam::verify(in, out0);
         TypeParam::verify(in, out1);
         TypeParam::verify(in, out2);
         TypeParam::verify(in, out3);
+        TypeParam::verify(in, out4);
         TypeParam::benchmark("copy_stencil_tuple_unrolled", comp);
     }
     GT_REGRESSION_TEST(copy_stencil_tuple_array, test_environment<>, stencil_backend_t) {
-        // expected to perform bad on GPU because array of structures
+        // expected to perform bad on GPU because array of structures (if element > 128 bits)
         using float_t = typename TypeParam::float_t;
         auto in = [](int i, int j, int k) {
-            return array<float_t, 4>{
-                float_t(i + j + k), float_t(i + j + k + 1), float_t(i + j + k + 2), float_t(i + j + k + 3)};
+            return array<float_t, 5>{//
+                float_t(i + j + k),
+                float_t(i + j + k + 1),
+                float_t(i + j + k + 2),
+                float_t(i + j + k + 3),
+                float_t(i + j + k + 4)};
         };
-        auto out = TypeParam::template make_storage<array<float_t, 4>>();
+        auto out = TypeParam::template make_storage<array<float_t, 5>>();
         auto comp =
-            [&out, grid = TypeParam::make_grid(), in = TypeParam::template make_const_storage<array<float_t, 4>>(in)] {
+            [&out, grid = TypeParam::make_grid(), in = TypeParam::template make_const_storage<array<float_t, 5>>(in)] {
                 run_single_stage(copy_functor_tuple(), stencil_backend_t(), grid, in, out);
             };
         comp();
@@ -128,30 +152,15 @@ namespace {
         TypeParam::benchmark("copy_stencil_tuple_array", comp);
     }
 
-    GT_REGRESSION_TEST(copy_stencil_tuple_tuple, test_environment<>, stencil_backend_t) {
-        // expected to perform bad on GPU because array of structures
-        using float_t = typename TypeParam::float_t;
-        auto in = [](int i, int j, int k) { return tuple{i + j + k, i + j + k + 1, i + j + k + 2, i + j + k + 3}; };
-        auto out = TypeParam::template make_storage<tuple<float_t, float_t, float_t, float_t>>();
-        auto comp = [&out,
-                        grid = TypeParam::make_grid(),
-                        in = TypeParam::template make_const_storage<tuple<float_t, float_t, float_t, float_t>>(in)] {
-            run_single_stage(copy_functor_tuple(), stencil_backend_t(), grid, in, out);
-        };
-        comp();
-        TypeParam::verify(in, out);
-        TypeParam::benchmark("copy_stencil_tuple_tuple", comp);
-    }
-
     GT_REGRESSION_TEST(copy_stencil_tuple_extra_dim, test_environment<>, stencil_backend_t) {
         // extra dimension without tuple behavior
         using float_t = typename TypeParam::float_t;
         auto in = [](int i, int j, int k, int t) { return i + j + k + t; };
-        auto out = TypeParam::template builder<float_t>(integral_constant<int, 4>{}).build();
+        auto out = TypeParam::template builder<float_t>(integral_constant<int, 5>{}).build();
         auto comp =
             [&out,
                 grid = TypeParam::make_grid(),
-                in = TypeParam::template builder<float_t const>(integral_constant<int, 4>{}).initializer(in).build()] {
+                in = TypeParam::template builder<float_t const>(integral_constant<int, 5>{}).initializer(in).build()] {
                 run_single_stage(copy_functor_data_dims(), stencil_backend_t(), grid, in, out);
             };
         comp();
@@ -165,18 +174,24 @@ namespace {
         auto out1 = TypeParam::make_storage();
         auto out2 = TypeParam::make_storage();
         auto out3 = TypeParam::make_storage();
-        auto out = sid::composite::keys<integral_constant<int, 0>,
+        auto out4 = TypeParam::make_storage();
+        auto out = sid::composite::keys< //
+            integral_constant<int, 0>,
             integral_constant<int, 1>,
             integral_constant<int, 2>,
-            integral_constant<int, 3>>::make_values(out0, out1, out2, out3);
+            integral_constant<int, 3>,
+            integral_constant<int, 4>>::make_values(out0, out1, out2, out3, out4);
         auto in0 = TypeParam::make_const_storage(in);
         auto in1 = TypeParam::make_const_storage(in);
         auto in2 = TypeParam::make_const_storage(in);
         auto in3 = TypeParam::make_const_storage(in);
-        auto in_sid = sid::composite::keys<integral_constant<int, 0>,
+        auto in4 = TypeParam::make_const_storage(in);
+        auto in_sid = sid::composite::keys< //
+            integral_constant<int, 0>,
             integral_constant<int, 1>,
             integral_constant<int, 2>,
-            integral_constant<int, 3>>::make_values(in0, in1, in2, in3);
+            integral_constant<int, 3>,
+            integral_constant<int, 4>>::make_values(in0, in1, in2, in3, in4);
         auto comp = [&out, grid = TypeParam::make_grid(), &in = in_sid] {
             run_single_stage(copy_functor_tuple(), stencil_backend_t(), grid, in, out);
         };
@@ -185,16 +200,17 @@ namespace {
         TypeParam::verify(in, out1);
         TypeParam::verify(in, out2);
         TypeParam::verify(in, out3);
+        TypeParam::verify(in, out4);
         TypeParam::benchmark("copy_stencil_tuple_composite", comp);
     }
 
     GT_REGRESSION_TEST(copy_stencil_tuple_dim2tuple, test_environment<>, stencil_backend_t) {
         using float_t = typename TypeParam::float_t;
         auto in = [](int i, int j, int k, int t) { return i + j + k + t; };
-        auto out_ds = TypeParam::template builder<float_t>(integral_constant<int, 4>{}).build();
-        auto out = sid::dimension_to_tuple_like<integral_constant<int, 3>, 4>(out_ds);
-        auto in_ds = TypeParam::template builder<float_t const>(integral_constant<int, 4>{}).initializer(in).build();
-        auto in_transformed = sid::dimension_to_tuple_like<integral_constant<int, 3>, 4>(in_ds);
+        auto out_ds = TypeParam::template builder<float_t>(integral_constant<int, 5>{}).build();
+        auto out = sid::dimension_to_tuple_like<integral_constant<int, 3>, 5>(out_ds);
+        auto in_ds = TypeParam::template builder<float_t const>(integral_constant<int, 5>{}).initializer(in).build();
+        auto in_transformed = sid::dimension_to_tuple_like<integral_constant<int, 3>, 5>(in_ds);
         auto comp = [&out, grid = TypeParam::make_grid(), &in = in_transformed] {
             run_single_stage(copy_functor_tuple(), stencil_backend_t(), grid, in, out);
         };
@@ -203,22 +219,4 @@ namespace {
         TypeParam::benchmark("copy_stencil_tuple_dim2tuple", comp);
     }
 
-    GT_REGRESSION_TEST(copy_stencil_tuple_dim2tuple_unrolled, test_environment<>, stencil_backend_t) {
-        using float_t = typename TypeParam::float_t;
-        auto in = [](int i, int j, int k, int t) { return i + j + k + t; };
-        auto out_ds = TypeParam::template builder<float_t>(integral_constant<int, 4>{}).build();
-        static_assert(is_sid<decltype(out_ds)>::value);
-        auto out = sid::dimension_to_tuple_like<integral_constant<int, 3>, 4>(out_ds);
-        static_assert(is_sid<decltype(out)>::value);
-        auto in_ds = TypeParam::template builder<float_t const>(integral_constant<int, 4>{}).initializer(in).build();
-        static_assert(is_sid<decltype(in_ds)>::value);
-        auto in_transformed = sid::dimension_to_tuple_like<integral_constant<int, 3>, 4>(in_ds);
-        static_assert(is_sid<decltype(in_transformed)>::value);
-        auto comp = [&out, grid = TypeParam::make_grid(), &in = in_transformed] {
-            run_single_stage(copy_functor_tuple_unrolled(), stencil_backend_t(), grid, in, out);
-        };
-        comp();
-        TypeParam::verify(in, out_ds);
-        TypeParam::benchmark("copy_stencil_tuple_dim2tuple_unrolled", comp);
-    }
 } // namespace
