0d68a60a67ecea57d876e27ccf15c63e5dcd78c4
Hannes Vogt <vogt@hey.com>
2023-04-20 13:56:20 +0200

    fix has_value

--- include/gridtools/common/integral_constant.hpp
+++ include/gridtools/common/integral_constant.hpp
@@ -15,13 +15,6 @@
 #include "host_device.hpp"
 
 namespace gridtools {
-    namespace integral_constant_impl_ {
-        template <class, class = void>
-        struct has_value_type : std::false_type {};
-        template <class T>
-        struct has_value_type<T, std::void_t<typename T::value_type>> : std::true_type {};
-    } // namespace integral_constant_impl_
-
     // This predicate checks if the the class has `std::integral_constant` as a public base.
     // Note that it is not the same as `class is an instantiation of gridtools::integral_constant`.
     // Also it is not the same as `class has `integral nested value_type type and value static member`.
@@ -51,13 +44,11 @@ namespace gridtools {
     template <class, class = void>
     struct to_integral_type;
     template <class T>
-    struct to_integral_type<T,
-        std::enable_if_t<!integral_constant_impl_::has_value_type<T>::value && std::is_integral_v<T>>> {
+    struct to_integral_type<T, std::enable_if_t<std::is_integral_v<T>>> {
         using type = T;
     };
     template <class T>
-    struct to_integral_type<T,
-        std::enable_if_t<integral_constant_impl_::has_value_type<T>::value && is_integral_constant<T>::value>> {
+    struct to_integral_type<T, std::enable_if_t<is_integral_constant<T>::value>> {
         using type = typename T::value_type;
     };
 
