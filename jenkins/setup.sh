a980aa60b90cefdfd619c46e2a5a4d305e836873
Hannes Vogt <hannes@havogt.de>
2023-07-04 11:55:59 +0200

    CI: fix ault hip (#1758)
    Additional: allow force enabling Python requirement for CI

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -10,9 +10,7 @@ envfile=./jenkins/envs/${label}_$env.sh
 export GT_PERFORMANCE_HISTORY_PATH_PREFIX=/apps/${label}/SSL/gridtools/jenkins/gridtools-performance-history-gt2
 
 # use the machines python virtualenv with required modules installed
-if [[ $label = ault ]]; then
-    venv_dir=/users/fthaler/public/jenkins/gridtools-venv
-elif [[ $label = dom ]]; then
+if [[ $label = dom ]]; then
     venv_dir=/apps/daint/SSL/gridtools/jenkins/venv
 else
     venv_dir=/apps/$label/SSL/gridtools/jenkins/venv
