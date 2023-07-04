891a02a2aadf22388d3643ca2c162aca22f26c0d
Hannes Vogt <hannes@havogt.de>
2023-07-04 11:55:59 +0200

    CI: fix ault hip (#1758)
    Additional: allow force enabling Python requirement for CI

--- jenkins/envs/ault.sh
+++ jenkins/envs/ault.sh
@@ -2,10 +2,8 @@
 
 source $(dirname "$BASH_SOURCE")/base.sh
 
-source /users/fthaler/public/jenkins/spack/share/spack/setup-env.sh
-
-spack load boost
-spack load cmake
+module load cmake/3.21.3
+module load boost
 
 export GTRUN_BUILD_COMMAND='make -j 8'
 export GTRUN_SBATCH_NODES=1
