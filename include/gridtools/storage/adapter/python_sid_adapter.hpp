1d0a10c501420a2e3855128c848d32b0ab4f5cd5
Stefano Ubbiali <subbiali@phys.ethz.ch>
2023-07-18 17:03:25 +0200

    Python SID adapter: Add support for HIP/ROCm buffers (#1759)
    Light-weight PR to make the Python bindings work with AMD GPU buffers. Complementary to GridTools/gt4py#1278.
    
    ---------
    
    Co-authored-by: Hannes Vogt <hannes@havogt.de>

--- include/gridtools/storage/adapter/python_sid_adapter.hpp
+++ include/gridtools/storage/adapter/python_sid_adapter.hpp
@@ -268,7 +268,13 @@ namespace gridtools {
             static_assert(std::is_trivially_copy_constructible_v<T>,
                 "as_cuda_sid should be instantiated with the trivially copyable type");
 
+#if defined(__HIP__)
+            // This is a custom property that has to be added manually (not provided by cupy).
+            // Should be replaced by `dlpack` for uniform array interface support.
+            auto iface = src.attr("__hip_array_interface__").cast<pybind11::dict>();
+#else
             auto iface = src.attr("__cuda_array_interface__").cast<pybind11::dict>();
+#endif
 
             // shape
             array<size_t, Dim> shape;
