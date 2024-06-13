88e7d91f7005766ef848e5c56b7393233daaed04
Hannes Vogt <hannes@havogt.de>
2024-06-13 14:32:03 +0200

    Update nanobind to v2 (#1777)
    - nb::any is replaced by `nb::ssize_t(-1)`
    - Because of a change in `~ndarray`, we need to make sure that Python is initialized in our tests (see https://github.com/wjakob/nanobind/issues/377)

--- tests/unit_tests/storage/adapter/test_nanobind_adapter.cpp
+++ tests/unit_tests/storage/adapter/test_nanobind_adapter.cpp
@@ -8,22 +8,29 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <gridtools/storage/adapter/nanobind_adapter.hpp>
+
+#include <Python.h>
 #include <array>
+#include <gridtools/common/integral_constant.hpp>
+#include <gridtools/sid/concept.hpp>
 
 #include <gtest/gtest.h>
 
-#include <gridtools/common/integral_constant.hpp>
-#include <gridtools/sid/concept.hpp>
-#include <gridtools/storage/adapter/nanobind_adapter.hpp>
-
 namespace nb = nanobind;
 
-TEST(NanobindAdapter, DataDynStrides) {
+class python_init_fixture : public ::testing::Test {
+  protected:
+    void SetUp() override { Py_Initialize(); }
+    void TearDown() override { Py_FinalizeEx(); }
+};
+
+TEST_F(python_init_fixture, NanobindAdapterDataDynStrides) {
     const auto data = reinterpret_cast<void *>(0xDEADBEEF);
     constexpr int ndim = 2;
     constexpr std::array<std::size_t, ndim> shape = {3, 4};
     constexpr std::array<std::intptr_t, ndim> strides = {1, 3};
-    nb::ndarray<int, nb::shape<nb::any, nb::any>> ndarray{data, ndim, shape.data(), nb::handle{}, strides.data()};
+    nb::ndarray<int, nb::shape<-1, -1>> ndarray{data, ndim, shape.data(), nb::handle{}, strides.data()};
 
     const auto sid = gridtools::nanobind::as_sid(ndarray);
     const auto s_origin = sid_get_origin(sid);
@@ -35,27 +42,27 @@ TEST(NanobindAdapter, DataDynStrides) {
     EXPECT_EQ(strides[1], gridtools::get<1>(s_strides));
 }
 
-TEST(NanobindAdapter, StaticStridesMatch) {
+TEST_F(python_init_fixture, NanobindAdapterStaticStridesMatch) {
     const auto data = reinterpret_cast<void *>(0xDEADBEEF);
     constexpr int ndim = 2;
     constexpr std::array<std::size_t, ndim> shape = {3, 4};
     constexpr std::array<std::intptr_t, ndim> strides = {1, 3};
-    nb::ndarray<int, nb::shape<nb::any, nb::any>> ndarray{data, ndim, shape.data(), nb::handle{}, strides.data()};
+    nb::ndarray<int, nb::shape<-1, -1>> ndarray{data, ndim, shape.data(), nb::handle{}, strides.data()};
 
-    const auto sid = gridtools::nanobind::as_sid(ndarray, gridtools::nanobind::stride_spec<1, nanobind::any>{});
+    const auto sid = gridtools::nanobind::as_sid(ndarray, gridtools::nanobind::stride_spec<1, -1>{});
     const auto s_strides = sid_get_strides(sid);
 
     EXPECT_EQ(strides[0], gridtools::get<0>(s_strides).value);
     EXPECT_EQ(strides[1], gridtools::get<1>(s_strides));
 }
 
-TEST(NanobindAdapter, StaticStridesMismatch) {
+TEST_F(python_init_fixture, NanobindAdapterStaticStridesMismatch) {
     const auto data = reinterpret_cast<void *>(0xDEADBEEF);
     constexpr int ndim = 2;
     constexpr std::array<std::size_t, ndim> shape = {3, 4};
     constexpr std::array<std::intptr_t, ndim> strides = {1, 3};
-    nb::ndarray<int, nb::shape<nb::any, nb::any>> ndarray{data, ndim, shape.data(), nb::handle{}, strides.data()};
+    nb::ndarray<int, nb::shape<-1, -1>> ndarray{data, ndim, shape.data(), nb::handle{}, strides.data()};
 
     EXPECT_THROW(
-        gridtools::nanobind::as_sid(ndarray, gridtools::nanobind::stride_spec<2, nanobind::any>{}), std::invalid_argument);
+        gridtools::nanobind::as_sid(ndarray, gridtools::nanobind::stride_spec<2, -1>{}), std::invalid_argument);
 }
