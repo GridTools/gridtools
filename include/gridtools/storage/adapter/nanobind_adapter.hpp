1508d09381db09fbcc23477391aff7306b319ba6
Hannes Vogt <hannes@havogt.de>
2024-06-25 20:31:45 +0200

    Make nanobind adapter backward compatible (#1790)

--- include/gridtools/storage/adapter/nanobind_adapter.hpp
+++ include/gridtools/storage/adapter/nanobind_adapter.hpp
@@ -24,25 +24,33 @@
 
 namespace gridtools {
     namespace nanobind_sid_adapter_impl_ {
+#if NB_VERSION_MAJOR >= 2
+        using array_size_t = nanobind::ssize_t;
+#else
+        using array_size_t = std::size_t;
+#endif
 
-        // Use `-1` for dynamic stride, use an integral value for static stride.
-        template <nanobind::ssize_t... Values>
-        using stride_spec = std::integer_sequence<nanobind::ssize_t, Values...>;
+        inline constexpr array_size_t dynamic_size = -1;
+
+        // Use `dynamic_size` for dynamic stride, use an integral value for static
+        // stride.
+        template <array_size_t... Values>
+        using stride_spec = std::integer_sequence<array_size_t, Values...>;
 
         template <class IndexSequence>
         struct dynamic_strides_helper;
 
         template <std::size_t... Indices>
         struct dynamic_strides_helper<std::index_sequence<Indices...>> {
-            using type = stride_spec<(void(Indices), -1)...>;
+            using type = stride_spec<(void(Indices), dynamic_size)...>;
         };
 
         template <std::size_t N>
         using fully_dynamic_strides = typename dynamic_strides_helper<std::make_index_sequence<N>>::type;
 
-        template <nanobind::ssize_t SpecValue>
-        auto select_static_stride_value(std::size_t dyn_value) {
-            if constexpr (SpecValue == -1) {
+        template <array_size_t SpecValue>
+        constexpr auto select_static_stride_value(std::size_t dyn_value) {
+            if constexpr (SpecValue == dynamic_size) {
                 return dyn_value;
             } else {
                 if (SpecValue != dyn_value) {
@@ -52,20 +60,20 @@ namespace gridtools {
             }
         }
 
-        template <nanobind::ssize_t... SpecValues, std::size_t... IndexValues>
-        auto select_static_strides_helper(
+        template <array_size_t... SpecValues, std::size_t... IndexValues>
+        constexpr auto select_static_strides_helper(
             stride_spec<SpecValues...>, const std::size_t *dyn_values, std::index_sequence<IndexValues...>) {
 
             return gridtools::tuple{select_static_stride_value<SpecValues>(dyn_values[IndexValues])...};
         }
 
-        template <nanobind::ssize_t... SpecValues>
-        auto select_static_strides(stride_spec<SpecValues...> spec, const std::size_t *dyn_values) {
+        template <array_size_t... SpecValues>
+        constexpr auto select_static_strides(stride_spec<SpecValues...> spec, const std::size_t *dyn_values) {
             return select_static_strides_helper(spec, dyn_values, std::make_index_sequence<sizeof...(SpecValues)>{});
         }
 
         template <class T,
-            nanobind::ssize_t... Sizes,
+            array_size_t... Sizes,
             class... Args,
             class Strides = fully_dynamic_strides<sizeof...(Sizes)>,
             class StridesKind = sid::unknown_kind>
@@ -93,7 +101,9 @@ namespace gridtools {
 
     namespace nanobind {
         using nanobind_sid_adapter_impl_::as_sid;
+        using nanobind_sid_adapter_impl_::dynamic_size;
         using nanobind_sid_adapter_impl_::fully_dynamic_strides;
         using nanobind_sid_adapter_impl_::stride_spec;
+
     } // namespace nanobind
 } // namespace gridtools
