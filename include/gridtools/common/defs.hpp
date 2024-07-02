e540f83f3af1055a8c0927f073bdaa5dc45eae39
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-07-02 13:42:06 +0200

    Improve formatting a bit

--- include/gridtools/common/defs.hpp
+++ include/gridtools/common/defs.hpp
@@ -28,7 +28,9 @@ namespace gridtools {
 #endif
 
 #if defined(__has_builtin)
-#if __has_builtin(__builtin_assume) || (defined(GT_CUDA_ARCH) && ((__CUDACC_VER_MAJOR__ == 11 && __CUDACC_VER_MINOR__ >= 2) || __CUDACC_VER_MAJOR__ > 11))
+#if __has_builtin(__builtin_assume) \
+    || (defined(GT_CUDA_ARCH) && \
+        ((__CUDACC_VER_MAJOR__ == 11 && __CUDACC_VER_MINOR__ >= 2) || __CUDACC_VER_MAJOR__ >= 12))
 #define GT_ASSUME(x) __builtin_assume(x)
 #else
 #define GT_ASSUME(x)
