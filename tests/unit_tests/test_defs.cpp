3d5f405eb742715ae37bb8ac1bd653260d6bf62a
Felix Thaler <thaler@cscs.ch>
2024-07-08 13:46:10 +0200

    Fix GT_ASSUME for NVCC and Enable GT_ASSUME on Recent GCC Versions (#1789)
    - Fixes non-functional `GT_ASSUME` on NVCC due to `__has_builtin(__builtin_assume)` returning 0.
    - Adds a test to check correctly defined device-side `GT_ASSUME` on NVCC.
    - Enables `GT_ASSUME` based on C++-23 `[[assume(x)]]` on recent versions of GCC.

---
+++ tests/unit_tests/test_defs.cpp
@@ -0,0 +1,28 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2023, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <string_view>
+
+#include <boost/preprocessor/stringize.hpp>
+
+#include <gridtools/common/defs.hpp>
+
+#if defined(__NVCC__) && defined(__CUDA_ARCH__) && \
+    (__CUDACC_VER_MAJOR__ == 11 && __CUDACC_VER_MINOR__ >= 2 || __CUDACC__VER_MAJOR__ > 11)
+
+#if !defined(GT_ASSUME)
+#error "GT_ASSUME is undefined"
+#else
+static_assert(std::string_view(BOOST_PP_STRINGIZE(GT_ASSUME(x))) ==
+    std::string_view(BOOST_PP_STRINGIZE(__builtin_assume(x))),
+        BOOST_PP_STRINGIZE(GT_ASSUME(x)) " != " BOOST_PP_STRINGIZE(__builtin_assume(x)));
+#endif
+
+#endif
