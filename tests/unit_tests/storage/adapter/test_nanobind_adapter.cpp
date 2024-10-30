6c600e434964c1fbd4b0b574117f55c10a331f1f
Felix Thaler <thaler@cscs.ch>
2024-10-30 15:43:03 +0000

    Respect ReadOnly Property in Nanobind Adapter (#1809)
    Create a SID with const element type if a Nanobind array is defined
    read-only. Requires Nanobind 2.1.0 (with merged
    https://github.com/wjakob/nanobind/pull/491).
    
    ---------
    
    Co-authored-by: Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>

--- tests/unit_tests/storage/adapter/test_nanobind_adapter.cpp
+++ tests/unit_tests/storage/adapter/test_nanobind_adapter.cpp
@@ -42,6 +42,26 @@ TEST_F(python_init_fixture, NanobindAdapterDataDynStrides) {
     EXPECT_EQ(strides[1], gridtools::get<1>(s_strides));
 }
 
+TEST_F(python_init_fixture, NanobindAdapterReadOnly) {
+    const auto data = reinterpret_cast<void *>(0xDEADBEEF);
+    constexpr int ndim = 2;
+    constexpr std::array<std::size_t, ndim> shape = {3, 4};
+    constexpr std::array<std::intptr_t, ndim> strides = {1, 3};
+    nb::ndarray<int, nb::shape<-1, -1>, nb::ro> ndarray{data, ndim, shape.data(), nb::handle{}, strides.data()};
+
+    const auto sid = gridtools::nanobind::as_sid(ndarray);
+    using element_t = gridtools::sid::element_type<decltype(sid)>;
+    static_assert(std::is_same_v<element_t, int const>);
+
+    const auto s_origin = sid_get_origin(sid);
+    const auto s_strides = sid_get_strides(sid);
+    const auto s_ptr = s_origin();
+
+    EXPECT_EQ(s_ptr, data);
+    EXPECT_EQ(strides[0], gridtools::get<0>(s_strides));
+    EXPECT_EQ(strides[1], gridtools::get<1>(s_strides));
+}
+
 TEST_F(python_init_fixture, NanobindAdapterStaticStridesMatch) {
     const auto data = reinterpret_cast<void *>(0xDEADBEEF);
     constexpr int ndim = 2;
